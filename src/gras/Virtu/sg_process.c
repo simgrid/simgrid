a4a2728d8538392a95a9a94f47633222594a6ea0
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2004-06-18 18:05:05 +0000

    Various cleanups; SG almost work
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@115 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/gras/Virtu/sg_process.c
+++ src/gras/Virtu/sg_process.c
@@ -31,12 +31,9 @@ gras_process_init() {
     if (!(hd=(gras_hostdata_t *)malloc(sizeof(gras_hostdata_t)))) 
       RAISE_MALLOC;
 
-    hd->portLen = 0;
-    hd->port=NULL;
-    hd->port2chan=NULL;
-    for (i=0; i<GRAS_MAX_CHANNEL; i++) {
-      hd->proc[i]=0;
-    }
+    TRY(gras_dynar_new(&(hd->ports),sizeof(gras_sg_portrec_t),NULL));
+
+    memset(hd->proc, 0, sizeof(hd->proc[0]) * GRAS_MAX_CHANNEL); 
 
     if (MSG_host_set_data(MSG_host_self(),(void*)hd) != MSG_OK) {
       return unknown_error;
@@ -51,6 +48,7 @@ gras_process_init() {
 	    MSG_host_get_name(MSG_host_self()),GRAS_MAX_CHANNEL);
 
   pd->chan = i;
+  pd->sock = NULL;
   hd->proc[ i ] = MSG_process_self_PID();
 
   /* take a free RAW channel for this process */
@@ -61,6 +59,7 @@ gras_process_init() {
 	    MSG_host_get_name(MSG_host_self()),GRAS_MAX_CHANNEL);
   }
   pd->rawChan = i;
+  pd->rawSock = NULL;
   hd->proc[ i ] = MSG_process_self_PID();
 
   VERB2("Creating process '%s' (%d)",
@@ -72,9 +71,10 @@ gras_process_init() {
 gras_error_t
 gras_process_exit() {
   gras_hostdata_t *hd=(gras_hostdata_t *)MSG_host_get_data(MSG_host_self());
-  gras_procdata_t *pd=(gras_procdata_t *)MSG_process_get_data(MSG_process_self());
+  gras_procdata_t *pd=gras_procdata_get();
   int myPID=MSG_process_self_PID();
-  int i;
+  int cpt;
+  gras_sg_portrec_t pr;
 
   gras_assert0(hd && pd,"Run gras_process_init!!\n");
 
@@ -82,18 +82,16 @@ gras_process_exit() {
 	MSG_process_get_name(MSG_process_self()),MSG_process_self_PID());
 
   if (gras_dynar_length(pd->msg_queue))
-    WARN1("process %d terminated, but some queued messages where not handled",MSG_process_self_PID());
+    WARN1("process %d terminated, but some messages are still queued",
+	  MSG_process_self_PID());
 
-  for (i=0; i< GRAS_MAX_CHANNEL; i++)
-    if (myPID == hd->proc[i])
-      hd->proc[i] = 0;
+  for (cpt=0; cpt< GRAS_MAX_CHANNEL; cpt++)
+    if (myPID == hd->proc[cpt])
+      hd->proc[cpt] = 0;
 
-  for (i=0; i<hd->portLen; i++) {
-    if (hd->port2chan[ i ] == pd->chan) {
-      memmove(&(hd->port[i]),      &(hd->port[i+1]),      (hd->portLen -i -1) * sizeof(int));
-      memmove(&(hd->port2chan[i]), &(hd->port2chan[i+1]), (hd->portLen -i -1) * sizeof(int));
-      hd->portLen--;
-      i--; /* counter the effect of the i++ at the end of the iteration */
+  gras_dynar_foreach(hd->ports, cpt, pr) {
+    if (pr.port == pd->chan || pr.port == pd->rawChan) {
+      gras_dynar_cursor_rm(hd->ports, &cpt);
     }
   }
 
@@ -105,7 +103,8 @@ gras_process_exit() {
  * **************************************************************************/
 
 gras_procdata_t *gras_procdata_get(void) {
-  gras_procdata_t *pd=(gras_procdata_t *)MSG_process_get_data(MSG_process_self());
+  gras_procdata_t *pd=
+    (gras_procdata_t *)MSG_process_get_data(MSG_process_self());
 
   gras_assert0(pd,"Run gras_process_init!");
 
