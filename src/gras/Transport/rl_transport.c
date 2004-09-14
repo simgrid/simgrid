2d4fc06bb64ca1d0031a0dd49bfcc60d2ec44b32
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2004-09-14 09:16:30 +0000

    add an header AIX seems to want
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@406 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/gras/Transport/rl_transport.c
+++ src/gras/Transport/rl_transport.c
@@ -9,6 +9,7 @@
    under the terms of the license (GNU LGPL) which comes with this package. */
 
 #include <errno.h>
+#include <sys/time.h> /* struct timeval */
 #include <sys/types.h>
 #include <sys/socket.h>
 
