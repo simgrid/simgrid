3d630a8a0429ae6e380bf1e002a6e55c21cd078f
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2004-12-09 14:52:47 +0000

    Fix (C) dates; minor documentation improvements
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@572 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/gras/core.h
+++ include/gras/core.h
@@ -2,7 +2,7 @@
 
 /* gras/core.h - Unsorted part of the GRAS public interface                 */
 
-/* Copyright (c) 2004 Martin Quinson. All rights reserved.                  */
+/* Copyright (c) 2003, 2004 Martin Quinson. All rights reserved.            */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
--- include/gras/messages.h
+++ include/gras/messages.h
@@ -3,7 +3,7 @@
 /* messaging - high level communication (send/receive messages)             */
 /* module's public interface exported to end user.                          */
 
-/* Copyright (c) 2004 Martin Quinson. All rights reserved.                  */
+/* Copyright (c) 2003, 2004 Martin Quinson. All rights reserved.            */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
