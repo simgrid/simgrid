6882f6dbdfb8ba6d6b2aad182ad005d7d98c8e53
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2004-12-09 14:53:53 +0000

    fix (C) dates; proper xbt_log function
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@574 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/xbt_modinter.h
+++ src/xbt_modinter.h
@@ -1,8 +1,8 @@
 /* $Id$ */
 
-/* xbt_modinter - Interface to XBT modules                                  */
+/* xbt_modinter - How to init/exit the XBT modules                          */
 
-/* Copyright (c) 2004 Martin Quinson. All rights reserved.                  */
+/* Copyright (c) 2003, 2004 Martin Quinson. All rights reserved.            */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
@@ -11,7 +11,7 @@
 #define XBT_MODINTER_H
 
 /* Modules definitions */
-void gras_log_init(void);
+void xbt_log_init(int *argc,char **argv, const char *defaultlog);
 void xbt_log_exit(void);
 
 #endif /* XBT_MODINTER_H */
