caf4fa6d0b422db71469a6548a1abf46698a0c5f
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2004-12-10 06:53:31 +0000

    There is now a proper gras_init function
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@599 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/xbt/module.c
+++ src/xbt/module.c
@@ -2,7 +2,7 @@
 
 /* module handling                                                          */
 
-/* Copyright (c) 2004 Martin Quinson. All rights reserved.                  */
+/* Copyright (c) 2003, 2004 Martin Quinson. All rights reserved.            */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
@@ -13,16 +13,11 @@
 #include "xbt/dynar.h"
 #include "xbt/config.h"
 
-#include "gras/process.h" /* FIXME: bad loop */
-
 #include "xbt/module.h" /* this module */
 
 #include "xbt_modinter.h"  /* prototype of other module's init/exit in XBT */
-#include "gras_modinter.h" /* same in GRAS */
 
 XBT_LOG_NEW_DEFAULT_SUBCATEGORY(module,xbt, "module handling");
-
-static int xbt_running_process = 0;
 
 struct xbt_module_ {
   xbt_dynar_t *deps;
@@ -34,10 +29,7 @@ struct xbt_module_ {
 
 void 
 xbt_init(int *argc, char **argv) {
-  static short int first_run = 1;
-  if(first_run)
-    xbt_init_defaultlog(argc, argv, NULL);
-  first_run = 0;
+  xbt_init_defaultlog(argc, argv, NULL);
 }
 
 /**
@@ -45,37 +37,26 @@ xbt_init(int *argc, char **argv) {
  * @argc:
  * @argv:
  *
- * Initialize the gras mecanisms.
+ * Initialize the xbt mecanisms.
  */
 void
 xbt_init_defaultlog(int *argc,char **argv, const char *defaultlog) {
-  INFO0("Initialize GRAS");
+  static short int first_run = 1;
+  if (!first_run)
+    return;
+  
+  first_run = 0;
+  INFO0("Initialize XBT");
   
   xbt_log_init(argc,argv,defaultlog);
-   
-  gras_process_init(); /* calls procdata_init, which calls dynar_new */
-  /** init other submodules */
-  if (xbt_running_process++ == 0) {
-    gras_msg_init();
-    gras_trp_init();
-    gras_datadesc_init();
-  }
 }
 
 /**
  * xbt_exit:
  *
- * Finalize the gras mecanisms.
+ * Finalize the xbt mecanisms.
  */
 void 
 xbt_exit(){
-  INFO0("Exiting GRAS");
-  gras_process_exit();
-  if (--xbt_running_process == 0) {
-    gras_msg_exit();
-    gras_trp_exit();
-    gras_datadesc_exit();
-  }
   xbt_log_exit();
-  DEBUG0("Exited GRAS");
 }
