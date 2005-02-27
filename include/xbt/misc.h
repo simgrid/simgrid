6591c9df82136dbb9cb6e5b09651b3441f02e3e8
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2005-02-27 23:35:12 +0000

    Move gcc attributes wrappers to this file (needed for splintification)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@1093 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/xbt/misc.h
+++ include/xbt/misc.h
@@ -12,6 +12,37 @@
 #ifndef XBT_MISC_H
 #define XBT_MISC_H
 
+/* Attributes are only in recent versions of GCC */
+#if     __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ > 4)
+# define _XBT_GNUC_PRINTF( format_idx, arg_idx )    \
+	   __attribute__((__format__ (__printf__, format_idx, arg_idx)))
+# define _XBT_GNUC_SCANF( format_idx, arg_idx )     \
+	       __attribute__((__format__ (__scanf__, format_idx, arg_idx)))
+# define _XBT_GNUC_FORMAT( arg_idx )                \
+		   __attribute__((__format_arg__ (arg_idx)))
+# define _XBT_GNUC_NORETURN __attribute__((__noreturn__))
+
+#else   /* !__GNUC__ */
+# define _XBT_GNUC_PRINTF( format_idx, arg_idx )
+# define _XBT_GNUC_SCANF( format_idx, arg_idx )
+# define _XBT_GNUC_FORMAT( arg_idx )
+# define _XBT_GNUC_NORETURN
+
+#endif  /* !__GNUC__ */
+
+/* inline and __FUNCTION__ are only in GCC when -ansi is off */
+
+#if defined(__GNUC__) && ! defined(__STRICT_ANSI__)
+
+# define _XBT_GNUC_FUNCTION __FUNCTION__
+# define _XBT_INLINE inline
+#else
+# define _XBT_GNUC_FUNCTION "function"
+# define _XBT_INLINE 
+#endif
+
+
+
 #ifndef max
 #  define max(a, b) (((a) > (b))?(a):(b))
 #endif
