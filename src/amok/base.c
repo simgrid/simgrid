fa7b0bf08f43186d444bc5bf846e2459ea89a8c7
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2005-02-27 23:21:31 +0000

    loads all of gras.h, not only parts
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@1085 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/amok/base.c
+++ src/amok/base.c
@@ -7,8 +7,7 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include "xbt/error.h"
-#include "gras/datadesc.h"
+#include "gras.h"
 #include "amok/base.h"
 
 XBT_LOG_NEW_DEFAULT_SUBCATEGORY(amok,XBT_LOG_ROOT_CAT,"All AMOK categories");
