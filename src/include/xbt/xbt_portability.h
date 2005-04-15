0ac9a797df75936bc93ce7dad9e0086850f064e0
alegrand <alegrand@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2005-04-15 17:18:45 +0000

    Sorry about that. I had forgotten to commit this file. It supersedes the placeholder from Martin.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@1227 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/include/xbt/xbt_portability.h
+++ src/include/xbt/xbt_portability.h
@@ -1,3 +1,20 @@
+/* $Id$ */
+/*  xbt/xbt_portability.h -- all system dependency                          */
+/* Private portability layer                                                */
 
+/* Copyright (c) 2004,2005 Martin Quinson. All rights reserved.             */
 
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#ifndef _XBT_PORTABILITY_H
+#define _XBT_PORTABILITY_H
+
+/** @brief get time in seconds 
+
+  * gives  the  number  of  seconds since the Epoch (00:00:00 UTC, January 1, 1970).
+  * Most users should use gras_os_time and should not use this function unless 
+    they really know what they are doing. */
 double xbt_os_time(void);
+
+#endif
