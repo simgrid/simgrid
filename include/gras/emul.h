aa10d062e88adc07c982d5961ecbca33972a52f8
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2005-05-10 13:04:27 +0000

    Merge chrono.h (timing macros) and cond.h (conditional execution) into emul.h (emulation support)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@1260 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/gras/emul.h
+++ include/gras/emul.h
@@ -1,20 +1,36 @@
-/* 	$Id$	 */
+/* $Id$                     */
 
-/* chrono.h - timer macros for GRAS                                         */
-
-/* Copyright (c) 2005 Martin Quinson, Arnaud Legrand. All rights reserved.  */
+/* gras/emul.h - public interface to emulation support                      */
+/*                (specific parts for SG or RL)                             */
+ 
+/* Copyright (c) 2003, 2004 Martin Quinson. All rights reserved.            */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
+#ifndef GRAS_COND_H
+#define GRAS_COND_H
 
-#ifndef GRAS_CHRONO_H
-#define GRAS_CHRONO_H
-
-#include "xbt/misc.h"
-#include "gras/cond.h"
+#include "xbt/misc.h" /* BEGIN_DECL */
 
 BEGIN_DECL()
+
+/** @addtogroup GRAS_emul
+ *  @brief Handling code specific to the simulation or to the reality (Virtualization).
+ * 
+ *  Please note that those are real functions and not pre-processor defines. This is to ensure
+ *  that the same object code can be linked against the SG library or the RL one without recompilation.
+ * 
+ *  @{
+ */
+  
+/** \brief Returns true only if the program runs on real life */
+int gras_if_RL(void);
+
+/** \brief Returns true only if the program runs within the simulator */
+int gras_if_SG(void);
+
+/** @} */
 
 int gras_bench_always_begin(const char *location, int line);
 int gras_bench_always_end(void);
@@ -44,4 +60,5 @@ int gras_bench_once_end(void);
 
 END_DECL()
 
-#endif /* GRAS_CHRONO_H */
+#endif /* GRAS_COND_H */
+
