404a5c6adee41f1161eee8e71278e2230abdd459
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2005-05-27 19:43:13 +0000

    A proper (even if really KISS) implementation of gras_os_myname
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@1297 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/gras/Virtu/rl_dns.c
+++ src/gras/Virtu/rl_dns.c
@@ -8,16 +8,39 @@
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
 #include "gras/Virtu/virtu_rl.h"
+#include <netdb.h>  /* {end,set}hostent() gethostby{addr,name}() */
 
-/* A portable DNS resolver is a nightmare to do in a portable manner */
 
-/* the ADNS library is a good candidate for inclusion in the source tree, but
- * it would be a bit too much. We need a stripped down version of it, and it's
- * a bit too late for this tonight.
- * 
- * Next time maybe ;)
- */
+/* A portable DNS resolver is a nightmare to do in a portable manner.
+   keep it simple/stupid for now. */
+
 const char *gras_os_myname(void) {
-   return "(unknown host)";
+  static char *myname = NULL;
+ 
+  if (myname)
+    return (const char*) myname; 
+ 
+  myname = xbt_new(char, 255);
+    
+  if (gethostname(myname, 255) == -1) {
+    /* gethostname() failed! Trying with localhost instead. 
+       We first need to query the DNS to make sure localhost is resolved 
+       See the note in nws/Portability/dnsutil.c about {end,set}hostent() */
+    struct hostent *tmp;
+    sethostent(0);
+    tmp = gethostbyname("localhost");
+    endhostent();
+       
+    if (tmp) {
+      strncat(myname, tmp->h_name, 255);
+    } else {       
+      /* Erm. localhost cannot be resolved. There's something wrong in the user DNS setting */
+      sprintf(myname, "(misconfigured host)");
+    }
+  }
+   
+  myname[254] = '\0';
+ 
+  return myname;
 }
 
