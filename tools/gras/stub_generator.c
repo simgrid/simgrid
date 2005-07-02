e8092ea60f159017ed79fe804bc2cde24a3e8816
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2005-07-02 14:38:46 +0000

    reduce the amount of loaded headers
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@1547 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- tools/gras/stub_generator.c
+++ tools/gras/stub_generator.c
@@ -8,10 +8,7 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include"xbt/sysdep.h"
-#include"xbt/dict.h"
-#include"xbt/dynar.h"
-#include"xbt/error.h"
+#include "xbt/sysdep.h"
 #include "surf/surf_parse.h"
 #include "surf/surf.h"
 
