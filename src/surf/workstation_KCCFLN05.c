e58005c971800b9901e68659faf34a71c1e8ba9c
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2005-08-07 10:59:43 +0000

    Use xbt_dict_get_or_null to avoid the exception madness
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@1593 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/surf/workstation_KCCFLN05.c
+++ src/surf/workstation_KCCFLN05.c
@@ -251,7 +251,7 @@ static void route_new(int src_id, int dst_id, char **links, int nb_link,
   route->size= nb_link;
   link_list = route->links = xbt_new0(network_link_KCCFLN05_t, nb_link);
   for (i = 0; i < nb_link; i++) {
-    link_list[i] = xbt_dict_get(network_link_set, links[i]);
+    link_list[i] = xbt_dict_get_or_null(network_link_set, links[i]);
     free(links[i]);
   }
   free(links);
