89f782568b30327f2950e31f9a27db094b0900a9
alegrand <alegrand@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2005-08-16 20:41:28 +0000

    moving stuff to enable surf_parser hijacking
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@1652 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ include/surf/surf_parse.h
@@ -0,0 +1,16 @@
+/* 	$Id$	 */
+
+/* Copyright (c) 2005 Arnaud Legrand. All rights reserved.                  */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#ifndef _SURF_SURF_PARSE_H
+#define _SURF_SURF_PARSE_H
+
+#include "surf/surfxml.h"
+
+typedef   int (*int_f_void_t)(void);
+extern int_f_void_t surf_parse;
+
+#endif
