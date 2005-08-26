00e10260c18a854d25ebf608ab62f3b59e262a61
alegrand <alegrand@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2005-08-26 01:43:22 +0000

    fixing the doc...
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@1667 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/xbt/fifo.h
+++ include/xbt/fifo.h
@@ -7,25 +7,49 @@
 
 #ifndef _XBT_FIFO_H
 #define _XBT_FIFO_H
+#include "xbt/misc.h" /* BEGIN_DECL */
+
+BEGIN_DECL()
+
+/** \addtogroup XBT_fifo
+ *  @{ */
+
+/** @name 1. Constructor/destructor
+ *  @{
+ */
 
 /** \brief  Bucket structure 
- *  \ingroup XBT_fifo
 */
 typedef struct xbt_fifo_item *xbt_fifo_item_t;
 
 /** \brief  FIFO structure
- *  \ingroup XBT_fifo
 */
 typedef struct xbt_fifo *xbt_fifo_t;
 
-/* API */
 xbt_fifo_t xbt_fifo_new(void);
 void xbt_fifo_free(xbt_fifo_t);
+/** @} */
 
+/** @name 2. Perl-like functions
+ *  @{
+ */
 xbt_fifo_item_t xbt_fifo_push(xbt_fifo_t, void *);
 void *xbt_fifo_pop(xbt_fifo_t);
 xbt_fifo_item_t xbt_fifo_unshift(xbt_fifo_t, void *);
 void *xbt_fifo_shift(xbt_fifo_t);
+int xbt_fifo_size(xbt_fifo_t);
+int xbt_fifo_is_in(xbt_fifo_t, void *);
+/** @} */
+
+/** @name 3. Manipulating directly items
+ *
+ *  @{
+ */
+
+xbt_fifo_item_t xbt_fifo_newitem(void);
+void xbt_fifo_set_item_content(xbt_fifo_item_t, void *);
+void *xbt_fifo_get_item_content(xbt_fifo_item_t);
+void xbt_fifo_freeitem(xbt_fifo_item_t);
 
 void xbt_fifo_push_item(xbt_fifo_t, xbt_fifo_item_t);
 xbt_fifo_item_t xbt_fifo_pop_item(xbt_fifo_t);
@@ -35,25 +59,12 @@ xbt_fifo_item_t xbt_fifo_shift_item(xbt_fifo_t);
 void xbt_fifo_remove(xbt_fifo_t, void *);
 void xbt_fifo_remove_item(xbt_fifo_t, xbt_fifo_item_t);
 
-int xbt_fifo_is_in(xbt_fifo_t, void *);
-
-void **xbt_fifo_to_array(xbt_fifo_t);
-xbt_fifo_t xbt_fifo_copy(xbt_fifo_t);
-
-xbt_fifo_item_t xbt_fifo_newitem(void);
-void xbt_fifo_set_item_content(xbt_fifo_item_t, void *);
-void *xbt_fifo_get_item_content(xbt_fifo_item_t);
-void xbt_fifo_freeitem(xbt_fifo_item_t);
-
-int xbt_fifo_size(xbt_fifo_t);
-
 xbt_fifo_item_t xbt_fifo_getFirstItem(xbt_fifo_t l);
 xbt_fifo_item_t xbt_fifo_getNextItem(xbt_fifo_item_t i);
 xbt_fifo_item_t xbt_fifo_getPrevItem(xbt_fifo_item_t i);
 
 /** 
  * \brief List iterator
- * \ingroup XBT_fifo
  * asserts and stuff
  * \param f a list (#xbt_fifo_t)
  * \param i a bucket (#xbt_fifo_item_t)
@@ -67,5 +78,17 @@ xbt_fifo_item_t xbt_fifo_getPrevItem(xbt_fifo_item_t i);
      ((i)?(n=(type)(xbt_fifo_get_item_content(i))):(NULL));             \
        i=xbt_fifo_getNextItem(i))
 
+/** @} */
 
+/** @name 4. Miscanaleous
+ *
+ *  @{
+ */
+void **xbt_fifo_to_array(xbt_fifo_t);
+xbt_fifo_t xbt_fifo_copy(xbt_fifo_t);
+/** @} */
+
+END_DECL()
+
+/** @} */
 #endif				/* _XBT_FIFO_H */
