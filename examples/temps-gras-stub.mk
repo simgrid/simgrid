6af8dcc83018e0230ffa04464ca1843254dac795
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-03-15 16:44:43 +0000

    work even in subdirs
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@1948 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- examples/temps-gras-stub.mk
+++ examples/temps-gras-stub.mk
@@ -1,11 +1,11 @@
 ## Generate the temp source files on need
 ##
 
-$(foreach proc, $(PROCESSES), _$(NAME)_$(proc).c) _$(NAME)_simulator.c: $(srcdir)/$(NAME)_deployment.xml ../../../tools/gras/gras_stub_generator@EXEEXT@
-	[ x@EXEEXT@ = x ] || exenv=wine; $$exenv ../../../tools/gras/gras_stub_generator@EXEEXT@ $(NAME) $(srcdir)/$(NAME)_deployment.xml >/dev/null
+$(foreach proc, $(PROCESSES), _$(NAME)_$(proc).c) _$(NAME)_simulator.c: $(srcdir)/$(NAME)_deployment.xml $(top_srcdir)/tools/gras/gras_stub_generator@EXEEXT@
+	[ x@EXEEXT@ = x ] || exenv=wine; $$exenv $(top_srcdir)/tools/gras/gras_stub_generator@EXEEXT@ $(NAME) $(srcdir)/$(NAME)_deployment.xml >/dev/null
 
-../../../tools/gras/gras_stub_generator@EXEEXT@:
-	make -C   ../../../tools/gras/ gras_stub_generator@EXEEXT@
+$(top_srcdir)/tools/gras/gras_stub_generator@EXEEXT@:
+	make -C $(top_srcdir)/tools/gras/ gras_stub_generator@EXEEXT@
 
 ##
 ## Cleanups
