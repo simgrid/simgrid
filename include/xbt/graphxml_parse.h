bfc9b3ffef6e41c90e64b271ed981fabe05676cc
alegrand <alegrand@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-03-20 15:37:16 +0000

    Adding a XML graph parser
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@1961 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ include/xbt/graphxml_parse.h
@@ -0,0 +1,46 @@
+/* 	$Id $	 */
+
+/* Copyright (c) 2006 Arnaud Legrand. All rights reserved.                  */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#ifndef _XBT_GRAPHXML_PARSE_H
+#define _XBT_GRAPHXML_PARSE_H
+
+#include <stdio.h>
+#include "xbt/misc.h"
+#include "xbt/graphxml.h"
+
+/* Entry-point of the graphxml parser. */
+extern int_f_void_t xbt_graph_parse;
+
+/* Hook for the different tags. They can be redefined at will whereas
+   the versions without the _fun can't. */
+extern void_f_void_t STag_graph_fun;
+extern void_f_void_t ETag_graph_fun;
+extern void_f_void_t STag_node_fun;
+extern void_f_void_t ETag_node_fun;
+extern void_f_void_t STag_edge_fun;
+extern void_f_void_t ETag_edge_fun;
+
+void xbt_graph_parse_open(const char *file);
+void xbt_graph_parse_close(void);
+void xbt_graph_parse_reset_parser(void);
+void xbt_graph_parse_get_double(double *value,const char *string);
+
+/* Prototypes of the functions offered by flex */
+int xbt_graph_parse_lex(void);
+int xbt_graph_parse_get_lineno(void);
+FILE *xbt_graph_parse_get_in(void);
+FILE *xbt_graph_parse_get_out(void);
+int xbt_graph_parse_get_leng(void);
+char *xbt_graph_parse_get_text(void);
+void xbt_graph_parse_set_lineno(int line_number);
+void xbt_graph_parse_set_in(FILE * in_str);
+void xbt_graph_parse_set_out(FILE * out_str);
+int xbt_graph_parse_get_debug(void);
+void xbt_graph_parse_set_debug(int bdebug);
+int xbt_graph_parse_lex_destroy(void);
+
+#endif
---
+++ src/xbt/graphxml_parse.c
@@ -0,0 +1,127 @@
+/* 	$Id$	 */
+
+/* Copyright (c) 2006 Arnaud Legrand. All rights reserved.                  */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include "xbt/misc.h"
+#include "xbt/log.h"
+#include "xbt/asserts.h"
+
+#include "xbt/dynar.h"
+#include "xbt/graphxml_parse.h"
+
+XBT_LOG_NEW_DEFAULT_SUBCATEGORY(graphxml_parse, xbt ,"Logging specific to the graphxml parsing  module");
+
+#undef CLEANUP
+#include "graphxml.c"
+
+static xbt_dynar_t xbt_graph_input_buffer_stack=NULL;
+static xbt_dynar_t xbt_graph_file_to_parse_stack=NULL;
+
+static void nil_function(void)
+{
+  return;
+}
+
+void_f_void_t STag_graph_fun = nil_function;
+void_f_void_t ETag_graph_fun = nil_function;
+void_f_void_t STag_node_fun = nil_function;
+void_f_void_t ETag_node_fun = nil_function;
+void_f_void_t STag_edge_fun = nil_function;
+void_f_void_t ETag_edge_fun = nil_function;
+
+YY_BUFFER_STATE xbt_graph_input_buffer;
+FILE *xbt_graph_file_to_parse;
+
+void xbt_graph_parse_reset_parser(void)
+{
+  STag_graph_fun = nil_function;
+  ETag_graph_fun = nil_function;
+  STag_node_fun = nil_function;
+  ETag_node_fun = nil_function;
+  STag_edge_fun = nil_function;
+  ETag_edge_fun = nil_function;
+}
+
+void STag_graph(void)
+{
+  STag_graph_fun();
+}
+
+void ETag_graph(void)
+{
+  ETag_graph_fun();
+}
+
+
+void STag_node(void)
+{
+  STag_node_fun();
+}
+
+void ETag_node(void)
+{
+  ETag_node_fun();
+}
+
+
+void STag_edge(void)
+{
+  STag_edge_fun();
+}
+
+void ETag_edge(void)
+{
+  ETag_edge_fun();
+}
+
+
+
+void  xbt_graph_parse_open(const char *file) {
+  if(!file) {
+    WARN0("I hope you know what you're doing... you just gave me a NULL pointer!");
+    return;
+  }
+  if(!xbt_graph_input_buffer_stack) 
+    xbt_graph_input_buffer_stack = xbt_dynar_new(sizeof(YY_BUFFER_STATE),NULL);
+  if(!xbt_graph_file_to_parse_stack) 
+    xbt_graph_file_to_parse_stack = xbt_dynar_new(sizeof(FILE*),NULL);
+
+  xbt_graph_file_to_parse = fopen(file,"r");  /* FIXME should use something like surf_fopen */
+  xbt_assert1((xbt_graph_file_to_parse), "Unable to open \"%s\"\n",file);
+  xbt_graph_input_buffer = xbt_graph_parse__create_buffer( xbt_graph_file_to_parse, 10);
+  xbt_graph_parse__switch_to_buffer(xbt_graph_input_buffer);
+  xbt_graph_parse_lineno = 1;
+}
+
+void  xbt_graph_parse_close(void) {
+  if(xbt_graph_input_buffer_stack) 
+    xbt_dynar_free(&xbt_graph_input_buffer_stack);
+  if(xbt_graph_file_to_parse_stack) 
+    xbt_dynar_free(&xbt_graph_file_to_parse_stack);
+
+  if(xbt_graph_file_to_parse) {
+    xbt_graph_parse__delete_buffer(xbt_graph_input_buffer);
+    fclose(xbt_graph_file_to_parse);
+  }
+}
+
+
+static int _xbt_graph_parse(void)
+{
+  return xbt_graph_parse_lex();
+}
+
+int_f_void_t xbt_graph_parse = _xbt_graph_parse;
+
+void xbt_graph_parse_get_double(double *value,const char *string)
+{ 
+  int ret = 0;
+
+  ret = sscanf(string, "%lg", value);
+  xbt_assert2((ret==1), "Parse error line %d : %s not a number", xbt_graph_parse_lineno,
+	      string);
+}
+
