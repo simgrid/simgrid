b5567dfb63846f0f2a8ea9aea27dfa081060015b
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-03-31 08:58:41 +0000

    Sizes are now in bytes in GRAS & AMOK (plus 'the thing we live for is debuging')
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2045 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- examples/amok/bandwidth/bandwidth.c
+++ examples/amok/bandwidth/bandwidth.c
@@ -68,9 +68,9 @@ int maestro (int argc,char *argv[]);
 int maestro(int argc,char *argv[]) {
   maestro_data_t g;
   double sec, bw;
-  int buf_size=32;
-  int exp_size=1024*50;
-  int msg_size=512;
+  int buf_size=32      *1024;
+  int exp_size=1024*50 *1024;
+  int msg_size=512     *1024;
   gras_socket_t peer;
 
   gras_init(&argc, argv);
@@ -89,16 +89,17 @@ int maestro(int argc,char *argv[]) {
 
   INFO0("Test the BW between me and one of the sensors");  
   amok_bw_test(peer,buf_size,exp_size,msg_size,&sec,&bw);
-  INFO6("Experience between me and %s:%d (%d kb in msgs of %d kb) took %f sec, achieving %f kb/s",
+  INFO6("Experience between me and %s:%d (%d bytes in msgs of %d bytes) took %f sec, achieving %f kb/s",
 	argv[1],atoi(argv[2]),
 	exp_size,msg_size,
-	sec,bw);
+	sec,((double)bw)/1024.0);
 
-  INFO0("Test the BW between the two sensors");  
+  INFO4("Test the BW between %s:%s and %s:%s",argv[1],argv[2],argv[3],argv[4]);
   amok_bw_request(argv[1],atoi(argv[2]),argv[3],atoi(argv[4]),
 		  buf_size,exp_size,msg_size,&sec,&bw);	
-  INFO2("Experience took took %f sec, achieving %f kb/s",
-	sec,bw);
+  INFO6("Experience between %s:%s and %s:%s took took %f sec, achieving %f kb/s",
+	argv[1],argv[2],argv[3],argv[4],
+	sec,((double)bw)/1024.0);
 
   /* ask sensors to quit */                    
   gras_msgtype_declare("quit",NULL);
