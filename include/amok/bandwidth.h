4f52518f3f44161c269e8ba3ee45bcc6179c3b90
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-04-04 14:34:31 +0000

    Introduce saturation mecanism
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2079 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/amok/bandwidth.h
+++ include/amok/bandwidth.h
@@ -78,46 +78,22 @@ void amok_bw_request(const char* from_name,unsigned int from_port,
 double * amok_bw_matrix(xbt_dynar_t hosts, /* dynar of xbt_host_t */
                         int buf_size_bw, int exp_size_bw, int msg_size_bw);
 
-/** @} */
-#if 0   
-
 /* ***************************************************************************
  * Link saturation
  * ***************************************************************************/
 
-/**
- * grasbw_saturate_start:
- * @from_name: Name of the host we are asking to do a experiment with (to_name:to_port)
- * @from_port: port on which the process we are asking for an experiment is listening
- * (for message, do not give a raw socket here. The needed raw socket will be negociated 
- * between the peers)
- * @to_name: Name of the host with which we should conduct the experiment
- * @to_port: port on which the peer process is listening for message
- * @msgSize: Size of each message sent.
- * @timeout: How long in maximum should be the saturation.
- *
- * Ask the process 'from_name:from_port' to start to saturate the link between itself
- * and to_name:to_name.
- */
-void grasbw_saturate_start(const char* from_name,unsigned int from_port,
-				  const char* to_name,unsigned int to_port,
-				  unsigned int msgSize, unsigned int timeout);
 
-/**
- * grasbw_saturate_stop:
- * @from_name: Name of the host we are asking to do a experiment with (to_name:to_port)
- * @from_port: port on which the process we are asking for an experiment is listening
- * (for message, do not give a raw socket here. The needed raw socket will be negociated 
- * between the peers)
- * @to_name: Name of the host with which we should conduct the experiment
- * @to_port: port on which the peer process is listening for message
- *
- * Ask the process 'from_name:from_port' to stop saturating the link between itself
- * and to_name:to_name.
- */
-void grasbw_saturate_stop(const char* from_name,unsigned int from_port,
-				 const char* to_name,unsigned int to_port);
+void amok_bw_saturate_start(const char* from_name,unsigned int from_port,
+			    const char* to_name,unsigned int to_port,
+			    unsigned int msg_size, unsigned int duration);
 
+void amok_bw_saturate_begin(const char* to_name,unsigned int to_port,
+			    unsigned int msg_size, unsigned int duration,
+			    /*out*/ double *elapsed, double *bw);
 
-#endif /* if 0 */
+void amok_bw_saturate_stop(const char* from_name,unsigned int from_port,
+			   /*out*/ unsigned int *time, unsigned int *bw);
+
+/** @} */
+
 #endif /* AMOK_BANDWIDTH_H */
