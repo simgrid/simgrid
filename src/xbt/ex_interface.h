baff7c0f4f11e19eb8f4a1ff80a9dd5c519e9dc3
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-04-07 15:42:30 +0000

    The neat backtraces need a semi-public symbol from the exception mecanism exported to where we deal with exception propagation
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2111 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ src/xbt/ex_interface.h
@@ -0,0 +1,17 @@
+/* $Id$ */
+
+/* ex -- exception handling                                                 */
+/* This file is not to be loaded from anywhere but ex.c                     */
+
+/* Copyright (c) 2003, 2004 Martin Quinson. All rights reserved.            */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#ifndef _XBT_EX_INTERFACE_H_
+#define _XBT_EX_INTERFACE_H_
+
+void xbt_ex_setup_backtrace(xbt_ex_t *e);
+
+#endif  /* _XBT_EX_INTERFACE_H_ */
+
