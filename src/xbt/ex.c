e35a24968303072a0c1b06fd0f65d166bbcc7eac
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-04-14 08:05:29 +0000

    Rework the headers to follow our habits
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2151 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/xbt/ex.c
+++ src/xbt/ex.c
@@ -1,33 +1,15 @@
-/*
-**  OSSP ex - Exception Handling (modified to fit into SimGrid)
-**  Copyright (c) 2005 Martin Quinson
-**  Copyright (c) 2002-2004 Ralf S. Engelschall <rse@engelschall.com>
-**  Copyright (c) 2002-2004 The OSSP Project <http://www.ossp.org/>
-**  Copyright (c) 2002-2004 Cable & Wireless <http://www.cw.com/>
-**
-**  This file is part of OSSP ex, an exception handling library
-**  which can be found at http://www.ossp.org/pkg/lib/ex/.
-**
-**  Permission to use, copy, modify, and distribute this software for
-**  any purpose with or without fee is hereby granted, provided that
-**  the above copyright notice and this permission notice appear in all
-**  copies.
-**
-**  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
-**  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-**  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-**  IN NO EVENT SHALL THE AUTHORS AND COPYRIGHT HOLDERS AND THEIR
-**  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-**  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-**  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
-**  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-**  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-**  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-**  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-**  SUCH DAMAGE.
-**
-**  ex.c: exception handling (compiler part)
-*/
+/* $Id$ */
+
+/* ex - Exception Handling (modified to fit into SimGrid from OSSP version) */
+
+/*  Copyright (c) 2005-2006 Martin Quinson                                  */
+/*  Copyright (c) 2002-2004 Ralf S. Engelschall <rse@engelschall.com>       */
+/*  Copyright (c) 2002-2004 The OSSP Project <http://www.ossp.org/>         */
+/*  Copyright (c) 2002-2004 Cable & Wireless <http://www.cw.com/>           */
+/*  All rights reserved.                                                    */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
 
 #include <stdio.h>
 #include <stdlib.h>
