b3e20bf1b744d84c199388592a5c33b7f6245707
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-04-21 13:50:47 +0000

    Vpath fixups: gras_stub_generator is generated. It is thus in builddir, not srcdir
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2170 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- examples/temps-gras-stub.mk
+++ examples/temps-gras-stub.mk
@@ -1,11 +1,11 @@
 ## Generate the temp source files on need
 ##
 
-$(foreach proc, $(PROCESSES), _$(NAME)_$(proc).c) _$(NAME)_simulator.c: $(srcdir)/$(NAME)_deployment.xml $(top_srcdir)/tools/gras/gras_stub_generator@EXEEXT@
-	[ x@EXEEXT@ = x ] || exenv=wine; $$exenv $(top_srcdir)/tools/gras/gras_stub_generator@EXEEXT@ $(NAME) $(srcdir)/$(NAME)_deployment.xml >/dev/null
+$(foreach proc, $(PROCESSES), _$(NAME)_$(proc).c) _$(NAME)_simulator.c: $(srcdir)/$(NAME)_deployment.xml $(top_builddir)/tools/gras/gras_stub_generator@EXEEXT@
+	[ x@EXEEXT@ = x ] || exenv=wine; $$exenv $(top_builddir)/tools/gras/gras_stub_generator@EXEEXT@ $(NAME) $(srcdir)/$(NAME)_deployment.xml >/dev/null
 
-$(top_srcdir)/tools/gras/gras_stub_generator@EXEEXT@:
-	make -C $(top_srcdir)/tools/gras/ gras_stub_generator@EXEEXT@
+$(top_builddir)/tools/gras/gras_stub_generator@EXEEXT@:
+	make -C $(top_builddir)/tools/gras/ gras_stub_generator@EXEEXT@
 
 ##
 ## Cleanups
