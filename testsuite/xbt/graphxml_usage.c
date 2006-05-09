9fdceb6aa2b5c38ac0d7701b1fe38542a63cbc04
alegrand <alegrand@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-05-09 15:25:52 +0000

    Cleaning output.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2197 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- testsuite/xbt/graphxml_usage.c
+++ testsuite/xbt/graphxml_usage.c
@@ -7,17 +7,16 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include <stdio.h>
-#include <xbt/module.h>
+#include "xbt/module.h"
+#include "xbt/sysdep.h"
 #include "xbt/graph.h"
 #include "xbt/graphxml.h"
-#include "xbt/dynar.h"
-#include "xbt/sysdep.h"
-#include "../src/xbt/graph_private.h"
-void *node_label_and_data(xbt_node_t node, const char *label,
-			  const char *data);
-void *node_label_and_data(xbt_node_t node, const char *label,
-			  const char *data)
+#include "xbt/log.h"
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(test, "Logging specific to graphxml test");
+
+static void *node_label_and_data(xbt_node_t node, const char *label,
+				 const char *data)
 {
   char *lbl = xbt_strdup(label);
   return lbl;
@@ -25,83 +24,125 @@ void *node_label_and_data(xbt_node_t node, const char *label,
 
 static const char *node_name(xbt_node_t n)
 {
-  return n->data;
+  return xbt_graph_node_get_data(n);
 }
 
 void test(char *graph_file);
 void test(char *graph_file)
 {
+  int test_node_deletion = 1;
+  int test_edge_deletion = 1;
+  int test_export_xml = 1;
+  int test_export_dot = 1;
+  int test_export_length = 1;
+  int test_shortest_paths = 1;
+  int test_topo_sort = 1;
+
   int i, j;
   unsigned long n;
-  xbt_dynar_t dynar = NULL;
-  xbt_dynar_t dynar1 = NULL;
+
+  xbt_dynar_t edges = NULL;
+  xbt_dynar_t nodes = NULL;
   xbt_node_t *sorted = NULL;
   xbt_node_t *route = NULL;
 
   xbt_graph_t graph =
       xbt_graph_read(graph_file, &node_label_and_data, NULL);
+  double *adj = NULL;
 
   n = xbt_dynar_length(xbt_graph_get_nodes(graph));
 
-  double *adj = xbt_graph_get_length_matrix(graph);
+  if (test_export_xml) {
+    INFO0("---- Testing XML export. Exporting to testgraph.xml ----");
+    xbt_graph_export_graphxml(graph, "testgraph.xml", NULL, NULL, NULL,
+			      NULL);
+  }
+  if (test_export_dot) {
+    INFO0("---- Testing GraphViz export. Exporting to testgraph.dot ----");
+    xbt_graph_export_graphviz(graph, "testgraph.dot", node_name, NULL);
+  }
 
-  xbt_graph_export_graphxml(graph, "testgraph.xml", NULL, NULL, NULL,
-			    NULL);
-  xbt_graph_export_graphviz(graph, "graph.dot", node_name, NULL);
-
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < n; j++) {
-      fprintf(stderr, "%le\t", adj[i * n + j]);
+  if (test_export_length) {
+    char *buf = NULL;
+    INFO0("---- Dumping Edge lengths ----");
+    adj = xbt_graph_get_length_matrix(graph);
+    buf = calloc(n * 20, sizeof(char));
+    for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++) {
+	sprintf(buf + strlen(buf), "%le\t", adj[i * n + j]);
+      }
+      INFO1("%s", buf);
+      buf[0] = '\000';
     }
-    fprintf(stderr, "\n");
+    free(buf);
+    free(adj);
+  }
+
+  if (test_shortest_paths) {
+    char *buf = NULL;
+    INFO0("---- Testing Shortest Paths ----");
+    route = xbt_graph_shortest_paths(graph);
+    buf = calloc(n * 40, sizeof(char));
+    for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++) {
+	if (route[i * n + j])
+	  snprintf(buf+strlen(buf), 40, "%s\t", 
+		   node_name(route[i * n + j]));
+      }
+      INFO1("%s", buf);
+      buf[0] = '\000';
+    }
+    free(buf);
+    free(route);
+  }
+
+  if (test_topo_sort) {
+    INFO0("---- Testing Topological Sort ----");
+    sorted = xbt_graph_topo_sort(graph);
+    for (i = 0; i < n; i++) {
+      if (sorted[i]) {
+	INFO3("sorted[%d] = %s (%p)", i, node_name(sorted[i]), sorted[i]);
+      }
+    }
+    free(sorted);
   }
 
 
-  route = xbt_graph_shortest_paths(graph);
+  if (test_node_deletion) {
+    INFO0("---- Testing Node Deletion ----");
+    nodes = xbt_graph_get_nodes(graph);
+    edges = xbt_graph_get_edges(graph);
+    INFO2("Before Node deletion: %lu nodes, %lu edges",
+	  xbt_dynar_length(nodes), xbt_dynar_length(edges));
 
-  /*  for(i=0;i<n;i++) */
-  /*     { */
-  /*       for(j=0;j<n;j++) */
-  /* 	{ */
-  /* 	  if( route[i*n+j]) */
-  /* 	  fprintf(stderr,"%s\t",(char*)(route[i*n+j])->data) ); */
-  /* 	} */
-  /*       fprintf(stderr,"\n" ); */
-  /*     } */
-
-
-  sorted = xbt_graph_topo_sort(graph);
-
-  for (i = 0; i < n; i++) {
-    if (sorted[i]) {
-      /* fprintf(stderr,"sorted[%d] =%p\n",i,sorted[i] ); */
-      fprintf(stderr, "%s\t", (char *) ((sorted[i])->data)
-	  );
-
-      fprintf(stderr, "\n");
-    }
+    while (xbt_dynar_length(nodes))
+      xbt_graph_free_node(graph,
+			  *((xbt_node_t *) xbt_dynar_get_ptr(nodes, 0)),
+			  NULL, NULL);
+    INFO2("After Node deletion:  %lu nodes, %lu edges",
+	  xbt_dynar_length(nodes), xbt_dynar_length(edges));
   }
 
-  /*  while(xbt_dynar_length(dynar)) */
-  /*     xbt_graph_free_node(graph,*((xbt_node_t*)xbt_dynar_get_ptr(dynar,0)),NULL,NULL); */
+  if (test_edge_deletion) {
+    INFO0("---- Testing Edge Deletion ----");
+    nodes = xbt_graph_get_nodes(graph);
+    edges = xbt_graph_get_edges(graph);
+    INFO2("Before Edge deletion: %lu nodes, %lu edges",
+	  xbt_dynar_length(nodes), xbt_dynar_length(edges));
 
-  dynar = xbt_graph_get_edges(graph);
-  while (xbt_dynar_length(dynar))
-    xbt_graph_free_edge(graph,
-			*((xbt_edge_t *) xbt_dynar_get_ptr(dynar, 0)),
-			NULL);
+    while (xbt_dynar_length(edges))
+      xbt_graph_free_edge(graph,
+			  *((xbt_edge_t *) xbt_dynar_get_ptr(edges, 0)),
+			  NULL);
 
-  printf("%lu edges\n", xbt_dynar_length(dynar));
-  dynar1 = xbt_graph_get_nodes(graph);
-  printf("%lu nodes\n", xbt_dynar_length(dynar1));
+    INFO2("After Edge deletion:  %lu nodes, %lu edges",
+	  xbt_dynar_length(nodes), xbt_dynar_length(edges));
+  }
 
-  free(adj);
-  free(route);
   xbt_graph_free_graph(graph, NULL, NULL, NULL);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   xbt_init(&argc, argv);
   if (argc == 1) {
     fprintf(stderr, "Usage : %s graph.xml\n", argv[0]);
