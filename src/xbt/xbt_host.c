26860eef5251a64ec0441bd4c2e0181515d577b2
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-05-19 09:44:14 +0000

    And now, this damn 5-lines function without trivial bug. :( Sorry for the spam
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2250 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/xbt/xbt_host.c
+++ src/xbt/xbt_host.c
@@ -25,7 +25,7 @@ xbt_host_t xbt_host_new(const char *name, int port)  {
 xbt_host_t xbt_host_from_string(const char *hostport)  {
    xbt_host_t res=xbt_new(s_xbt_host_t, 1);
    char *name=xbt_strdup(hostport);
-   char *port_str=strchr(hostport,':');
+   char *port_str=strchr(name,':');
    xbt_assert1(port_str,"argument of xbt_host_from_string should be of form <hostname>:<port>, it's '%s'", hostport);
    *port_str='\0';
    port_str++;
