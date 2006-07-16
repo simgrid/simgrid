d294cef35bcf12a367aa4e444b6e52ed8aff99ee
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-07-16 11:00:10 +0000

    Split gtut in several pages, add lesson 5
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2586 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- doc/gtut-files/5-globals.c
+++ doc/gtut-files/5-globals.c
@@ -1,5 +1,22 @@
 #include <gras.h>
 
+typedef struct {
+   int killed;
+} *server_data_t;
+   
+
+int server_kill_cb(gras_msg_cb_ctx_t ctx, void *payload) {
+  gras_socket_t client = gras_msg_cb_ctx_from(ctx);
+  server_data_t *globals=(server_data_t*)gras_userdata_get();
+   
+  fprintf(stderr,"Argh, killed by %s:%d! Bye folks...\n",
+   	  gras_socket_peer_name(client), gras_socket_peer_port(client));
+  
+  globals->killed = 1;
+   
+  return 1;
+} /* end_of_kill_callback */
+
 int server_hello_cb(gras_msg_cb_ctx_t ctx, void *payload) {
   gras_socket_t client = gras_msg_cb_ctx_from(ctx);
 
@@ -7,18 +24,27 @@ int server_hello_cb(gras_msg_cb_ctx_t ctx, void *payload) {
    	  gras_socket_peer_name(client), gras_socket_peer_port(client));
   
   return 1;
-} /* end_of_callback */
+} /* end_of_hello_callback */
 
 int server(int argc, char *argv[]) {
   gras_socket_t mysock;   /* socket on which I listen */
+  server_data_t globals;
   
   gras_init(&argc,argv);
 
+  globals=gras_userdata_new(server_data_t);
+  globals->killed=0;
+
   gras_msgtype_declare("hello", NULL);
+  gras_msgtype_declare("kill", NULL);
   mysock = gras_socket_server(atoi(argv[1]));
    
   gras_cb_register(gras_msgtype_by_name("hello"),&server_hello_cb);   
-  gras_msg_handle(60);
+  gras_cb_register(gras_msgtype_by_name("kill"),&server_kill_cb);
+
+  while (!globals->killed) {
+     gras_msg_handle(-1); /* blocking */
+  }
     
   gras_exit();
   return 0;
@@ -31,6 +57,7 @@ int client(int argc, char *argv[]) {
   gras_init(&argc,argv);
 
   gras_msgtype_declare("hello", NULL);
+  gras_msgtype_declare("kill", NULL);
   mysock = gras_socket_server_range(1024, 10000, 0, 0);
   
   fprintf(stderr,"Client ready; listening on %d\n", gras_socket_my_port(mysock));
@@ -39,7 +66,12 @@ int client(int argc, char *argv[]) {
   toserver = gras_socket_client(argv[1], atoi(argv[2]));
   
   gras_msg_send(toserver,gras_msgtype_by_name("hello"), NULL);
-  fprintf(stderr,"That's it, we sent the data to the server on %s\n", gras_socket_peer_name(toserver));
+  fprintf(stderr,"we sent the data to the server on %s. Let's do it again for fun\n", gras_socket_peer_name(toserver));
+  gras_msg_send(toserver,gras_msgtype_by_name("hello"), NULL);
+   
+  fprintf(stderr,"Ok. Enough. Have a rest, and then kill the server\n");
+  gras_os_sleep(5); /* sleep 1 second and half */
+  gras_msg_send(toserver,gras_msgtype_by_name("kill"), NULL);
 
   gras_exit();
   return 0;
