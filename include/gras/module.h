dc35ba75078e8ed895386c9be316774d3235f869
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-07-19 14:05:17 +0000

    New, proper, module mecanism. Still missing dependency tracking, but already allow modules to declare init/exit functions and per-process globals initialized during some join/leave functions. Once every module switch to that model, libdata can be killed (but both can coexist for now)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2625 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ include/gras/module.h
@@ -0,0 +1,21 @@
+
+#ifndef GRAS_MODULE_H
+#define GRAS_MODULE_H
+
+#include <xbt.h>
+
+/* Function users of module should use */
+void gras_module_join(const char *name);
+void gras_module_leave(const char *name);
+
+
+/* Functions module implementor should use */
+void gras_module_add(const char *name, unsigned int data_size, int *ID,
+		     void_f_void_t  *init_f, void_f_void_t  *exit_f,
+		     void_f_pvoid_t *join_f, void_f_pvoid_t *leave_f) ;
+
+
+void *gras_moddata_by_id(unsigned int ID);
+
+
+#endif /* GRAS_MODULE_H */
