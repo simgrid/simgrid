5b654b49c8603ff2f3fe7cfae9816cb585df0200
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-07-19 14:16:15 +0000

    gras_modules come into the dance, cleanup the GRAS+AMOK initialization schema
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2630 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ src/amok/amok_base.c
@@ -0,0 +1,24 @@
+/* $Id$ */
+
+/* base - several addons to do specific stuff not in GRAS itself            */
+
+/* Copyright (c) 2003, 2004 Martin Quinson. All rights reserved.            */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include "gras.h"
+#include "amok/amok_modinter.h"
+
+XBT_LOG_NEW_SUBCATEGORY(amok,XBT_LOG_ROOT_CAT,"All AMOK categories");
+
+void amok_init(void) {
+
+  /* Create all the modules */
+  amok_pm_modulecreate();
+}
+
+void amok_exit(void) {
+   /* FIXME: No real module mechanism in GRAS so far, nothing to do. */
+}
+
---
+++ src/amok/amok_modinter.h
@@ -0,0 +1,19 @@
+/* $Id$ */
+
+/* amok modinter - interface to AMOK modules initialization and such        */
+
+/* Copyright (c) 2006 Martin Quinson. All rights reserved.                  */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#ifndef AMOK_BASE_H
+#define AMOK_BASE_H
+
+void amok_init(void);
+void amok_exit(void);
+
+/* module creation functions */
+void amok_pm_modulecreate(void);
+
+#endif /* AMOK_BASE_H */
