1a4cdddd9caa6ce3ed6cf860bebbf96f237f7241
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-08-02 13:52:33 +0000

    kill unused struct
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2674 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/xbt/xbt_main.c
+++ src/xbt/xbt_main.c
@@ -23,14 +23,6 @@ XBT_LOG_NEW_DEFAULT_SUBCATEGORY(module,xbt, "module handling");
 char *xbt_binary_name=NULL; /* Mandatory to retrieve neat backtraces */
 int xbt_initialized=0;
 
-struct xbt_module_ {
-  xbt_dynar_t *deps;
-  xbt_cfg_t *cfg;
-  int ref;
-  xbt_module_new_fct_t new;
-  xbt_module_finalize_fct_t finalize;
-};
-
 /** @brief Initialize the xbt mechanisms. */
 void 
 xbt_init(int *argc, char **argv) {
