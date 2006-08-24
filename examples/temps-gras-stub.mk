4074cbf2951df85b8ccaf4f2b49c86e140971dfd
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2006-08-24 18:46:45 +0000

    stick to the file naming convention presented in the tutorial
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@2740 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- examples/temps-gras-stub.mk
+++ examples/temps-gras-stub.mk
@@ -1,8 +1,8 @@
 ## Generate the temp source files on need
 ##
 
-$(foreach proc, $(PROCESSES), _$(NAME)_$(proc).c) _$(NAME)_simulator.c: $(srcdir)/$(NAME)_deployment.xml $(top_builddir)/tools/gras/gras_stub_generator@EXEEXT@
-	[ x@EXEEXT@ = x ] || exenv=wine; $$exenv $(top_builddir)/tools/gras/gras_stub_generator@EXEEXT@ $(NAME) $(srcdir)/$(NAME)_deployment.xml >/dev/null
+$(foreach proc, $(PROCESSES), _$(NAME)_$(proc).c) _$(NAME)_simulator.c: $(srcdir)/$(NAME).xml $(top_builddir)/tools/gras/gras_stub_generator@EXEEXT@
+	[ x@EXEEXT@ = x ] || exenv=wine; $$exenv $(top_builddir)/tools/gras/gras_stub_generator@EXEEXT@ $(NAME) $(srcdir)/$(NAME).xml >/dev/null
 
 $(top_builddir)/tools/gras/gras_stub_generator@EXEEXT@:
 	make -C $(top_builddir)/tools/gras/ gras_stub_generator@EXEEXT@
