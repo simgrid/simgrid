b87d5e4bfb92ba7092d707fbb1ecbb69aa7b0ff8
velho <velho@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-02-20 14:50:17 +0000

    Added a comentary given credit to the author of CSDP library. At the beggining of each related file.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3163 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/csdp/blockmat.h
+++ include/csdp/blockmat.h
@@ -1,4 +1,8 @@
 /*
+  This file is part of the CSDP 5.0 package developed by Dr. Brian Borchers.
+  Which can be distributed under the LGPL licence. The source code and 
+  manual can be downloaded at http://euler.nmt.edu/~brian/csdp.html.
+
   This file contains definitions for the block matrix data structures used
   in CSDP 3.0.  Note that there are an additional set of definitions used
   for sparse constraint matrices.
--- include/csdp/parameters.h
+++ include/csdp/parameters.h
@@ -1,4 +1,7 @@
 /*
+  This file is part of the CSDP 5.0 package developed by Dr. Brian Borchers.
+  Which can be distributed under the LGPL licence. The source code and 
+  manual can be downloaded at http://euler.nmt.edu/~brian/csdp.html.
 
   This include file contains declarations for a number of parameters that 
   can affect the performance of CSDP.  You can adjust these parameters by 
@@ -7,6 +10,8 @@
     2. Declare struct paramstruc params;
     3. Call init_params(params); to get default values.
     4. Change the value of the parameter that you're interested in.
+
+
 
   */
 
