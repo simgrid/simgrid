421c862bdd97e9846933dea70327d47e4b0a122c
alegrand <alegrand@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-02-28 10:24:32 +0000

    Making room for simix.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3169 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ src/simix/smx_global.c
@@ -0,0 +1,9 @@
+/* 	$Id$	 */
+
+/* Copyright (c) 2007 Arnaud Legrand, Bruno Donnassolo.
+   All rights reserved.                                          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include "simix/simix.h"
