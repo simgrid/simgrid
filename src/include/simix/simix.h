25075d4291d4336a78b61f058a20903caeb8dd96
donassbr <donassbr@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-03-01 10:32:40 +0000

    Initial structure. Nothing works yet.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3176 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/include/simix/simix.h
+++ src/include/simix/simix.h
@@ -1,11 +1,101 @@
 /* 	$Id$	 */
 
-/* Copyright (c) 2007 Arnaud Legrand, Bruno Donnassolo.
-   All rights reserved.                                          */
+/* Copyright (c) 2002,2003,2004 Arnaud Legrand. All rights reserved.        */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
 #ifndef _SIMIX_SIMIX_H
 #define _SIMIX_SIMIX_H
+
+#include "xbt/misc.h"
+#include "simix/datatypes.h"
+
+SG_BEGIN_DECL()
+
+
+/************************** Global ******************************************/
+XBT_PUBLIC(void) SIMIX_config(const char *name, ...);
+XBT_PUBLIC(void) SIMIX_global_init(int *argc, char **argv);
+XBT_PUBLIC(void) SIMIX_global_init_args(int *argc, char **argv);
+XBT_PUBLIC(SIMIX_error_t) SIMIX_main(void);
+XBT_PUBLIC(SIMIX_error_t) SIMIX_clean(void);
+XBT_PUBLIC(void) SIMIX_function_register(const char *name, smx_process_code_t code);
+XBT_PUBLIC(smx_process_code_t) SIMIX_get_registered_function(const char *name);
+// SIMIX_deploy_application???
+XBT_PUBLIC(void) SIMIX_launch_application(const char *file);
+
+XBT_PUBLIC(double) SIMIX_get_clock(void);
+
+/************************** Host handling ***********************************/
+//pointer to above layer
+XBT_PUBLIC(SIMIX_error_t) SIMIX_host_set_data(smx_host_t host, void *data);
+XBT_PUBLIC(void*) SIMIX_host_get_data(smx_host_t host);
+
+XBT_PUBLIC(const char *) SIMIX_host_get_name(smx_host_t host);
+XBT_PUBLIC(smx_host_t) SIMIX_host_self(void);
+/* int SIMIX_get_msgload(void); This function lacks specification; discard it */
+XBT_PUBLIC(double) SIMIX_get_host_speed(smx_host_t h);
+XBT_PUBLIC(int) SIMIX_host_is_avail (smx_host_t h);
+
+XBT_PUBLIC(void) SIMIX_create_environment(const char *file);
+
+XBT_PUBLIC(smx_host_t) SIMIX_get_host_by_name(const char *name);
+XBT_PUBLIC(int) SIMIX_get_host_number(void);
+XBT_PUBLIC(smx_host_t *)SIMIX_get_host_table(void);
+
+/************************** Process handling *********************************/
+XBT_PUBLIC(smx_process_t) SIMIX_process_create(const char *name,
+			       smx_process_code_t code, void *data,
+			       smx_host_t host);
+XBT_PUBLIC(smx_process_t) SIMIX_process_create_with_arguments(const char *name,
+					      smx_process_code_t code, void *data,
+					      smx_host_t host, int argc, char **argv);
+XBT_PUBLIC(void) SIMIX_process_kill(smx_process_t process);
+XBT_PUBLIC(void) SIMIX_process_killall(void);
+
+XBT_PUBLIC(SIMIX_error_t) SIMIX_process_change_host(smx_process_t process, smx_host_t host);
+//above layer
+XBT_PUBLIC(void*) SIMIX_process_get_data(smx_process_t process);
+XBT_PUBLIC(SIMIX_error_t) SIMIX_process_set_data(smx_process_t process, void *data);
+
+XBT_PUBLIC(smx_host_t) SIMIX_process_get_host(smx_process_t process);
+XBT_PUBLIC(const char *)SIMIX_process_get_name(smx_process_t process);
+XBT_PUBLIC(smx_process_t) SIMIX_process_self(void);
+
+XBT_PUBLIC(SIMIX_error_t) SIMIX_process_suspend(smx_process_t process);
+XBT_PUBLIC(SIMIX_error_t) SIMIX_process_resume(smx_process_t process);
+XBT_PUBLIC(int) SIMIX_process_is_suspended(smx_process_t process);
+
+XBT_PUBLIC(SIMIX_error_t) SIMIX_process_sleep(double nb_sec);
+XBT_PUBLIC(SIMIX_error_t) SIMIX_get_errno(void);
+
+/************************** Synchro handling **********************************/
+
+/******Mutex******/
+XBT_PUBLIC(smx_mutex_t) SIMIX_mutex_init(void);
+XBT_PUBLIC(void) SIMIX_mutex_lock(smx_mutex_t mutex);
+XBT_PUBLIC(void) SIMIX_mutex_trylock(smx_mutex_t mutex);
+XBT_PUBLIC(void) SIMIX_mutex_unlock(smx_mutex_t mutex);
+XBT_PUBLIC(void) SIMIX_mutex_destroy(smx_mutex_t mutex);
+
+/*****Conditional*****/
+XBT_PUBLIC(smx_cond_t) SIMIX_cond_init(void);
+XBT_PUBLIC(void) SIMIX_cond_signal(smx_cond_t cond);
+XBT_PUBLIC(void) SIMIX_cond_wait(smx_cond_t cond,smx_mutex_t mutex);
+XBT_PUBLIC(void) SIMIX_cond_wait_timeout(smx_cond_t cond,smx_mutex_t mutex, double max_duration);
+XBT_PUBLIC(void) SIMIX_cond_broadcast(smx_cond_t cond);
+XBT_PUBLIC(void) SIMIX_cond_destroy(smx_cond_t cond);
+
+
+/************************** Action handling ************************************/
+XBT_PUBLIC(smx_action_t) SIMIX_communicate(smx_host_t sender,smx_host_t receiver, double size);
+XBT_PUBLIC(smx_action_t) SIMIX_execute(smx_host_t host,double amount);
+XBT_PUBLIC(SIMIX_error_t) SIMIX_action_cancel(smx_action_t action);
+XBT_PUBLIC(void) SIMIX_action_set_priority(smx_action_t action, double priority);
+XBT_PUBLIC(SIMIX_error_t) SIMIX_action_destroy(smx_action_t action);
+
+//SIMIX_action_wait_for_computation(smx_process_t process, smx_action_t action);
+
+SG_END_DECL()
 #endif                          /* _SIMIX_SIMIX_H */
--- src/simix/smx_global.c
+++ src/simix/smx_global.c
@@ -6,4 +6,297 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include "simix/simix.h"
+#include "private.h"
+#include "xbt/sysdep.h"
+#include "xbt/log.h"
+#include "xbt/ex.h" /* ex_backtrace_display */
+XBT_LOG_NEW_DEFAULT_SUBCATEGORY(simix_kernel, simix,
+				"Logging specific to SIMIX (kernel)");
+
+//int __stop_at_time = -1.0 ;
+
+SIMIX_Global_t simix_global = NULL;
+
+/* static void MarkAsFailed(m_task_t t, TBX_HashTable_t failedProcessList); */
+/* static xbt_fifo_t SIMIX_buildFailedHostList(double a, double b); */
+
+/** \defgroup msg_simulation   SIMIX simulation Functions
+ *  \brief This section describes the functions you need to know to
+ *  set up a simulation. You should have a look at \ref SIMIX_examples 
+ *  to have an overview of their usage.
+ *    \htmlonly <!-- DOXYGEN_NAVBAR_LABEL="Simulation functions" --> \endhtmlonly
+ */
+
+/********************************* SIMIX **************************************/
+
+/** \ingroup msg_simulation
+ * \brief Initialize some SIMIX internal data.
+ */
+void SIMIX_global_init_args(int *argc, char **argv)
+{
+  SIMIX_global_init(argc,argv);
+}
+
+/** \ingroup msg_simulation
+ * \brief Initialize some SIMIX internal data.
+ */
+void SIMIX_global_init(int *argc, char **argv)
+{
+	s_smx_process_t proc;
+
+  if (!simix_global) {
+    surf_init(argc, argv);	/* Initialize some common structures. Warning, it sets msg_global=NULL */
+     
+    simix_global = xbt_new0(s_SIMIX_Global_t,1);
+
+    xbt_context_init();
+    simix_global->host = xbt_fifo_new();
+    simix_global->process_to_run = xbt_swag_new(xbt_swag_offset(proc,process_hookup));
+    simix_global->process_list = xbt_swag_new(xbt_swag_offset(proc,process_hookup));
+    simix_global->current_process = NULL;
+    simix_global->registered_functions = xbt_dict_new();
+  }
+}
+
+void __SIMIX_display_process_status(void)
+{
+   smx_process_t process = NULL;
+   //xbt_fifo_item_t item = NULL;
+   //int i;
+   int nbprocess=xbt_swag_size(simix_global->process_list);
+   
+   INFO1("SIMIX: %d processes are still running, waiting for something.",
+	 nbprocess);
+   /*  List the process and their state */
+   INFO0("SIMIX: <process>(<pid>) on <host>: <status>.");
+   xbt_swag_foreach(process, simix_global->process_list) {
+      simdata_process_t p_simdata = (simdata_process_t) process->simdata;
+     // simdata_host_t h_simdata=(simdata_host_t)p_simdata->host->simdata;
+      char *who;
+	
+      asprintf(&who,"SIMIX:  %s on %s: %s",
+	       process->name,
+				 p_simdata->host->name,
+	       (process->simdata->blocked)?"[blocked] "
+	       :((process->simdata->suspended)?"[suspended] ":""));
+      
+      free(who);
+   }
+}
+
+/* FIXME: Yeah, I'll do it in a portable maner one day [Mt] */
+#include <signal.h>
+
+static void _XBT_CALL inthandler(int ignored)
+{
+   INFO0("CTRL-C pressed. Displaying status and bailing out");
+   __SIMIX_display_process_status();
+   exit(1);
+}
+
+/** \ingroup msg_simulation
+ * \brief Launch the SIMIX simulation
+ */
+SIMIX_error_t SIMIX_main(void)
+{
+  smx_process_t process = NULL;
+  int i;
+  double elapsed_time = 0.0;
+  int state_modifications = 1;
+   
+  /* Prepare to display some more info when dying on Ctrl-C pressing */
+  signal(SIGINT,inthandler);
+   
+  /* Clean IO before the run */
+  fflush(stdout);
+  fflush(stderr);
+
+  surf_solve(); /* Takes traces into account. Returns 0.0 */
+/* xbt_fifo_size(msg_global->process_to_run) */
+  while (1) {
+    xbt_context_empty_trash();
+    if(xbt_swag_size(simix_global->process_to_run) && (elapsed_time>0)) {
+      DEBUG0("**************************************************");
+    }
+    /*
+    if((__stop_at_time>0) && (SIMIX_get_clock() >= __stop_at_time)) {
+      DEBUG0("Let's stop here!");
+    }*/
+
+    while ((process = xbt_swag_extract(simix_global->process_to_run))) {
+      DEBUG2("Scheduling %s on %s",	     
+	     process->name,
+	     process->simdata->host->name);
+      simix_global->current_process = process;
+/*       fflush(NULL); */
+      xbt_context_schedule(process->simdata->context);
+      simix_global->current_process = NULL;
+    }
+    
+    {
+      surf_action_t action = NULL;
+      surf_resource_t resource = NULL;
+      //m_task_t task = NULL;
+
+      void *fun = NULL;
+      void *arg = NULL;
+
+      xbt_dynar_foreach(resource_list, i, resource) {
+	if(xbt_swag_size(resource->common_public->states.failed_action_set) ||
+	   xbt_swag_size(resource->common_public->states.done_action_set))
+	  state_modifications = 1;
+      }
+      
+      if(!state_modifications) {
+	DEBUG1("%f : Calling surf_solve",SIMIX_get_clock());
+	elapsed_time = surf_solve();
+	DEBUG1("Elapsed_time %f",elapsed_time);
+	
+	if (elapsed_time<0.0) {
+	  /*       fprintf(stderr, "We're done %g\n",elapsed_time); */
+	  break;
+	}
+      }
+
+      while (surf_timer_resource->extension_public->get(&fun,(void*)&arg)) {
+	DEBUG2("got %p %p", fun, arg);
+	if(fun==SIMIX_process_create_with_arguments) {
+	  process_arg_t args = arg;
+	  DEBUG2("Launching %s on %s", args->name, args->host->name);
+	  process = SIMIX_process_create_with_arguments(args->name, args->code, 
+						      args->data, args->host,
+						      args->argc,args->argv);
+	  if(args->kill_time > SIMIX_get_clock()) {
+	    surf_timer_resource->extension_public->set(args->kill_time, 
+						       (void*) &SIMIX_process_kill,
+						       (void*) process);
+	  }
+	  xbt_free(args);
+	}
+	if(fun==SIMIX_process_kill) {
+	  process = arg;
+	  DEBUG2("Killing %s on %s", process->name, 
+		 process->simdata->host->name);
+	  SIMIX_process_kill(process);
+	}
+      }
+      
+      xbt_dynar_foreach(resource_list, i, resource) {
+	while ((action =
+		xbt_swag_extract(resource->common_public->states.
+				 failed_action_set))) {
+				 /*
+	  task = action->data;
+	  if(task) {
+	    int _cursor;
+	    DEBUG1("** %s failed **",task->name);
+	    xbt_dynar_foreach(task->simdata->sleeping,_cursor,process) {
+	      DEBUG3("\t preparing to wake up %s(%d) on %s",	     
+		     process->name,process->simdata->PID,
+		     process->simdata->host->name);
+	      xbt_fifo_unshift(msg_global->process_to_run, process);
+	    }
+	    process=NULL;
+	  }*/
+	}
+	while ((action =
+		xbt_swag_extract(resource->common_public->states.
+				 done_action_set))) {
+	  /*
+		task = action->data;
+	  if(task) {
+	    int _cursor;
+	    DEBUG1("** %s done **",task->name);
+	    xbt_dynar_foreach(task->simdata->sleeping,_cursor,process) {
+	      DEBUG3("\t preparing to wake up %s(%d) on %s",	     
+		     process->name,process->simdata->PID,
+		     process->simdata->host->name);
+	      xbt_fifo_unshift(msg_global->process_to_run, process);
+	    }
+	    process=NULL;
+	  }*/
+	}
+      }
+    }
+    state_modifications = 0;
+  }
+
+  if (xbt_swag_size(simix_global->process_list) == 0) {
+    INFO0("Congratulations ! Simulation terminated : all processes are over");
+    return SIMIX_OK;
+  } else {
+    INFO0("Oops ! Deadlock or code not perfectly clean.");
+    __SIMIX_display_process_status();
+    if(XBT_LOG_ISENABLED(simix, xbt_log_priority_debug) ||
+       XBT_LOG_ISENABLED(simix_kernel, xbt_log_priority_debug)) {
+      DEBUG0("Aborting!");
+      xbt_abort();
+    }
+
+    INFO0("Return a Warning.");
+    return SIMIX_WARNING;
+  }
+}
+
+/** \ingroup msg_simulation
+ * \brief Kill all running process
+
+ * \param reset_PIDs should we reset the PID numbers. A negative
+ *   number means no reset and a positive number will be used to set the PID
+ *   of the next newly created process.
+ */
+void SIMIX_process_killall()
+{
+  smx_process_t p = NULL;
+  smx_process_t self = SIMIX_process_self();
+
+  while((p=xbt_swag_extract(simix_global->process_list))) {
+    if(p!=self) SIMIX_process_kill(p);
+  }
+
+  xbt_context_empty_trash();
+
+  if(self) {
+    xbt_context_yield();
+  }
+
+  return;
+}
+
+/** \ingroup msg_simulation
+ * \brief Clean the SIMIX simulation
+ */
+SIMIX_error_t SIMIX_clean(void)
+{
+  xbt_fifo_item_t i = NULL;
+  smx_host_t h = NULL;
+  smx_process_t p = NULL;
+
+
+  while((p=xbt_swag_extract(simix_global->process_list))) {
+    SIMIX_process_kill(p);
+  }
+  xbt_context_exit();
+
+  xbt_fifo_foreach(simix_global->host,i,h,smx_host_t) {
+    __SIMIX_host_destroy(h);
+  }
+  xbt_fifo_free(simix_global->host);
+  xbt_swag_free(simix_global->process_to_run);
+  xbt_swag_free(simix_global->process_list);
+  xbt_dict_free(&(simix_global->registered_functions));
+
+  simix_config_finalize();
+  free(simix_global);
+  surf_exit();
+
+  return SIMIX_OK;
+}
+
+
+/** \ingroup msg_easier_life
+ * \brief A clock (in second).
+ */
+double SIMIX_get_clock(void) {
+  return surf_get_clock();
+}
