4b6bf43780ca98a105f87986ac4194428cdf2c70
cherierm <cherierm@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-03-01 16:37:35 +0000

    export some function declared in trace_mgr.h
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3184 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/include/surf/trace_mgr.h
+++ src/include/surf/trace_mgr.h
@@ -17,25 +17,25 @@ typedef struct tmgr_trace *tmgr_trace_t;
 typedef struct tmgr_trace_event *tmgr_trace_event_t;
 
 /* Creation functions */
-tmgr_history_t tmgr_history_new(void);
-void tmgr_history_free(tmgr_history_t history);
+XBT_PUBLIC(tmgr_history_t) tmgr_history_new(void);
+XBT_PUBLIC(void) tmgr_history_free(tmgr_history_t history);
 
-tmgr_trace_t tmgr_trace_new(const char *filename);
-tmgr_trace_t tmgr_empty_trace_new(void);
-void tmgr_trace_free(tmgr_trace_t trace);
+XBT_PUBLIC(tmgr_trace_t) tmgr_trace_new(const char *filename);
+XBT_PUBLIC(tmgr_trace_t) tmgr_empty_trace_new(void);
+XBT_PUBLIC(void) tmgr_trace_free(tmgr_trace_t trace);
 
-tmgr_trace_event_t tmgr_history_add_trace(tmgr_history_t history,
+XBT_PUBLIC(tmgr_trace_event_t) tmgr_history_add_trace(tmgr_history_t history,
 					  tmgr_trace_t trace,
 					  double start_time, int offset,
 					  void *resource);
 
 /* Access functions */
-double tmgr_history_next_date(tmgr_history_t history);
-tmgr_trace_event_t tmgr_history_get_next_event_leq(tmgr_history_t history,
+XBT_PUBLIC(double) tmgr_history_next_date(tmgr_history_t history);
+XBT_PUBLIC(tmgr_trace_event_t) tmgr_history_get_next_event_leq(tmgr_history_t history,
 						   double date,
 						   double *value,
 						   void **resource);
 
-void tmgr_finalize(void);
+XBT_PUBLIC(void) tmgr_finalize(void);
 
 #endif				/* _SURF_TMGR_H */
