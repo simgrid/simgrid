f08a5b3c82ad8d1b8c2176eaf66c65992fa8f402
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-03-22 16:08:54 +0000

    declaration of toupper
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3338 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/surf/surf.c
+++ src/surf/surf.c
@@ -5,6 +5,8 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
+#include <ctype.h>
+
 #include "surf_private.h"
 #include "xbt/module.h"
 
