94295894aaa30ec7c01f5deae1da046677897a72
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-05-10 12:58:35 +0000

    Some more doc
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3499 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/include/xbt/xbt_thread.h
+++ src/include/xbt/xbt_thread.h
@@ -31,6 +31,7 @@ SG_BEGIN_DECL()
   XBT_PUBLIC(xbt_thread_t) xbt_thread_create(pvoid_f_pvoid_t start_routine,void* param);
   XBT_PUBLIC(void) xbt_thread_exit(int *retcode);
   XBT_PUBLIC(xbt_thread_t) xbt_thread_self(void);
+  /* xbt_thread_join frees the joined thread (ie the XBT wrapper around it, the OS frees the rest) */
   XBT_PUBLIC(void) xbt_thread_join(xbt_thread_t thread,void ** thread_return);
   XBT_PUBLIC(void) xbt_thread_yield(void);
 
