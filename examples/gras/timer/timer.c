a9325e9a82ed6cc0f6e487e2e61138e50b222519
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-06-06 15:19:29 +0000

    Convert to tesh
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3573 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- examples/gras/timer/timer.c
+++ examples/gras/timer/timer.c
@@ -26,7 +26,7 @@ static void repetitive_action(void) {
    
    /* Stop if nothing to do yet */
    if (globals->still_to_do <= 0) {
-     INFO1("[%.0f] Repetitive_action has nothing to do yet",gras_os_time());
+     INFO0("Repetitive_action has nothing to do yet");
      return;
    }
    
@@ -35,7 +35,7 @@ static void repetitive_action(void) {
       gras_timer_cancel_repeat(REPEAT_INTERVAL,repetitive_action);
    }
 
-   INFO2("[%.0f] repetitive_action decrementing globals->still_to_do. New value: %d",gras_os_time(),
+   INFO1("repetitive_action decrementing globals->still_to_do. New value: %d",
 	 globals->still_to_do-1);
    
    globals->still_to_do--; /* should be the last line of the action since value=0 stops the program */
@@ -44,8 +44,7 @@ static void repetitive_action(void) {
 static void delayed_action(void) {
    my_globals *globals=(my_globals*)gras_userdata_get();
    
-   INFO2("[%.0f] delayed_action setting globals->still_to_do to %d",
-	 gras_os_time(),LOOP_COUNT);
+   INFO1("delayed_action setting globals->still_to_do to %d",LOOP_COUNT);
 
    globals->still_to_do = LOOP_COUNT;
 } /* end_of_delayed_action */
@@ -57,26 +56,25 @@ int client(int argc,char *argv[]) {
   globals=gras_userdata_new(my_globals);
   globals->still_to_do = -1;
 
-  INFO2("[%.0f] Programming the repetitive_action with a frequency of %f sec", gras_os_time(), REPEAT_INTERVAL);
+  INFO1("Programming the repetitive_action with a frequency of %f sec", REPEAT_INTERVAL);
   gras_timer_repeat(REPEAT_INTERVAL,repetitive_action);
    
-  INFO2("[%.0f] Programming the delayed_action for after %f sec", gras_os_time(), DELAY_INTERVAL);
+  INFO1("Programming the delayed_action for after %f sec", DELAY_INTERVAL);
   gras_timer_delay(REPEAT_INTERVAL,delayed_action);
 
-  INFO1("[%.0f] Have a rest", gras_os_time());  
+  INFO0("Have a rest");  
   gras_os_sleep(DELAY_INTERVAL / 2.0);
    
-  INFO1("[%.0f] Canceling the delayed_action.",gras_os_time());
+  INFO0("Canceling the delayed_action.");
   gras_timer_cancel_delay(REPEAT_INTERVAL,delayed_action);
 
-  INFO2("[%.0f] Re-programming the delayed_action for after %f sec", gras_os_time(),DELAY_INTERVAL);
+  INFO1("Re-programming the delayed_action for after %f sec",DELAY_INTERVAL);
   gras_timer_delay(REPEAT_INTERVAL,delayed_action);
 
   while (globals->still_to_do == -1 ||  /* Before delayed action runs */
 	 globals->still_to_do > 0 /* after delayed_action, and not enough repetitive_action */) {
 
-     DEBUG2("[%.0f] Prepare to handle messages for 5 sec (still_to_do=%d)",
-	    gras_os_time(), globals->still_to_do);
+     DEBUG1("Prepare to handle messages for 5 sec (still_to_do=%d)", globals->still_to_do);
      gras_msg_handle(5.0);
   }
   gras_exit();
