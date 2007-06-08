c281d400a7c848c061c23bd01300073bac759e04
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-06-08 13:52:22 +0000

    Do not use configure to generate the test script, factorize some Makefile.am stuff in examples/gras/tests.mk, use full precision in simulation timestamping and use two simulation tests for 32bits and 64bits since messages will be different in both cases, leading to different timestampings
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3592 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ examples/gras/tests.mk
@@ -0,0 +1,29 @@
+# This file factorize all the testing infrastructure for the GRAS examples
+#
+# it's made complicated by the facts that:
+#  - we use tesh, we need to find it back
+#  - we don't want to generate the test_* files with configure, so we
+#    have to declare some variables in front of the shell command
+#    running the test
+#  - we want to fully test the surf on the way. So, we have to ask for
+#    full precision timestamps in the tests. On the other hand,
+#    message size differ when we are on 32bits and when we are on
+#    64bits (obviously), so we have to maintain 2 tests per directory
+#    and pick the right one here.
+
+if GRAS_ARCH_32_BITS
+  TESTS= test_rl test_sg_32
+test-sg:
+	$(TESTS_ENVIRONMENT) test_sg_32
+else
+  TESTS= test_rl test_sg_64
+test-sg:
+	$(TESTS_ENVIRONMENT) test_sg_32
+endif
+
+test-rl:
+	$(TESTS_ENVIRONMENT) test_rl
+TESTS_ENVIRONMENT=srcdir=$(srcdir) EXEEXT=$(EXEEXT) @top_builddir@/tools/tesh/tesh
+
+
+EXTRA_DIST+=test_rl test_sg_32 test_sg_64
