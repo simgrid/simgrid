b7e5b28fc032abb5aae910f5186d8df8c9fac98c
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-07-06 20:27:38 +0000

    Stubs of the functions allowing the upper JAVA layer (user interface) to get back some information from the bottom one (context implementation). These are only stubs here because this file is not involved in libsimgrid4java; xbt_jcontext is used instead, but I'd prefer to wait before commiting this one
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3671 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/xbt/context.c
+++ src/xbt/context.c
@@ -420,3 +420,16 @@ void xbt_context_kill(xbt_context_t context)
 	return;
 }
 /* @} */
+
+/* Stub of the stuff to interact with JAVA threads; not used in native lib */
+void  xbt_context_set_jprocess(xbt_context_t context, void *jp) {}
+void* xbt_context_get_jprocess(xbt_context_t context)           { return NULL; }
+
+void  xbt_context_set_jmutex(xbt_context_t context,void *jm)    {}
+void* xbt_context_get_jmutex(xbt_context_t context)             { return NULL; }
+
+void  xbt_context_set_jcond(xbt_context_t context,void *jc)     {}
+void* xbt_context_get_jcond(xbt_context_t context)              { return NULL; }
+
+void  xbt_context_set_jenv(xbt_context_t context,void* je)      {}
+void* xbt_context_get_jenv(xbt_context_t context)               { return NULL; }
