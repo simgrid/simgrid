4b9fef86b73d5262c31dab7e82aaed3f2d62e660
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-07-09 16:30:13 +0000

    This file is a stub for XBT, it has nothing to do in the gras/ dir
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3695 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ src/xbt/xbt_os_thread_stubs.c
@@ -0,0 +1,100 @@
+/* $Id$ */
+
+/* xbt_os_thread_stub -- empty functions used in SG, but harmful in RL      */
+/* More specifically, it is loaded by xbt/context.c when CONTEXT_UCONTEXT   */
+/* When CONTEXT_THREAD, xbt_os_thread is loaded instead (to get real func)  */
+/* In RL, xbt_os_thread is part of GRAS_RL_SRC                              */
+
+/* Copyright (c) 2007 Martin Quinson.                                       */
+/* All rights reserved.                                                     */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include "xbt_modinter.h"
+#include "xbt/sysdep.h"
+#include "xbt/xbt_os_thread.h"
+#include "portable.h" /* CONTEXT_THREADS */
+
+#ifndef CONTEXT_THREADS
+
+/* xbt_threads is loaded in libsimgrid when they are used to implement the xbt_context.
+ * The decision (and the loading) is made in xbt/context.c.
+ */
+
+/* Mod_init/exit mecanism */
+void xbt_os_thread_mod_init(void) {}
+   
+void xbt_os_thread_mod_exit(void) {}
+
+
+/* Main functions */
+
+xbt_os_thread_t xbt_os_thread_create(pvoid_f_pvoid_t start_routine,void* param) {
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_thread_create)");
+}
+
+void xbt_os_thread_exit(int *retcode){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_thread_exit)");
+}
+
+xbt_os_thread_t xbt_os_thread_self(void){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_thread_self)");
+}
+
+void xbt_os_thread_os_yield(void){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_thread_yield)");
+}
+
+
+xbt_os_mutex_t xbt_os_mutex_init(void){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_mutex_init)");
+}
+
+void xbt_os_mutex_lock(xbt_os_mutex_t mutex){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_mutex_lock)");
+}
+
+void xbt_os_mutex_unlock(xbt_os_mutex_t mutex){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_mutex_unlock)");
+}
+
+void xbt_os_mutex_destroy(xbt_os_mutex_t mutex){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_mutex_destroy)");
+}
+
+xbt_os_cond_t xbt_os_cond_init(void){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_cond_init)");
+}
+
+void xbt_os_cond_wait(xbt_os_cond_t cond, xbt_os_mutex_t mutex){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_cond_wait)");
+}
+
+void xbt_os_cond_signal(xbt_os_cond_t cond){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_cond_signal)");
+}
+
+void xbt_os_cond_broadcast(xbt_os_cond_t cond){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_cond_broadcast)");
+}
+
+void xbt_os_cond_destroy(xbt_os_cond_t cond){
+   xbt_backtrace_display();
+   xbt_die("No pthread in SG when compiled against the ucontext (xbt_os_cond_destroy)");
+}
+
+
+#endif
