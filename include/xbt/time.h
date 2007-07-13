e4954bd08ec2284d5d90d3211116269c01b64066
donassbr <donassbr@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-07-13 14:42:06 +0000

    I forgot it.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3767 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ include/xbt/time.h
@@ -0,0 +1,21 @@
+/* xbt/time.h -- Time tools  								                                */
+/* Usable in simulator, (or in real life when mixing with GRAS)             */
+
+/* Copyright (c) 2007 Martin Quinson. All rights reserved.                  */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include "xbt/misc.h" /* SG_BEGIN_DECL */
+
+SG_BEGIN_DECL()
+
+/**
+ *
+ * Time management functions, returns the system time or sleeps a process. They work both on the simulated and real systems(GRAS). 
+ */
+
+XBT_PUBLIC(double) xbt_os_time(void);
+XBT_PUBLIC(void) xbt_os_sleep(double sec);
+
+SG_END_DECL()
