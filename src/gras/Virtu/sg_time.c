4d227f62c71b127feab03b55fefceee772a3eeaa
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-07-13 19:08:04 +0000

    Kill dead files
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3771 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/gras/Virtu/sg_time.c
+++
@@ -1,9 +0,0 @@
-/* $Id$ */
-
-/* time - time related syscal wrappers                                      */
-
-/* Copyright (c) 2003-2007 Martin Quinson.  All rights reserved.            */
-
-/* This program is free software; you can redistribute it and/or modify it
- * under the terms of the license (GNU LGPL) which comes with this package. */
-
