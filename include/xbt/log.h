b4f00bf50283a629193daf4ae11616605b023abc
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-07-14 07:41:35 +0000

    doc improvement
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3779 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/xbt/log.h
+++ include/xbt/log.h
@@ -357,7 +357,7 @@ extern xbt_log_layout_t xbt_log_default_layout;
 /**
  * \ingroup XBT_log 
  * \param catName name of the category
- * \param priority minimal priority to be enabled to return true
+ * \param priority minimal priority to be enabled to return true (must be #e_xbt_log_priority_t)
  * \hideinitializer
  *
  * Returns true if the given priority is enabled for the category.
