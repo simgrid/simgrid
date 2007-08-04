2bc1e4a2e624d5d75d7ecc6f068da889e21076fe
alegrand <alegrand@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-08-04 21:22:48 +0000

    Adding debug.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3947 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- examples/gras/synchro/philosopher.c
+++ examples/gras/synchro/philosopher.c
@@ -131,8 +131,10 @@ int philosopher (int argc,char *argv[]) {
        
   INFO0("Cancel all childs");
   /* nuke them threads */
-  for (i=0; i<philosopher_amount; i++)
+  for (i=0; i<philosopher_amount; i++) {
+    INFO1("Canceling %p",philosophers[i]);
      xbt_thread_cancel(philosophers[i]);
+  }
 
   xbt_mutex_unlock(dead_end);
   gras_exit();
