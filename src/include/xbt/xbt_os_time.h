343790d5b27cea6cead9ed4f0ec7e05ea331aa58
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-08-06 11:57:33 +0000

    Fix win build of smpi
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@3981 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/include/xbt/xbt_os_time.h
+++ src/include/xbt/xbt_os_time.h
@@ -21,10 +21,10 @@ XBT_PUBLIC(double) xbt_os_time(void);
 XBT_PUBLIC(void) xbt_os_sleep(double sec);
 
 typedef struct s_xbt_os_timer *xbt_os_timer_t;
-xbt_os_timer_t xbt_os_timer_new(void);
-void xbt_os_timer_free(xbt_os_timer_t timer);
-void xbt_os_timer_start(xbt_os_timer_t timer);
-void xbt_os_timer_stop(xbt_os_timer_t timer);
-double xbt_os_timer_elapsed(xbt_os_timer_t timer);
+XBT_PUBLIC(xbt_os_timer_t) xbt_os_timer_new(void);
+XBT_PUBLIC(void)   xbt_os_timer_free(xbt_os_timer_t timer);
+XBT_PUBLIC(void)   xbt_os_timer_start(xbt_os_timer_t timer);
+XBT_PUBLIC(void)   xbt_os_timer_stop(xbt_os_timer_t timer);
+XBT_PUBLIC(double) xbt_os_timer_elapsed(xbt_os_timer_t timer);
 
 #endif
