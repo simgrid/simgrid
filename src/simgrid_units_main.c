5dac8b8bdde8be418dedb56d7b05a4ab5ec4d7af
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-09-18 09:33:43 +0000

    Regenerate unit tests
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@4640 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/simgrid_units_main.c
+++ src/simgrid_units_main.c
@@ -9,8 +9,51 @@
 extern xbt_test_unit_t _xbt_current_unit;
 
 /* SGU: BEGIN PROTOTYPES */
-  /* SGU: BEGIN FILE ./xbt/cunit.c */
+  /* SGU: BEGIN FILE xbt/cunit.c */
     void test_expected_failure(void);
+  /* SGU: END FILE */
+
+  /* SGU: BEGIN FILE xbt/ex.c */
+    void test_controlflow(void);
+    void test_value(void);
+    void test_variables(void);
+    void test_cleanup(void);
+  /* SGU: END FILE */
+
+  /* SGU: BEGIN FILE xbt/dynar.c */
+    void test_dynar_int(void);
+    void test_dynar_double(void);
+    void test_dynar_string(void);
+    void test_dynar_sync_int(void);
+  /* SGU: END FILE */
+
+  /* SGU: BEGIN FILE xbt/dict.c */
+    void test_dict_basic(void);
+    void test_dict_remove(void);
+    void test_dict_nulldata(void);
+    void test_dict_crash(void);
+    void test_dict_multicrash(void);
+  /* SGU: END FILE */
+
+  /* SGU: BEGIN FILE xbt/set.c */
+    void test_set_basic(void);
+    void test_set_change(void);
+    void test_set_retrieve(void);
+    void test_set_remove(void);
+  /* SGU: END FILE */
+
+  /* SGU: BEGIN FILE xbt/swag.c */
+    void test_swag_basic(void);
+  /* SGU: END FILE */
+
+  /* SGU: BEGIN FILE xbt/xbt_str.c */
+    void test_split_quoted(void);
+  /* SGU: END FILE */
+
+  /* SGU: BEGIN FILE xbt/config.c */
+    void test_config_memuse(void);
+    void test_config_validation(void);
+    void test_config_use(void);
   /* SGU: END FILE */
 
 /* SGU: END PROTOTYPES */
@@ -25,9 +68,59 @@ int main(int argc, char *argv[]) {
   int i;
 
   /* SGU: BEGIN SUITES DECLARATION */
-    /* SGU: BEGIN FILE ./xbt/cunit.c */
+    /* SGU: BEGIN FILE xbt/cunit.c */
       suite = xbt_test_suite_by_name("cunit","Testsuite mechanism autotest");
       xbt_test_suite_push(suite, "expect", test_expected_failure, "expected failures");
+    /* SGU: END FILE */
+
+    /* SGU: BEGIN FILE xbt/ex.c */
+      suite = xbt_test_suite_by_name("xbt_ex","Exception Handling");
+      xbt_test_suite_push(suite, "controlflow", test_controlflow,  "basic nested control flow");
+      xbt_test_suite_push(suite, "value", test_value, "exception value passing");
+      xbt_test_suite_push(suite, "variables", test_variables, "variable value preservation");
+      xbt_test_suite_push(suite, "cleanup", test_cleanup, "cleanup handling");
+    /* SGU: END FILE */
+
+    /* SGU: BEGIN FILE xbt/dynar.c */
+      suite = xbt_test_suite_by_name("dynar","Dynar data container");
+      xbt_test_suite_push(suite, "int", test_dynar_int, "Dynars of integers");
+      xbt_test_suite_push(suite, "double", test_dynar_double, "Dynars of doubles");
+      xbt_test_suite_push(suite, "string", test_dynar_string, "Dyars of strings");
+      xbt_test_suite_push(suite, "synchronized int", test_dynar_sync_int, "Synchronized dynars of integers");
+    /* SGU: END FILE */
+
+    /* SGU: BEGIN FILE xbt/dict.c */
+      suite = xbt_test_suite_by_name("dict","Dict data container");
+      xbt_test_suite_push(suite, "basic", test_dict_basic, "Basic usage: change, retrieve, traverse");
+      xbt_test_suite_push(suite, "remove", test_dict_remove, "Removing some values");
+      xbt_test_suite_push(suite, "nulldata", test_dict_nulldata, "NULL data management");
+      xbt_test_suite_push(suite, "crash", test_dict_crash, "Crash test");
+      xbt_test_suite_push(suite, "multicrash", test_dict_multicrash, "Multi-dict crash test");
+    /* SGU: END FILE */
+
+    /* SGU: BEGIN FILE xbt/set.c */
+      suite = xbt_test_suite_by_name("set","Set data container");
+      xbt_test_suite_push(suite, "basic", test_set_basic, "Basic usage");
+      xbt_test_suite_push(suite, "change", test_set_change, "Changing some values");
+      xbt_test_suite_push(suite, "retrieve", test_set_retrieve, "Retrieving some values");
+      xbt_test_suite_push(suite, "remove", test_set_remove, "Removing some values");
+    /* SGU: END FILE */
+
+    /* SGU: BEGIN FILE xbt/swag.c */
+      suite = xbt_test_suite_by_name("swag","Swag data container");
+      xbt_test_suite_push(suite, "basic", test_swag_basic, "Basic usage");
+    /* SGU: END FILE */
+
+    /* SGU: BEGIN FILE xbt/xbt_str.c */
+      suite = xbt_test_suite_by_name("xbt_str","String Handling");
+      xbt_test_suite_push(suite, "xbt_str_split_quoted", test_split_quoted,  "test the function xbt_str_split_quoted");
+    /* SGU: END FILE */
+
+    /* SGU: BEGIN FILE xbt/config.c */
+      suite = xbt_test_suite_by_name("config","Configuration support");
+      xbt_test_suite_push(suite, "memuse", test_config_memuse, "Alloc and free a config set");
+      xbt_test_suite_push(suite, "validation", test_config_validation, "Validation tests");
+      xbt_test_suite_push(suite, "use", test_config_use, "Data retrieving tests");
     /* SGU: END FILE */
 
   /* SGU: END SUITES DECLARATION */
