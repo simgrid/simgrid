e4bab1d29858681449ae3bb3051954f8a935bb82
cherierm <cherierm@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-09-21 15:39:30 +0000

    correction of sem.acquire() method
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@4656 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/java/simgrid/msg/Sem.java
+++ src/java/simgrid/msg/Sem.java
@@ -1,13 +1,3 @@
-/*
- * $Id$
- *
- * Copyright 2006,2007 Martin Quinson, Malek Cherier           
- * All right reserved. 
- *
- * This program is free software; you can redistribute 
- * it and/or modify it under the terms of the license 
- *(GNU LGPL) which comes with this package. 
- */
 package simgrid.msg;
 
 public class Sem { 
@@ -38,7 +28,7 @@ public class Sem {
 	}
 
 	public synchronized void release() {
-		++permits_;
+		++(this.permits_);
 		notify();
 	}
 }
