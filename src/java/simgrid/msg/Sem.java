f636c3eccc59ae7c0b863b93f029e6aee4615c4e
cherierm <cherierm@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-09-24 14:56:14 +0000

    just add the copyright of the file
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@4706 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/java/simgrid/msg/Sem.java
+++ src/java/simgrid/msg/Sem.java
@@ -1,6 +1,21 @@
+/*
+ * simgrid.msg.Sem.java	1.00 07/05/01
+ *
+ * Copyright 2006,2007 Martin Quinson, Malek Cherier           
+ * All right reserved. 
+ *
+ * This program is free software; you can redistribute 
+ * it and/or modify it under the terms of the license 
+ *(GNU LGPL) which comes with this package. 
+ */
+ 
 package simgrid.msg;
 
 public class Sem { 
+	
+	/******************************************************************/
+	/* Simple semaphore implementation, from Doug Lea (public domain) */
+	/******************************************************************/
 	private int permits_;
 	
 	public Sem(int i) {
