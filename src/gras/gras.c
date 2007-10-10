b50a07be6e7ad1e96521f16137571f7f84f97f5f
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-10-10 15:55:26 +0000

    cosmetics
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@4797 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/gras/gras.c
+++ src/gras/gras.c
@@ -25,7 +25,7 @@
 #include "gras/Msg/msg_private.h"
 #include "portable.h" /* hexa_*(); signalling stuff */
 
-XBT_LOG_NEW_DEFAULT_SUBCATEGORY(gras,XBT_LOG_ROOT_CAT,"All GRAS categories (cf. section \ref GRAS_API)");
+XBT_LOG_NEW_DEFAULT_CATEGORY(gras,"All GRAS categories (cf. section \ref GRAS_API)");
 static int gras_running_process = 0;
 #if defined(HAVE_SIGNAL) && defined(HAVE_SIGNAL_H)
 static void gras_sigusr_handler(int sig) {
