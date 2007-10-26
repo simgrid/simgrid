0b70a26c1bb6cac81da302eb5e2add20cda2b48f
quasar <quasar@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-10-26 18:24:57 +0000

    relax ordering enforcement
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@4884 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/surf/surfxml.h
+++ include/surf/surfxml.h
@@ -1,5 +1,5 @@
 /* XML processor/application API for surf/surfxml.dtd.
- * Generated 2007/10/23 11:08:07.
+ * Generated 2007/10/26 19:58:25.
  *
  * This program was generated with the FleXML XML processor generator.
  * FleXML is Copyright (C) 1999-2005 Kristoffer Rose.  All rights reserved.
