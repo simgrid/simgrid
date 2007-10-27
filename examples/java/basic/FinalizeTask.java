7cbd735597a79656bb5ca077c07e1f03225f6297
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-10-27 11:47:05 +0000

    Oops, forgot to add this file
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@4910 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ examples/java/basic/FinalizeTask.java
@@ -0,0 +1,19 @@
+/*
+ * $Id: CommTimeTask.java 3685 2007-07-08 21:02:03Z mquinson $
+ *
+ * Copyright 2006,2007 Martin Quinson, Malek Cherier         
+ * All rights reserved. 
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. 
+ */
+
+import simgrid.msg.*;
+
+public class FinalizeTask extends Task {
+    	
+   public FinalizeTask() throws JniException, NativeException{
+      create("",0,0);
+   }
+}
+    
