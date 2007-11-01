551298c9962cf73d7ef8e0e8c49d2c670ca3e3d9
quasar <quasar@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-11-01 16:42:09 +0000

    simdag properties example
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@4954 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ examples/simdag/properties/sd_prop.c
@@ -0,0 +1,232 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "simdag/simdag.h"
+#include "xbt/ex.h"
+#include "xbt/log.h"
+#include "xbt/dynar.h"
+#include "xbt/dict.h"
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(sd_test,
+			     "Logging specific to this SimDag example");
+
+int main(int argc, char **argv) {
+  int i;
+
+  /* initialisation of SD */
+  SD_init(&argc, argv);
+
+  /*  xbt_log_control_set("sd.thres=debug"); */
+
+  if (argc < 2) {
+    INFO1("Usage: %s platform_file", argv[0]);
+    INFO1("example: %s sd_platform.xml", argv[0]);
+    exit(1);
+  }
+
+  /* creation of the environment */
+  const char * platform_file = argv[1];
+  SD_create_environment(platform_file);
+
+  /* test the estimation functions */
+  const SD_workstation_t *workstations = SD_workstation_get_list();
+  SD_workstation_t w1 = workstations[0];
+  SD_workstation_t w2 = workstations[1];
+  SD_workstation_set_access_mode(w2, SD_WORKSTATION_SEQUENTIAL_ACCESS);
+  const char *name1 = SD_workstation_get_name(w1);
+  const char *name2 = SD_workstation_get_name(w2);
+
+  xbt_dict_t props;
+  INFO1("Property list for workstation %s", name1);
+  /* Get the property list of the workstation 1 */
+  props = SD_workstation_get_properties(w1);
+  xbt_dict_cursor_t cursor = NULL;
+  char *key,*data;
+
+  /* Print the properties of the workstation 1 */
+  xbt_dict_foreach(props,cursor,key,data) {
+  INFO2("Property: %s has value: %s",key,data);
+  }
+ 
+  /* Try to get a property that does not exist */
+  char *noexist=xbt_strdup("Memory");
+  const char *value = SD_workstation_get_property_value(w1,noexist);
+  if ( value == NULL) 
+    INFO1("Property: %s is undefined", noexist);
+  else
+    INFO2("Property: %s has value: %s", noexist, value);
+  
+  INFO1("Property list for workstation %s", name2);
+  /* Get the property list of the workstation 2 */
+  props = SD_workstation_get_properties(w2);
+  cursor = NULL;
+
+  /* Print the properties of the workstation 2 */
+  xbt_dict_foreach(props,cursor,key,data) {
+  INFO2("Property: %s on host: %s",key,data);
+  }
+
+  const double computation_amount1 = 2000000;
+  const double computation_amount2 = 1000000;
+  const double communication_amount12 = 2000000;
+  const double communication_amount21 = 3000000;
+  INFO3("Computation time for %f flops on %s: %f", computation_amount1, name1,
+	SD_workstation_get_computation_time(w1, computation_amount1));
+  INFO3("Computation time for %f flops on %s: %f", computation_amount2, name2,
+	SD_workstation_get_computation_time(w2, computation_amount2));
+
+  INFO2("Route between %s and %s:", name1, name2);   
+  const SD_link_t *route = SD_route_get_list(w1, w2);
+  int route_size = SD_route_get_size(w1, w2);
+  for (i = 0; i < route_size; i++) {
+    INFO3("\tLink %s: latency = %f, bandwidth = %f", SD_link_get_name(route[i]),
+	  SD_link_get_current_latency(route[i]), SD_link_get_current_bandwidth(route[i]));
+      
+    props = SD_link_get_properties(route[i]);
+    xbt_dict_cursor_t cursor = NULL;
+    char *key,*data;
+
+    /* Print the properties of the current link */
+    xbt_dict_foreach(props,cursor,key,data) {
+    INFO3("Link %s property: %s has value: %s",SD_link_get_name(route[i]),key,data);
+
+    /* Try to get a property that does not exist */
+    noexist=xbt_strdup("Other");
+    value = SD_link_get_property_value(route[i], noexist);
+    if ( value == NULL) 
+      INFO2("Property: %s for link %s is undefined", noexist, SD_link_get_name(route[i]));
+    else
+      INFO3("Link %s property: %s has value: %s",SD_link_get_name(route[i]),noexist,value);
+  }
+
+  }
+  INFO2("Route latency = %f, route bandwidth = %f", SD_route_get_current_latency(w1, w2),
+	SD_route_get_current_bandwidth(w1, w2));
+  INFO4("Communication time for %f bytes between %s and %s: %f", communication_amount12, name1, name2,
+	SD_route_get_communication_time(w1, w2, communication_amount12));
+  INFO4("Communication time for %f bytes between %s and %s: %f", communication_amount21, name2, name1,
+	SD_route_get_communication_time(w2, w1, communication_amount21));
+
+  /* creation of the tasks and their dependencies */
+  SD_task_t taskA = SD_task_create("Task A", NULL, 10.0);
+  SD_task_t taskB = SD_task_create("Task B", NULL, 40.0);
+  SD_task_t taskC = SD_task_create("Task C", NULL, 30.0);
+  SD_task_t taskD = SD_task_create("Task D", NULL, 60.0);
+  
+
+  SD_task_dependency_add(NULL, NULL, taskB, taskA);
+  SD_task_dependency_add(NULL, NULL, taskC, taskA);
+  SD_task_dependency_add(NULL, NULL, taskD, taskB);
+  SD_task_dependency_add(NULL, NULL, taskD, taskC);
+  /*  SD_task_dependency_add(NULL, NULL, taskA, taskD); /\* deadlock */
+
+  xbt_ex_t ex;
+
+  TRY {
+    SD_task_dependency_add(NULL, NULL, taskA, taskA); /* shouldn't work and must raise an exception */
+    xbt_die("Hey, I can add a dependency between Task A and Task A!");
+  }
+  CATCH (ex) {
+    if (ex.category != arg_error)
+
+      RETHROW; /* this is a serious error */
+    xbt_ex_free(ex);
+  }
+  
+  TRY {
+    SD_task_dependency_add(NULL, NULL, taskB, taskA); /* shouldn't work and must raise an exception */
+    xbt_die("Oh oh, I can add an already existing dependency!");
+  }
+  CATCH (ex) {
+    if (ex.category != arg_error)
+      RETHROW;
+    xbt_ex_free(ex);
+  }
+
+  TRY {
+    SD_task_dependency_remove(taskA, taskC); /* shouldn't work and must raise an exception */
+    xbt_die("Dude, I can remove an unknown dependency!");
+  }
+  CATCH (ex) {
+    if (ex.category != arg_error)
+      RETHROW;
+    xbt_ex_free(ex);
+  }
+
+  TRY {
+    SD_task_dependency_remove(taskC, taskC); /* shouldn't work and must raise an exception */
+    xbt_die("Wow, I can remove a dependency between Task C and itself!");
+  }
+  CATCH (ex) {
+    if (ex.category != arg_error)
+      RETHROW;
+    xbt_ex_free(ex);
+  }
+
+
+  /* if everything is ok, no exception is forwarded or rethrown by main() */
+
+  /* watch points */
+  SD_task_watch(taskD, SD_DONE);
+  SD_task_watch(taskB, SD_DONE);
+  SD_task_unwatch(taskD, SD_DONE);
+  
+
+  /* scheduling parameters */
+
+  const int workstation_number = 2;
+  const SD_workstation_t workstation_list[] = {w1, w2};
+  double computation_amount[] = {computation_amount1, computation_amount2};
+  double communication_amount[] =
+    {
+      0, communication_amount12,
+      communication_amount21, 0
+    };
+  double rate = -1.0;
+
+  /* estimated time */
+  SD_task_t task = taskD;
+  INFO2("Estimated time for '%s': %f", SD_task_get_name(task),
+	SD_task_get_execution_time(task, workstation_number, workstation_list,
+				   computation_amount, communication_amount, rate));
+
+  /* let's launch the simulation! */
+
+  SD_task_schedule(taskA, workstation_number, workstation_list,
+		   computation_amount, communication_amount, rate);
+  SD_task_schedule(taskB, workstation_number, workstation_list,
+		   computation_amount, communication_amount, rate);
+  SD_task_schedule(taskC, workstation_number, workstation_list,
+		   computation_amount, communication_amount, rate);
+  SD_task_schedule(taskD, workstation_number, workstation_list,
+		   computation_amount, communication_amount, rate);
+
+  SD_task_t *changed_tasks;
+
+  changed_tasks = SD_simulate(-1.0);
+  for (i = 0; changed_tasks[i] != NULL; i++) {
+    INFO3("Task '%s' start time: %f, finish time: %f",
+	  SD_task_get_name(changed_tasks[i]),
+	  SD_task_get_start_time(changed_tasks[i]),
+	  SD_task_get_finish_time(changed_tasks[i]));
+  }
+  
+  xbt_assert0(changed_tasks[0] == taskD &&
+	      changed_tasks[1] == taskB &&
+	      changed_tasks[2] == NULL,
+	      "Unexpected simulation results");
+
+  xbt_free(changed_tasks);
+
+  DEBUG0("Destroying tasks...");
+
+  SD_task_destroy(taskA);
+  SD_task_destroy(taskB);
+  SD_task_destroy(taskC);
+  SD_task_destroy(taskD);
+
+  DEBUG0("Tasks destroyed. Exiting SimDag...");
+
+  SD_exit();
+  return 0;
+}
+
