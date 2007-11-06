49aa15c63b8823fd0fc084e9e86d2fbd5ca025b7
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2007-11-06 08:01:54 +0000

    cleanups (among which example simplification)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@4973 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- examples/msg/properties/msg_prop.c
+++ examples/msg/properties/msg_prop.c
@@ -1,6 +1,6 @@
 /* 	$Id$	 */
 
-/* Copyright (c) 2006. All rights reserved.        */
+/* Copyright (c) 2007. SimGrid Team. All rights reserved.                   */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
@@ -22,43 +22,13 @@ int slave(int argc, char *argv[]);
 int forwarder(int argc, char *argv[]);
 MSG_error_t test_all(const char *platform_file, const char *application_file);
 
-typedef enum {
-  PORT_22 = 0,
-  MAX_CHANNEL
-} channel_t;
-
-#define FINALIZE ((void*)221297) /* a magic number to tell people to stop working */
-
 /** Emitter function  */
 int master(int argc, char *argv[])
 {
   int slaves_count = 0;
   m_host_t *slaves = NULL;
-  m_task_t *todo = NULL;
-  int number_of_tasks = 0;
-  double task_comp_size = 0;
-  double task_comm_size = 0;
-
 
   int i;
-
-  xbt_assert1(sscanf(argv[1],"%d", &number_of_tasks),
-	 "Invalid argument %s\n",argv[1]);
-  xbt_assert1(sscanf(argv[2],"%lg", &task_comp_size),
-	 "Invalid argument %s\n",argv[2]);
-  xbt_assert1(sscanf(argv[3],"%lg", &task_comm_size),
-	 "Invalid argument %s\n",argv[3]);
-
-  {                  /*  Task creation */
-    char sprintf_buffer[64];
-
-    todo = xbt_new(m_task_t, sizeof(m_task_t) * number_of_tasks);
-
-    for (i = 0; i < number_of_tasks; i++) {
-      sprintf(sprintf_buffer, "Task_%d", i);
-      todo[i] = MSG_task_create(sprintf_buffer, task_comp_size, task_comm_size, NULL);
-    }
-  }
 
   {                  /* Process organisation */
     slaves_count = argc - 4;
@@ -79,7 +49,7 @@ int master(int argc, char *argv[])
       }
 
      /* Try to get a property that does not exist */
-     char noexist[]="Unknown";
+     const char *noexist="Unknown";
      const char*value = MSG_host_get_property_value(slaves[i-4], noexist);
      if ( value == NULL) 
        INFO2("Property: %s for host %s is undefined", noexist, argv[i]);
@@ -90,120 +60,44 @@ int master(int argc, char *argv[])
       INFO0("Trying to modify a host property");
       char exist[]="Hdd";
       value = MSG_host_get_property_value(slaves[i-4],exist);
-      if ( value == NULL) 
-        INFO1("\tProperty: %s is undefined", exist);
-      else {
-        INFO2("\tProperty: %s old value: %s", exist, value);
-        xbt_dict_set(props, exist, strdup("250"), free);  
-      }
+      xbt_assert1(value,"\tProperty %s is undefined", exist);
+      INFO2("\tProperty: %s old value: %s", exist, value);
+      xbt_dict_set(props, exist, xbt_strdup("250"), free);  
  
       /* Test if we have changed the value */
       value = MSG_host_get_property_value(slaves[i-4],exist);
-      if ( value == NULL) 
-        INFO1("\tProperty: %s is undefined", exist);
-      else
-        INFO2("\tProperty: %s new value: %s", exist, value);
+      xbt_assert1(value,"\tProperty %s is undefined", exist);
+      INFO2("\tProperty: %s new value: %s", exist, value);
     }
   }
-
-  for (i = 0; i < number_of_tasks; i++) {
-    if(MSG_host_self()==slaves[i % slaves_count]) {
-    }
-    MSG_task_put(todo[i], slaves[i % slaves_count],
-                 PORT_22);
-  }
-  
-  for (i = 0; i < slaves_count; i++) 
-    MSG_task_put(MSG_task_create("finalize", 0, 0, FINALIZE),
-		 slaves[i], PORT_22);
   
   free(slaves);
-  free(todo);
   return 0;
 } /* end_of_master */
 
 /** Receiver function  */
 int slave(int argc, char *argv[])
 {
-  while(1) {
-    m_task_t task = NULL;
-    int a;
-    a = MSG_task_get(&(task), PORT_22);
-    if (a == MSG_OK) {
-      INFO1("Received \"%s\" ", MSG_task_get_name(task));
-      if(MSG_task_get_data(task)==FINALIZE) {
-	MSG_task_destroy(task);
-	break;
-      }
+  /* Get the property list of current slave process */
+  xbt_dict_t props = MSG_process_get_properties(MSG_process_self());
+  xbt_dict_cursor_t cursor=NULL;
+  char *key,*data;
 
-      /* Get the property list of current slave process */
-      xbt_dict_t props = MSG_process_get_properties(MSG_process_self());
-      xbt_dict_cursor_t cursor=NULL;
-      char *key,*data;
-
-      /* Print the properties of the process */
-      xbt_dict_foreach(props,cursor,key,data) {
-         INFO3("Property: %s for process %s has value: %s",key,MSG_process_get_name(MSG_process_self()),data);
-      }
-
-      /* Try to get a property that does not exist */
-      char noexist[]="UnknownProcessProp";
-      const char *value = MSG_process_get_property_value(MSG_process_self(),noexist);
-      if ( value == NULL) 
-        INFO2("Property: %s for process %s is undefined", noexist, MSG_process_get_name(MSG_process_self()));
-      else
-        INFO3("Property: %s for process %s has value: %s", noexist, MSG_process_get_name(MSG_process_self()), value);
-
-      MSG_task_execute(task);
-      MSG_task_destroy(task);
-    } else {
-	xbt_die("Hey ?! What's up ?");
-    }
+  /* Print the properties of the process */
+  xbt_dict_foreach(props,cursor,key,data) {
+     INFO3("Property: %s for process %s has value: %s",key,MSG_process_get_name(MSG_process_self()),data);
   }
+
+  /* Try to get a property that does not exist */
+  const char *noexist="UnknownProcessProp";
+  const char *value = MSG_process_get_property_value(MSG_process_self(),noexist);
+  if ( value == NULL) 
+    INFO2("Property: %s for process %s is undefined", noexist, MSG_process_get_name(MSG_process_self()));
+  else
+    INFO3("Property: %s for process %s has value: %s", noexist, MSG_process_get_name(MSG_process_self()), value);
+
   return 0;
 } /* end_of_slave */
-
-/** Forwarder function */
-int forwarder(int argc, char *argv[])
-{
-  int i;
-  int slaves_count;
-  m_host_t *slaves;
-
-  {                  /* Process organisation */
-    slaves_count = argc - 1;
-    slaves = calloc(slaves_count, sizeof(m_host_t));
-    
-    for (i = 1; i < argc; i++) {
-      slaves[i-1] = MSG_get_host_by_name(argv[i]);
-      xbt_assert1(slaves[i-1]!=NULL, "Unknown host %s. Stopping Now! ", argv[i]);
-    }
-  }
-
-  i=0;
-  while(1) {
-    m_task_t task = NULL;
-    int a;
-    a = MSG_task_get(&(task), PORT_22);
-    if (a == MSG_OK) {
-      INFO1("Received \"%s\" ", MSG_task_get_name(task));
-      if(MSG_task_get_data(task)==FINALIZE) {
-	INFO0("All tasks have been dispatched. Let's tell everybody the computation is over.");
-	for (i = 0; i < slaves_count; i++) 
-	  MSG_task_put(MSG_task_create("finalize", 0, 0, FINALIZE),
-		       slaves[i], PORT_22);
-	MSG_task_destroy(task);
-	break;
-      }
-      MSG_task_put(task, slaves[i % slaves_count],
-		   PORT_22);
-      i++;
-    } else {
-	xbt_die("Hey ?! What's up ?");    
-      }
-  }
-  return 0;
-} /* end_of_forwarder */
 
 /** Test function */
 MSG_error_t test_all(const char *platform_file,
@@ -213,14 +107,13 @@ MSG_error_t test_all(const char *platform_file,
 
   /* MSG_config("surf_workstation_model","KCCFLN05"); */
   {				/*  Simulation setting */
-    MSG_set_channel_number(MAX_CHANNEL);
+//    MSG_set_channel_number(MAX_CHANNEL);
     MSG_paje_output("msg_test.trace");
     MSG_create_environment(platform_file);
   }
   {                            /*   Application deployment */
     MSG_function_register("master", master);
     MSG_function_register("slave", slave);
-    MSG_function_register("forwarder", forwarder);
     MSG_launch_application(application_file);
   }
   res = MSG_main();
