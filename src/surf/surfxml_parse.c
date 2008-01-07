bb38b738b6810c3e4c0be80ffc5de5eb25e62435
cherierm <cherierm@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2008-01-07 14:37:23 +0000

    Ansi declaration of the variables (at the beginning of the block).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@5162 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/surf/surfxml_parse.c
+++ src/surf/surfxml_parse.c
@@ -851,11 +851,12 @@ static void convert_route_multi_to_routes(void)
   unsigned int cpt, cpt2, cursor;
   char *src_host_name, *dst_host_name, *key, *src, *dst, *val, *key_w, *data_w; 
   const char* sep="#";
+  xbt_dict_t set;
   xbt_dynar_t src_names = NULL, dst_names = NULL, links;
 
   if (!route_multi_elements) return;
 
-  xbt_dict_t set = cpu_set;
+  set = cpu_set;
   DEBUG1("%d", xbt_dict_length(workstation_set));				
   if (workstation_set != NULL && xbt_dict_length(workstation_set) > 0)
      set = workstation_set;
