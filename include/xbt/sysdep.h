22b96fa759d3b97167b2eb450d35f45cb7827bd7
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2008-03-07 13:37:54 +0000

    avoid redeclaring standard headers to avoid mismatches
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@5278 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/xbt/sysdep.h
+++ include/xbt/sysdep.h
@@ -33,8 +33,10 @@ XBT_PUBLIC(void) xbt_die(const char *msg) _XBT_GNUC_NORETURN;
 
 /* these ones live in str.h, but redeclare them here so that we do 
    not need to load the whole str.h and its heavy dependencies */
-XBT_PUBLIC(int) asprintf  (char **ptr, const char *fmt, /*args*/ ...) _XBT_GNUC_PRINTF(2,3);
-XBT_PUBLIC(int) vasprintf (char **ptr, const char *fmt, va_list ap);
+#ifndef __USE_GNU /* do not redeclare existing headers */
+  XBT_PUBLIC(int) asprintf  (char **ptr, const char *fmt, /*args*/ ...) _XBT_GNUC_PRINTF(2,3);
+  XBT_PUBLIC(int) vasprintf (char **ptr, const char *fmt, va_list ap);
+#endif
 XBT_PUBLIC(char*) bprintf   (const char*fmt, ...) _XBT_GNUC_PRINTF(1,2);
 
 /** @addtogroup XBT_syscall
