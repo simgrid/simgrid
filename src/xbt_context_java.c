9be7b15cf2a06f451f9d7ee8d85abf5a7308760e
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2008-04-10 08:40:45 +0000

    Commit broken files which were wrongly moved; right move in the next commit
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@5333 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/xbt_context_java.c
+++ src/xbt_context_java.c
@@ -3,98 +3,98 @@
 #include "xbt/function_types.h"
 #include "xbt/ex_interface.h"
 #include "xbt/xbt_context_factory.h"
-#include "xbt/xbt_jcontext.h"
+#include "xbt/xbt_context_java.h"
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(jmsg,"MSG for Java(TM)");
 
 /* callback: context fetching */
 static ex_ctx_t*
-xbt_jcontext_ex_ctx(void);
+xbt_ctx_java_ex_ctx(void);
 
 /* callback: termination */
 static void 
-xbt_jcontext_ex_terminate(xbt_ex_t *e);
+xbt_ctx_java_ex_terminate(xbt_ex_t *e);
 
 static xbt_context_t 
-xbt_jcontext_factory_create_context(const char* name, xbt_main_func_t code, void_f_pvoid_t startup_func, void* startup_arg, void_f_pvoid_t cleanup_func, void* cleanup_arg, int argc, char** argv);
+xbt_ctx_java_factory_create_context(const char* name, xbt_main_func_t code, void_f_pvoid_t startup_func, void* startup_arg, void_f_pvoid_t cleanup_func, void* cleanup_arg, int argc, char** argv);
 
 static int 
-xbt_jcontext_factory_create_maestro_context(xbt_context_t* maestro);
+xbt_ctx_java_factory_create_maestro_context(xbt_context_t* maestro);
 
 static int
-xbt_jcontext_factory_finalize(xbt_context_factory_t* factory);
+xbt_ctx_java_factory_finalize(xbt_context_factory_t* factory);
 
 static void 
-xbt_jcontext_free(xbt_context_t context);
+xbt_ctx_java_free(xbt_context_t context);
 
 static void 
-xbt_jcontext_kill(xbt_context_t context);
+xbt_ctx_java_kill(xbt_context_t context);
 
 static void 
-xbt_jcontext_schedule(xbt_context_t context);
+xbt_ctx_java_schedule(xbt_context_t context);
 
 static void 
-xbt_jcontext_yield(void);
+xbt_ctx_java_yield(void);
 
 static void 
-xbt_jcontext_start(xbt_context_t context);
+xbt_ctx_java_start(xbt_context_t context);
 
 static void 
-xbt_jcontext_stop(int exit_code);
+xbt_ctx_java_stop(int exit_code);
 
 static void 
-xbt_jcontext_swap(xbt_context_t context);
+xbt_ctx_java_swap(xbt_context_t context);
 
 static void
-xbt_jcontext_schedule(xbt_context_t context);
+xbt_ctx_java_schedule(xbt_context_t context);
 
 static void
-xbt_jcontext_yield(void);
+xbt_ctx_java_yield(void);
 
 static void
-xbt_jcontext_suspend(xbt_context_t context);
+xbt_ctx_java_suspend(xbt_context_t context);
 
 static void
-xbt_jcontext_resume(xbt_context_t context);
+xbt_ctx_java_resume(xbt_context_t context);
 
 
 /* callback: context fetching */
 static ex_ctx_t*
-xbt_jcontext_ex_ctx(void) 
+xbt_ctx_java_ex_ctx(void) 
 {
 	return current_context->exception;
 }
 
 /* callback: termination */
 static void 
-xbt_jcontext_ex_terminate(xbt_ex_t *e) 
+xbt_ctx_java_ex_terminate(xbt_ex_t *e) 
 {
 	xbt_ex_display(e);
 	abort();
 }
 
 int
-xbt_jcontext_factory_init(xbt_context_factory_t* factory)
+xbt_ctx_java_factory_init(xbt_context_factory_t* factory)
 {
 	/* context exception handlers */
-    __xbt_ex_ctx       = xbt_jcontext_ex_ctx;
-    __xbt_ex_terminate = xbt_jcontext_ex_terminate;	
+    __xbt_ex_ctx       = xbt_ctx_java_ex_ctx;
+    __xbt_ex_terminate = xbt_ctx_java_ex_terminate;	
     
     /* instantiate the context factory */
 	*factory = xbt_new0(s_xbt_context_factory_t,1);
 	
-	(*factory)->create_context = xbt_jcontext_factory_create_context;
-	(*factory)->finalize = xbt_jcontext_factory_finalize;
-	(*factory)->create_maestro_context = xbt_jcontext_factory_create_maestro_context;
-	(*factory)->name = "jcontext_factory";
+	(*factory)->create_context = xbt_ctx_java_factory_create_context;
+	(*factory)->finalize = xbt_ctx_java_factory_finalize;
+	(*factory)->create_maestro_context = xbt_ctx_java_factory_create_maestro_context;
+	(*factory)->name = "ctx_java_factory";
 	
 	return 0;
 }
 
 static int 
-xbt_jcontext_factory_create_maestro_context(xbt_context_t* maestro)
+xbt_ctx_java_factory_create_maestro_context(xbt_context_t* maestro)
 {
-	xbt_jcontext_t context = xbt_new0(s_xbt_jcontext_t, 1);
+	xbt_ctx_java_t context = xbt_new0(s_xbt_ctx_java_t, 1);
 	
 	context->exception = xbt_new(ex_ctx_t,1);
     XBT_CTX_INITIALIZE(context->exception);
@@ -105,7 +105,7 @@ xbt_jcontext_factory_create_maestro_context(xbt_context_t* maestro)
 }
 
 static int
-xbt_jcontext_factory_finalize(xbt_context_factory_t* factory)
+xbt_ctx_java_factory_finalize(xbt_context_factory_t* factory)
 {
 	free(maestro_context->exception);
 	free(*factory);
@@ -115,9 +115,9 @@ xbt_jcontext_factory_finalize(xbt_context_factory_t* factory)
 }
 
 static xbt_context_t 
-xbt_jcontext_factory_create_context(const char* name, xbt_main_func_t code, void_f_pvoid_t startup_func, void* startup_arg, void_f_pvoid_t cleanup_func, void* cleanup_arg, int argc, char** argv)
+xbt_ctx_java_factory_create_context(const char* name, xbt_main_func_t code, void_f_pvoid_t startup_func, void* startup_arg, void_f_pvoid_t cleanup_func, void* cleanup_arg, int argc, char** argv)
 {
-	xbt_jcontext_t context = xbt_new0(s_xbt_jcontext_t,1);
+	xbt_ctx_java_t context = xbt_new0(s_xbt_ctx_java_t,1);
 
 	context->name = xbt_strdup(name);
 
@@ -127,12 +127,12 @@ xbt_jcontext_factory_create_context(const char* name, xbt_main_func_t code, void
 	context->exception = xbt_new(ex_ctx_t,1);
 	XBT_CTX_INITIALIZE(context->exception);
 
-	context->free = xbt_jcontext_free;			
-	context->kill = xbt_jcontext_kill;			
-	context->schedule = xbt_jcontext_schedule;
-	context->yield = xbt_jcontext_yield;			
-	context->start = xbt_jcontext_start;			
-	context->stop = xbt_jcontext_stop;	
+	context->free = xbt_ctx_java_free;			
+	context->kill = xbt_ctx_java_kill;			
+	context->schedule = xbt_ctx_java_schedule;
+	context->yield = xbt_ctx_java_yield;			
+	context->start = xbt_ctx_java_start;			
+	context->stop = xbt_ctx_java_stop;	
 	context->jprocess = (jobject)startup_arg;
 	context->jenv = get_current_thread_env();
 
@@ -140,26 +140,26 @@ xbt_jcontext_factory_create_context(const char* name, xbt_main_func_t code, void
 }
 
 static void 
-xbt_jcontext_free(xbt_context_t context)
+xbt_ctx_java_free(xbt_context_t context)
 {
 	if(context) 
 	{
-		xbt_jcontext_t jcontext = (xbt_jcontext_t)context;
+		xbt_ctx_java_t ctx_java = (xbt_ctx_java_t)context;
 		
-		free(jcontext->name);
+		free(ctx_java->name);
 		
-		if(jcontext->jprocess) 
+		if(ctx_java->jprocess) 
 		{
-			jobject jprocess = jcontext->jprocess;
-			jcontext->jprocess = NULL;
+			jobject jprocess = ctx_java->jprocess;
+			ctx_java->jprocess = NULL;
 
 			/* if the java process is alive join it */
 			if(jprocess_is_alive(jprocess,get_current_thread_env())) 
 				jprocess_join(jprocess,get_current_thread_env());
 		}
 
-		if(jcontext->exception) 
-			free(jcontext->exception);
+		if(ctx_java->exception) 
+			free(ctx_java->exception);
 
 		free(context);
 		context = NULL;
@@ -167,10 +167,10 @@ xbt_jcontext_free(xbt_context_t context)
 }
 
 static void 
-xbt_jcontext_kill(xbt_context_t context)
+xbt_ctx_java_kill(xbt_context_t context)
 {
 	context->iwannadie = 1;
-	xbt_jcontext_swap(context);
+	xbt_ctx_java_swap(context);
 }
 
 /** 
@@ -183,10 +183,10 @@ xbt_jcontext_kill(xbt_context_t context)
  * Only the maestro can call this function to run a given process.
  */
 static void 
-xbt_jcontext_schedule(xbt_context_t context)
+xbt_ctx_java_schedule(xbt_context_t context)
 {
 	xbt_assert0((current_context == maestro_context),"You are not supposed to run this function here!");
-	xbt_jcontext_swap(context);
+	xbt_ctx_java_swap(context);
 }
 
 /** 
@@ -198,23 +198,23 @@ xbt_jcontext_schedule(xbt_context_t context)
  * to the maestro
  */
 static void 
-xbt_jcontext_yield(void)
+xbt_ctx_java_yield(void)
 {
 	xbt_assert0((current_context != maestro_context),"You are not supposed to run this function here!");
 	jprocess_unschedule(current_context);
 }
 
 static void 
-xbt_jcontext_start(xbt_context_t context)
+xbt_ctx_java_start(xbt_context_t context)
 {
-	jprocess_start(((xbt_jcontext_t)context)->jprocess,get_current_thread_env());
+	jprocess_start(((xbt_ctx_java_t)context)->jprocess,get_current_thread_env());
 }
 
 static void 
-xbt_jcontext_stop(int exit_code)
+xbt_ctx_java_stop(int exit_code)
 {
 	jobject jprocess = NULL;
-	xbt_jcontext_t jcontext;
+	xbt_ctx_java_t ctx_java;
 
 	if(current_context->cleanup_func)
 	    	(*(current_context->cleanup_func))(current_context->cleanup_arg);
@@ -222,19 +222,19 @@ xbt_jcontext_stop(int exit_code)
 	xbt_swag_remove(current_context, context_living);
 	xbt_swag_insert(current_context, context_to_destroy);
 	
-	jcontext = (xbt_jcontext_t)current_context;
+	ctx_java = (xbt_ctx_java_t)current_context;
 	
-	if(jcontext->iwannadie)
+	if(ctx_java->iwannadie)
 	{
 		/* The maestro call xbt_context_stop() with an exit code set to one */
-		if(jcontext->jprocess) 
+		if(ctx_java->jprocess) 
 		{
 			/* if the java process is alive schedule it */
-			if(jprocess_is_alive(jcontext->jprocess,get_current_thread_env())) 
+			if(jprocess_is_alive(ctx_java->jprocess,get_current_thread_env())) 
 			{
 				jprocess_schedule(current_context);
-				jprocess = jcontext->jprocess;
-				jcontext->jprocess = NULL;
+				jprocess = ctx_java->jprocess;
+				ctx_java->jprocess = NULL;
 				
 				/* interrupt the java process */
 				jprocess_exit(jprocess,get_current_thread_env());
@@ -245,8 +245,8 @@ xbt_jcontext_stop(int exit_code)
 	else
 	{
 		/* the java process exits */
-		jprocess = jcontext->jprocess;
-  		jcontext->jprocess = NULL;
+		jprocess = ctx_java->jprocess;
+  		ctx_java->jprocess = NULL;
 	}
 
 	/* delete the global reference associated with the java process */
@@ -254,7 +254,7 @@ xbt_jcontext_stop(int exit_code)
 }
 
 static void 
-xbt_jcontext_swap(xbt_context_t context)
+xbt_ctx_java_swap(xbt_context_t context)
 {
 	if(context) 
 	{
@@ -268,5 +268,5 @@ xbt_jcontext_swap(xbt_context_t context)
 	}
 	
 	if(current_context->iwannadie)
-		xbt_jcontext_stop(1);
+		xbt_ctx_java_stop(1);
 }
