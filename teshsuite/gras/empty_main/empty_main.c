62550fb1e1fe5ecd00a5b56ff95fcbbc034cf98d
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2008-04-30 09:37:25 +0000

    New test making sure that it's ok to have empty processes (ending before doing anyting) in simix. For now, it's not, but anyway
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@5360 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ teshsuite/gras/empty_main/empty_main.c
@@ -0,0 +1,24 @@
+/* $Id: gras.c 3859 2007-07-18 12:29:51Z donassbr $ */
+
+/* empty_main.c -- check what happens when the processes do nothing         */
+/* Thanks to Loris Marshal for reporting a problem in that case             */
+
+/* Copyright (c) 2007 Martin Quinson.                                       */
+/* All rights reserved.                                                     */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+
+#include "gras.h"
+
+int function (int argc,char *argv[]);
+
+int function (int argc,char *argv[]) { 
+  gras_init(&argc,argv);
+  //  gras_os_sleep(3);
+  gras_exit();
+  return 0;
+} 
+
+
