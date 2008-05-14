e5fbc3c2bb1c06106e5fea0e032868219cea9d50
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2008-05-14 21:21:10 +0000

    An implementation of the SHA1 hashing algorithm
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@5427 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ include/xbt/hash.h
@@ -0,0 +1,30 @@
+/* $Id: str.h,v 1.5 2007/05/02 10:08:55 mquinson Exp $ */
+
+/* hash.h - Various hashing functions.                                      */
+
+/* Copyright (c) 2007, Martin Quinson.                                      */
+/* All rights reserved.                                                     */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#ifndef XBT_HASH_H
+#define XBT_HASH_H
+#include "xbt/str.h"
+
+/* Chord needs a SHA1 algorithm. Let's drop it in there */
+typedef struct s_xbt_sha_ s_xbt_sha_t, *xbt_sha_t;
+
+XBT_PUBLIC(xbt_sha_t) xbt_sha_new (void);
+XBT_PUBLIC(void)      xbt_sha_free (xbt_sha_t sha);
+
+XBT_PUBLIC(void)      xbt_sha_feed (xbt_sha_t sha, const unsigned char *data, size_t len);
+XBT_PUBLIC(void)      xbt_sha_reset (xbt_sha_t sha);
+
+XBT_PUBLIC(void)      xbt_sha_print (xbt_sha_t sha, char *hash);
+XBT_PUBLIC(char *)    xbt_sha_read(xbt_sha_t sha);
+   
+XBT_PUBLIC(void)      xbt_sha (const char *data, char *hash);
+
+
+#endif /* XBT_HASH_H */
