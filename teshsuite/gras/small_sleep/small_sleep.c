09c8124a61c40287d7f4e0690eb723808491ebc1
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2008-05-19 13:20:05 +0000

    Test case trying to handle messages for a very short period
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@5439 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ teshsuite/gras/small_sleep/small_sleep.c
@@ -0,0 +1,35 @@
+/* $Id: gras.c 3859 2007-07-18 12:29:51Z donassbr $ */
+
+/* small_sleep.c -- check what happens when the processes do sleeps very shortly*/
+
+/* Copyright (c) 2007 Martin Quinson.                                       */
+/* All rights reserved.                                                     */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+
+#include "gras.h"
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(test,"Small sleep test");
+
+int function (int argc,char *argv[]);
+
+
+int function (int argc,char *argv[]) {
+  gras_init(&argc,argv);
+  gras_msg_handleall(100);
+  INFO0("Let's go -5");
+  gras_msg_handleall(1E-5);
+  INFO0("Let's go -10");
+  gras_msg_handleall(1E-10);
+  INFO0("Let's go -15");
+  gras_msg_handleall(1E-15);
+  INFO0("Let's go -20");
+  gras_msg_handleall(1E-20);
+  INFO0("done");
+  gras_exit();
+  return 0;
+} 
+
+
