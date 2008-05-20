e3e2024b794b44875934f421a156dcc7fcfd7341
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2008-05-20 16:09:22 +0000

    Implement properties on hosts and processes in RL (host is inited to empty, and process is inited to system process environment)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@5451 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/gras/Virtu/rl_process.c
+++ src/gras/Virtu/rl_process.c
@@ -11,17 +11,43 @@
 #include "gras/Virtu/virtu_rl.h"
 #include "portable.h"
 
+XBT_LOG_EXTERNAL_DEFAULT_CATEGORY(gras_virtu_process);
+
 /* globals */
 static gras_procdata_t *_gras_procdata = NULL;
 XBT_EXPORT_NO_IMPORT(char const *) _gras_procname = NULL;
+static xbt_dict_t _process_properties = NULL;
+static xbt_dict_t _host_properties = NULL;
+
+/* the environment, as specified by the opengroup, used to initialize the process properties */
+extern char **environ;          
 
 void gras_process_init() {
+  char **env_iter;
   _gras_procdata=xbt_new0(gras_procdata_t,1);
   gras_procdata_init();
+   
+  /* initialize the host & process properties */
+  _host_properties = xbt_dict_new();
+  _process_properties = xbt_dict_new();
+  env_iter = environ;
+  while (*env_iter) {
+     char *equal, *buf = xbt_strdup(*env_iter);
+     equal = strchr(buf, '=');
+     if (!equal) {
+	WARN1("The environment contains an entry without '=' char: %s (ignore it)",*env_iter);
+	continue;
+     }
+     *equal = '\0';
+     xbt_dict_set(_process_properties,buf,xbt_strdup(equal + 1),xbt_free_f);
+     free(buf);
+     env_iter++;
+  }   
 }
 void gras_process_exit() {
   gras_procdata_exit();
   free(_gras_procdata);
+  xbt_dict_free(&_process_properties);
 }
 
 const char *xbt_procname(void) {
@@ -57,15 +83,15 @@ void gras_agent_spawn(const char *name, void *data,
  * **************************************************************************/
 
 const char* gras_process_property_value(const char* name) {
-   THROW_UNIMPLEMENTED;
+   return xbt_dict_get_or_null(_process_properties,name);
 }
 xbt_dict_t gras_process_properties(void) {
-   THROW_UNIMPLEMENTED;
+   return _process_properties;
 }
 
 const char* gras_os_host_property_value(const char* name) {
-   THROW_UNIMPLEMENTED;
+   return xbt_dict_get_or_null(_host_properties,name);
 }
 xbt_dict_t gras_os_host_properties(void) {
-   THROW_UNIMPLEMENTED;
+   return _host_properties;
 }
