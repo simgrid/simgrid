b93bbbeb48fa3f92e3f8cf5d2c636ed7e0528a89
cherierm <cherierm@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2008-06-04 17:47:23 +0000

    source code to manage the Windows Unix and Mac file formats.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@5536 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ tools/tesh2/include/readline.h
@@ -0,0 +1,20 @@
+#ifndef __READLINE_H	
+#define __READLINE_H
+
+#include <com.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+long
+readline(FILE* stream, char **buf, size_t *n);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif /* !__READLINE_H */
+
