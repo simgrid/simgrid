375bf97d4031bfbb5012e5b28303982224740a1e
alegrand <alegrand@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2008-06-19 09:30:43 +0000

    Import of Frederic's files and set up a very basic test (juste parsing the files).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@5770 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ teshsuite/simdag/platforms/basic_parsing_test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "simdag/simdag.h"
+
+int main(int argc, char **argv) {
+  /* initialisation of SD */
+  SD_init(&argc, argv);
+
+  /* creation of the environment */
+  SD_create_environment(argv[1]);
+
+  SD_exit();
+  return 0;
+}
+
