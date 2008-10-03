a42c95e1e1dc366c25740f1e80be435e44cba993
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2008-10-03 13:00:15 +0000

    Redo the test to avoid stream redirections
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@5964 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- teshsuite/xbt/parallel_log_crashtest.c
+++ teshsuite/xbt/parallel_log_crashtest.c
@@ -17,6 +17,7 @@ int test_amount = 99;    /* Up to 999 to not break the logs (and thus the testin
 int crasher_amount = 99; /* Up to 99  to not break the logs (and thus the testing mecanism) */
 int *id; /* to pass a pointer to the threads without race condition */
 
+int more_info=0; /* SET IT TO TRUE TO GET MORE INFO */
 
 /*
  * Some additionnal code to let the father wait the childs
@@ -31,11 +32,14 @@ xbt_mutex_t dead_end;
 static void crasher_thread(void *arg) {
    int id = *(int*)arg;
    int i;
-   
+
    for (i=0; i<test_amount; i++) {
+     if (more_info)
       INFO10("%03d (%02d|%02d|%02d|%02d|%02d|%02d|%02d|%02d|%02d)",test_amount-i,id,id,id,id,id,id,id,id,id);
+     else
+       INFO0("XXX (XX|XX|XX|XX|XX|XX|XX|XX|XX)");
    }
-   
+
    xbt_mutex_acquire(mut_end);
    running_threads--;
    xbt_cond_signal(cond_end);
@@ -46,34 +50,34 @@ int crasher (int argc,char *argv[]);
 int crasher (int argc,char *argv[]) {
   int i;
   xbt_thread_t *crashers;
-   
+
   gras_init(&argc,argv);
 
   /* initializations of the philosopher mecanisms */
-  id = xbt_new0(int,crasher_amount); 
+  id = xbt_new0(int,crasher_amount);
   crashers = xbt_new(xbt_thread_t,crasher_amount);
-     
+
   for (i=0; i<crasher_amount; i++)
      id[i] = i;
-   
+
   /* setup the ending mecanism */
-  running_threads = crasher_amount; 
+  running_threads = crasher_amount;
   cond_end = xbt_cond_init();
   mut_end = xbt_mutex_init();
-  
+
   /* spawn threads */
   for (i=0; i<crasher_amount; i++) {
      char *name = bprintf("thread %d",i);
      crashers[i] = xbt_thread_create(name,&crasher_thread,&id[i]);
      free(name);
   }
-  
+
   /* wait for them */
   xbt_mutex_acquire(mut_end);
-  while (running_threads) 
+  while (running_threads)
      xbt_cond_wait(cond_end,mut_end);
   xbt_mutex_release(mut_end);
-       
+
   gras_exit();
   return 0;
 }
@@ -82,6 +86,6 @@ int main(int argc, char *argv[]){
   int errcode;
 
   errcode=crasher(argc,argv);
- 
+
   return errcode;
 }
