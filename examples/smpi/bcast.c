eb35f005960fefa613cc51e8f4d786f58b11849e
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-06-26 20:22:34 +0000

    Finalize the smpi test (it works)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@6374 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- examples/smpi/bcast.c
+++ examples/smpi/bcast.c
@@ -5,22 +5,26 @@ int main (int argc, char **argv) {
   int size, rank;
   int value = 3;
   double start_timer;
+   int quiet = 0;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+   if (argc > 1 && !strcmp(argv[1],"-q")) 
+     quiet=1;
+   
   start_timer = MPI_Wtime();
 
   if (0 == rank) {
     value = 17;
   }
-  printf("node %d has value %d\n", rank, value);
+  printf("node %d has value %d before broadcast\n", rank, value);
   MPI_Bcast(&value, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  printf("node %d has value %d\n", rank, value);
+  printf("node %d has value %d after broadcast\n", rank, value);
 
   MPI_Barrier( MPI_COMM_WORLD );
-  if ( 0 == rank)
+  if ( 0 == rank && !quiet)
 	    printf("Elapsed time on rank %d: %lf s\n", rank, MPI_Wtime()-start_timer);
   MPI_Finalize();
   return 0;
