4f8bb2f29a74af65e3db301ab1c4c0df611714c8
alegrand <alegrand@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-12-15 16:33:33 +0000

    We need getline so include the right file and do not hope that other header will provide it.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@6899 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/msg/msg_actions.c
+++ src/msg/msg_actions.c
@@ -3,6 +3,7 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
+#include "simgrid_config.h"     /* getline */
 #include "msg/private.h"
 #include "xbt/str.h"
 #include "xbt/dynar.h"
