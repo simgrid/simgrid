3fa2f757f7555af8df81ee0231592d559e933580
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-12-16 15:59:25 +0000

    reorganize the directory and add some files for pipol
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@6904 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ buildtools/ctest/test_simgrid_pipol.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+#PIPOL esn i386-linux-ubuntu-jaunty.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+#PIPOL esn i386-linux-ubuntu-karmic.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+#PIPOL esn amd64-linux-ubuntu-jaunty.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+#PIPOL esn amd64-linux-ubuntu-karmic.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+
+#PIPOL esn i386-linux-debian-etch.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+#PIPOL esn i386-linux-debian-lenny.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+#PIPOL esn amd64-linux-debian-etch.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+#PIPOL esn amd64-linux-debian-lenny.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+
+#PIPOL esn i386-linux-fedora-core11.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+#PIPOL esn amd64-linux-fedora-core11.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+
+#PIPOL esn i386-linux-mandriva-2009_powerpack.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+#PIPOL esn amd64-linux-mandriva-2009_powerpack.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
+
+#PIPOL esn i386_mac-mac-osx-server-leopard.dd.gz none 02:00 /home/mescal/navarro/script/verif.sh
