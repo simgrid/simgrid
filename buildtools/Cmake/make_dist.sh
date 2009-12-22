44f51520d32b8c7929597cb96f23aa71c822abb6
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-12-22 16:18:53 +0000

    cosmetics
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@6920 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/Cmake/make_dist.sh
+++ buildtools/Cmake/make_dist.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-# fail fast on errors
-set -e
-
 # This script creates a new dist archive from the svn
-source ~/simgrid-svn/buildtools/Cmake/cmake_simgrid.conf
+
+set -e # fail fast on errors 
+source ~/simgrid-svn/buildtools/Cmake/cmake_simgrid.conf # get config
 
 echo "get Linux dependencies"
 sudo aptitude install -y libtool automake1.10 autoconf libgcj10-dev gcc g++ bash flex flexml doxygen bibtex bibtool iconv bibtex2html addr2line valgrind transfig
