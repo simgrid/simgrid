14d333ab7273e9e7ef3fb037e350aa48082b452b
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-12-22 16:43:02 +0000

    convert to shell functions
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@6921 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/Cmake/build_from_dist.sh
+++ buildtools/Cmake/build_from_dist.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 # This script waits that the make_dist script finishes building the right archive in ~/simgrid
+# and then builds it using the autotools
 
 
 source ~/simgrid-svn/buildtools/Cmake/cmake_simgrid.conf
 
-get_version
-wait_archive
-
+build_from_autotools 
--- buildtools/Cmake/make_dist.sh
+++ buildtools/Cmake/make_dist.sh
@@ -7,22 +7,6 @@ source ~/simgrid-svn/buildtools/Cmake/cmake_simgrid.conf # get config
 echo "get Linux dependencies"
 sudo aptitude install -y libtool automake1.10 autoconf libgcj10-dev gcc g++ bash flex flexml doxygen bibtex bibtool iconv bibtex2html addr2line valgrind transfig
 
-echo "update the svn"
-cd ${SIMGRID_SVN_ROOT}
-svn up
-
-set -x # be verbose
-
-
-echo "rebuild the missing files for compilation"
-./bootstrap
-./configure --enable-maintainer-mode --disable-compile-optimizations
-
-echo "Make the archive"
-make all dist
-
-echo "Copy the archive in position"
-mkdir -p ${SIMGRID_BASEDIR}
-mv simgrid*.tar.gz ${SIMGRID_BASEDIR}
+make_dist
 
 echo "Done!"
