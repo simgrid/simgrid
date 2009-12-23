e69074fe5afbae103265c804c4d872685a6abce2
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-12-23 20:22:44 +0000

    Fix image name
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@6982 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/scripts/pipol.sh
+++ buildtools/scripts/pipol.sh
@@ -17,5 +17,5 @@ pipol-sub esn amd64-linux-ubuntu-karmic.dd.gz none 01:00 ~/simgrid-svn/buildtool
 pipol-sub esn amd64-linux-debian-testing.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
 pipol-sub esn amd64-linux-fedora-core11.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
 
-pipol-sub esn 386_mac-mac-osx-server-leopard.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+pipol-sub esn i386_mac-mac-osx-server-leopard.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
 
