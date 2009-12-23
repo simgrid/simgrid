1f8e028bcc54a7ceda097be4a61bf26d610565b1
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-12-23 20:21:34 +0000

    update target list
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@6980 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/scripts/pipol.sh
+++ buildtools/scripts/pipol.sh
@@ -1,17 +1,21 @@
 #! /bin/sh
 
-pipol-sub esn i386-linux-debian-testing.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/make_dist.sh
-sleep 200
+pipol-sub esn i386-linux-debian-testing.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/make_dist.sh
+sleep 300
 
-pipol-sub esn i386-linux-ubuntu-intrepid.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
-pipol-sub esn i386-linux-ubuntu-jaunty.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
-pipol-sub esn i386-linux-ubuntu-karmic.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
-pipol-sub esn i386-linux-debian-testing.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+pipol-sub esn i386-linux-ubuntu-intrepid.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+pipol-sub esn i386-linux-ubuntu-jaunty.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+pipol-sub esn i386-linux-ubuntu-karmic.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+pipol-sub esn i386-linux-debian-testing.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+pipol-sub esn i386-linux-fedora-core11.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
 
-pipol-sub esn amd64-linux-ubuntu-intrepid.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
-pipol-sub esn amd64-linux-ubuntu-jaunty.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
-pipol-sub esn amd64-linux-ubuntu-karmic.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
-pipol-sub esn amd64-linux-debian-testing.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+pipol-sub esn ia64-linux-debian-lenny.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+
+pipol-sub esn amd64-linux-ubuntu-intrepid.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+pipol-sub esn amd64-linux-ubuntu-jaunty.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+pipol-sub esn amd64-linux-ubuntu-karmic.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+pipol-sub esn amd64-linux-debian-testing.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
+pipol-sub esn amd64-linux-fedora-core11.dd.gz none 01:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
 
 pipol-sub esn 386_mac-mac-osx-server-leopard.dd.gz none 02:00 ~/simgrid-svn/buildtools/scripts/test_dist_with_cmake.sh
 
