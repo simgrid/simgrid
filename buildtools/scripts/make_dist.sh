496c46da8846a6cc1b309622d801e4ca203b2db7
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2009-12-24 10:47:23 +0000

    Ignore apt-get issues
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@6990 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/scripts/make_dist.sh
+++ buildtools/scripts/make_dist.sh
@@ -12,7 +12,7 @@ fi
 
 if [ ! -e /usr/bin/libtool ] || [ ! -e /usr/bin/automake ] || [ ! -e /usr/bin/flex ] ; then
   echo "get Linux dependencies"
-  sudo aptitude install -y libtool automake1.10 autoconf libgcj10-dev gcc g++ bash flex flexml doxygen bibtex bibtool iconv bibtex2html addr2line valgrind transfig
+  sudo aptitude install -y libtool automake1.10 autoconf libgcj10-dev gcc g++ bash flex flexml doxygen bibtex bibtool iconv bibtex2html addr2line valgrind transfig || true
 fi
 
 make_dist
