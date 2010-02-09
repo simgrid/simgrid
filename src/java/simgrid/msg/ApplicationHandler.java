19f844fdb8be388e6dac12126f5eb9023b9e4e65
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-02-09 16:36:36 +0000

    doc improvement
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@7070 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/java/simgrid/msg/ApplicationHandler.java
+++ src/java/simgrid/msg/ApplicationHandler.java
@@ -1,12 +1,13 @@
 /*
- * simgrid.msg.ApplicationHandler.java	1.00 07/05/01
  *
  * Copyright 2006,2007 Martin Quinson, Malek Cherier           
  * All right reserved. 
  *
  * This program is free software; you can redistribute 
  * it and/or modify it under the terms of the license 
- *(GNU LGPL) which comes with this package. 
+ * (GNU LGPL) which comes with this package.
+ *
+ * These are the upcalls used by the FleXML parser for application files
  */
 
 package simgrid.msg;
@@ -30,7 +31,7 @@ import java.util.Hashtable;
 public final class ApplicationHandler {
 
 	/* 
-	 * This class is used to create the processes descibed in the deployment file.
+	 * This class is used to create the processes described in the deployment file.
 	 */
 	static class ProcessFactory {
 		/**
