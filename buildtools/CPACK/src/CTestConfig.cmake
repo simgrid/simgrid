c61a5d6341341d2523453555dfa918fbfbd8fc19
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-03-03 16:20:50 +0000

    Add install to cmake.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@7176 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/CPACK/src/CTestConfig.cmake
+++ buildtools/CPACK/src/CTestConfig.cmake
@@ -48,9 +48,9 @@ if(supernovae)
 	SET(BUILDNAME "SUPERNOVAE" CACHE TYPE INTERNAL FORCE)
 endif(supernovae)
 
-if(NOT disable_gtnets)
+if(NOT disable_gtnets AND HAVE_GTNETS)
 	SET(BUILDNAME "GTNETS" CACHE TYPE INTERNAL FORCE)
-endif(NOT disable_gtnets)
+endif(NOT disable_gtnets AND HAVE_GTNETS)
 
 SET(SITE "${osname}_${DISTRIB2}_${cpu}")
 SET(CTEST_SITE "${osname}_${DISTRIB2}_${cpu}")
