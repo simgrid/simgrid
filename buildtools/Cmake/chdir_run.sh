5388ef6910cafc858fc38edcbee0566c07ea5ed1
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-03-11 15:04:02 +0000

    Refresh directories.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@7223 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/Cmake/chdir_run.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-# This script goes to the directory specified as first argument, and executes the rest of its arguments
-# It ought to be a solution to specify the PWD of commands in CTest, but I didn't find it yet
-
-
-cd $1
-shift
-exec "$@"
--- buildtools/Cmake/script_final/reserv.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-ssh pipol pipol-sub esn i386-linux-ubuntu-karmic.dd.gz none 02:00 /home/mescal/navarro/script/DISTRIB.sh
-
-ssh pipol pipol-sub esn i386-linux-ubuntu-jaunty.dd.gz none 02:00 /home/mescal/navarro/script/WAIT.sh
-ssh pipol pipol-sub esn i386-linux-ubuntu-karmic.dd.gz none 02:00 /home/mescal/navarro/script/WAIT.sh
