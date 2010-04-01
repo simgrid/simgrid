a74696e26072808485f4af3fe494ae8b854860d9
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-04-01 13:54:42 +0000

    nope, dudes, this function is a standard one.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@7419 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/xbt/str.h
+++ include/xbt/str.h
@@ -48,8 +48,6 @@ XBT_PUBLIC(char *) xbt_str_varsubst(char *str, xbt_dict_t patterns);
 XBT_PUBLIC(void) xbt_str_strip_spaces(char *);
 XBT_PUBLIC(char *) xbt_str_diff(char *a, char *b);
 
-XBT_PUBLIC(long) getline(char **lineptr, size_t * n, FILE * stream);
-
 XBT_PUBLIC(char*)xbt_str_from_file(FILE *file);
 
 /** @brief Classical alias to (char*)
