efd71a79ebaec3760d2799bd90c5e5cf96ac0bfa
coldpeace <coldpeace@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-04-08 15:33:08 +0000

    ruby examples
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@7479 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ examples/ruby/MasterSlave.rb
@@ -0,0 +1,80 @@
+# Debug it with this command:
+# make -C ../.. && valgrind ruby MasterSlave.rb --log=ruby.thres:debug 2>&1 | less
+require 'simgrid'
+include MSG
+#################################################
+# Class Master
+#################################################
+
+
+class Master < MSG::Process  
+  # main : that function that will be executed when running simulation
+
+  def main(args) # args is an array containing arguments for function master
+   size = args.size
+   for i in 0..size-1
+     MSG::info("args["+String(i)+"]="+args[i])
+   end
+  
+   raise "Master needs 3 arguments" if size < 3 
+   numberOfTask = Integer(args[0]) 
+   taskComputeSize = Float(args[1])
+   taskCommunicationSize = Float(args[2])
+   slaveCount = Integer(args[3]) 
+   
+   # Creates and sends the tasks
+    for i in 0..numberOfTask-1
+     task = RTask.new("Task_"+ i.to_s, taskComputeSize , taskCommunicationSize);
+     mailbox = "slave " + (i%slaveCount).to_s
+     MSG::info("Master Sending "+ task.name + " to " + mailbox + " with Comput Size " + 
+           task.compSize.to_s)
+     task.send(mailbox)
+     MSG::info("Master Done Sending " + task.name + " to " + mailbox)
+    end
+  
+   # Sending Finalize MSG::Tasks
+   #MSG::info("Master: All "+numberOfTask+" tasks have been dispatched. Let's tell everybody the computation is over.")
+   MSG::info("Master: All tasks have been dispatched. Let's tell everybody the computation is over.")
+   for i in 0..slaveCount-1
+     mailbox = "slave " + i.to_s
+     finalize_task = RTask.new("finalize",0,0)
+     finalize_task.send(mailbox)
+   end
+   MSG::info("Master : Everything's Done")
+  end    
+end
+#################################################
+# Class Slave
+#################################################
+class Slave < MSG::Process
+
+  def main(args)
+    mailbox = "slave " + args[0]
+    for i in 0..args.size-1
+      MSG::debug("args["+String(i)+"]="+args[i])
+    end
+
+    while true
+       task = Task.receive(mailbox)
+       if (task.name == "finalize")
+	       break
+       end
+       task.execute
+       MSG::debug("Slave '" + mailbox + "' done executing task "+ task.name + ".")
+    end
+    MSG::info("I'm done, see you")
+  end
+end
+#################################################
+# main chunck
+#################################################
+if (ARGV.length == 2) 
+	MSG.createEnvironment(ARGV[0])
+	MSG.deployApplication(ARGV[1])
+else 
+	MSG.createEnvironment("platform.xml")
+	MSG.deployApplication("deploy.xml")
+end
+MSG.run
+puts "Simulation time : " + MSG.getClock .to_s
+MSG.exit
---
+++ examples/ruby/PingPong.rb
@@ -0,0 +1,88 @@
+require 'simgrid'
+
+include MSG
+
+#####################
+#PingPongTask Class
+#####################
+
+class PingPongTask < MSG::Task
+  
+  # The initialize method has no effect 
+  @time 
+  def setTime(t)
+    @time = t
+  end
+  
+  def getTime()
+    return @time
+  end
+end
+
+####################
+# Sender Class
+####################
+
+class Sender < MSG::Process
+  
+ def main(args)
+   MSG::info("Hello from Sender")
+   hostCount = args.size
+   MSG::info("Host count :" + hostCount.to_s)
+   mailboxes = Array.new
+      
+   for i in 0..hostCount-1
+     mailboxes<< MSG::Host.getByName(args[i]).name
+   end
+   
+   for i in 0..hostCount-1
+     time = MSG.getClock
+     MSG::info("sender time :"+time.to_s)
+     task = PingPongTask.new("PingTask",10000,2000)
+     MSG::info("task created :" + task.name);
+     #task.join(time) -- MSG::task.join(data) is a Native method you can use to attach any data you want to the task
+     task.setTime(time)
+     task.send(mailboxes[i])
+   end
+   MSG::info("Bye!!")     
+   end
+ end
+  
+####################
+# Receiver Class
+####################
+
+class Receiver < MSG::Process
+  
+  def main(args)
+    MSG::info("Hello from Receiver")
+    time = MSG.getClock
+    host = MSG::Host.getHostProcess(self)
+    task = PingPongTask.receive(host.name)
+    timeGot = MSG.getClock
+    MSG::info("Got at time: "+timeGot.to_s)
+    #timeSent = task.data -- 
+    timeSent = task.getTime
+    MSG::info("Was sent at time "+timeSent.to_s)
+    communicationTime = timeGot - time
+    MSG::info("Communication Time: "+communicationTime.to_s)
+    MSG::info("--- bw "+(100000000/communicationTime).to_s+" ----")
+    MSG::info("Bye!!")
+  end
+  
+end
+
+#################################################
+# main chunck
+#################################################
+
+if (ARGV.length == 2) 
+	MSG.createEnvironment(ARGV[0])
+	MSG.deployApplication(ARGV[1])
+else 
+	MSG.createEnvironment("ping_pong_platform.xml")
+	MSG.deployApplication("ping_pong_deployment.xml")
+end
+
+MSG.run
+MSG.exit
