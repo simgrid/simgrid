9bc3931b2204551422f2699e9ff2b01b9fc4468e
coldpeace <coldpeace@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-04-13 14:42:54 +0000

    receiving line command arguments
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@7550 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- examples/lua/master_slave.lua
+++ examples/lua/master_slave.lua
@@ -67,8 +67,13 @@ end -- function ----------------------------------------------------------
 --]]
 
 require "simgrid"
+if (#arg == 2) then
+simgrid.platform(arg[1])
+simgrid.application(arg[2])
+else
 simgrid.platform("../msg/small_platform.xml")
 simgrid.application("../ruby/deploy.xml")
+end
 simgrid.run()
 simgrid.info("Simulation's over.See you.")
 simgrid.clean()
