ccb94928228f699f7b1031fe117264a0e2306679
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-04-15 09:12:46 +0000

    Enable java tests to be executed out of project directory.'
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@7582 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/Cmake/test_java.sh
+++ buildtools/Cmake/test_java.sh
@@ -1,16 +1,8 @@
 #! /bin/sh
-echo "\n\n"
-pwd
-export LD_LIBRARY_PATH="`pwd`/src/.libs:$LD_LIBRARY_PATH"
-export CLASSPATH="`pwd`/src/simgrid.jar:$CLASSPATH"
+echo "\n"
+export CLASSPATH=$3:$3/src/simgrid.jar:$CLASSPATH
 cd $1
-export CLASSPATH="$CLASSPATH:`pwd`"
-pwd
-
-echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
 echo "CLASSPATH = $CLASSPATH"
-
-echo "\n\n"
+pwd
 echo "java $2 *platform.xml *deployment.xml"
-echo "\n\n"
 java $2 *platform.xml *deployment.xml
