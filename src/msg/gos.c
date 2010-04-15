93d65fe49f4f8ee34be5f8178ba1f3c69dd83b44
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-04-15 13:05:24 +0000

    Make sure that it's ok to execute 0-sized tasks
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@7598 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/msg/gos.c
+++ src/msg/gos.c
@@ -53,6 +53,9 @@ MSG_error_t MSG_task_execute(m_task_t task)
 
   DEBUG1("Computing on %s", MSG_process_self()->simdata->m_host->name);
 
+  if (simdata->comp_amount == 0)
+    return MSG_OK;
+
   simdata->refcount++;
   SIMIX_mutex_lock(simdata->mutex);
   simdata->compute =
