9d37162b4f3a74907ed3c67d8784e4ef0f830f3e
velho <velho@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-04-19 09:04:46 +0000

    Cople with ctest 2.8 still compatible with previous versions (2.6.x).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@7609 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- tools/tesh/colorize.pl
+++ tools/tesh/colorize.pl
@@ -3,9 +3,9 @@ use Term::ANSIColor qw{:constants};
 $Term::ANSIColor::AUTORESET = 1;
 
 while(<>){
-    if($_ =~ m/^[-]\s.*/){
+    if($_ =~ m/^(\d+\:\s)?[-]\s.*/){
 	print BOLD RED $_;
-    }elsif ($_ =~ m/^[+]\s.*/){
+    }elsif ($_ =~ m/^(\d+\:\s)?[+]\s.*/){
 	print BOLD GREEN $_;
     }else{
 	print BOLD $_;
