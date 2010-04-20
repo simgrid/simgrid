970a7d49cee13218c0749de4757ab3cd062be9b1
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-04-20 13:38:31 +0000

    Be sure LD_LIBRARY_PATH is set for libsimgrid.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@7618 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/Cmake/test_java.sh
+++ buildtools/Cmake/test_java.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 echo "\n"
 export CLASSPATH=$3:$3/src/simgrid.jar:$CLASSPATH
+export LD_LIBRARY_PATH=$3/src/.libs:$LD_LIBRARY_PATH
 cd $1
-echo "CLASSPATH = $CLASSPATH"
+echo "CLASSPATH 	= $CLASSPATH"
+echo "LD_LIBRARY_PATH 	= $LD_LIBRARY_PATH"
 pwd
 echo "java $2 *platform.xml *deployment.xml"
 java $2 *platform.xml *deployment.xml
