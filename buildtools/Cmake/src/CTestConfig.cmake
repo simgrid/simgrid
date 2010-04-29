0561fd8fa0583c953b6a9209dfa765b5fee597ac
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-04-29 09:42:52 +0000

    Set cmake site with cmake variables and not with uname command.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@7657 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/Cmake/src/CTestConfig.cmake
+++ buildtools/Cmake/src/CTestConfig.cmake
@@ -1,35 +1,6 @@
 # Configure CTest. For details, see:
 # http://www.cmake.org/Wiki/CMake_Testing_With_CTest#Customizing_CTest
 
-#Get the hostname of current machine :
-exec_program(${HOSTNAME_CMD} OUTPUT_VARIABLE HOSTNAME)
-set(SITE "${HOSTNAME}")
-MARK_AS_ADVANCED(HOSTNAME_CMD)
-
-#Get the system information of current machine
-macro(getuname name flag)
-exec_program("${UNAME}" ARGS "${flag}" OUTPUT_VARIABLE "${name}")
-endmacro(getuname)
-MARK_AS_ADVANCED(UNAME)
-
-exec_program("${CAT}" ARGS "version" OUTPUT_VARIABLE VERSION)
-
-if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-  SET(DISTRIB2 "OSX")
-else(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-  #Try to get the distrib
-  exec_program("${CAT}" ARGS " /etc/issue" OUTPUT_VARIABLE DISTRIB)
-  MARK_AS_ADVANCED(CAT)
-  STRING(REPLACE " \\n \\l" "" DISTRIB2 ${DISTRIB})
-endif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-  
-getuname(osname -s)
-getuname(node -n)
-getuname(osrel  -r)
-getuname(cpu    -m)
-	
-#SET(BUILDNAME "${osname}_${DISTRIB2}_${cpu}")
-#SET(CTEST_SITE "${node}")
 SET(BUILDNAME "none" CACHE TYPE INTERNAL FORCE)
 
 if(with_context MATCHES ucontext AND NOT supernovae)
@@ -60,12 +31,16 @@ if(enable_memcheck)
 	SET(BUILDNAME "MEMCHECK" CACHE TYPE INTERNAL FORCE)
 endif(enable_memcheck)
 
+set(osname ${CMAKE_SYSTEM_NAME})
+set(cpu ${CMAKE_SYSTEM_PROCESSOR})
+set(DISTRIB2 ${CMAKE_SYSTEM_VERSION})
+
 SET(SITE "${osname}_${DISTRIB2}_${cpu}")
 SET(CTEST_SITE "${osname}_${DISTRIB2}_${cpu}")
 SET(CTEST_PROJECT_NAME "${PROJECT_NAME}")
 SET(CTEST_CUSTOM_MAXIMUM_FAILED_TEST_OUTPUT_SIZE "3000000")
 SET(CTEST_CUSTOM_MAXIMUM_PASSED_TEST_OUTPUT_SIZE "3000000")
-string(REPLACE "/" "" SITE ${SITE})
+
 exec_program("echo $PIPOL_IMAGE" OUTPUT_VARIABLE PIPOL_IMAGE)
 #message("PIPOL_IMAGE : \"${PIPOL_IMAGE}\"")
 if(NOT ${PIPOL_IMAGE} MATCHES "\n")
