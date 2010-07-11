027ae9451ba821fbbc3e5a60c0098000ba4a2864
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-07-11 13:48:49 +0000

    well well
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@8008 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- examples/simdag/dot/dot_test.c
+++ examples/simdag/dot/dot_test.c
@@ -1,6 +1,6 @@
 /* simple test trying to load a DOT file.                                   */
 
-/* Copyright (c) 20010. The SimGrid Team.
+/* Copyright (c) 2010. The SimGrid Team.
  * All rights reserved.                                                     */
 
 /* This program is free software; you can redistribute it and/or modify it
