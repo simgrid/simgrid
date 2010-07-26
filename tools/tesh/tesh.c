730b860d158272accdb1971c25fd1dd6978becf0
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-07-26 12:19:05 +0000

    Without this line paranoid mode doesn't work.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@8059 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- tools/tesh/tesh.c
+++ tools/tesh/tesh.c
@@ -6,6 +6,8 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
+#include "simgrid_config.h"     /* FILE for getline */
+
 /* specific to Borland Compiler */
 #ifdef __BORLANDDC__
 #pragma hdrstop
