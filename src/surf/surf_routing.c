7259806ba6184616640e46a4f0d2f39725ff17a2
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-10-04 08:13:07 +0000

    stop misusing : in the DTD. use _ as a separator instead
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@8333 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/surf/surf_routing.c
+++ src/surf/surf_routing.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009, 2010. The SimGrid Team.
+/* Copyright (c) 2009, 2010. The SimGrid Team. 
  * All rights reserved.                                                     */
 
 /* This program is free software; you can redistribute it and/or modify it
