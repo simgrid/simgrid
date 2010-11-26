dfd409976af034992cb31e49e3eb77543ee0a118
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-11-26 10:21:55 +0000

    Cosmetics
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@8667 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/pipol/Experimental.sh
+++ buildtools/pipol/Experimental.sh
@@ -21,8 +21,21 @@ cd simgrid-trunk
 mkdir tmp_build
 cd tmp_build
 
-cmake -Denable_lua=on -Denable_ruby=on -Denable_lib_static=on -Denable_graphviz=on -Denable_model-checking=on -Denable_tracing=on -Denable_latency_bound_tracking=on ..
-
+#ucontext
+cmake -Denable_lua=on \
+-Denable_ruby=on \
+-Denable_lib_static=on \
+-Denable_graphviz=on \
+-Denable_model-checking=off \
+-Denable_tracing=on \
+-Denable_latency_bound_tracking=off \
+-Denable_gtnets=off \
+-Denable_java=on \
+-Dwith_context=auto \
+-Denable_compile_optimizations=off \
+-Denable_compile_warnings=off \
+-Denable_supernovae=off \
+-Denable_smpi=on ..
 ctest -D ExperimentalStart
 ctest -D ExperimentalConfigure
 ctest -D ExperimentalBuild
@@ -30,10 +43,21 @@ ctest -D ExperimentalTest
 ctest -D ExperimentalSubmit
 make clean
 
-rm -rf *
-cmake -Denable_lua=on -Denable_ruby=on -Denable_lib_static=on -Denable_graphviz=on -Denable_model-checking=on -Denable_tracing=on -Denable_latency_bound_tracking=on -Dwith_context=pthread ..
+#pthread
+cmake -Denable_lua=on \
+-Denable_ruby=on \
+-Denable_lib_static=on \
+-Denable_graphviz=on \
+-Denable_model-checking=off \
+-Denable_tracing=on \
+-Denable_latency_bound_tracking=off \
+-Denable_gtnets=off \
+-Denable_java=on \
+-Dwith_context=pthread \
+-Denable_smpi=on ..
 ctest -D ExperimentalStart
 ctest -D ExperimentalConfigure
 ctest -D ExperimentalBuild
 ctest -D ExperimentalTest
 ctest -D ExperimentalSubmit
+make clean
