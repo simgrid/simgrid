f87bc85a78ade5e780ca2436a0885b88431f3f7a
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-11-28 10:45:21 +0000

    regenerate
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@8709 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/simgrid_units_main.c
+++ src/simgrid_units_main.c
@@ -71,6 +71,7 @@ extern xbt_test_unit_t _xbt_current_unit;
     void  test_dynar_dopar(void);
   /* SGU: END FILE */
 
+/* SGU: END PROTOTYPES */
 
 /*******************************/
 /* GENERATED FILE, DO NOT EDIT */
