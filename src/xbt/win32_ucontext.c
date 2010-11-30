20facf82f92baf2401e73847b694eea761622a4c
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-11-30 15:48:03 +0000

    Fix location of library license in win32 ucontextes
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@8762 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/xbt/win32_ucontext.c
+++ src/xbt/win32_ucontext.c
@@ -15,7 +15,7 @@
  *      Lesser General Public License for more details.
  *
  *      You should have received a copy of the GNU Lesser General Public
- *      License along with QueueUserAPCEx in the file COPYING.LIB;
+ *      License along with SimGrid in the file LICENSE-LGPL-2.1;
  *      if not, write to the Free Software Foundation, Inc.,
  *      59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */  
