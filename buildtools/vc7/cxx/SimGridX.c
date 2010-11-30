4c3f6e301c9b8d3b11361775e6403c7a2118de2f
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-11-30 17:47:55 +0000

    Remove obselete files.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@8776 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/vc7/cxx/SimGridX.c
+++
@@ -1,11 +0,0 @@
-#include <windows.h>
-
-#include <xbt/log.h>
-    
-/* logs */ 
-// XBT_LOG_NEW_DEFAULT_CATEGORY(SimGridX,"SimGrid for cxx");
-int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason,
-                          void *lpReserved) 
-{
-  return 1;
-}
--- buildtools/vc7/simgrid/simgrid.c
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
- int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason,
-                            void *lpReserved) 
-{
-  return 1;
-}
