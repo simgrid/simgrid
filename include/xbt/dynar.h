aaa58c7c03c5a91a3801f5fecdb79a79060ba5e9
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-11-30 15:54:27 +0000

    DUDES. We need one copy and only one copy of that damn prototype. Problems of hard coding sprints...
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@8766 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- include/xbt/dynar.h
+++ include/xbt/dynar.h
@@ -134,6 +134,8 @@ XBT_PUBLIC(void) xbt_dynar_map(const xbt_dynar_t dynar,
  *  @{
  */
 
+XBT_INLINE void *xbt_dynar_set_at_ptr(const xbt_dynar_t dynar,
+                                      const unsigned long idx);
 XBT_INLINE XBT_PUBLIC(void *) xbt_dynar_get_ptr(const xbt_dynar_t dynar,
                                                 const unsigned long idx);
 XBT_PUBLIC(void *) xbt_dynar_insert_at_ptr(xbt_dynar_t const dynar,
