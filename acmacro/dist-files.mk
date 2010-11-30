e2030cb82d5b8f8b39696794f036e5ffc435f9bf
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-11-30 17:32:43 +0000

    Remove the Acmacro directory.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@8770 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- acmacro/dist-files.mk
+++
@@ -1,9 +0,0 @@
-# Makefile chunk which allows to display the files which should be included
-# into the distribution.
-
-# It is intended to be included in all Makefile.am 
-
-dist-files:
-	@for n in $(DISTFILES) ; do echo $(SRCFILE)$$n; done
-	@echo
-	@for n in $(DIST_SUBDIRS) ; do if [ x$$n != x. ] ; then $(MAKE) -C $$n dist-files SRCFILE=$(SRCFILE)$$n/ ; fi; done
--- acmacro/fancy_configury.m4
+++
@@ -1,7 +0,0 @@
-dnl SG_CONFIGURE_PART(MESSAGE)
-dnl Idea borrowed from PHP, which borrowed it from mm
-AC_DEFUN([SG_CONFIGURE_PART],[
-  AC_MSG_RESULT()
-  AC_MSG_RESULT([${T_MD}$1${T_ME}])
-])
-    
