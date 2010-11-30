e96d56e5fd2a71e414dca55d501f6756bd0c0eb5
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-11-30 15:49:08 +0000

    fix the name and path to simgrid_update_xml in error messages refering to it
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@8764 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/surf/surfxml_parse.c
+++ src/surf/surfxml_parse.c
@@ -212,22 +212,24 @@ void STag_surfxml_platform(void)
   sscanf(A_surfxml_platform_version, "%lg", &version);
 
   xbt_assert0((version >= 1.0), "******* BIG FAT WARNING *********\n "
-              "You're using an ancient XML file. "
-              "Since SimGrid 3.1, units are Bytes, Flops, and seconds "
-              "instead of MBytes, MFlops and seconds. "
-              "A script (surfxml_update.pl) to help you convert your old "
-              "platform files "
-              "is available in the contrib/platform_generation directory "
-              "of the simgrid repository. Please check also out the "
-              "SURF section of the ChangeLog for the 3.1 version. "
-              "Last, do not forget to also update your values for "
-              "the calls to MSG_task_create (if any).");
+      "You're using an ancient XML file.\n"
+      "Since SimGrid 3.1, units are Bytes, Flops, and seconds "
+      "instead of MBytes, MFlops and seconds.\n"
+
+      "Use simgrid_update_xml to update your file automatically. "
+      "This program is installed automatically with SimGrid, or "
+      "available in the tools/ directory of the source archive.\n"
+
+      "Please check also out the SURF section of the ChangeLog for "
+      "the 3.1 version for more information. \n"
+
+      "Last, do not forget to also update your values for "
+      "the calls to MSG_task_create (if any).");
   xbt_assert0((version >= 3.0), "******* BIG FAT WARNING *********\n "
-              "You're using an old XML file. "
-              "A script (surfxml_update.pl) to help you convert your old "
-              "platform files "
-              "is available in the contrib/platform_generation directory "
-              "of the simgrid repository.");
+      "You're using an old XML file.\n"
+      "Use simgrid_update_xml to update your file automatically. "
+      "This program is installed automatically with SimGrid, or "
+      "available in the tools/ directory of the source archive.");
 
   surfxml_call_cb_functions(STag_surfxml_platform_cb_list);
 
