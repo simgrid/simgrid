729e1f4f12f958c075048ec551bc97da73454c3f
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-12-01 15:25:32 +0000

    Deploy only kvm system.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@8812 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/pipol/deploy_simple.sh
+++ buildtools/pipol/deploy_simple.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-ssh pipol pipol-sub esn i386-linux-ubuntu-intrepid.dd.gz none 02:00 "~/Experimental.sh"
-ssh pipol pipol-sub esn amd64-linux-ubuntu-intrepid.dd.gz none 02:00 "~/Experimental.sh"
-
-ssh pipol pipol-sub esn i386-linux-ubuntu-jaunty.dd.gz none 02:00 "~/Experimental.sh"
-ssh pipol pipol-sub esn amd64-linux-ubuntu-jaunty.dd.gz none 02:00 "~/Experimental.sh"
-
-ssh pipol pipol-sub esn i386-linux-ubuntu-karmic.dd.gz none 02:00 "~/Experimental.sh"
-ssh pipol pipol-sub esn amd64-linux-ubuntu-karmic.dd.gz none 02:00 "~/Experimental.sh"
-
-ssh pipol pipol-sub esn i386-linux-fedora-core11.dd.gz none 02:00 "~/Experimental.sh"
-ssh pipol pipol-sub esn amd64-linux-fedora-core11.dd.gz none 02:00 "~/Experimental.sh"
-
 ssh pipol pipol-sub esn i386_kvm-linux-debian-lenny none 02:00 "~/Experimental.sh"
 ssh pipol pipol-sub esn i386_kvm-linux-debian-testing none 02:00 "~/Experimental.sh"
 
