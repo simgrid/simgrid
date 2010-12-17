fcccdc1509fa82173ee3eee9967a8a20fed4ed4d
mquinson <mquinson@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-12-17 08:38:48 +0000

    load the right header to make sure that we have getline
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@9281 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/xbt/xbt_replay_trace_reader.c
+++ src/xbt/xbt_replay_trace_reader.c
@@ -4,6 +4,7 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 #include "gras_config.h"
+#include "simgrid_config.h" /* getline */
 #include <errno.h>
 #include "xbt/sysdep.h"
 #include "xbt/log.h"
