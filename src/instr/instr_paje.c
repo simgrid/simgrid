c0cd87aa44735bbe29b683e4aeec31b31bfe3777
schnorr <schnorr@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2010-12-29 11:38:04 +0000

    [trace] dump previous events because container is about to be destroyed
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@9333 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/instr/instr_paje.c
+++ src/instr/instr_paje.c
@@ -265,6 +265,9 @@ void destroyContainer (container_t container)
   //trace my destruction
   new_pajeDestroyContainer(container);
 
+  //obligation to dump previous events because they might
+  //reference the container that is about to be destroyed
+  TRACE_last_timestamp_to_dump = surf_get_clock();
   TRACE_paje_dump_buffer();
 
   //free
