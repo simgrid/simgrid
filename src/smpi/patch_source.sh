4df5122b9e0586dfe7e7625d6a78966c47da509c
pini <pini@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2011-01-18 14:14:23 +0000

    No need to use a temporary file here.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@9430 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/smpi/patch_source.sh
+++ src/smpi/patch_source.sh
@@ -2,6 +2,4 @@
 INFILE="$1"
 OUTFILE="$2"
 SPFILE="replace_globals.cocci"
-spatch -sp_file ${SPFILE} $1 -o $2.tmp >/dev/null 2>/dev/null
-./fixsrc.pl < $2.tmp > $2
-rm $2.tmp
+spatch -sp_file ${SPFILE} ${INFILE} 2>/dev/null | patch -o - | ./fixsrc.pl > ${OUTFILE}
