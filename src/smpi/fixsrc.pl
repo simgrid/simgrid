8fe050131314bf1a2d03587cf9b8d6a24304183b
pini <pini@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2011-01-18 14:14:21 +0000

    Always add the needed include file.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@9429 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/smpi/fixsrc.pl
+++ src/smpi/fixsrc.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/perl
 
+# Add include for mandatory header file
+print "#include <smpi_cocci.h>\n";
+
 # FIXME: here we make the assumption that people don't do things like put
 # multiple statements after a declaration, but separated by semicolons. It's a
 # reasonable assumption for the time being, but technically it could cause
 # problems for some code.
 OUTER: while ($line = <STDIN>) {
-    if ($line =~ /#include <mpi\.h>/) {
-        print "#include <smpi.h>\n";
-        print "#include <smpi_cocci.h>\n";
-    } elsif ($line =~ /SMPI_VARINIT/) {
+    if ($line =~ /SMPI_VARINIT/) {
         do {
             chomp $line;
             $line =~ s/\s+/ /g;
