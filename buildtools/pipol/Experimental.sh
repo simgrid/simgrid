8af3cb11bfe73e4813ebeed796e74445b5b2207d
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2011-01-25 11:55:41 +0000

    Update pipol files
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@9487 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/pipol/Experimental.sh
+++ buildtools/pipol/Experimental.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SYSTEM=`uname`
-
 if [ -e ./pipol ] ; then
 	rm -rf ./pipol/$PIPOL_HOST
 	mkdir ./pipol/$PIPOL_HOST
@@ -13,28 +11,47 @@ fi
 cd ./pipol/$PIPOL_HOST
 
 svn checkout svn://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk simgrid-trunk --quiet
+cd simgrid-trunk
 
 sh ./simgrid-trunk/buildtools/pipol/liste_install.sh
+sh ./buildtools/pipol/install_gtnets.sh ./gtnets_install
+
 perl ./simgrid-trunk/buildtools/pipol/cmake.pl
 perl ./simgrid-trunk/buildtools/pipol/ruby.pl
 
-cd simgrid-trunk
+rm CMakeCache.txt
 
-cmake -Denable_lua=on \
+#supernovae
+cmake \
+-Denable_lua=on \
 -Denable_ruby=on \
--Denable_lib_static=on \
--Denable_model-checking=off \
--Denable_tracing=on \
--Denable_latency_bound_tracking=off \
--Denable_gtnets=off \
 -Denable_java=on \
--Denable_compile_optimizations=off \
--Denable_compile_warnings=off \
--Denable_supernovae=off \
--Denable_smpi=on .
+-Denable_tracing=on \
+-Denable_smpi=on \
+-Denable_supernovae=on \
+-Denable_compile_optimizations=on \
+-Denable_compile_warnings=on \
+-Denable_lib_static=off \
+-Denable_model-checking=off \
+-Denable_latency_bound_tracking=off \
+-Denable_gtnets=off .
 ctest -D ExperimentalStart
 ctest -D ExperimentalConfigure
 ctest -D ExperimentalBuild
 ctest -D ExperimentalTest
+ctest -D ExperimentalSubmit
+make clean
+
+#gtnets
+cmake \
+-Denable_latency_bound_tracking=on \
+-Denable_gtnets=on \
+-Dgtnets_path=./gtnets_install \
+-Denable_coverage=on \
+ctest -D ExperimentalStart
+ctest -D ExperimentalConfigure
+ctest -D ExperimentalBuild
+ctest -D ExperimentalTest
+ctest -D ExperimentalCoverage
 ctest -D ExperimentalSubmit
 make clean
--- buildtools/pipol/install_gtnets.sh
+++ buildtools/pipol/install_gtnets.sh
@@ -1,60 +1,63 @@
-if [ x$1 != x ]  ; then
-	prefix=$1;
+SYSTEM=`uname`
+if [ $SYSTEM = Linux ] ; then
+	if [ x$1 != x ]  ; then
+		prefix=$1;
+	fi
+	
+	if [ -e $prefix/lib ] ; then
+		echo -n ""
+	else	
+		echo "Creating directory $prefix/lib";
+		mkdir -p $prefix/lib;
+	fi
+	
+	if [ -e $prefix/include/gtnets ] ; then
+		echo -n "";
+	else	
+		echo "Creating directory $prefix/include/gtnets";
+		mkdir -p $prefix/include/gtnets;
+	fi
+	
+	localdir=`pwd`;
+	cd $prefix;
+	prefix=`pwd`;
+	cd $localdir;
+	echo "Install to prefix = $prefix";
+	
+	echo "Downloading GTNetS from SVN SimGrid's repository";
+	svn checkout svn://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/GTNetS/ --quiet
+	cd GTNetS
+	echo "Uncompressing package";
+	unzip gtnets-current.zip > /dev/null
+	tar zxvf gtnets-current-patch.tgz  > /dev/null
+	cd gtnets-current
+	cat ../00*.patch | patch -p1 > /dev/null
+	
+	ARCH_32=`uname -m | cut -d'_' -f2`
+	
+	if [ x$ARCH_32 = x64 ] ; then #only if 64 bit processor family
+	cat ../AMD64-FATAL-Removed-DUL_SIZE_DIFF-Added-fPIC-compillin.patch | patch -p1 > /dev/null
+	fi
+	
+	ln -sf Makefile.linux Makefile
+	echo "Creating dependencies";
+	make -j 3 depend > /dev/null
+	echo "Compiling GTNetS debug libs";
+	make -j 3 debug > /dev/null 2>&1
+	echo "Compiling GTNetS optimal libs";
+	make -j 3 opt > /dev/null 2>&1
+	wait
+	
+	cd ../../
+	
+	echo "Copying files to $prefix/lib";
+	cp -fr ./GTNetS/gtnets-current/*.so $prefix/lib/
+	ln -sf $prefix/lib/libgtsim-opt.so $prefix/lib/libgtnets.so
+	
+	echo "Copying files to $prefix/include/gtnets";
+	cp -fr ./GTNetS/gtnets-current/SRC/*.h $prefix/include/gtnets
+	wait
+	
+	echo "Done with gtnets installation";
+	rm -rf ./GTNetS
 fi
-
-if [ -e $prefix/lib ] ; then
-	echo -n ""
-else	
-	echo "Creating directory $prefix/lib";
-	mkdir -p $prefix/lib;
-fi
-
-if [ -e $prefix/include/gtnets ] ; then
-	echo -n "";
-else	
-	echo "Creating directory $prefix/include/gtnets";
-	mkdir -p $prefix/include/gtnets;
-fi
-
-localdir=`pwd`;
-cd $prefix;
-prefix=`pwd`;
-cd $localdir;
-echo "Install to prefix = $prefix";
-
-echo "Downloading GTNetS from SVN SimGrid's repository";
-svn checkout svn://scm.gforge.inria.fr/svn/simgrid/contrib/trunk/GTNetS/ --quiet
-cd GTNetS
-echo "Uncompressing package";
-unzip gtnets-current.zip > /dev/null
-tar zxvf gtnets-current-patch.tgz  > /dev/null
-cd gtnets-current
-cat ../00*.patch | patch -p1 > /dev/null
-
-ARCH_32=`uname -m | cut -d'_' -f2`
-
-if [ x$ARCH_32 = x64 ] ; then #only if 64 bit processor family
-cat ../AMD64-FATAL-Removed-DUL_SIZE_DIFF-Added-fPIC-compillin.patch | patch -p1 > /dev/null
-fi
-
-ln -sf Makefile.linux Makefile
-echo "Creating dependencies";
-make -j 3 depend > /dev/null
-echo "Compiling GTNetS debug libs";
-make -j 3 debug > /dev/null 2>&1
-echo "Compiling GTNetS optimal libs";
-make -j 3 opt > /dev/null 2>&1
-wait
-
-cd ../../
-
-echo "Copying files to $prefix/lib";
-cp -fr ./GTNetS/gtnets-current/*.so $prefix/lib/
-ln -sf $prefix/lib/libgtsim-opt.so $prefix/lib/libgtnets.so
-
-echo "Copying files to $prefix/include/gtnets";
-cp -fr ./GTNetS/gtnets-current/SRC/*.h $prefix/include/gtnets
-wait
-
-echo "Done with gtnets installation";
-rm -rf ./GTNetS
