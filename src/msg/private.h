c2d643caa1dca8e9c16e271a22176e58cbab7881
cristianrosa <cristianrosa@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2011-01-27 13:59:20 +0000

    CHECK_HOST() is very inefficient and only for debugging purposes, redefine it as nothing.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@9517 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- src/msg/private.h
+++ src/msg/private.h
@@ -92,8 +92,10 @@ XBT_PUBLIC_DATA(MSG_Global_t) msg_global;
 #define MSG_RETURN(val) do {PROCESS_SET_ERRNO(val);return(val);} while(0)
 /* #define CHECK_ERRNO()  ASSERT((PROCESS_GET_ERRNO()!=MSG_HOST_FAILURE),"Host failed, you cannot call this function.") */
 
-#define CHECK_HOST()  xbt_assert1(SIMIX_req_host_get_state(SIMIX_host_self())==1,\
-                                  "Host failed, you cannot call this function. (state=%d)",SIMIX_req_host_get_state(SIMIX_host_self()))
+/*#define CHECK_HOST()  xbt_assert1(SIMIX_req_host_get_state(SIMIX_host_self())==1,\
+                                  "Host failed, you cannot call this function. (state=%d)",SIMIX_req_host_get_state(SIMIX_host_self()))*/
+#define CHECK_HOST()
+
 
 m_host_t __MSG_host_create(smx_host_t workstation, void *data);
 
