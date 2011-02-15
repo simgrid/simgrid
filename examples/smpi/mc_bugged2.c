af501088d2630992f48a552dc250273aa09fe10b
cristianrosa <cristianrosa@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2011-02-15 16:09:02 +0000

    Add another SMPI bugged example to test the MC.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@9632 48e7efb5-ca39-0410-a469-dd3cf9ba447f

---
+++ examples/smpi/mc_bugged2.c
@@ -0,0 +1,47 @@
+/* A simple bugged MPI_ISend and MPI_IRecv test */
+
+/* Copyright (c) 2009, 2011. The SimGrid Team.
+ * All rights reserved.                                                     */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include <stdio.h>
+#include <mpi.h>
+#include <mc/modelchecker.h>
+
+
+int main(int argc, char **argv)
+{
+  int recv_buff, err, size, rank;
+  MPI_Status status;
+
+  /* Initialize MPI */
+  err = MPI_Init(&argc, &argv);
+  if (err != MPI_SUCCESS) {
+    printf("MPI initialization failed!\n");
+    exit(1);
+  }
+
+  err = MPI_Comm_size(MPI_COMM_WORLD, &size);   /* Get nr of tasks */
+  err = MPI_Comm_rank(MPI_COMM_WORLD, &rank);   /* Get id of this process */
+  if (size % 3 != 0) {
+    printf("run this program with exactly 3*N processes \n");
+    MPI_Finalize();
+    exit(0);
+  }
+
+  if (rank % 3 == 0) {
+    MPI_Recv(&recv_buff, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    printf("(%d) receive %d\n", rank, recv_buff);
+    MPI_Recv(&recv_buff, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    printf("(%d) receive %d\n", rank, recv_buff);
+  }else{
+    MPI_Send(&rank, 1, MPI_INT, (rank / 3) * 3, 42, MPI_COMM_WORLD);
+    printf("Sent %d to rank %d\n", rank, (rank / 3) * 3);
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
