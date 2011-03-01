simgrid/simgrid

commit 05400c7ba9980ef221912b7cee424a8e0a263866

Remove side effects from xbt_asserts calls
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@9743 48e7efb5-ca39-0410-a469-dd3cf9ba447f

src/xbt/config.c
@@ -321,7 +321,8 @@ void xbt_cfg_register_str(xbt_cfg_t * cfg, const char *entry)
   xbt_assert1(tok, "Invalid maximum in config element descriptor %s",
               entry);
 
-  xbt_assert2(*(tok++) == '_',
+  tok++;
+  xbt_assert2(*tok == '_',
               "Invalid config element descriptor: %s%s", entry,
               "; Should be <name>:<min nb>_to_<max nb>_<type>");
 

examples/msg/alias/masterslave_forwarder_with_alias.c
@@ -38,16 +38,15 @@ int master(int argc, char *argv[])
   int number_of_tasks = 0;
   double task_comp_size = 0;
   double task_comm_size = 0;
-
-
   int i;
+  int read;
 
-  xbt_assert1(sscanf(argv[1], "%d", &number_of_tasks),
-              "Invalid argument %s\n", argv[1]);
-  xbt_assert1(sscanf(argv[2], "%lg", &task_comp_size),
-              "Invalid argument %s\n", argv[2]);
-  xbt_assert1(sscanf(argv[3], "%lg", &task_comm_size),
-              "Invalid argument %s\n", argv[3]);
+  read = sscanf(argv[1], "%d", &number_of_tasks);
+  xbt_assert1(read, "Invalid argument %s\n", argv[1]);
+  read = sscanf(argv[2], "%lg", &task_comp_size);
+  xbt_assert1(read, "Invalid argument %s\n", argv[2]);
+  read = sscanf(argv[3], "%lg", &task_comm_size);
+  xbt_assert1(read, "Invalid argument %s\n", argv[3]);
 
   {
     /*  Task creation */

examples/msg/gtnets/gtnets.c
@@ -51,8 +51,9 @@ int master(int argc, char *argv[])
   }
 
   /* data size */
-  xbt_assert1(sscanf(argv[1], "%lg", &task_comm_size),
-              "Invalid argument %s\n", argv[1]);
+  int read;
+  read = sscanf(argv[1], "%lg", &task_comm_size);
+  xbt_assert1(read, "Invalid argument %s\n", argv[1]);
 
   /* slave name */
   slavename = argv[2];

examples/msg/icomms/peer.c
@@ -95,7 +95,9 @@ int receiver(int argc, char *argv[])
   XBT_INFO("sleep_start_time : %f , sleep_test_time : %f", sleep_start_time,
         sleep_test_time);
 
-  xbt_assert1(sscanf(argv[1], "%d", &id),
+  int read;
+  read = sscanf(argv[1], "%d", &id);
+  xbt_assert1(read,
               "Invalid argument %s\n", argv[1]);
 
   MSG_process_sleep(sleep_start_time);

examples/msg/masterslave/masterslave_bypass.c
@@ -164,16 +164,15 @@ int master(int argc, char *argv[])
   int number_of_tasks = 0;
   double task_comp_size = 0;
   double task_comm_size = 0;
-
-
   int i;
+  int read;
 
-  xbt_assert1(sscanf(argv[1], "%d", &number_of_tasks),
-              "Invalid argument %s\n", argv[1]);
-  xbt_assert1(sscanf(argv[2], "%lg", &task_comp_size),
-              "Invalid argument %s\n", argv[2]);
-  xbt_assert1(sscanf(argv[3], "%lg", &task_comm_size),
-              "Invalid argument %s\n", argv[3]);
+  read = sscanf(argv[1], "%d", &number_of_tasks);
+  xbt_assert1(read, "Invalid argument %s\n", argv[1]);
+  read = sscanf(argv[2], "%lg", &task_comp_size);
+  xbt_assert1(read, "Invalid argument %s\n", argv[2]);
+  read = sscanf(argv[3], "%lg", &task_comm_size);
+  xbt_assert1(read, "Invalid argument %s\n", argv[3]);
 
   {                             /*  Task creation */
     char sprintf_buffer[64];

examples/msg/masterslave/masterslave_cluster.c
@@ -36,16 +36,15 @@ int master(int argc, char *argv[])
   int number_of_tasks = 0;
   double task_comp_size = 0;
   double task_comm_size = 0;
-
-
   int i;
+  int read;
 
-  xbt_assert1(sscanf(argv[1], "%d", &number_of_tasks),
-              "Invalid argument %s\n", argv[1]);
-  xbt_assert1(sscanf(argv[2], "%lg", &task_comp_size),
-              "Invalid argument %s\n", argv[2]);
-  xbt_assert1(sscanf(argv[3], "%lg", &task_comm_size),
-              "Invalid argument %s\n", argv[3]);
+  read = sscanf(argv[1], "%d", &number_of_tasks);
+  xbt_assert1(read, "Invalid argument %s\n", argv[1]);
+  read = sscanf(argv[2], "%lg", &task_comp_size);
+  xbt_assert1(read, "Invalid argument %s\n", argv[2]);
+  read = sscanf(argv[3], "%lg", &task_comm_size);
+  xbt_assert1(read, "Invalid argument %s\n", argv[3]);
 
   {                             /*  Task creation */
     char sprintf_buffer[64];

examples/msg/masterslave/masterslave_console.c
@@ -71,9 +71,10 @@ int slave(int argc, char *argv[])
   int res;
   int id = -1;
   char mailbox[80];
+  int read;
 
-  xbt_assert1(sscanf(argv[1], "%d", &id),
-              "Invalid argument %s\n", argv[1]);
+  read = sscanf(argv[1], "%d", &id);
+  xbt_assert1(read, "Invalid argument %s\n", argv[1]);
 
   sprintf(mailbox, "slave-%d", id);
 

examples/msg/masterslave/masterslave_failure.c
@@ -35,16 +35,15 @@ int master(int argc, char *argv[])
   int number_of_tasks = 0;
   double task_comp_size = 0;
   double task_comm_size = 0;
-
-
   int i;
+  int read;
 
-  xbt_assert1(sscanf(argv[1], "%d", &number_of_tasks),
-              "Invalid argument %s\n", argv[1]);
-  xbt_assert1(sscanf(argv[2], "%lg", &task_comp_size),
-              "Invalid argument %s\n", argv[2]);
-  xbt_assert1(sscanf(argv[3], "%lg", &task_comm_size),
-              "Invalid argument %s\n", argv[3]);
+  read = sscanf(argv[1], "%d", &number_of_tasks);
+  xbt_assert1(read, "Invalid argument %s\n", argv[1]);
+  read = sscanf(argv[2], "%lg", &task_comp_size);
+  xbt_assert1(read, "Invalid argument %s\n", argv[2]);
+  read = sscanf(argv[3], "%lg", &task_comm_size);
+  xbt_assert1(read, "Invalid argument %s\n", argv[3]);
 
   {                             /* Process organisation */
     slaves_count = argc - 4;

commit 459137091dd22537f85e76a7ec119e07e3af15a8

Fix a bug introduced by r9743
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@9748 48e7efb5-ca39-0410-a469-dd3cf9ba447f

src/xbt/config.c
@@ -321,10 +321,10 @@ void xbt_cfg_register_str(xbt_cfg_t * cfg, const char *entry)
   xbt_assert1(tok, "Invalid maximum in config element descriptor %s",
               entry);
 
-  tok++;
   xbt_assert2(*tok == '_',
               "Invalid config element descriptor: %s%s", entry,
               "; Should be <name>:<min nb>_to_<max nb>_<type>");
+  tok++;
 
   for (type = 0;
        type < xbt_cfgelm_type_count