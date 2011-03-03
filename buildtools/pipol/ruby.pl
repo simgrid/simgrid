a4613a7bf0e54af07dc4cef9c7bb3dde32a3a099
navarrop <navarrop@48e7efb5-ca39-0410-a469-dd3cf9ba447f>
2011-03-03 14:03:10 +0000

    Install ruby 1.9 with script on fedora
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/simgrid/simgrid/trunk@9755 48e7efb5-ca39-0410-a469-dd3cf9ba447f

--- buildtools/pipol/ruby.pl
+++ buildtools/pipol/ruby.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 use strict;
-
+my($ruby_version);
 if( -e "/usr/bin/apt-get")
 {
-	my($ruby_version) = `apt-cache search ruby1.9.1-dev`;
+	$ruby_version = `apt-cache search ruby1.9.1-dev`;
 	
 	if( $ruby_version=~ /^$/){
 	$ruby_version = `apt-cache search ruby1.9-dev`;
@@ -25,12 +25,8 @@ if( -e "/usr/bin/apt-get")
 	my($ruby_lib)=`ls /usr/lib/lib$ruby_version.so`;
 	chomp $ruby_lib;
 	`sudo ln -sf "$ruby_lib" /usr/lib/libruby.so`;
-	
-	$ruby_version=`ruby --version`;
-	print "ruby = $ruby_version";
-	print "libruby = ".`ls /usr/lib/libruby.so`;
 }
-elsif(-e "/usr/bin/yum")
-{
-	`sudo yum -y -q install ruby-devel ruby`
-}
+
+$ruby_version=`ruby --version`;
+print "ruby = $ruby_version";
+print "libruby = ".`ls /usr/lib/libruby.so`;
