8f695a21df0785cb68d5c123d0869adf33915ad4
Navarrop <Pierre.Navarro@imag.fr>
2011-04-08 10:09:26 +0200

    Update pipol scripts

--- buildtools/pipol/Experimental.sh
+++ buildtools/pipol/Experimental.sh
@@ -14,6 +14,10 @@ git clone git://scm.gforge.inria.fr/simgrid/simgrid.git simgrid --quiet
 cd simgrid
 
 if [ x$PIPOL_IMAGE == "xamd64-windows-server-2008-64bits.dd.gz" ] ; then
+
+	export PATH=/cygdrive/c/:/cygdrive/c/GnuWin32/bin/:/cygdrive/c/Windows/system32:/cygdrive/c/Windows
+	export PATH=$PATH:/cygdrive/c/strawberry/c/bin:/cygdrive/c/strawberry/perl/site/bin:/cygdrive/c/strawberry/perl/bin
+	
 	cmake \
 	-G"Unix Makefiles" \
 	-Denable_lua=off \
--- buildtools/pipol/deploy.sh
+++ buildtools/pipol/deploy.sh
@@ -13,11 +13,7 @@ ssh pipol pipol-sub esn amd64-linux-ubuntu-lucid.dd.gz none 02:00 "~/Experimenta
 ssh pipol pipol-sub esn amd64_2010-linux-ubuntu-maverick.dd.gz none 02:00 "~/Experimental.sh"
 
 #___________________________________________________________________________________________________
-#Fedora 11__________________________________________________________________________________________
-ssh pipol pipol-sub esn i386-linux-fedora-core11.dd.gz none 02:00 "~/Experimental.sh"
-ssh pipol pipol-sub esn amd64-linux-fedora-core11.dd.gz none 02:00 "~/Experimental.sh"
-
-#Fedora 12
+#Fedora 12__________________________________________________________________________________________
 ssh pipol pipol-sub esn i386-linux-fedora-core12.dd.gz none 02:00 "~/Experimental.sh"
 ssh pipol pipol-sub esn amd64-linux-fedora-core12.dd.gz none 02:00 "~/Experimental.sh"
 
@@ -36,9 +32,9 @@ ssh pipol pipol-sub esn amd64_kvm-linux-debian-testing none 02:00 "~/Experimenta
 
 #___________________________________________________________________________________________________
 #MacOS Snow Leopard 10.6____________________________________________________________________________
-ssh pipol pipol-sub esn x86_64_mac-mac-osx-server-snow-leopard.dd.gz none 02:00 "~/Experimental.sh"
-ssh pipol pipol-sub esn x86_mac-mac-osx-server-snow-leopard.dd.gz none 02:00 "~/Experimental.sh"
+#ssh pipol pipol-sub esn x86_64_mac-mac-osx-server-snow-leopard.dd.gz none 02:00 "~/Experimental.sh"
+#ssh pipol pipol-sub esn x86_mac-mac-osx-server-snow-leopard.dd.gz none 02:00 "~/Experimental.sh"
 
 #___________________________________________________________________________________________________
 #windows-server-2008-64bits_________________________________________________________________________
-ssh pipol pipol-sub esn amd64-windows-server-2008-64bits-navarro-2011-03-15-122256.dd.gz none 02:00 "~/Experimental.sh"
+#ssh pipol pipol-sub esn amd64-windows-server-2008-64bits-navarro-2011-03-15-122256.dd.gz none 02:00 "~/Experimental.sh"
