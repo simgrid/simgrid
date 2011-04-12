6a2bdaec0bc0f5c8222cd7426692c0866bfadc45
Christophe Thiéry <christopho128@gmail.com>
2011-04-12 11:49:35 +0200

    Cosmetics

--- src/surf/surf_routing.c
+++ src/surf/surf_routing.c
@@ -4,8 +4,6 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-
-
 #include <float.h>
 #include "gras_config.h"
 
