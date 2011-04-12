f04659b6df988856be778c0221f777a6283c2855
Lucas Schnorr <Lucas.Schnorr@imag.fr>
2011-04-12 11:04:47 +0200

    cleaning graphicator include headers and code

--- tools/graphicator/graphicator.c
+++ tools/graphicator/graphicator.c
@@ -8,45 +8,23 @@
 #include <unistd.h>
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <math.h>
-
-
-#include "simdag/simdag.h"
-#include "xbt/log.h"
-#include "xbt/dict.h"
-#include "xbt/ex.h"
-#include "xbt/graph.h"
-#include "surf/surf.h"
-#include "surf/surf_private.h"
+#include "msg/msg.h"
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(graphicator,
                              "Graphicator Logging System");
 
 int main(int argc, char **argv)
 {
-  char *platformFile = NULL;
-  char *graphvizFile = NULL;
-
-  xbt_ex_t e;
-
   MSG_global_init(&argc, argv);
 
   if (argc < 3){
     XBT_INFO("Usage: %s <platform_file.xml> <graphviz_file.dot>", argv[0]);
     return 1;
   }
-  platformFile = argv[1];
-  graphvizFile = argv[2];
+  char *platformFile = argv[1];
+  char *graphvizFile = argv[2];
 
-  TRY {
-    MSG_create_environment(platformFile);
-  } CATCH(e) {
-    xbt_die("Error while loading %s: %s",platformFile,e.msg);
-  }
+  MSG_create_environment(platformFile);
 
   //creating the graph structure
   xbt_graph_t graph = TRACE_platform_graph();
