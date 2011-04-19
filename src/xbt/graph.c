3406010cd31d6455d8ab9f96b312920dac884062
Martin Quinson <martin.quinson@loria.fr>
2011-04-19 16:51:58 +0200

    Make sure we have getline when we use it

--- src/xbt/graph.c
+++ src/xbt/graph.c
@@ -7,6 +7,7 @@
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
 #include <stdlib.h>
+#include "simgrid_config.h" /* getline */
 #include "xbt/sysdep.h"
 #include "xbt/log.h"
 #include "xbt/graph.h"
