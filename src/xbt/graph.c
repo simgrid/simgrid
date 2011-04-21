1189d1797cc934d847d6641d809bbe060729f064
Martin Quinson <martin.quinson@loria.fr>
2011-04-21 14:02:11 +0200

    load simgrid_config ASAP so that _GNU_SOURCE really get defined before stdio is loaded

--- src/xbt/graph.c
+++ src/xbt/graph.c
@@ -6,8 +6,8 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include <stdlib.h>
 #include "simgrid_config.h" /* getline */
+#include <stdlib.h>
 #include "xbt/sysdep.h"
 #include "xbt/log.h"
 #include "xbt/graph.h"
