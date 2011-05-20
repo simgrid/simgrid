f3a29a7f010121ae3814dbfe16b4d81761bfc549
Arnaud Giersch <arnaud.giersch@iut-bm.univ-fcomte.fr>
2011-05-20 15:59:20 +0200

    Regenerate files in maintainer mode.

--- src/xbt/graphxml.c
+++ src/xbt/graphxml.c
@@ -1054,7 +1054,6 @@ int xbt_graph_parse__flex_debug = 0;
 #define YY_RESTORE_YY_MORE_OFFSET
 char *xbt_graph_parse_text;
 /* Validating XML processor for src/xbt/graphxml.dtd.
- * Generated 2010/12/01 17:15:43.
  *
  * This program was generated with the FleXML XML processor generator.
  * FleXML is Copyright (C) 1999-2005 Kristoffer Rose.  All rights reserved.
