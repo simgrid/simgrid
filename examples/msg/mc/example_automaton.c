855eeebe46f239ac98e86c7ce382eb278c15dd82
Marion Guthmuller <marion.guthmuller@loria.fr>
2011-06-24 17:20:26 +0200

    model-checker : example changed

--- examples/msg/mc/example_automaton.c
+++ examples/msg/mc/example_automaton.c
@@ -14,9 +14,9 @@ extern xbt_automaton_t automaton;
 
 
 int p=1;
-int r=1;
+int r=0;
 int q=1;
-int e=1;
+int e=0;
 int d=1;
 
 
@@ -55,7 +55,7 @@ int server(int argc, char *argv[])
     }
     MSG_task_receive(&task, "mymailbox");
     count++;
-    r=(r+1)%2;
+    e=(e+1)%2;
     //d=(d+1)%2;
     //XBT_INFO("r (server) = %d", r);
      
