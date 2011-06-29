b987f4a4f0426df6cbe419e90cf67e0aaf1d0aef
Navarrop <Pierre.Navarro@imag.fr>
2011-06-29 14:30:41 +0200

    Drand48 does not exist on win32

--- src/instr/instr_private.h
+++ src/instr/instr_private.h
@@ -11,6 +11,11 @@
 
 #ifdef HAVE_TRACING
 
+/* Need to define function drand48 for Windows */
+#ifdef _WIN32
+	#define drand48() rand()/(float)(RAND_MAX);
+#endif
+
 #define INSTR_DEFAULT_STR_SIZE 500
 
 #include "instr/instr.h"
