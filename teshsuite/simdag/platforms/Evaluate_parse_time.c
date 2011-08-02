e02760f1b5ad6109db83b196c33d612d0ded7408
Martin Quinson <martin.quinson@loria.fr>
2011-08-02 16:40:44 +0200

    use XBT portability layer to timing functions

--- teshsuite/simdag/platforms/Evaluate_parse_time.c
+++ teshsuite/simdag/platforms/Evaluate_parse_time.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008, 2009, 2010. The SimGrid Team.
+/* Copyright (c) 2008, 2009, 2010, 2011. The SimGrid Team.
  * All rights reserved.                                                     */
 
 /* This program is free software; you can redistribute it and/or modify it
@@ -10,49 +10,24 @@
 #include <stdlib.h>
 #include "simdag/simdag.h"
 #include "surf/surf_private.h"
-#include <time.h>
+#include "xbt/xbt_os_time.h"
 
-#define BILLION  1000000000L;
 extern routing_global_t global_routing;
 
 int main(int argc, char **argv)
 {
-	struct timespec start, stop;
-	double accum;
+  xbt_os_timer_t timer = xbt_os_timer_new();
 
-	/* initialisation of SD */
+	/* initialization of SD */
 	SD_init(&argc, argv);
 
-#if _POSIX_TIMERS > 0
-	if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) {
-	perror( "clock gettime" );
-	return EXIT_FAILURE;
-	}
-#else
-      struct timeval tv;
-      gettimeofday(&tv, NULL);
-      start.tv_sec = tv.tv_sec;
-      start.tv_nsec = tv.tv_usec*1000;
-#endif
-	/* creation of the environment */
+	/* creation of the environment, timed */
+	xbt_os_timer_start(timer);
 	SD_create_environment(argv[1]);
+  xbt_os_timer_stop(timer);
 
-#if _POSIX_TIMERS > 0
-	if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) {
-	perror( "clock gettime" );
-	return EXIT_FAILURE;
-	}
-#else
-      gettimeofday(&tv, NULL);
-      stop.tv_sec = tv.tv_sec;
-      stop.tv_nsec = tv.tv_usec*1000;
-#endif
-
-	accum = ( stop.tv_sec - start.tv_sec )
-		   + (double)( stop.tv_nsec - start.tv_nsec )
-			 / (double)BILLION;
-
-	printf( "%lf\n", accum );
+  /* Display the result and exit after cleanup */
+	printf( "%lf\n", xbt_os_timer_elapsed(timer) );
 
 	sleep(20);
 
