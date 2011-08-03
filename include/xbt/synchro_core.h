c035934bb16bebcd87127d880078145cb234ecb2
Martin Quinson <martin.quinson@loria.fr>
2011-08-03 16:09:06 +0200

    improve reference documentation

--- include/xbt/synchro_core.h
+++ include/xbt/synchro_core.h
@@ -29,44 +29,84 @@ SG_BEGIN_DECL()
  * 
  *  @{
  */
-  /** \brief Thread data type (opaque structure) */
+  /** @brief Thread data type (opaque object)
+   *  @hideinitializer
+   */
 typedef struct s_xbt_thread_ *xbt_thread_t;
 
+/** @brief Creates a new thread.
+ * 
+ * @param name          the name used in the logs for the newly created thread
+ * @param start_routine function to run
+ * @param param         parameter to pass to the function to run
+ * @param joinable      whether the new thread should be started joinable or detached
+ */
 XBT_PUBLIC(xbt_thread_t) xbt_thread_create(const char *name,
                                            void_f_pvoid_t start_routine,
                                            void *param, int joinable);
+
+/** @brief Get a reference to the currently running thread */
 XBT_PUBLIC(xbt_thread_t) xbt_thread_self(void);
+
+/** @brief Get the name of a given thread */
 XBT_PUBLIC(const char *) xbt_thread_name(xbt_thread_t t);
+
+/** @brief Get a reference to the name of the currently running thread */
 XBT_PUBLIC(const char *) xbt_thread_self_name(void);
-  /* xbt_thread_join frees the joined thread (ie the XBT wrapper around it, the OS frees the rest) */
+
+/** @brief Wait for the termination of the given thread, and free it (ie the XBT wrapper around it, the OS frees the rest) */
 XBT_PUBLIC(void) xbt_thread_join(xbt_thread_t thread);
-  /* Ends the life of the poor victim (not always working if it's computing, but working if it's blocked in the OS) */
+
+/** @brief Ends the life of the poor victim (not always working if it's computing, but working if it's blocked in the OS) */
 XBT_PUBLIC(void) xbt_thread_cancel(xbt_thread_t thread);
-  /* suicide */
+
+/** @brief commit suicide */
 XBT_PUBLIC(void) xbt_thread_exit(void);
-  /* current thread pass control to any possible thread wanting it */
+
+/** @brief the current thread passes control to any possible thread wanting it */
 XBT_PUBLIC(void) xbt_thread_yield(void);
 
 
-  /** \brief Thread mutex data type (opaque structure) */
+
+/** @brief Thread mutex data type (opaque object)
+ *  @hideinitializer
+ */
 typedef struct s_xbt_mutex_ *xbt_mutex_t;
 
+/** @brief Creates a new mutex variable */
 XBT_PUBLIC(xbt_mutex_t) xbt_mutex_init(void);
+
+/** @brief Blocks onto the given mutex variable */
 XBT_PUBLIC(void) xbt_mutex_acquire(xbt_mutex_t mutex);
+
+/** @brief Releases the given mutex variable */
 XBT_PUBLIC(void) xbt_mutex_release(xbt_mutex_t mutex);
+
+/** @brief Blocks onto the given mutex variable, but only for the given amount of time. a timeout exception is raised if it was impossible to acquire it in the given time frame */
 XBT_PUBLIC(void) xbt_mutex_timedacquire(xbt_mutex_t mutex, double delay);
+
+/** @brief Destroyes the given mutex variable */
 XBT_PUBLIC(void) xbt_mutex_destroy(xbt_mutex_t mutex);
 
 
-  /** \brief Thread condition data type (opaque structure) */
+/** @brief Thread condition data type (opaque object)
+ *  @hideinitializer
+ */
 typedef struct s_xbt_cond_ *xbt_cond_t;
 
+/** @brief Creates a condition variable */
 XBT_PUBLIC(xbt_cond_t) xbt_cond_init(void);
+
+/** @brief Blocks onto the given condition variable */
 XBT_PUBLIC(void) xbt_cond_wait(xbt_cond_t cond, xbt_mutex_t mutex);
+/** @brief Blocks onto the given condition variable, but only for the given amount of time. a timeout exception is raised if it was impossible to acquire it in the given time frame */
 XBT_PUBLIC(void) xbt_cond_timedwait(xbt_cond_t cond,
                                     xbt_mutex_t mutex, double delay);
+/** @brief Signals the given mutex variable */
 XBT_PUBLIC(void) xbt_cond_signal(xbt_cond_t cond);
+/** @brief Broadcasts the given mutex variable */
 XBT_PUBLIC(void) xbt_cond_broadcast(xbt_cond_t cond);
+/** @brief Destroys the given mutex variable */
 XBT_PUBLIC(void) xbt_cond_destroy(xbt_cond_t cond);
 
 /** @} */
