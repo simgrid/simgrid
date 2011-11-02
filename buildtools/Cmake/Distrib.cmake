4de70d5dec2df0a4a9f0f35bf275238117e3cae1
Arnaud Giersch <arnaud.giersch@iut-bm.univ-fcomte.fr>
2011-11-02 21:09:22 +0100

    Fix path for out-of-source builds.

--- buildtools/Cmake/Distrib.cmake
+++ buildtools/Cmake/Distrib.cmake
@@ -21,7 +21,7 @@ endif( NOT MANPAGE_DIR)
 
 add_custom_target(manpages ALL
         COMMAND ${CMAKE_COMMAND} -E make_directory ${MANPAGE_DIR}
-	COMMAND pod2man tools/simgrid_update_xml.pl > ${MANPAGE_DIR}/simgrid_update_xml.1
+	COMMAND pod2man ${CMAKE_HOME_DIRECTORY}/tools/simgrid_update_xml.pl > ${MANPAGE_DIR}/simgrid_update_xml.1
 	COMMENT "Generating manpages"
 )
 install(FILES ${MANPAGE_DIR}/simgrid_update_xml.1
