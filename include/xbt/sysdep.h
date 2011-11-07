2bc96c63a2cdfac701be8224bf30527aceb25c61
Arnaud Giersch <arnaud.giersch@iut-bm.univ-fcomte.fr>
2011-11-07 22:24:44 +0100

    Kill bogus fixme: xbt_free_f already exists.

--- include/xbt/sysdep.h
+++ include/xbt/sysdep.h
@@ -144,7 +144,6 @@ void *xbt_realloc(void *p, size_t s)
 /** @brief like free
     @hideinitializer */
 #define xbt_free free           /*nothing specific to do here. A poor valgrind replacement? */
-/*#define xbt_free_fct free * replacement with the guareenty of being a function  FIXME:KILLME*/
 
 /** @brief like free, but you can be sure that it is a function  */
 XBT_PUBLIC(void) xbt_free_f(void *p);
