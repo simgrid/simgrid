9553ec877f1dc96e9b44fbac31d47773e40adae3
Marion Guthmuller <marion.guthmuller@loria.fr>
2011-11-17 10:06:29 +0100

    model-checker : examples changed

--- examples/msg/mc/example_liveness_with_cycle2.c
+++ examples/msg/mc/example_liveness_with_cycle2.c
@@ -11,9 +11,9 @@
 #include "y.tab.c"
 
 #define AMOUNT_OF_CLIENTS 2
-#define CS_PER_PROCESS 4
+#define CS_PER_PROCESS 3
 
-XBT_LOG_NEW_DEFAULT_CATEGORY(example_liveness_with_cycle, "my log messages");
+XBT_LOG_NEW_DEFAULT_CATEGORY(example_liveness_with_cycle2, "my log messages");
 
 
 int p=0; 
@@ -32,8 +32,8 @@ int coordinator(int argc, char *argv[])
 {
   xbt_dynar_t requests = xbt_dynar_new(sizeof(char *), NULL);   // dynamic vector storing requests (which are char*)
   int CS_used = 0;              // initially the CS is idle
-  int todo = AMOUNT_OF_CLIENTS * CS_PER_PROCESS;        // amount of releases we are expecting
-  while (todo > 0) {
+
+  while(1){
     m_task_t task = NULL;
     MSG_task_receive(&task, "coordinator");
     const char *kind = MSG_task_get_name(task); //is it a request or a release?
@@ -58,14 +58,12 @@ int coordinator(int argc, char *argv[])
 	if(strcmp(req, "1") == 0){
 	  xbt_dynar_pop(requests, &req);
 	  MSG_task_send(MSG_task_create("grant", 0, 1000, NULL), req);
-	  todo--;
 	}else{
-	   CS_used = 0;
+	  CS_used = 0;
 	}
       } else {                  // nobody wants it
         XBT_INFO("CS release. resource now idle");
         CS_used = 0;
-        todo--;
       }
     }
     MSG_task_destroy(task);
@@ -79,34 +77,34 @@ int client(int argc, char *argv[])
   int my_pid = MSG_process_get_PID(MSG_process_self());
 
   char *my_mailbox = bprintf("%s", argv[1]);
-  int i;
+ 
+  while(1){  
 
-  // request the CS 4 times, sleeping a bit in between
-  for (i = 0; i < CS_PER_PROCESS; i++) {
-      
     XBT_INFO("Ask the request");
+   
     MSG_task_send(MSG_task_create("request", 0, 1000, my_mailbox), "coordinator");
-
+    
     if(strcmp(my_mailbox, "2") == 0){
       p = 1;
       q = 0;
-      XBT_DEBUG("Propositions changed : p=1, q=0");
+      XBT_INFO("Propositions changed (p=1, q=0)");
     }
 
     // wait the answer
+
     m_task_t grant = NULL;
     MSG_task_receive(&grant, my_mailbox);
 
-    if(strcmp(my_mailbox, "2") == 0){
+    if((strcmp(my_mailbox, "2") == 0) && (strcmp(MSG_task_get_name(grant), "grant") == 0)){
       q = 1;
       p = 0;
-      XBT_DEBUG("Propositions changed : p=0, q=1");
+      XBT_INFO("Propositions changed (q=1, p=0)");
     }
-
 
     MSG_task_destroy(grant);
     XBT_INFO("%s got the answer. Sleep a bit and release it", argv[1]);
     MSG_process_sleep(1);
+    
     MSG_task_send(MSG_task_create("release", 0, 1000, NULL), "coordinator");
 
     MSG_process_sleep(my_pid);
@@ -114,7 +112,7 @@ int client(int argc, char *argv[])
     if(strcmp(my_mailbox, "2") == 0){
       q=0;
       p=0;
-      XBT_DEBUG("Propositions changed : p=0, q=0");
+      XBT_INFO("Propositions changed (q=0, p=0)");
     }
     
   }
