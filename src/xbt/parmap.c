1c86ddc00bb20b4f3779a815f732a1c1cb14139a
Christophe Thiéry <christopho128@gmail.com>
2011-12-05 17:05:10 +0100

    Remove parmap_private.h since parmap.c is the only file using it

--- src/xbt/parmap.c
+++ src/xbt/parmap.c
@@ -14,11 +14,46 @@
 #else
 	#include "xbt/xbt_os_thread.h"
 #endif
-#include <errno.h>
-#include "parmap_private.h"
+#include "xbt/parmap.h"
+#include "xbt/log.h"
+#include "xbt/function_types.h"
+#include "xbt/dynar.h"
+#include "xbt/xbt_os_thread.h"
+#include "xbt/sysdep.h"
 
 XBT_LOG_NEW_DEFAULT_SUBCATEGORY(xbt_parmap, xbt, "parmap: parallel map");
 XBT_LOG_NEW_SUBCATEGORY(xbt_parmap_unit, xbt_parmap, "parmap unit testing");
+
+typedef enum{
+  PARMAP_WORK = 0,
+  PARMAP_DESTROY
+} e_xbt_parmap_flag_t;
+
+#ifdef HAVE_FUTEX_H
+typedef struct s_xbt_event{
+  int work;
+  int done;
+  unsigned int thread_counter;
+  unsigned int threads_to_wait;
+}s_xbt_event_t, *xbt_event_t;
+
+void xbt_event_init(xbt_event_t event);
+void xbt_event_signal(xbt_event_t event);
+void xbt_event_wait(xbt_event_t event);
+void xbt_event_end(xbt_event_t event);
+#endif
+
+typedef struct s_xbt_parmap {
+  e_xbt_parmap_flag_t status;
+#ifdef HAVE_FUTEX_H
+  xbt_event_t sync_event;
+#endif
+  unsigned int num_workers;
+  unsigned int workers_max_id;
+  void_f_pvoid_t fun;
+  xbt_dynar_t data;
+  unsigned int index;
+} s_xbt_parmap_t;
 
 static void *_xbt_parmap_worker_main(void *parmap);
 #ifdef HAVE_FUTEX_H
