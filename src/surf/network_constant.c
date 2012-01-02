f22c16b371ac671bbfa142a804a15cad2a204072
Martin Quinson <martin.quinson@loria.fr>
2012-01-02 16:04:29 +0100

    this configuration option does not exist anymore (and it was useless: the AS routing mode always superseeds it)

--- src/surf/network_constant.c
+++ src/surf/network_constant.c
@@ -231,6 +231,5 @@ void surf_network_model_init_Constant()
   netcste_define_callbacks();
   xbt_dynar_push(model_list, &surf_network_model);
 
-  xbt_cfg_set_string(_surf_cfg_set, "routing", "none");
   routing_model_create(sizeof(double), NULL);
 }
