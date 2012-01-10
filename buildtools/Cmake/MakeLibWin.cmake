121c5d3bc8a3e543d852de5f1ff6fa715b55bd51
unknown <pierre@.(none)>
2012-01-10 15:27:38 +0100

    Only use MinGW compilator

--- buildtools/Cmake/MakeLibWin.cmake
+++ buildtools/Cmake/MakeLibWin.cmake
@@ -3,24 +3,15 @@
 add_library(simgrid STATIC ${simgrid_sources})
 add_library(gras STATIC ${gras_sources})
 
-if(MSVC)
-    set_target_properties(gras     PROPERTIES COMPILE_FLAGS "/D _XBT_DLL_STATIC"
+if(CMAKE_COMPILER_IS_GNUCC)
+    set_target_properties(gras     PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_STATIC"
                                                      OUTPUT_NAME   "gras")
-    set_target_properties(simgrid  PROPERTIES COMPILE_FLAGS "/D _XBT_DLL_STATIC"
+    set_target_properties(simgrid  PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_STATIC"
                                                      OUTPUT_NAME   "simgrid")
-else(MSVC)
-    if(CMAKE_COMPILER_IS_GNUCC)
-        set_target_properties(gras     PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_STATIC"
-                                                         OUTPUT_NAME   "gras")
-        set_target_properties(simgrid  PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_STATIC"
-                                                         OUTPUT_NAME   "simgrid")
-    else(CMAKE_COMPILER_IS_GNUCC)
-        message(FATAL_ERROR "Compilateur non connu!!!")
-    endif(CMAKE_COMPILER_IS_GNUCC)
-endif(MSVC)
+endif(CMAKE_COMPILER_IS_GNUCC)
 
-set(GRAS_DEP "ws2_32")
-set(SIMGRID_DEP "ws2_32 -lpcre")
+set(GRAS_DEP "ws2_32 -lpthread")
+set(SIMGRID_DEP "ws2_32 -lpcre -lpthread")
 
 target_link_libraries(simgrid 	${SIMGRID_DEP})
 target_link_libraries(gras 	${GRAS_DEP})
