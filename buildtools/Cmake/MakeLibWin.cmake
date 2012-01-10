92b5df93e3d02736a7bc4982c2ba897a066f8d44
unknown <pierre@.(none)>
2012-01-10 18:05:36 +0100

    Make Simgrid compile with MinGW and gcc4.6 Need new flags. For now on gcc 64bits it fails so we must use gcc 32bits on 64platforms.

--- buildtools/Cmake/MakeLibWin.cmake
+++ buildtools/Cmake/MakeLibWin.cmake
@@ -3,15 +3,18 @@
 add_library(simgrid STATIC ${simgrid_sources})
 add_library(gras STATIC ${gras_sources})
 
-if(CMAKE_COMPILER_IS_GNUCC)
-    set_target_properties(gras     PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_STATIC"
+set_target_properties(gras     PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_STATIC"
                                                      OUTPUT_NAME   "gras")
-    set_target_properties(simgrid  PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_STATIC"
-                                                     OUTPUT_NAME   "simgrid")
-endif(CMAKE_COMPILER_IS_GNUCC)
+set_target_properties(simgrid  PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_STATIC"                                                  OUTPUT_NAME   "simgrid")
 
 set(GRAS_DEP "ws2_32 -lpthread")
 set(SIMGRID_DEP "ws2_32 -lpcre -lpthread")
+
+if(ARCH_32_BITS)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32 -march=i486")
+else(ARCH_32_BITS)
+        message(FATAL_ERROR "Sorry, Simgrid fails with full 64bits for now! Please contact us.")
+endif(ARCH_32_BITS)
 
 target_link_libraries(simgrid 	${SIMGRID_DEP})
 target_link_libraries(gras 	${GRAS_DEP})
