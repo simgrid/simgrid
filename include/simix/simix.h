eea3e00f521485a3753f57990a52c1121e7fb049
Christophe Thiéry <christopho128@gmail.com>
2012-01-11 13:11:53 +0100

    Remove declaration of unexisting functions

--- include/simix/simix.h
+++ include/simix/simix.h
@@ -203,9 +203,6 @@ XBT_PUBLIC(void *) SIMIX_req_comm_get_src_data(smx_action_t comm);
 XBT_PUBLIC(void *) SIMIX_req_comm_get_dst_data(smx_action_t comm);
 XBT_PUBLIC(smx_process_t) SIMIX_req_comm_get_src_proc(smx_action_t comm);
 XBT_PUBLIC(smx_process_t) SIMIX_req_comm_get_dst_proc(smx_action_t comm);
-XBT_PUBLIC(void) SIMIX_req_comm_set_copy_data_callback(void (*callback)(smx_action_t,size_t));
-XBT_PUBLIC(void) SIMIX_req_comm_copy_pointer_callback(smx_action_t comm, size_t buff_size);
-XBT_PUBLIC(void) SIMIX_req_comm_copy_buffer_callback(smx_action_t comm, size_t buff_size);
 
 #ifdef HAVE_LATENCY_BOUND_TRACKING
 XBT_PUBLIC(int) SIMIX_req_comm_is_latency_bounded(smx_action_t comm);
