feb324b7b93196bfb93ac0f7a9187f2406dca650
Marion Guthmuller <marion.guthmuller@loria.fr>
2012-01-12 09:41:14 +0100

    model-checker : prototypes of unused functions removed

--- include/mc/modelchecker.h
+++ include/mc/modelchecker.h
@@ -2,6 +2,4 @@
 
 XBT_PUBLIC(void) MC_assert(int);
 XBT_PUBLIC(void) MC_assert_stateful(int);
-XBT_PUBLIC(void) MC_assert_pair_stateful(int);
-XBT_PUBLIC(void) MC_assert_pair_stateless(int);
 XBT_PUBLIC(int) MC_random(int min, int max);
