ea2c8ccd0b9ec582382f5f546d4a7faf5e6ce0e4
Christophe Thiéry <christopho128@gmail.com>
2012-01-20 15:39:58 +0100

    Add an option --cfg=surf/nthreads:N to update Surf models in parallel

--- src/surf/surf_config.c
+++ src/surf/surf_config.c
@@ -252,6 +252,11 @@ static void _surf_cfg_cb_contexts_parallel_mode(const char *name, int pos)
   }
 }
 
+static void _surf_cfg_cb_surf_nthreads(const char *name, int pos)
+{
+  surf_set_nthreads(xbt_cfg_get_int(_surf_cfg_set, name));
+}
+
 static void _surf_cfg_cb__surf_network_coordinates(const char *name,
                                                    int pos)
 {
@@ -469,7 +474,7 @@ void surf_config_init(int *argc, char **argv)
         xbt_cfgelm_int, &default_value_int, 1, 1,
         _surf_cfg_cb_contexts_parallel_threshold, NULL);
 
-    /* minimal number of user contexts to be run in parallel */
+    /* synchronization mode for parallel user contexts */
 #ifdef HAVE_FUTEX_H
     default_value = xbt_strdup("futex");
 #else //No futex on mac and posix is unimplememted yet
@@ -479,6 +484,13 @@ void surf_config_init(int *argc, char **argv)
         "Synchronization mode to use when running contexts in parallel (either futex, posix or busy_wait)",
         xbt_cfgelm_string, &default_value, 1, 1,
         _surf_cfg_cb_contexts_parallel_mode, NULL);
+
+    /* number of parallel threads for Surf */
+    default_value_int = 1;
+    xbt_cfg_register(&_surf_cfg_set, "surf/nthreads",
+                     "Number of parallel threads used to update Surf models",
+                     xbt_cfgelm_int, &default_value_int, 1, 1,
+                     _surf_cfg_cb_surf_nthreads, NULL);
 
     default_value = xbt_strdup("no");
     xbt_cfg_register(&_surf_cfg_set, "network/coordinates",
