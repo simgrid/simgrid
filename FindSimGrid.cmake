94d78a4ad0c558d9cc03e26eddb51087d51d0a00
Martin Quinson <martin.quinson@loria.fr>
2012-01-27 21:58:27 +0100

    also fix the way we look for the simgrid library

--- FindSimGrid.cmake
+++ FindSimGrid.cmake
@@ -1,7 +1,7 @@
 #IF YOU HAVE INSTALL SIMGRID IN A SPECIAL DIRECTORY
 #YOU CAN SPECIFY SIMGRID_ROOT OR GRAS_ROOT
 
-find_library(HAVE_SIMGRID_LIB
+find_library(SIMGRID_LIB
     NAME simgrid
     HINTS
 	$ENV{SIMGRID_ROOT}
@@ -15,11 +15,11 @@ find_path(SIMGRID_INCLUDES msg.h
 )
 
 message(STATUS "Looking for lib SimGrid")
-if(HAVE_SIMGRID_LIB)
+if(SIMGRID_LIB)
   message(STATUS "Looking for lib SimGrid - found")
   if(CMAKE_CACHE_MAJOR_VERSION EQUAL "2" AND CMAKE_CACHE_MINOR_VERSION GREATER "7") #need cmake version 2.8
-	  get_filename_component(simgrid_version ${HAVE_SIMGRID_LIB} REALPATH)
-	  string(REPLACE "${HAVE_SIMGRID_LIB}." "" simgrid_version "${simgrid_version}")
+	  get_filename_component(simgrid_version ${SIMGRID_LIB} REALPATH)
+	  string(REPLACE "${SIMGRID_LIB}." "" simgrid_version "${simgrid_version}")
 	  string(REGEX MATCH "^[0-9]" SIMGRID_MAJOR_VERSION "${simgrid_version}")
 	  string(REGEX MATCH "^[0-9].[0-9]" SIMGRID_MINOR_VERSION "${simgrid_version}")
 	  string(REGEX MATCH "^[0-9].[0-9].[0-9]" SIMGRID_PATCH_VERSION "${simgrid_version}")
@@ -27,9 +27,9 @@ if(HAVE_SIMGRID_LIB)
 	  string(REGEX REPLACE "^${SIMGRID_MAJOR_VERSION}." "" SIMGRID_MINOR_VERSION "${SIMGRID_MINOR_VERSION}")
 	  message(STATUS "Simgrid version : ${SIMGRID_MAJOR_VERSION}.${SIMGRID_MINOR_VERSION}")
   endif(CMAKE_CACHE_MAJOR_VERSION EQUAL "2" AND CMAKE_CACHE_MINOR_VERSION GREATER "7")
-else(HAVE_SIMGRID_LIB)
+else(SIMGRID_LIB)
   message(STATUS "Looking for lib SimGrid - not found")
-endif(HAVE_SIMGRID_LIB)
+endif(SIMGRID_LIB)
 
 message(STATUS "Looking for msg.h")
 if(SIMGRID_INCLUDES)
@@ -38,7 +38,7 @@ else(SIMGRID_INCLUDES)
   message(STATUS "Looking for msg.h - not found")
 endif(SIMGRID_INCLUDES)
 
-if(HAVE_SIMGRID_LIB AND SIMGRID_INCLUDES)
-else(HAVE_SIMGRID_LIB AND SIMGRID_INCLUDES)
+if(SIMGRID_LIB AND SIMGRID_INCLUDES)
+else(SIMGRID_LIB AND SIMGRID_INCLUDES)
     message(FATAL_ERROR "Unable to find both the library and the include files. Setting the environment variable SIMGRID_ROOT may help.")
-endif(HAVE_SIMGRID_LIB AND SIMGRID_INCLUDES)
+endif(SIMGRID_LIB AND SIMGRID_INCLUDES)
