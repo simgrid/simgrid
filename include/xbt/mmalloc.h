c40ca1c8de2080575f5b3a0de33a6d4bf6474f10
Martin Quinson <martin.quinson@loria.fr>
2012-02-01 16:20:45 +0100

    cosmetics and comment improvement

--- include/xbt/mmalloc.h
+++ include/xbt/mmalloc.h
@@ -1,3 +1,13 @@
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+   This file was then part of the GNU C Library. */
+
+/* Copyright (c) 2010-2012. The SimGrid Team.
+ * All rights reserved.                                                     */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+
 #ifndef MMALLOC_H
 #define MMALLOC_H 1
 
@@ -7,31 +17,24 @@
 #  include <sys/types.h>        /* for size_t */
 #  include <stdio.h>            /* for NULL */
 #endif
-//#include "./include/ansidecl.h"
 
 /* Allocate SIZE bytes of memory.  */
-
 extern void *mmalloc(void *md, size_t size);
 
 /* Re-allocate the previously allocated block in void*, making the new block
    SIZE bytes long.  */
-
 extern void *mrealloc(void *md, void *ptr, size_t size);
 
 /* Allocate NMEMB elements of SIZE bytes each, all initialized to 0.  */
-
 extern void *mcalloc(void *md, size_t nmemb, size_t size);
 
 /* Free a block allocated by `mmalloc', `mrealloc' or `mcalloc'.  */
-
 extern void mfree(void *md, void *ptr);
 
 /* Allocate SIZE bytes allocated to ALIGNMENT bytes.  */
-
 extern void *mmemalign(void *md, size_t alignment, size_t size);
 
 /* Allocate SIZE bytes on a page boundary.  */
-
 extern void *mvalloc(void *md, size_t size);
 
 extern void *mmalloc_attach(int fd, void *baseaddr);
@@ -39,10 +42,6 @@ extern void *mmalloc_attach(int fd, void *baseaddr);
 extern void mmalloc_pre_detach(void *md);
 
 extern void *mmalloc_detach(void *md);
-
-extern int mmalloc_setkey(void *md, int keynum, void *key);
-
-extern void *mmalloc_getkey(void *md, int keynum);
 
 /* return the heap used when NULL is passed as first argument to any mm* function */
 extern void *mmalloc_get_default_md(void);
