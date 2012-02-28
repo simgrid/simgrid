4020ef296bdd73b30f7ca2270b14a61b1cd4fba5
navarro <navarro@caraja.(none)>
2012-02-28 18:30:14 +0100

    Fix token_ring execution.

--- examples/msg/token_ring/ring_call.c
+++ examples/msg/token_ring/ring_call.c
@@ -13,7 +13,7 @@ int host(int argc, char *argv[]);
 unsigned int task_comp_size = 50000000;
 unsigned int task_comm_size = 1000000;
 
-xbt_dynar_t hosts; /* All declared hosts */
+int nb_hosts; /* All declared hosts */
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(ring,
                              "Messages specific for this msg example");
@@ -24,7 +24,6 @@ int host(int argc, char *argv[])
   char mailbox[256];
   m_task_t task = NULL;
   _XBT_GNUC_UNUSED int res;
-
   if (host_number == 0){ //master  send then receive
     sprintf(mailbox, "%d", host_number+1);
     task = MSG_task_create("Token", task_comp_size, task_comm_size, NULL);
@@ -41,7 +40,7 @@ int host(int argc, char *argv[])
     xbt_assert(res == MSG_OK, "MSG_task_get failed");
     XBT_INFO("Host \"%d\" received \"%s\"",host_number, MSG_task_get_name(task));
 
-    if(host_number+1 == xbt_dynar_length(hosts) )
+    if(host_number+1 == nb_hosts)
       sprintf(mailbox, "0");
     else
       sprintf(mailbox, "%d", host_number+1);
@@ -53,14 +52,15 @@ int host(int argc, char *argv[])
 
 int main(int argc, char **argv)
 {
-	int i,res;
+  int i,res;
   MSG_global_init(&argc, argv);
   MSG_create_environment(argv[1]);
-  hosts =  MSG_hosts_as_dynar();
+  xbt_dynar_t hosts = MSG_hosts_as_dynar();
+  nb_hosts =  xbt_dynar_length(hosts);
   MSG_function_register("host", host);
 
-  XBT_INFO("Number of host '%zu'",xbt_dynar_length(hosts));
-  for(i = 0 ; i<xbt_dynar_length(hosts); i++)
+  XBT_INFO("Number of host '%d'",nb_hosts);
+  for(i = 0 ; i<nb_hosts; i++)
   {
     char* name_host = bprintf("%d",i);
     MSG_process_create( name_host, host, NULL, xbt_dynar_get_as(hosts,i,m_host_t) );
