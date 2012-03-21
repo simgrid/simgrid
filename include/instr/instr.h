cec5bbee980eec970e660c65ec9e81f5fbac5135
schnorr <Lucas.Schnorr@imag.fr>
2012-03-21 09:36:43 +0100

    [trace] remove deprecated function from the compilation support when tracing is disable

--- include/instr/instr.h
+++ include/instr/instr.h
@@ -65,7 +65,6 @@ XBT_PUBLIC(void) TRACE_link_srcdst_variable_sub_with_time (double time, const ch
 #define TRACE_category(category)
 #define TRACE_category_with_color(category,color)
 #define TRACE_msg_set_task_category(task,category)
-#define TRACE_msg_set_process_category(process,category,color)
 #define TRACE_smpi_set_category(category)
 #define TRACE_sd_set_task_category(task,category)
 
