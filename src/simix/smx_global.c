faadab8061f6f30c378abc524b586b76381ef7c6
schnorr <Lucas.Schnorr@imag.fr>
2012-03-27 16:48:26 +0200

    cosmetics, fix indentation

--- src/simix/smx_global.c
+++ src/simix/smx_global.c
@@ -334,33 +334,33 @@ void SIMIX_display_process_status(void)
       const char* action_description = "unknown";
       switch (process->waiting_action->type) {
 
-	case SIMIX_ACTION_EXECUTE:
-	  action_description = "execution";
-	  break;
+      case SIMIX_ACTION_EXECUTE:
+        action_description = "execution";
+        break;
 
-	case SIMIX_ACTION_PARALLEL_EXECUTE:
-	  action_description = "parallel execution";
-	  break;
+      case SIMIX_ACTION_PARALLEL_EXECUTE:
+        action_description = "parallel execution";
+        break;
 
-	case SIMIX_ACTION_COMMUNICATE:
-	  action_description = "communication";
-	  break;
+      case SIMIX_ACTION_COMMUNICATE:
+        action_description = "communication";
+        break;
 
-	case SIMIX_ACTION_SLEEP:
-	  action_description = "sleeping";
-	  break;
+      case SIMIX_ACTION_SLEEP:
+        action_description = "sleeping";
+        break;
 
-	case SIMIX_ACTION_SYNCHRO:
-	  action_description = "synchronization";
-	  break;
+      case SIMIX_ACTION_SYNCHRO:
+        action_description = "synchronization";
+        break;
 
-	case SIMIX_ACTION_IO:
-	  action_description = "I/O";
-	  break;
+      case SIMIX_ACTION_IO:
+        action_description = "I/O";
+        break;
       }
       XBT_INFO("Process %lu (%s@%s): waiting for %s action %p (%s) in state %d to finish",
           process->pid, process->name, process->smx_host->name,
-	  action_description, process->waiting_action,
+          action_description, process->waiting_action,
           process->waiting_action->name, (int)process->waiting_action->state);
     }
     else {
