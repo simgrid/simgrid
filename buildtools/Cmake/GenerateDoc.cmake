5a25b8089e4305ec691c460487a77e5c1d07aeb8
Laurent Bobelin <lbobelin@mintcar.lip.ens-lyon.fr>
2012-03-29 10:10:58 +0200

    Added a platform desc doc. Some infos are still missing, but I have to check them with some qualified guys ...

--- buildtools/Cmake/GenerateDoc.cmake
+++ buildtools/Cmake/GenerateDoc.cmake
@@ -84,6 +84,7 @@ if(DOXYGEN_PATH AND FIG2DEV_PATH AND BIBTEX2HTML_PATH AND GOOD_BIBTEX2HTML_VERSI
 		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_HOME_DIRECTORY}/doc/webcruft/Paje_MSG_screenshot.jpg     ${CMAKE_HOME_DIRECTORY}/doc/html/
 		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_HOME_DIRECTORY}/doc/triva-graph_configuration.png        ${CMAKE_HOME_DIRECTORY}/doc/html/
 		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_HOME_DIRECTORY}/doc/triva-graph_visualization.png        ${CMAKE_HOME_DIRECTORY}/doc/html/
+		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_HOME_DIRECTORY}/doc/AS_hierarchy.png        ${CMAKE_HOME_DIRECTORY}/doc/html/		
 		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_HOME_DIRECTORY}/doc/simgrid.css                          ${CMAKE_HOME_DIRECTORY}/doc/html/
 	)
 	
@@ -91,7 +92,7 @@ if(DOXYGEN_PATH AND FIG2DEV_PATH AND BIBTEX2HTML_PATH AND GOOD_BIBTEX2HTML_VERSI
 	    COMMAND ${CMAKE_COMMAND} -E echo "XX First Doxygen pass"
 		COMMAND ${DOXYGEN_PATH}/doxygen Doxyfile
 		COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/index_create.pl simgrid.tag index-API.doc
-		COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/toc_create.pl pls.doc index.doc FAQ.doc gtut-introduction.doc install.doc bindings.doc options.doc tracing.doc
+		COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/toc_create.pl pls.doc index.doc FAQ.doc gtut-introduction.doc install.doc bindings.doc options.doc tracing.doc platform.doc
 		
 		COMMAND ${CMAKE_COMMAND} -E echo "XX Second Doxygen pass"
 		COMMAND ${DOXYGEN_PATH}/doxygen Doxyfile
