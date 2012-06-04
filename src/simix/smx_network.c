simgrid/simgrid

commit 13b40eda22ea85f68f80b623a5cd441b8dc49d71

make it clear that this is a boolean field

src/simix/smx_private.h
@@ -140,7 +140,7 @@ typedef struct s_smx_action {
       void *dst_buff;
       size_t src_buff_size;
       size_t *dst_buff_size;
-      char copied;
+      int copied:1;                   /* whether the data were already copied */
 
       void* src_data;                     /* User data associated to communication */
       void* dst_data;

commit 1377c28e7b9f315616ee6fddc6baa755e0380e75

A boolean should be unsigned if you want to compare it to 1.
Without the "unsigned" keyword, the boolean could be either 0 or -1.
This made the Lua tests crash because of a condition "copied == 1".
This bug was introduced by commit 13b40eda ("make it clear that this is a
boolean field").

src/simix/smx_network.c
@@ -858,7 +858,7 @@ void SIMIX_comm_copy_data(smx_action_t comm)
 {
   size_t buff_size = comm->comm.src_buff_size;
   /* If there is no data to be copy then return */
-  if (!comm->comm.src_buff || !comm->comm.dst_buff || comm->comm.copied == 1)
+  if (!comm->comm.src_buff || !comm->comm.dst_buff || comm->comm.copied)
     return;
 
   XBT_DEBUG("Copying comm %p data from %s (%p) -> %s (%p) (%zu bytes)",

src/simix/smx_private.h
@@ -140,7 +140,7 @@ typedef struct s_smx_action {
       void *dst_buff;
       size_t src_buff_size;
       size_t *dst_buff_size;
-      int copied:1;                   /* whether the data were already copied */
+      unsigned int copied:1;          /* whether the data were already copied */
 
       void* src_data;                     /* User data associated to communication */
       void* dst_data;