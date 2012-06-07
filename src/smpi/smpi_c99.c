55be59cf67a26f643bb72a9d92af8b52f75b332c
pierre <pierre@chontal.imag.fr>
2012-06-07 15:59:08 +0200

    Hack to compile smpi on Mac (but need to fix;)

--- src/smpi/smpi_c99.c
+++ src/smpi/smpi_c99.c
@@ -12,5 +12,13 @@ static void smpi_free_static(int status, void* arg) {
 }
 
 void smpi_register_static(void* arg) {
+
+#ifndef APPLE
+// FIXME
+// On Apple this error occurs:
+//	Undefined symbols for architecture x86_64:
+//	  "_on_exit", referenced from:
+//	      _smpi_register_static in smpi_c99.c.o
    on_exit(&smpi_free_static, arg);
+#endif
 }
