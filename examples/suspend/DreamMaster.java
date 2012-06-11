bc6b026eff4c569bdcc80dd4afda9c5d5530d5bb
Samuel Lepetit <samuel.lepetit@inria.fr>
2012-06-11 17:45:31 +0200

    Add suspend example/test

---
+++ examples/suspend/DreamMaster.java
@@ -0,0 +1,21 @@
+package suspend;
+
+import org.simgrid.msg.Host;
+import org.simgrid.msg.Msg;
+import org.simgrid.msg.Process;
+import org.simgrid.msg.MsgException;
+public class DreamMaster extends Process {
+	public DreamMaster(Host host, String name, String[]args) {
+		super(host,name,args);
+	} 
+	public void main(String[] args) throws MsgException {
+		Msg.info("Let's create a lazy guy.");
+		Process lazyGuy = new LazyGuy(getHost(),"Lazy",null);
+		lazyGuy.start();
+		Msg.info("Let's wait a little bit...");
+		waitFor(10);
+		Msg.info("Let's wake the lazy guy up! >:) BOOOOOUUUHHH!!!!");
+		lazyGuy.restart();
+		Msg.info("OK, goodbye now.");
+	}
+}
