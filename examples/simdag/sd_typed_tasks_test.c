54775a76fadd0bca35aace3d5456a251205162f0
suter <frederic.suter@cc.in2p3.fr>
2012-06-20 14:55:56 +0200

    make use of the newly introduced MxN data redistribution and actually autoschedule one such task

--- examples/simdag/sd_typed_tasks_test.c
+++ examples/simdag/sd_typed_tasks_test.c
@@ -19,7 +19,8 @@ int main(int argc, char **argv)
   unsigned int ctr;
   const char *platform_file;
   const SD_workstation_t *workstations;
-  SD_task_t task, taskA, taskB, taskC, taskD, taskE;
+  SD_task_t task, seq_comp1, e2e_comm, seq_comp2;
+  SD_task_t par_comp1, redist, par_comp2, par_comp3;
   xbt_dynar_t changed_tasks;
 
   double computation_amount[4];
@@ -48,28 +49,34 @@ int main(int argc, char **argv)
        SD_workstation_get_power(workstations[i]));
 
   /* creation of some typed tasks and their dependencies */
-  taskA = SD_task_create_comp_seq("Task A", NULL, 1e9);
-  taskB = SD_task_create_comm_e2e("Task B", NULL, 1e7);
-  taskC = SD_task_create_comp_seq("Task C", NULL, 1e9);
-  taskD = SD_task_create_comp_par_amdahl("Task D", NULL, 1e9, 0.2);
-  taskE = SD_task_create("Task E", NULL, 1e9);
+  seq_comp1 = SD_task_create_comp_seq("Seq. comp. 1", NULL, 1e9);
+  e2e_comm = SD_task_create_comm_e2e("E2E comm.", NULL, 1e7);
+  seq_comp2 = SD_task_create_comp_seq("Seq. comp 2.", NULL, 1e9);
+  par_comp1 = SD_task_create_comp_par_amdahl("Par. Comp. 1", NULL, 1e9, 0.2);
+  redist = SD_task_create_comm_par_mxn_1d_block("MxN redist", NULL, 1.2e8);
+  par_comp2 = SD_task_create_comp_par_amdahl("Par. Comp. 2", NULL, 3e8, 0.5);
 
-  SD_task_dependency_add(NULL, NULL, taskA, taskB);
-  SD_task_dependency_add(NULL, NULL, taskB, taskC);
+  par_comp3 = SD_task_create("Par. Comp. 3", NULL, 1e9);
 
-  SD_task_schedulel(taskA, 1, workstations[8]);
-  SD_task_schedulel(taskC, 1, workstations[9]);
+  SD_task_dependency_add(NULL, NULL, seq_comp1, e2e_comm);
+  SD_task_dependency_add(NULL, NULL, e2e_comm, seq_comp2);
 
-  SD_task_distribute_comp_amdhal(taskD, 4);
-  SD_task_schedulev(taskD, 4, workstations);
+  SD_task_dependency_add(NULL, NULL, par_comp1, redist);
+  SD_task_dependency_add(NULL, NULL, redist, par_comp2);
+
+  SD_task_schedulel(seq_comp1, 1, workstations[8]);
+  SD_task_schedulel(seq_comp2, 1, workstations[9]);
+
+  SD_task_schedulev(par_comp1, 4, workstations);
+  SD_task_schedulev(par_comp2, 3, workstations);
 
   for (i=0;i<4;i++){
     workstation_list[i]=workstations[i+4];
     /* Apply Amdahl's law manually assuming a 20% serial part */
-    computation_amount[i]=(0.2 + (1 - 0.2)/4) * SD_task_get_amount(taskE);
+    computation_amount[i]=(0.2 + (1 - 0.2)/4) * SD_task_get_amount(par_comp3);
   }
 
-  SD_task_schedule(taskE, 4, workstation_list,
+  SD_task_schedule(par_comp3, 4, workstation_list,
                    computation_amount, communication_amount, -1);
 
   changed_tasks = SD_simulate(-1.0);
@@ -77,17 +84,8 @@ int main(int argc, char **argv)
     XBT_INFO("Task '%s' start time: %f, finish time: %f",
           SD_task_get_name(task),
           SD_task_get_start_time(task), SD_task_get_finish_time(task));
+    SD_task_destroy(task);
   }
-
-  XBT_DEBUG("Destroying tasks...");
-
-  SD_task_destroy(taskA);
-  SD_task_destroy(taskB);
-  SD_task_destroy(taskC);
-  SD_task_destroy(taskD);
-  SD_task_destroy(taskE);
-
-  XBT_DEBUG("Tasks destroyed. Exiting SimDag...");
 
   SD_exit();
   return 0;
