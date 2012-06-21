a26485609e54f8f4cfe82e9e1d7364431654830a
Marion Guthmuller <marion.guthmuller@loria.fr>
2012-06-21 11:34:27 +0200

    model-checker : factorization of code for model-checking of safety properties in stateless or stateful

--- src/msg/msg_global.c
+++ src/msg/msg_global.c
@@ -117,21 +117,6 @@ MSG_error_t MSG_main(void)
   return MSG_OK;
 }
 
-MSG_error_t MSG_main_stateful(void)
-{
-  /* Clean IO before the run */
-  fflush(stdout);
-  fflush(stderr);
-
-  if (MC_IS_ENABLED) {
-    MC_modelcheck_stateful();
-  }
-  else {
-    SIMIX_run();
-  }
-  return MSG_OK;
-}
-
 
 MSG_error_t MSG_main_liveness(xbt_automaton_t a)
 {
