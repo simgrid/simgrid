1601d48185d2455b18eec9230869d3d6dbc486c5
Martin Quinson <martin.quinson@loria.fr>
2012-10-24 10:13:38 +0200

    make it work on x86 too. Other arches are still broken for MC and this is wrong

--- buildtools/Cmake/MakeLib.cmake
+++ buildtools/Cmake/MakeLib.cmake
@@ -117,10 +117,13 @@ endif(HAVE_GTNETS)
 if(HAVE_MC)
   # The availability of libunwind was checked in CompleteInFiles.cmake
   #   (that includes FindLibunwind.cmake), so simply load it now.
+  
+  # This supposes that the host machine is either an AMD or a X86.
+  # This is deeply wrong, and should be fixed by manually loading -lunwind-PLAT (FIXME)
   if(PROCESSOR_x86_64)
     SET(SIMGRID_DEP "${SIMGRID_DEP} -lunwind-x86_64")
-  else(PROCESSOR_x86_64)
-    SET(SIMGRID_DEP "${SIMGRID_DEP} -lunwind")
+  else(PROCESSOR_x86_64)    
+    SET(SIMGRID_DEP "${SIMGRID_DEP} -lunwind-x86")
   endif(PROCESSOR_x86_64)
 endif(HAVE_MC)
 
