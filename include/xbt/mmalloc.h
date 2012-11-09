7e02819630f97eb2bd43ff422d156cc8a60929c5
Marion Guthmuller <marion.guthmuller@loria.fr>
2012-11-09 18:33:19 +0100

    model-checker : getter function for chunks used in heap

--- include/xbt/mmalloc.h
+++ include/xbt/mmalloc.h
@@ -70,6 +70,8 @@ void mmalloc_backtrace_display(void *addr);
 
 int is_free_area(void *area, xbt_mheap_t heap);
 
+size_t mmalloc_get_chunks_used(xbt_mheap_t);
+
 
 
 #endif                          /* MMALLOC_H */
