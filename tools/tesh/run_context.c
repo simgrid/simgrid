fc4b0d44a9da64f74e4df9f56d8f1d5dd3a2bad4
Arnaud Giersch <arnaud.giersch@iut-bm.univ-fcomte.fr>
2012-11-22 16:44:49 +0100

    Make tesh build (?).

--- tools/tesh/run_context.c
+++ tools/tesh/run_context.c
@@ -79,7 +79,7 @@ void rctx_init(void)
   struct sigaction newact;
   int i;
   fg_job = 0;
-  bg_jobs = xbt_dynar_new_sync(sizeof(rctx_t), kill_it);
+  bg_jobs = xbt_dynar_new(sizeof(rctx_t), kill_it);
   armageddon_mutex = xbt_os_mutex_init();
   armageddon_initiator = NULL;
   sigwaiter_mutex = xbt_os_mutex_init();
