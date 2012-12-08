2aac52ff8c0f69f5cdbacb1728a8a1119469586b
Martin Quinson <martin.quinson@loria.fr>
2012-12-08 12:18:07 +0100

    the ref guide is not separated anymore

--- buildtools/Cmake/GenerateDoc.cmake
+++ buildtools/Cmake/GenerateDoc.cmake
@@ -77,13 +77,9 @@ add_custom_target(sync-gforge-doc
   COMMAND chmod g+rw -R doc/
   COMMAND chmod a+rX -R doc/
   COMMAND ssh scm.gforge.inria.fr mkdir -p /home/groups/simgrid/htdocs/simgrid/${release_version}/html/ || true
-  COMMAND ssh scm.gforge.inria.fr mkdir -p /home/groups/simgrid/htdocs/simgrid/${release_version}/ref_guide/html/ || true
 
   COMMAND rsync --verbose --cvs-exclude --compress --delete --delete-excluded --rsh=ssh --ignore-times --recursive --links --perms --times --omit-dir-times
   doc/html/ scm.gforge.inria.fr:/home/groups/simgrid/htdocs/simgrid/${release_version}/doc/ || true
-
-  COMMAND rsync --verbose --cvs-exclude --compress --delete --delete-excluded --rsh=ssh --ignore-times --recursive --links --perms --times --omit-dir-times
-  doc/ref_guide/html/ scm.gforge.inria.fr:/home/groups/simgrid/htdocs/simgrid/${release_version}/ref_guide/html || true
 
   COMMAND scp doc/html/simgrid_modules2.png doc/html/simgrid_modules.png doc/webcruft/simgrid_logo_2011.png
   doc/webcruft/simgrid_logo_2011_small.png scm.gforge.inria.fr:/home/groups/simgrid/htdocs/simgrid/${release_version}/
