12ab8155c115cf98d7c07683d2e70604543368d5
Arnaud Giersch <arnaud.giersch@iut-bm.univ-fcomte.fr>
2013-01-30 10:33:13 +0100

    Add more missing files to the archive.

--- buildtools/Cmake/DefinePackages.cmake
+++ buildtools/Cmake/DefinePackages.cmake
@@ -766,6 +766,7 @@ set(CMAKE_SOURCE_FILES
   buildtools/Cmake/test_prog/prog_AC_CHECK_MCSC.c
   buildtools/Cmake/test_prog/prog_getline.c
   buildtools/Cmake/test_prog/prog_gnu_dynlinker.c
+  buildtools/Cmake/test_prog/prog_gtnets.cpp
   buildtools/Cmake/test_prog/prog_mutex_timedlock.c
   buildtools/Cmake/test_prog/prog_printf_null.c
   buildtools/Cmake/test_prog/prog_sem_init.c
