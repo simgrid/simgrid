a1c9cce9c0a7343dbb4df4dbede6ac58552cee56
Arnaud Giersch <arnaud.giersch@iut-bm.univ-fcomte.fr>
2013-02-05 16:22:51 +0100

    [pipol] Remove tests for simgrid-java.

--- buildtools/pipol/Nightly_simgrid.sh
+++ buildtools/pipol/Nightly_simgrid.sh
@@ -101,19 +101,6 @@ ctest -D NightlySubmit
 export SIMGRID_ROOT=`pwd`
 export LD_LIBRARY_PATH=`pwd`/lib
 export DYLD_LIBRARY_PATH=`pwd`/lib #for mac
-cd ..
-
-git clone git://scm.gforge.inria.fr/simgrid/simgrid-java.git simgrid-java --quiet
-cd simgrid-java
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:`pwd`/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/lib #for mac
-
-cmake .
-ctest -D NightlyStart
-ctest -D NightlyConfigure
-ctest -D NightlyBuild
-ctest -D NightlyTest
-ctest -D NightlySubmit
 
 cd ../
 git clone git://scm.gforge.inria.fr/simgrid/simgrid-ruby.git simgrid-ruby --quiet
