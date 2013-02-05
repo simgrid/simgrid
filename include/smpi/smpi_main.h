edf41bc65fbed038733c540706f9db856be2c80d
Augustin Degomme <degomme@idpann.imag.fr>
2013-02-05 18:19:47 +0100

    rename main function for windows

--- include/smpi/smpi_main.h
+++ include/smpi/smpi_main.h
@@ -1,6 +1,6 @@
 #define main smpi_simulated_main__(int argc, char **argv);\
 int main(int argc, char **argv){\
-MAIN__(&smpi_simulated_main__,&argc,argv);\
+smpi_main(&smpi_simulated_main__,argc,argv);\
 return 0;\
 }\
 int smpi_simulated_main__
