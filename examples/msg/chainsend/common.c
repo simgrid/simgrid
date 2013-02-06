bdddc6b8ce75aa2137ff203fb39f4fdbb2dcc483
Arnaud Giersch <arnaud.giersch@iut-bm.univ-fcomte.fr>
2013-02-06 21:26:17 +0100

    MSG_comm_get_status must be called before MSG_comm_destroy.

--- examples/msg/chainsend/common.c
+++ examples/msg/chainsend/common.c
@@ -10,8 +10,8 @@ int process_pending_connections(xbt_dynar_t q)
   xbt_dynar_foreach(q, iter, comm) {
     empty = 1;
     if (MSG_comm_test(comm)) {
-      MSG_comm_destroy(comm);
       status = MSG_comm_get_status(comm);
+      MSG_comm_destroy(comm);
       xbt_assert(status == MSG_OK, "process_pending_connections() failed");
       xbt_dynar_cursor_rm(q, &iter);
       empty = 0;
