229835cacb9004cc39e3d2b4246dc081d03ebca5
Augustin Degomme <degomme@idpann.imag.fr>
2013-02-19 16:51:29 +0100

    force lib names to stop changing when activating smpi on windows

--- buildtools/Cmake/MakeLibWin.cmake
+++ buildtools/Cmake/MakeLibWin.cmake
@@ -1,8 +1,10 @@
 ### Make Libs
 #>gcc c:\simgrid-trunk\examples\msg\icomms\peer.c -static -Lc:\simgrid-trunk\lib -lsimgrid -Ic:\simgrid-trunk\include -lwsock32
+
+
 add_library(simgrid SHARED ${simgrid_sources})
 
-set_target_properties(simgrid  PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_EXPORT -DDLL_EXPORT" LINK_FLAGS "-shared" VERSION ${libsimgrid_version} OUTPUT_NAME "simgrid" IMPORT_PREFIX "lib" IMPORT_SUFFIX ".dll")
+set_target_properties(simgrid  PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_EXPORT -DDLL_EXPORT" LINK_FLAGS "-shared" VERSION ${libsimgrid_version} PREFIX "lib" SUFFIX ".dll" IMPORT_PREFIX "lib" IMPORT_SUFFIX ".dll")
 
 
 # libpthreadGC2.dll
@@ -24,8 +26,6 @@ endif()
 
 set(SIMGRID_DEP "-lws2_32 -L${PATH_PCRE_LIB} -L${PATH_PTHREAD_LIB} -lm -lpcre -lpthreadGC2")
 
-#set(SMPI_DEP "${LIBRARY_OUTPUT_PATH}/libsimgrid.dll")
-
 if(ARCH_32_BITS)
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32 -march=i486")
 else()
@@ -39,11 +39,16 @@ find_path(PEXPORTS_PATH NAMES pexports.exe PATHS NO_DEFAULT_PATHS)
 message(STATUS "pexports: ${PEXPORTS_PATH}")
 if(PEXPORTS_PATH)
   add_custom_command(TARGET simgrid POST_BUILD
-    COMMAND ${PEXPORTS_PATH}/pexports.exe ${CMAKE_BINARY_DIR}/lib/simgrid.dll > ${CMAKE_BINARY_DIR}/lib/simgrid.def)
+    COMMAND ${PEXPORTS_PATH}/pexports.exe ${CMAKE_BINARY_DIR}/lib/libsimgrid.dll > ${CMAKE_BINARY_DIR}/lib/libsimgrid.def)
 endif()
 
 if(enable_smpi)
+  set(SMPI_DEP "${LIBRARY_OUTPUT_PATH}/libsimgrid.dll")
   add_library(smpi SHARED ${SMPI_SRC})
-  set_target_properties(smpi PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_EXPORT -DDLL_EXPORT" LINK_FLAGS "-shared" VERSION ${libsmpi_version} OUTPUT_NAME "smpi" IMPORT_SUFFIX ".dll")
-  target_link_libraries(smpi simgrid)
+  set_target_properties(smpi PROPERTIES COMPILE_FLAGS "-D_XBT_DLL_EXPORT -DDLL_EXPORT" LINK_FLAGS "-shared" VERSION ${libsmpi_version} PREFIX "lib" SUFFIX ".dll" IMPORT_PREFIX "lib" IMPORT_SUFFIX ".dll")
+  target_link_libraries(smpi ${SMPI_DEP})
+  if(PEXPORTS_PATH)
+    add_custom_command(TARGET smpi POST_BUILD
+     COMMAND ${PEXPORTS_PATH}/pexports.exe ${CMAKE_BINARY_DIR}/lib/libsmpi.dll > ${CMAKE_BINARY_DIR}/lib/libsmpi.def)
+  endif()
 endif()
