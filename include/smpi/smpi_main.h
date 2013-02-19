69a014268d6f2fe30237d083f006edb7d4d428fa
Augustin Degomme <degomme@idpann.imag.fr>
2013-02-19 16:54:26 +0100

    change name of the real executed main function to a windows specific one

--- include/smpi/smpi_main.h
+++ include/smpi/smpi_main.h
@@ -1,6 +1,6 @@
-#define main smpi_simulated_main__(int argc, char **argv);\
+#define main smpi_windows_main__(int argc, char **argv);\
 int main(int argc, char **argv){\
-smpi_main(&smpi_simulated_main__,argc,argv);\
+smpi_main(&smpi_windows_main__,argc,argv);\
 return 0;\
 }\
-int smpi_simulated_main__
+int smpi_windows_main__
