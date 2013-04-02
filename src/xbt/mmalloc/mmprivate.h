23244e6abc0ffce10651b283c8955a63b86fe303
Marion Guthmuller <marion.guthmuller@loria.fr>
2013-04-02 14:05:10 +0200

    model-checker : signed int instead of unsigned int

--- src/xbt/mmalloc/mmprivate.h
+++ src/xbt/mmalloc/mmprivate.h
@@ -156,7 +156,7 @@ typedef struct {
       size_t nfree;               /* Free fragments in a fragmented block.  */
       ssize_t frag_size[MAX_FRAGMENT_PER_BLOCK];
       //void *bt[MAX_FRAGMENT_PER_BLOCK][XBT_BACKTRACE_SIZE]; /* Where it was malloced (or realloced lastly) */
-      unsigned int ignore[MAX_FRAGMENT_PER_BLOCK];
+      int ignore[MAX_FRAGMENT_PER_BLOCK];
       heap_area_t equal_to[MAX_FRAGMENT_PER_BLOCK];
     } busy_frag;
     struct {
@@ -164,7 +164,7 @@ typedef struct {
       size_t busy_size; /* Actually used space, in bytes */
       //void *bt[XBT_BACKTRACE_SIZE]; /* Where it was malloced (or realloced lastly) */
       //int bt_size;
-      unsigned int ignore;
+      int ignore;
       heap_area_t equal_to;
     } busy_block;
     /* Heap information for a free block (that may be the first of a free cluster).  */
