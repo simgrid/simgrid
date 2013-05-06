c94ebf727d3bac66028b7130aab3ee668b1949c0
Paul Bédaride <paul.bedaride@gmail.com>
2013-05-06 14:36:15 +0200

    Fix to handle warnings+optimizations on CI [#15544]

--- include/surf/surfxml_parse.h
+++ include/surf/surfxml_parse.h
@@ -24,6 +24,10 @@ XBT_PUBLIC(void) surf_parse_error(const char *msg,...) _XBT_GNUC_PRINTF(1,2) _XB
 XBT_PUBLIC(void) surf_parse_warn(const char *msg,...) _XBT_GNUC_PRINTF(1,2);
 XBT_PUBLIC(double) surf_parse_get_double(const char *string);
 XBT_PUBLIC(int) surf_parse_get_int(const char *string);
+XBT_PUBLIC(double) surf_parse_get_time(const char *string);
+XBT_PUBLIC(double) surf_parse_get_bandwidth(const char *string);
+XBT_PUBLIC(double) surf_parse_get_power(const char *string);
+
 /* Prototypes of the functions offered by flex */
 XBT_PUBLIC(int) surf_parse_lex(void);
 XBT_PUBLIC(int) surf_parse_get_lineno(void);
