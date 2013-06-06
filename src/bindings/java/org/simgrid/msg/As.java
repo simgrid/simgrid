e50b37c2b4b53892d701c4f8076ef14232a212c0
Paul Bédaride <paul.bedaride@gmail.com>
2013-06-06 21:51:32 +0200

    Add missing files

---
+++ src/bindings/java/org/simgrid/msg/As.java
@@ -0,0 +1,40 @@
+/*
+ * Bindings to the MSG hosts
+ *
+ * Copyright 2006-2012 The SimGrid Team           
+ * All right reserved. 
+ *
+ * This program is free software; you can redistribute 
+ * it and/or modify it under the terms of the license 
+ *(GNU LGPL) which comes with this package. 
+ *
+ */  
+package org.simgrid.msg;
+
+public class As {
+
+    private long bind;
+    
+    protected As() {
+	};
+
+    
+    public long getBind() { return bind; }
+
+    public String toString (){
+	return this.getName(); 
+    }
+    public native String getName();
+
+    public native As[] getSons();
+
+    public native String getProperty(String name);
+
+    	/**
+	 * Class initializer, to initialize various JNI stuff
+	 */
+	public static native void nativeInit();
+	static {
+		nativeInit();
+	}
+}
