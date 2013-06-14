5283ccec7776f1a878edba1172fd837e373a8dfb
Augustin Degomme <degomme@idpann.imag.fr>
2013-06-14 09:06:23 +0200

    wrong file name in cmake file

--- buildtools/Cmake/CompleteInFiles.cmake
+++ buildtools/Cmake/CompleteInFiles.cmake
@@ -858,7 +858,7 @@ else()
   configure_file(${CMAKE_HOME_DIRECTORY}/examples/smpi/replay/actions_waitall.txt ${CMAKE_BINARY_DIR}/examples/smpi/replay/actions_waitall.txt COPYONLY)
   configure_file(${CMAKE_HOME_DIRECTORY}/examples/smpi/replay/actions_reducescatter.txt ${CMAKE_BINARY_DIR}/examples/smpi/replay/actions_reducescatter.txt COPYONLY)
   configure_file(${CMAKE_HOME_DIRECTORY}/examples/smpi/replay/actions_gather.txt ${CMAKE_BINARY_DIR}/examples/smpi/replay/actions_gather.txt COPYONLY)
-  configure_file(${CMAKE_HOME_DIRECTORY}/examples/smpi/replay/actions_allgather.txt ${CMAKE_BINARY_DIR}/examples/smpi/replay/actions_allgather.txt COPYONLY)
+  configure_file(${CMAKE_HOME_DIRECTORY}/examples/smpi/replay/actions_allgatherv.txt ${CMAKE_BINARY_DIR}/examples/smpi/replay/actions_allgatherv.txt COPYONLY)
   configure_file(${CMAKE_HOME_DIRECTORY}/teshsuite/smpi/hostfile ${CMAKE_BINARY_DIR}/teshsuite/smpi/hostfile COPYONLY)
 
   set(generated_files_to_clean
