20bd6775d978362f99c47ba22aef63540f771b98
suter <frederic.suter@cc.in2p3.fr>
2013-07-10 14:28:00 +0200

    add missing prototype declarations

--- include/msg/msg.h
+++ include/msg/msg.h
@@ -87,6 +87,7 @@ XBT_PUBLIC(void) MSG_file_dump(msg_file_t fd);
 
 XBT_PUBLIC(int) MSG_file_unlink(msg_file_t fd);
 XBT_PUBLIC(xbt_dict_t) MSG_file_ls(const char *mount, const char *path);
+XBT_PUBLIC(size_t) MSG_storage_get_free_size(msg_storage_t sd);
 
 /************************** AS Router handling ************************************/
 XBT_PUBLIC(const char *) MSG_as_router_get_property_value(const char* asr, const char *name);
