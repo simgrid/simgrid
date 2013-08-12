586c2c145fe2ad01beca9945d823f71f6887a8b9
Marion Guthmuller <marion.guthmuller@loria.fr>
2013-08-12 23:25:22 +0200

    get size of struct s_xbt_os_timer

--- src/include/xbt/xbt_os_time.h
+++ src/include/xbt/xbt_os_time.h
@@ -20,10 +20,11 @@ XBT_PUBLIC(double) xbt_os_time(void);
 XBT_PUBLIC(void) xbt_os_sleep(double sec);
 
 typedef struct s_xbt_os_timer *xbt_os_timer_t;
+
 XBT_PUBLIC(xbt_os_timer_t) xbt_os_timer_new(void);
 XBT_PUBLIC(void) xbt_os_timer_free(xbt_os_timer_t timer);
 XBT_PUBLIC(double) xbt_os_timer_elapsed(xbt_os_timer_t timer);
-
+XBT_PUBLIC(size_t) xbt_os_timer_size(void);
 
 XBT_PUBLIC(void) xbt_os_walltimer_start(xbt_os_timer_t timer);
 XBT_PUBLIC(void) xbt_os_walltimer_resume(xbt_os_timer_t timer);
