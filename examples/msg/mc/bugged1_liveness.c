443f173e17963d97c3ff28f6dec85846b92bc69d
Marion Guthmuller <marion.guthmuller@loria.fr>
2013-08-14 11:51:21 +0200

    model-checker : update MSG bugged mutual exclusion example

--- examples/msg/mc/bugged1_liveness.c
+++ examples/msg/mc/bugged1_liveness.c
@@ -1,6 +1,6 @@
 /***************** Centralized Mutual Exclusion Algorithm *********************/
 /* This example implements a centralized mutual exclusion algorithm.          */
-/* CS requests of client 1 not satisfied                                      */
+/* Bug : CS requests of client 1 not satisfied                                      */
 /* LTL property checked : G(r->F(cs)); (r=request of CS, cs=CS ok)            */
 /******************************************************************************/
 
@@ -27,7 +27,8 @@ int coordinator(int argc, char *argv[])
 {
 
   int CS_used = 0;   
-  msg_task_t task = NULL, answer = NULL;        
+  msg_task_t task = NULL, answer = NULL; 
+  xbt_dynar_t requests = xbt_dynar_new(sizeof(char *), NULL);
 
   while(1){  
     MSG_task_receive(&task, "coordinator");
@@ -35,7 +36,8 @@ int coordinator(int argc, char *argv[])
     if (!strcmp(kind, "request")) {    
       char *req = MSG_task_get_data(task);
       if (CS_used) {           
-        XBT_INFO("CS already used.");
+        XBT_INFO("CS already used. Queue the request.");
+        xbt_dynar_push(requests, &req);
       } else {               
         if(strcmp(req, "1") != 0){
           XBT_INFO("CS idle. Grant immediatly");
@@ -45,9 +47,20 @@ int coordinator(int argc, char *argv[])
           answer = NULL;
         }
       }
-    } else {         
-      XBT_INFO("CS release. resource now idle");
-      CS_used = 0;
+    } else {      
+      if (!xbt_dynar_is_empty(requests)) {
+        XBT_INFO("CS release. Grant to queued requests (queue size: %lu)", xbt_dynar_length(requests));
+        char *req;
+        xbt_dynar_pop(requests, &req);
+        if(strcmp(req, "1") != 0){
+          MSG_task_send(MSG_task_create("grant", 0, 1000, NULL), req);
+        }else{
+          xbt_dynar_push(requests, &req);
+        }
+      }else{
+        XBT_INFO("CS release. resource now idle");
+        CS_used = 0;
+      }
     }
     MSG_task_destroy(task);
     task = NULL;
