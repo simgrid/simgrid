d18bab6bdd1ec8b733e9ad31aaba20ebec4dbd96
suter <frederic.suter@cc.in2p3.fr>
2013-09-25 18:25:00 +0200

    empty struct means trouble, add a dummy field for now

--- include/msg/datatypes.h
+++ include/msg/datatypes.h
@@ -139,6 +139,7 @@ typedef s_xbt_dictelm_t s_msg_storage_t;
 
 typedef struct msg_storage_priv  {
   // TODO PV: fill it (or not) !
+  void * dummy;
 } s_msg_storage_priv_t, *msg_storage_priv_t;
 
 /*************** Begin GPU ***************/
