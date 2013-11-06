simgrid/simgrid

commit 58ea59660df7ff5af144848bd7e5bc7d8b66bf37

Make smpi_process_init public, and avoid errors with smpiff.

include/smpi/smpif.h.in
@@ -17,6 +17,7 @@
 
 XBT_PUBLIC_DATA(__thread int) smpi_current_rank;
 
+XBT_PUBLIC(void) smpi_process_init(int *argc, char ***argv);
 XBT_PUBLIC(int) smpi_process_argc(void);
 XBT_PUBLIC(int) smpi_process_getarg(integer* index, char* dst, ftnlen len);
 XBT_PUBLIC(int) smpi_global_size(void);

src/smpi/private.h
@@ -94,7 +94,6 @@ typedef struct s_smpi_mpi_request {
 #endif
 } s_smpi_mpi_request_t;
 
-void smpi_process_init(int *argc, char ***argv);
 void smpi_process_destroy(void);
 void smpi_process_finalize(void);
 int smpi_process_finalized(void);

commit 18bbe5b38ef4565c39f452b42a5b5417c5ad61ed

smpi build without F2C was broken since 58ea5966

include/smpi/smpi.h
@@ -726,6 +726,7 @@ XBT_PUBLIC(int) __attribute__((weak)) MAIN__(void);
 XBT_PUBLIC(int) smpi_main(int (*realmain) (int argc, char *argv[]),int argc, char *argv[]);
 XBT_PUBLIC(void) __attribute__((weak)) user_main_(void);
 XBT_PUBLIC(int) smpi_process_index(void);
+XBT_PUBLIC(void) smpi_process_init(int *argc, char ***argv);
 
 /* Trace replay specific stuff */
 XBT_PUBLIC(void) smpi_replay_init(int *argc, char***argv);

include/smpi/smpif.h.in
@@ -17,7 +17,6 @@
 
 XBT_PUBLIC_DATA(__thread int) smpi_current_rank;
 
-XBT_PUBLIC(void) smpi_process_init(int *argc, char ***argv);
 XBT_PUBLIC(int) smpi_process_argc(void);
 XBT_PUBLIC(int) smpi_process_getarg(integer* index, char* dst, ftnlen len);
 XBT_PUBLIC(int) smpi_global_size(void);