6aef63b1290da4be0e1e1d4ae05907d0d6884a2e
Arnaud Giersch <arnaud.giersch@iut-bm.univ-fcomte.fr>
2013-11-19 08:33:42 +0100

    Enable warnings for C++, and stop hiding potential problems.

--- buildtools/Cmake/Flags.cmake
+++ buildtools/Cmake/Flags.cmake
@@ -19,6 +19,8 @@ if(enable_compile_warnings)
     endif()
   endif()
 
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra")
+
   set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Wall") # FIXME: Q&D hack
 
   set(CMAKE_JAVA_COMPILE_FLAGS "-Xlint")
