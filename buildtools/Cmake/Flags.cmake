d5b6802a23fab93ad4131057f458e6a0316fc428
Arnaud Giersch <arnaud.giersch@iut-bm.univ-fcomte.fr>
2014-01-07 15:15:12 +0100

    Add -std=gnu99 to compile flags.
    That's the C dialect we're using anyway, and it allows initial
    declarations in for loops.

--- buildtools/Cmake/Flags.cmake
+++ buildtools/Cmake/Flags.cmake
@@ -2,7 +2,7 @@ set(warnCFLAGS "")
 set(optCFLAGS "")
 
 if(NOT __VISUALC__ AND NOT __BORLANDC__)
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}-g3")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}-std=gnu99 -g3")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}-g3")
   set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g")
 else()
