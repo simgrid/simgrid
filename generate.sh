3168d6749a413d4bec67b88b8905501a219212b5
Gabriel Corona <gabriel.corona@loria.fr>
2014-01-17 11:30:07 +0100

    [mc] Use literal values in DWARF constant to name mappings

--- generate.sh
+++ generate.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-# Generate files from dwarf.h
+# Generate files from a given dwarf.h
 
-(for tag in $(grep -o 'DW_TAG_[^ ]*' /usr/include/dwarf.h) ; do
-    echo "case $tag: return \"$tag\";"
-done) > src/mc/mc_dwarf_tagnames.h
-
-(for attr in $(grep -o 'DW_AT_[^ ]*' /usr/include/dwarf.h) ; do
-    echo "case $attr: return \"$attr\";"
-done) > src/mc/mc_dwarf_attrnames.h
+cat "$1" | grep DW_TAG_ | sed 's/.*\(DW_TAG_[^ ]*\) = \(0x[0-9a-f]*\).*/case \2: return "\1";/' > src/mc/mc_dwarf_tagnames.h
+cat "$1" | grep DW_AT_ | sed 's/.*\(DW_AT_[^ ]*\) = \(0x[0-9a-f]*\).*/case \2: return "\1";/' >  src/mc/mc_dwarf_attrnames.h
