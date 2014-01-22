2f294c4b59f922f3e15494d33296667a425347bb
Arnaud Giersch <arnaud.giersch@iut-bm.univ-fcomte.fr>
2014-01-22 15:45:51 +0100

    Connect another new log category.

--- src/xbt/log.c
+++ src/xbt/log.c
@@ -1,6 +1,6 @@
 /* log - a generic logging facility in the spirit of log4j                  */
 
-/* Copyright (c) 2004-2013. The SimGrid Team.
+/* Copyright (c) 2004-2014. The SimGrid Team.
  * All rights reserved.                                                     */
 
 /* This program is free software; you can redistribute it and/or modify it
@@ -701,6 +701,7 @@ static void xbt_log_connect_categories(void)
   XBT_LOG_CONNECT(surf_route);
   XBT_LOG_CONNECT(surf_routing_generic);
   XBT_LOG_CONNECT(surf_route_cluster);
+  XBT_LOG_CONNECT(surf_route_cluster_torus);
   XBT_LOG_CONNECT(surf_route_dijkstra);
   XBT_LOG_CONNECT(surf_route_floyd);
   XBT_LOG_CONNECT(surf_route_full);
