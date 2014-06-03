625acbc2ff480f1dd8e001dd0a39cbeda4bdd235
Paul Bédaride <paul.bedaride@gmail.com>
2014-06-03 12:53:28 +0200

    Add javadoc into simgrid_full.jar

--- buildtools/Cmake/MakeJava.cmake
+++ buildtools/Cmake/MakeJava.cmake
@@ -110,6 +110,8 @@ add_custom_command(
   COMMAND ${JAVA_ARCHIVE} -uvf ${SIMGRID_FULL_JAR} "NATIVE"
   COMMAND ${CMAKE_COMMAND} -E remove ${SIMGRID_JAR}_finalized
   COMMAND ${CMAKE_COMMAND} -E touch ${SIMGRID_JAR}_finalized
+  COMMAND ${JAVADOC_PATH}/javadoc -quiet -d doc ${CMAKE_HOME_DIRECTORY}/src/bindings/java/org/simgrid/*.java ${CMAKE_HOME_DIRECTORY}/src/bindings/java/org/simgrid/*/*.java
+  COMMAND ${JAVA_ARCHIVE} -uvf ${SIMGRID_FULL_JAR} doc
   )
 add_custom_target(simgrid-java_jar ALL DEPENDS ${SIMGRID_JAR}_finalized)
 
