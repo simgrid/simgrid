0d56c9a185dd4cad84f2927417ad7125d0ffd15e
Augustin Degomme <augustin.degomme@imag.fr>
2014-06-27 16:20:34 +0200

    as linking may be done through c++, add flags to remove some old/new warnings to cxx flags.

--- buildtools/Cmake/Flags.cmake
+++ buildtools/Cmake/Flags.cmake
@@ -19,7 +19,7 @@ if(enable_compile_warnings)
     endif()
   endif()
 
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wno-unused-function -Wno-unused-parameter -Wno-strict-aliasing -Wno-format-nonliteral -Werror")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wno-unused-function -Wno-unused-parameter -Wno-strict-aliasing -Wclobbered -Wno-error=clobbered -Wno-format-nonliteral -Werror")
 
   set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Wall") # FIXME: Q&D hack
 
