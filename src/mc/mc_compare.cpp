848de43e8b662c557853f4466115937687e8fd63
Marion Guthmuller <marion.guthmuller@inria.fr>
2014-06-27 13:56:49 +0200

    model-checker : if no inttypes found in c++

--- src/mc/mc_compare.cpp
+++ src/mc/mc_compare.cpp
@@ -4,6 +4,7 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
+#define __STDC_FORMAT_MACROS
 #include <inttypes.h>
 #include <boost/unordered_set.hpp>
 
