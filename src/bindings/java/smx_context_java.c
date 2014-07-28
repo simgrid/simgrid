8c9fdae81e190f129167b276e1817256607bfe14
Adrien Lebre <alebre@pc18.home>
2014-07-28 18:02:23 +0200

    missing files - adrien

--- src/bindings/java/smx_context_java.c
+++ src/bindings/java/smx_context_java.c
@@ -155,9 +155,8 @@ void smx_ctx_java_stop(smx_context_t context)
     JNIEnv *env = get_current_thread_env();
     XBT_DEBUG("Gonnal launch Killed Error");
     jxbt_throw_by_name(env, "org/simgrid/msg/ProcessKilledError", xbt_strdup("Process killed :)"));
-    // TODO emptty/adsein, comment the following line. This avoid to raise process cancelled exception at the Java level but 
-    // it impacts the shutdown of VMs :( see java-cloud example
-	THROWF(cancel_error, 0, "process cancelled");
+    XBT_DEBUG("Trigger a cancel error at the C level");
+    THROWF(cancel_error, 0, "process cancelled");
   } else {
     smx_ctx_base_stop(context);
     /* detach the thread and kills it */
