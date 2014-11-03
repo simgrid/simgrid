bc75e22e7ea7c0fdc45e5622d8f95b90bdcf2c27
Augustin Degomme <augustin.degomme@imag.fr>
2014-11-03 14:05:39 +0100

    Add mpich3 tests for MPI_Info calls. As expected, the get_nthkey does not behave as expected, and it won't.

--- buildtools/Cmake/DefinePackages.cmake
+++ buildtools/Cmake/DefinePackages.cmake
@@ -1081,6 +1081,7 @@ set(TESHSUITE_CMAKEFILES_TXT
   teshsuite/smpi/mpich3-test/f90/util/CMakeLists.txt
   teshsuite/smpi/mpich3-test/f90/rma/CMakeLists.txt
   teshsuite/smpi/mpich3-test/group/CMakeLists.txt
+  teshsuite/smpi/mpich3-test/info/CMakeLists.txt
   teshsuite/smpi/mpich3-test/init/CMakeLists.txt
   teshsuite/smpi/mpich3-test/pt2pt/CMakeLists.txt
   teshsuite/smpi/mpich3-test/topo/CMakeLists.txt
