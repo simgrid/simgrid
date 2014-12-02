4ae28ba754094a7b6e69816d6f05853eb6deb54b
Augustin Degomme <augustin.degomme@imag.fr>
2014-12-02 19:17:49 +0100

    forgot to add this include

--- src/mc/mc_global.c
+++ src/mc/mc_global.c
@@ -4,6 +4,7 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
+#include "mc_base.h"
 
 #ifndef _XBT_WIN32
 #include <unistd.h>
