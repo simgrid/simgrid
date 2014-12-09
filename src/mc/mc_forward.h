49e7aa2b3f702043ff559f3f3d11ff208b36d1f4
Gabriel Corona <gabriel.corona@loria.fr>
2014-12-09 11:21:49 +0100

    [mc] Remove redundant typedefs
    Clang complains about typedef redefinitions in pre-C11 mode.

--- src/mc/mc_forward.h
+++ src/mc/mc_forward.h
@@ -8,17 +8,14 @@
 #define MC_FORWARD_H
 
 #include <mc/datatypes.h>
+#include "mc_interface.h"
 
 typedef struct s_mc_object_info s_mc_object_info_t, *mc_object_info_t;
-typedef struct s_mc_transition s_mc_transition_t, *mc_transition_t;
 typedef struct s_dw_type s_dw_type_t, *dw_type_t;
 typedef struct s_memory_map s_memory_map_t, *memory_map_t;
 typedef struct s_dw_variable s_dw_variable_t, *dw_variable_t;
 typedef struct s_dw_frame s_dw_frame_t, *dw_frame_t;
-
 typedef struct s_mc_pages_store s_mc_pages_store_t, *mc_pages_store_t;
-typedef struct s_mc_snapshot s_mc_snapshot_t, *mc_snapshot_t;
-
 typedef struct s_mc_model_checker s_mc_model_checker_t, *mc_model_checker_t;
 extern mc_model_checker_t mc_model_checker;
 
