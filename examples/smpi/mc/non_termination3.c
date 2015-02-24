ec38bc5664db40ada68448c162478e0d20189fa0
Marion Guthmuller <marion.guthmuller@inria.fr>
2015-02-24 17:18:16 +0100

    model-checker : new files forgotten

---
+++ examples/smpi/mc/non_termination3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <simgrid/modelchecker.h>
+
+int x = 0;
+int y = 0;
+
+int main(int argc, char **argv) {
+
+  int recv_x, recv_y, size, rank;
+  MPI_Status status;
+  
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm_size(MPI_COMM_WORLD, &size);   /* Get nr of tasks */
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);   /* Get id of this process */
+
+  MC_ignore(&(status.count), sizeof(status.count));
+
+  if (rank == 0) {
+    while (x<5) {
+      MPI_Recv(&recv_x, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      MPI_Recv(&recv_y, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    }
+  } else {
+    while (x<5) {
+      int old_x = x;
+      x = old_x - y;
+      MPI_Send(&x, 1, MPI_INT, 0, 42, MPI_COMM_WORLD);
+      y = old_x + y;
+      MPI_Send(&y, 1, MPI_INT, 0, 42, MPI_COMM_WORLD);
+    }
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
