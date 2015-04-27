4de9b8aa2789008b9873dec89f1bc97ac24c0f84
Gabriel Corona <gabriel.corona@loria.fr>
2015-04-27 11:01:00 +0200

    [mc] Add missing XBT_CONNECT, move some out of HAVE_MC

--- src/xbt/log.c
+++ src/xbt/log.c
@@ -639,7 +639,6 @@ static void xbt_log_connect_categories(void)
   XBT_LOG_CONNECT(mc_compare);
   XBT_LOG_CONNECT(mc_diff);
   XBT_LOG_CONNECT(mc_dwarf);
-  XBT_LOG_CONNECT(mc_global);
   XBT_LOG_CONNECT(mc_hash);
   XBT_LOG_CONNECT(mc_ignore);
   XBT_LOG_CONNECT(mc_liveness);
@@ -649,7 +648,18 @@ static void xbt_log_connect_categories(void)
   XBT_LOG_CONNECT(mc_request);
   XBT_LOG_CONNECT(mc_safety);
   XBT_LOG_CONNECT(mc_visited);
+  XBT_LOG_CONNECT(mc_client);
+  XBT_LOG_CONNECT(mc_client_api);
+  XBT_LOG_CONNECT(mc_comm_pattern);
+  XBT_LOG_CONNECT(mc_main);
+  XBT_LOG_CONNECT(mc_process);
+  XBT_LOG_CONNECT(mc_protocol);
+  XBT_LOG_CONNECT(mc_server);
+  XBT_LOG_CONNECT(mc_state);
 #endif
+  XBT_LOG_CONNECT(mc_global);
+  XBT_LOG_CONNECT(mc_config);
+  XBT_LOG_CONNECT(mc_record);
 
   /* msg */
   XBT_LOG_CONNECT(msg);
