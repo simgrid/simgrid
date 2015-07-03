4557e0e9fa7e468726fb4df3a7b140f8de24d165
Gabriel Corona <gabriel.corona@loria.fr>
2015-07-03 14:41:09 +0200

    [jenkins] Enable debug (Ã-g) in the builds

--- buildtools/jenkins/build.sh
+++ buildtools/jenkins/build.sh
@@ -13,6 +13,15 @@ die () {
   exit $status
 }
 
+# Get an ON/OFF string from a command:
+onoff() {
+  if "$@" > /dev/null ; then
+    echo ON
+  else
+    echo OFF
+  fi
+}
+
 ulimit -c 0
 
 if [ -d $WORKSPACE/build ]
@@ -27,21 +36,31 @@ make dist
 tar xzf `cat VERSION`.tar.gz
 cd `cat VERSION`
 
-if [ "$build_mode" = "Debug" ]
-then
-cmake -Denable_documentation=OFF -Denable_coverage=OFF -Denable_java=ON -Denable_smpi_MPICH3_testsuite=ON -Denable_model-checking=OFF -Denable_lua=ON -Denable_compile_optimizations=ON -Denable_compile_warnings=ON .
-fi
+case "$build_mode" in
+  "Debug")
+  ;;
 
-if [ "$build_mode" = "ModelChecker" ]
-then
-cmake -Denable_documentation=OFF -Denable_coverage=OFF -Denable_java=ON -Denable_smpi_MPICH3_testsuite=ON -Denable_model-checking=ON -Denable_lua=ON -Denable_compile_optimizations=OFF -Denable_compile_warnings=ON .
-fi
+  "ModelChecker")
+  ;;
 
-if [ "$build_mode" = "DynamicAnalysis" ]
-then
-cmake -Denable_documentation=OFF -Denable_lua=OFF -Denable_java=ON -Denable_tracing=ON -Denable_smpi=ON -Denable_compile_optimizations=OFF -Denable_compile_warnings=ON -Denable_lib_static=OFF -Denable_model-checking=OFF -Denable_latency_bound_tracking=OFF -Denable_gtnets=OFF -Denable_jedule=OFF -Denable_mallocators=OFF -Denable_memcheck=ON -Denable_smpi_MPICH3_testsuite=OFF .
-fi
+  "DynamicAnalysis")
+  ;;
 
+  *)
+  die 1 "Unknown build_mode $build_mode"
+  ;;
+esac
+
+cmake -Denable_debug=ON -Denable_documentation=OFF -Denable_coverage=OFF \
+  -Denable_model-checking=$(onoff test "$build_mode" = "ModelChecker") \
+  -Denable_compile_optimization=$(onoff test "$build_mode" = "Debug") \
+  -Denable_smpi_MPICH3_testsuite=$(onoff test "$build_mode" != "DynamicAnalysis") \
+  -Denable_lua=$(onoff test "$build_mode" != "DynamicAnalysis") \
+  -Denable_mallocators=$(onoff test "$build_mode" != "DynamicAnalysis") \
+  -Denable_memcheck=$(onoff test "$build_mode" = "DynamicAnalysis") \
+  -Denable_compile_warnings=ON -Denable_smpi=ON -Denable_lib_static=OFF \
+  -Denable_latency_bound_tracking=OFF -Denable_gtnets=OFF -Denable_jedule=OFF \
+  -Denable_tracing=ON -Denable_java=ON
 make
 
 cd $WORKSPACE/build
@@ -53,16 +72,6 @@ ctest -T test --no-compress-output || true
 if [ -f Testing/TAG ] ; then
    xsltproc $WORKSPACE/buildtools/jenkins/ctest2junit.xsl Testing/`head -n 1 < Testing/TAG`/Test.xml > CTestResults.xml
    mv CTestResults.xml $WORKSPACE
-fi
-
-if [ "$build_mode" = "Debug" ]
-then
-cmake -Denable_documentation=OFF -Denable_coverage=OFF -Denable_java=ON -Denable_smpi_MPICH3_testsuite=ON -Denable_model-checking=OFF -Denable_lua=ON -Denable_compile_optimizations=ON -Denable_compile_warnings=ON .
-fi
-
-if [ "$build_mode" = "ModelChecker" ]
-then
-cmake -Denable_documentation=OFF -Denable_coverage=OFF -Denable_java=ON -Denable_smpi_MPICH3_testsuite=ON -Denable_model-checking=ON -Denable_lua=ON -Denable_compile_optimizations=OFF -Denable_compile_warnings=ON .
 fi
 
 if [ "$build_mode" = "DynamicAnalysis" ]
