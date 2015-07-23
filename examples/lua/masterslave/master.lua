74767fdf91b4382b7ab3acf955cfd48effcfa062
Christian Heinrich <franz-christian.heinrich@inria.fr>
2015-07-23 19:00:14 +0200

    [Lua] Ported masterslave code to Lua 5.3

--- examples/lua/masterslave/master.lua
+++ examples/lua/masterslave/master.lua
@@ -4,23 +4,23 @@
 -- This program is free software; you can redistribute it and/or modify it
 -- under the terms of the license (GNU LGPL) which comes with this package.
 
-function Master(...) 
+function Master(...)
 
-  if #arg ~= 4 then
+  if select("#", ...) ~= 4 then
     error("Wrong number of arguments (got " .. #arg ..
         ", expected 4: nb_tasks comp_size comm_size slave_count)")
   end
 
+  local nb_task, comp_size, comm_size, slave_count = select(1, ...)
   simgrid.info("Hello from lua, I'm the master")
 
-  local nb_task, comp_size, comm_size, slave_count = unpack(arg)
 
   -- Dispatch the tasks
 
   for i = 1, nb_task do
     local task = simgrid.task.new("Task " .. i, comp_size, comm_size)
     local task_name = task:get_name()
-    local alias = "slave " .. (i % slave_count)
+    local alias = "slave " .. string.format("%d", i % slave_count)
     simgrid.info("Sending  '" .. task_name .. "' to '" .. alias .."'")
     task:send(alias) -- C user data set to NULL
     simgrid.info("Done sending '".. task_name .. "' to '" .. alias .."'")
