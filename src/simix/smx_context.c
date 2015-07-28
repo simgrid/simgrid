03182900f42e677d326f17bb723c42913b99d1ae
Gabriel Corona <gabriel.corona@loria.fr>
2015-07-28 14:24:07 +0200

    [mc] Fail if we can't unprotect the stack guard

--- src/simix/smx_context.c
+++ src/simix/smx_context.c
@@ -163,8 +163,9 @@ void *SIMIX_context_stack_new(void)
 
 #ifndef _XBT_WIN32
     if (mprotect(stack, smx_context_guard_size, PROT_NONE) == -1) {
-      XBT_WARN("Failed to protect stack: %s", strerror(errno));
-      /* That's not fatal, pursue anyway. */
+      xbt_die("Failed to protect stack: %s", strerror(errno));
+      /* This is fatal. We are going to fail at some point when
+         we tryi reusing this. */
     }
 #endif
     stack = (char *)stack + smx_context_guard_size;
