2e774b855d784f3445dadc41415afc082e888d60
Martin Quinson <martin.quinson@loria.fr>
2015-08-07 15:51:58 +0200

    fix make dist

--- buildtools/Cmake/DefinePackages.cmake
+++ buildtools/Cmake/DefinePackages.cmake
@@ -1254,6 +1254,7 @@ set(PLATFORMS_EXAMPLES
   examples/platforms/platform.xml
   examples/platforms/prop.xml
   examples/platforms/routing_cluster.xml
+  examples/platforms/routing_none.xml
   examples/platforms/storage/content/small_content.txt
   examples/platforms/storage/content/storage_content.txt
   examples/platforms/storage/content/win_storage_content.txt
