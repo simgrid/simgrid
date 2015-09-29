aa91571018408a593871a08e44b56cdc9323dd37
Gabriel Corona <gabriel.corona@loria.fr>
2015-09-29 16:09:19 +0200

    Mark some internal symbols as hidden on ELF in instr

--- src/instr/instr_private.h
+++ src/instr/instr_private.h
@@ -7,6 +7,8 @@
 #ifndef INSTR_PRIVATE_H_
 #define INSTR_PRIVATE_H_
 
+#include <xbt/base.h>
+
 #include "simgrid/instr.h"
 #include "instr/instr_interface.h"
 #include "internal_config.h"
@@ -224,23 +226,23 @@ typedef struct s_newEvent {
   val_t value;
 }s_newEvent_t;
 
-extern xbt_dict_t created_categories;
-extern xbt_dict_t declared_marks;
-extern xbt_dict_t user_host_variables;
-extern xbt_dict_t user_vm_variables;
-extern xbt_dict_t user_link_variables;
-extern double TRACE_last_timestamp_to_dump;
+extern XBT_PRIVATE xbt_dict_t created_categories;
+extern XBT_PRIVATE xbt_dict_t declared_marks;
+extern XBT_PRIVATE xbt_dict_t user_host_variables;
+extern XBT_PRIVATE xbt_dict_t user_vm_variables;
+extern XBT_PRIVATE xbt_dict_t user_link_variables;
+extern XBT_PRIVATE double TRACE_last_timestamp_to_dump;
 
 /* instr_paje_header.c */
-void TRACE_header(int basic, int size);
+XBT_PRIVATE void TRACE_header(int basic, int size);
 
 /* from paje.c */
-void TRACE_init(void);
-void TRACE_finalize(void);
-void TRACE_paje_init(void);
-void TRACE_paje_start(void);
-void TRACE_paje_end(void);
-void TRACE_paje_dump_buffer (int force);
+XBT_PRIVATE void TRACE_init(void);
+XBT_PRIVATE void TRACE_finalize(void);
+XBT_PRIVATE void TRACE_paje_init(void);
+XBT_PRIVATE void TRACE_paje_start(void);
+XBT_PRIVATE void TRACE_paje_end(void);
+XBT_PRIVATE void TRACE_paje_dump_buffer (int force);
 XBT_PUBLIC(void) new_pajeDefineContainerType(type_t type);
 XBT_PUBLIC(void) new_pajeDefineVariableType(type_t type);
 XBT_PUBLIC(void) new_pajeDefineStateType(type_t type);
@@ -263,32 +265,32 @@ XBT_PUBLIC(void) new_pajeEndLink (double timestamp, container_t container, type_
 XBT_PUBLIC(void) new_pajeNewEvent (double timestamp, container_t container, type_t type, val_t value);
 
 /* from instr_config.c */
-int TRACE_needs_platform (void);
-int TRACE_is_enabled(void);
-int TRACE_platform(void);
-int TRACE_platform_topology(void);
-int TRACE_is_configured(void);
-int TRACE_categorized (void);
-int TRACE_uncategorized (void);
-int TRACE_msg_process_is_enabled(void);
-int TRACE_msg_vm_is_enabled(void);
-int TRACE_buffer (void);
-int TRACE_disable_link(void);
-int TRACE_disable_power(void);
-int TRACE_onelink_only (void);
-int TRACE_disable_destroy (void);
-int TRACE_basic (void);
-int TRACE_display_sizes (void);
-char *TRACE_get_comment (void);
-char *TRACE_get_comment_file (void);
-int TRACE_precision (void);
-char *TRACE_get_filename(void);
-char *TRACE_get_viva_uncat_conf (void);
-char *TRACE_get_viva_cat_conf (void);
-void TRACE_generate_viva_uncat_conf (void);
-void TRACE_generate_viva_cat_conf (void);
-void instr_pause_tracing (void);
-void instr_resume_tracing (void);
+XBT_PRIVATE int TRACE_needs_platform (void);
+XBT_PRIVATE int TRACE_is_enabled(void);
+XBT_PRIVATE int TRACE_platform(void);
+XBT_PRIVATE int TRACE_platform_topology(void);
+XBT_PRIVATE int TRACE_is_configured(void);
+XBT_PRIVATE int TRACE_categorized (void);
+XBT_PRIVATE int TRACE_uncategorized (void);
+XBT_PRIVATE int TRACE_msg_process_is_enabled(void);
+XBT_PRIVATE int TRACE_msg_vm_is_enabled(void);
+XBT_PRIVATE int TRACE_buffer (void);
+XBT_PRIVATE int TRACE_disable_link(void);
+XBT_PRIVATE int TRACE_disable_power(void);
+XBT_PRIVATE int TRACE_onelink_only (void);
+XBT_PRIVATE int TRACE_disable_destroy (void);
+XBT_PRIVATE int TRACE_basic (void);
+XBT_PRIVATE int TRACE_display_sizes (void);
+XBT_PRIVATE char *TRACE_get_comment (void);
+XBT_PRIVATE char *TRACE_get_comment_file (void);
+XBT_PRIVATE int TRACE_precision (void);
+XBT_PRIVATE char *TRACE_get_filename(void);
+XBT_PRIVATE char *TRACE_get_viva_uncat_conf (void);
+XBT_PRIVATE char *TRACE_get_viva_cat_conf (void);
+XBT_PRIVATE void TRACE_generate_viva_uncat_conf (void);
+XBT_PRIVATE void TRACE_generate_viva_cat_conf (void);
+XBT_PRIVATE void instr_pause_tracing (void);
+XBT_PRIVATE void instr_resume_tracing (void);
 
 /* Public functions used in SMPI */
 XBT_PUBLIC(int) TRACE_smpi_is_enabled(void);
@@ -298,12 +300,12 @@ XBT_PUBLIC(int) TRACE_smpi_is_sleeping(void);
 XBT_PUBLIC(int) TRACE_smpi_view_internals(void);
 
 /* from resource_utilization.c */
-void TRACE_surf_host_set_utilization(const char *resource,
+XBT_PRIVATE void TRACE_surf_host_set_utilization(const char *resource,
                                      const char *category,
                                      double value,
                                      double now,
                                      double delta);
-void TRACE_surf_link_set_utilization(const char *resource,
+XBT_PRIVATE void TRACE_surf_link_set_utilization(const char *resource,
                                      const char *category,
                                      double value,
                                      double now,
@@ -311,11 +313,11 @@ void TRACE_surf_link_set_utilization(const char *resource,
 XBT_PUBLIC(void) TRACE_surf_resource_utilization_alloc(void);
 
 /* instr_paje.c */
-extern xbt_dict_t trivaNodeTypes;
-extern xbt_dict_t trivaEdgeTypes;
-long long int instr_new_paje_id (void);
-void PJ_container_alloc (void);
-void PJ_container_release (void);
+extern XBT_PRIVATE xbt_dict_t trivaNodeTypes;
+extern XBT_PRIVATE xbt_dict_t trivaEdgeTypes;
+XBT_PRIVATE long long int instr_new_paje_id (void);
+XBT_PRIVATE void PJ_container_alloc (void);
+XBT_PRIVATE void PJ_container_release (void);
 XBT_PUBLIC(container_t) PJ_container_new (const char *name, e_container_types kind, container_t father);
 XBT_PUBLIC(container_t) PJ_container_get (const char *name);
 XBT_PUBLIC(container_t) PJ_container_get_or_null (const char *name);
@@ -326,55 +328,55 @@ XBT_PUBLIC(void) PJ_container_free_all (void);
 XBT_PUBLIC(void) PJ_container_remove_from_parent (container_t container);
 
 /* instr_paje_types.c */
-void PJ_type_alloc (void);
-void PJ_type_release (void);
+XBT_PRIVATE void PJ_type_alloc (void);
+XBT_PRIVATE void PJ_type_release (void);
 XBT_PUBLIC(type_t)  PJ_type_get_root (void);
-type_t PJ_type_container_new (const char *name, type_t father);
-type_t PJ_type_event_new (const char *name, type_t father);
-type_t PJ_type_variable_new (const char *name, const char *color, type_t father);
+XBT_PRIVATE type_t PJ_type_container_new (const char *name, type_t father);
+XBT_PRIVATE type_t PJ_type_event_new (const char *name, type_t father);
 type_t PJ_type_link_new (const char *name, type_t father, type_t source, type_t dest);
-type_t PJ_type_state_new (const char *name, type_t father);
+XBT_PRIVATE XBT_PRIVATE type_t PJ_type_variable_new (const char *name, const char *color, type_t father);
+XBT_PRIVATE type_t PJ_type_state_new (const char *name, type_t father);
 XBT_PUBLIC(type_t)  PJ_type_get (const char *name, const type_t father);
 XBT_PUBLIC(type_t)  PJ_type_get_or_null (const char *name, type_t father);
-void PJ_type_free (type_t type);
 void PJ_type_free_all (void);
+XBT_PRIVATE XBT_PRIVATE void PJ_type_free (type_t type);
 
 /* instr_paje_values.c */
 XBT_PUBLIC(val_t)  PJ_value_new (const char *name, const char *color, type_t father);
 XBT_PUBLIC(val_t)  PJ_value_get_or_new (const char *name, const char *color, type_t father);
 XBT_PUBLIC(val_t)  PJ_value_get (const char *name, const type_t father);
-void PJ_value_free (val_t value);
+XBT_PRIVATE void PJ_value_free (val_t value);
 
-void print_pajeDefineContainerType(paje_event_t event);
-void print_pajeDefineVariableType(paje_event_t event);
-void print_pajeDefineStateType(paje_event_t event);
-void print_pajeDefineEventType(paje_event_t event);
-void print_pajeDefineLinkType(paje_event_t event);
-void print_pajeDefineEntityValue (paje_event_t event);
-void print_pajeCreateContainer(paje_event_t event);
-void print_pajeDestroyContainer(paje_event_t event);
-void print_pajeSetVariable(paje_event_t event);
-void print_pajeAddVariable(paje_event_t event);
-void print_pajeSubVariable(paje_event_t event);
-void print_pajeSetState(paje_event_t event);
-void print_pajePushState(paje_event_t event);
-void print_pajePopState(paje_event_t event);
-void print_pajeResetState(paje_event_t event);
-void print_pajeStartLink(paje_event_t event);
-void print_pajeEndLink(paje_event_t event);
-void print_pajeNewEvent (paje_event_t event);
+XBT_PRIVATE void print_pajeDefineContainerType(paje_event_t event);
+XBT_PRIVATE void print_pajeDefineVariableType(paje_event_t event);
+XBT_PRIVATE void print_pajeDefineStateType(paje_event_t event);
+XBT_PRIVATE void print_pajeDefineEventType(paje_event_t event);
+XBT_PRIVATE void print_pajeDefineLinkType(paje_event_t event);
+XBT_PRIVATE void print_pajeDefineEntityValue (paje_event_t event);
+XBT_PRIVATE void print_pajeCreateContainer(paje_event_t event);
+XBT_PRIVATE void print_pajeDestroyContainer(paje_event_t event);
+XBT_PRIVATE void print_pajeSetVariable(paje_event_t event);
+XBT_PRIVATE void print_pajeAddVariable(paje_event_t event);
+XBT_PRIVATE void print_pajeSubVariable(paje_event_t event);
+XBT_PRIVATE void print_pajeSetState(paje_event_t event);
+XBT_PRIVATE void print_pajePushState(paje_event_t event);
+XBT_PRIVATE void print_pajePopState(paje_event_t event);
+XBT_PRIVATE void print_pajeResetState(paje_event_t event);
+XBT_PRIVATE void print_pajeStartLink(paje_event_t event);
+XBT_PRIVATE void print_pajeEndLink(paje_event_t event);
+XBT_PRIVATE void print_pajeNewEvent (paje_event_t event);
 
-void print_TIPushState(paje_event_t event);
-void print_TICreateContainer(paje_event_t event);
-void print_TIDestroyContainer(paje_event_t event);
-void TRACE_TI_start(void);
-void TRACE_TI_end(void);
-void TRACE_TI_init(void);
+XBT_PRIVATE void print_TIPushState(paje_event_t event);
+XBT_PRIVATE void print_TICreateContainer(paje_event_t event);
+XBT_PRIVATE void print_TIDestroyContainer(paje_event_t event);
+XBT_PRIVATE void TRACE_TI_start(void);
+XBT_PRIVATE void TRACE_TI_end(void);
+XBT_PRIVATE void TRACE_TI_init(void);
 
-void print_NULL (paje_event_t event);
-void TRACE_paje_dump_buffer (int force);
-void dump_comment_file (const char *filename);
-void dump_comment (const char *comment);
+XBT_PRIVATE void print_NULL (paje_event_t event);
+XBT_PRIVATE void TRACE_paje_dump_buffer (int force);
+XBT_PRIVATE void dump_comment_file (const char *filename);
+XBT_PRIVATE void dump_comment (const char *comment);
 
 
 
