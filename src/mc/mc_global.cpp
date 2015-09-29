da8defd4747d6b357afb7498d0c2f41a223403e8
Gabriel Corona <gabriel.corona@loria.fr>
2015-09-29 13:26:53 +0200

    [mc] Quick hack to display system memory information (optional)

--- src/mc/mc_global.cpp
+++ src/mc/mc_global.cpp
@@ -507,6 +507,8 @@ void MC_print_statistics(mc_stats_t stats)
     if (_sg_mc_comms_determinism)
       XBT_INFO("Recv-deterministic : %s", !initial_global_state->recv_deterministic ? "No" : "Yes");
   }
+  if (getenv("SIMGRID_MC_SYSTEM_STATISTICS"))
+    system("free");
 }
 
 void MC_automaton_load(const char *file)
