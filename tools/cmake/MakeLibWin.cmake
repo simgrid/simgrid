4787d30c2fcd7987135ecc4ff9d11eb530d851bd
Martin Quinson <martin.quinson@loria.fr>
2015-10-03 22:58:31 +0200

    [windows] We don't use winsock2 since the death of GRAS, a few years ago

--- tools/cmake/MakeLibWin.cmake
+++ tools/cmake/MakeLibWin.cmake
@@ -1,5 +1,4 @@
 ### Make Libs
-#>gcc c:\simgrid-trunk\examples\msg\icomms\peer.c -static -Lc:\simgrid-trunk\lib -lsimgrid -Ic:\simgrid-trunk\include -lwsock32
 
 add_library(simgrid SHARED ${simgrid_sources})
 
@@ -15,20 +14,11 @@ else()
        PREFIX "lib" SUFFIX ".dll" 
        IMPORT_PREFIX "lib" IMPORT_SUFFIX ".dll")
 
-  set(SIMGRID_DEP "-lws2_32 -lm")
+  set(SIMGRID_DEP "-lm")
 
   if (HAVE_PTHREAD)
     set(SIMGRID_DEP "${SIMGRID_DEP} -lpthread")
   endif()
-
-  #if(ARCH_32_BITS)
-  #  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32 -march=i486 -D_I_X86_")
-  #  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32 -march=i486 -D_I_X86_")
-  #else()
-  #  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m64 -D_AMD64_")
-  #  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m64 -D_AMD64_")
-  #  #        message(FATAL_ERROR "Sorry, Simgrid fails with full 64bits for now! Please contact us.")
-  #endif()
 endif()
 
 target_link_libraries(simgrid 	${SIMGRID_DEP})
