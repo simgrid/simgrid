f60efb7e9e5f49bab54fa609d40a55ad1d72cb90
Martin Quinson <martin.quinson@loria.fr>
2015-10-21 18:56:43 +0200

    integrate an example/test for energy+VMs

--- examples/msg/energy/vm/energy_vm.c
+++ examples/msg/energy/vm/energy_vm.c
@@ -14,58 +14,49 @@
 #include "xbt/log.h"
 #include "xbt/asserts.h"
 
-XBT_LOG_NEW_DEFAULT_CATEGORY(msg_test,
-		"Messages specific for this msg example");
+XBT_LOG_NEW_DEFAULT_CATEGORY(energy_vm, "Messages of this example");
 
-static msg_host_t host1 = NULL;
-static msg_host_t host2 = NULL;
-static msg_vm_t vm1 = NULL;
-
-int dvfs(int argc, char *argv[]);
 
 static int worker_func() {
-	//MSG_process_on_exit(shutdown_vm, NULL);
-
 	msg_task_t task1 = MSG_task_create("t1", 300E6, 0, NULL);
 	MSG_task_execute (task1);
 	MSG_task_destroy(task1);
-	XBT_INFO("Worker fun done");
+	XBT_INFO("This worker is done.");
 	return 0;
 }
 
-int dvfs(int argc, char *argv[])
+static int dvfs(int argc, char *argv[])
 {
-	host1 = MSG_host_by_name("MyHost1");
-	host2 = MSG_host_by_name("MyHost2");
+	msg_host_t host1 = MSG_host_by_name("MyHost1");
+	msg_host_t host2 = MSG_host_by_name("MyHost2");
+	msg_host_t host3 = MSG_host_by_name("MyHost3");
 
 	/* Host 1 */
-	XBT_INFO("Creating and starting a VM");
-	vm1 = MSG_vm_create(host1,
-			"vm1", /* name */
-			4, /* number of cpus */
-			2048, /* ram size */
-			100, /* net cap */
-			NULL, /* disk path */
-			1024 * 20, /* disk size */
-			10, /* mig netspeed */
-			50 /* dp intensity */
-	);
-
-	// on Host1 create two tasks, one inside a VM  the other one  directly on the host
+	XBT_INFO("Creating and starting two VMs");
+	msg_vm_t vm1 = MSG_vm_create(host1, "vm1", 4, 2048, 100, NULL, 1024 * 20, 10,50);
 	MSG_vm_start(vm1);
+	msg_vm_t vm2 = MSG_vm_create(host3, "vm2", 4, 2048, 100, NULL, 1024 * 20, 10,50);
+	MSG_vm_start(vm2);
+
+	XBT_INFO("Create two tasks on Host1: one inside a VM, the other directly on the host");
 	MSG_process_create("p11", worker_func, NULL, vm1);
 	MSG_process_create("p12", worker_func, NULL, host1);
-	//XBT_INFO("Task on Host started");
 
-
-	// on Host2, create two tasks directly on the host: Energy of host 1 and host 2 should be the same.
+	XBT_INFO("Create two tasks on Host2: both directly on the host");
 	MSG_process_create("p21", worker_func, NULL, host2);
 	MSG_process_create("p22", worker_func, NULL, host2);
 
-	/* Wait and see */
+	XBT_INFO("Create two tasks on Host3: both inside a VM");
+	MSG_process_create("p31", worker_func, NULL, vm2);
+	MSG_process_create("p32", worker_func, NULL, vm2);
+
+	XBT_INFO("Wait 5 seconds. The tasks are still running (they run for 3 seconds, but 2 tasks are co-located, so they run for 6 seconds)");
 	MSG_process_sleep(5);
+	XBT_INFO("Wait another 5 seconds. The tasks stop at some point in between");
 	MSG_process_sleep(5);
+
 	MSG_vm_shutdown(vm1);
+	MSG_vm_shutdown(vm2);
 
 	return 0;
 }
@@ -76,25 +67,19 @@ int main(int argc, char *argv[])
 	sg_energy_plugin_init();
 	MSG_init(&argc, argv);
 
-	if (argc != 3) {
-		XBT_CRITICAL("Usage: %s platform_file deployment_file\n",
-				argv[0]);
-		XBT_CRITICAL
-		("example: %s msg_platform.xml msg_deployment.xml\n",
-				argv[0]);
-		exit(1);
+	if (argc != 2) {
+		XBT_CRITICAL("Usage: %s platform_file\n", argv[0]);
+		xbt_die("example: %s msg_platform.xml\n", argv[0]);
 	}
 
 	MSG_create_environment(argv[1]);
 
 	/*   Application deployment */
-	MSG_function_register("dvfs_test", dvfs);
-
-	MSG_launch_application(argv[2]);
+	MSG_process_create("dvfs",dvfs,NULL,MSG_host_by_name("MyHost1"));
 
 	res = MSG_main();
 
-	XBT_INFO("Total simulation time: %.2f", MSG_get_clock());
+	XBT_INFO("Total simulation time: %.2f; All hosts must have the exact same energy consumption.", MSG_get_clock());
 
 	if (res == MSG_OK)
 		return 0;
