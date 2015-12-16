ec3037933160bfb763b890149e83d5e88a6d91a7
Martin Quinson <martin.quinson@loria.fr>
2015-12-16 17:13:17 +0100

    cosmetics

--- src/surf/plugins/energy.cpp
+++ src/surf/plugins/energy.cpp
@@ -4,9 +4,9 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include "energy.hpp"
-#include "../cpu_cas01.hpp"
-#include "../virtual_machine.hpp"
+#include <src/surf/plugins/energy.hpp>
+#include <src/surf/cpu_cas01.hpp>
+#include <src/surf/virtual_machine.hpp>
 
 /** @addtogroup SURF_plugin_energy
 
