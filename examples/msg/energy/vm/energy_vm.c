58de6e0115d311e93b28c712e9e74b89fd81d40a
Martin Quinson <martin.quinson@loria.fr>
2015-12-21 22:39:17 +0100

    give all machines the exact same energetic profil, and adapt the tests accordingly

--- examples/msg/energy/vm/energy_vm.c
+++ examples/msg/energy/vm/energy_vm.c
@@ -33,13 +33,13 @@ static int dvfs(int argc, char *argv[])
 
 	/* Host 1 */
 	XBT_INFO("Creating and starting two VMs");
-	msg_vm_t vm1 = MSG_vm_create(host1, "vm1", 4, 2048, 100, NULL, 1024 * 20, 10,50);
-	MSG_vm_start(vm1);
-	msg_vm_t vm2 = MSG_vm_create(host3, "vm2", 4, 2048, 100, NULL, 1024 * 20, 10,50);
-	MSG_vm_start(vm2);
+	msg_vm_t vm_host1 = MSG_vm_create(host1, "vm_host1", 4, 2048, 100, NULL, 1024 * 20, 10,50);
+	MSG_vm_start(vm_host1);
+	msg_vm_t vm_host3 = MSG_vm_create(host3, "vm_host3", 4, 2048, 100, NULL, 1024 * 20, 10,50);
+	MSG_vm_start(vm_host3);
 
 	XBT_INFO("Create two tasks on Host1: one inside a VM, the other directly on the host");
-	MSG_process_create("p11", worker_func, NULL, vm1);
+	MSG_process_create("p11", worker_func, NULL, vm_host1);
 	MSG_process_create("p12", worker_func, NULL, host1);
 
 	XBT_INFO("Create two tasks on Host2: both directly on the host");
@@ -47,16 +47,16 @@ static int dvfs(int argc, char *argv[])
 	MSG_process_create("p22", worker_func, NULL, host2);
 
 	XBT_INFO("Create two tasks on Host3: both inside a VM");
-	MSG_process_create("p31", worker_func, NULL, vm2);
-	MSG_process_create("p32", worker_func, NULL, vm2);
+	MSG_process_create("p31", worker_func, NULL, vm_host3);
+	MSG_process_create("p32", worker_func, NULL, vm_host3);
 
 	XBT_INFO("Wait 5 seconds. The tasks are still running (they run for 3 seconds, but 2 tasks are co-located, so they run for 6 seconds)");
 	MSG_process_sleep(5);
 	XBT_INFO("Wait another 5 seconds. The tasks stop at some point in between");
 	MSG_process_sleep(5);
 
-	MSG_vm_shutdown(vm1);
-	MSG_vm_shutdown(vm2);
+	MSG_vm_shutdown(vm_host1);
+	MSG_vm_shutdown(vm_host3);
 
 	return 0;
 }
