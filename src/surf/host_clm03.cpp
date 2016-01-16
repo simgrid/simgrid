294d29b978f60a0586480ca62008be34d68c7f82
Martin Quinson <martin.quinson@loria.fr>
2016-01-16 15:36:12 +0100

    reindent

--- src/surf/host_clm03.cpp
+++ src/surf/host_clm03.cpp
@@ -55,16 +55,16 @@ double HostCLM03Model::shareResources(double now){
 
   XBT_DEBUG("model %p, %s min_by_cpu %f, %s min_by_net %f, %s min_by_sto %f",
       this, typeid(surf_cpu_model_pm).name(), min_by_cpu,
-	        typeid(surf_network_model).name(), min_by_net,
-			typeid(surf_storage_model).name(), min_by_sto);
+      typeid(surf_network_model).name(), min_by_net,
+      typeid(surf_storage_model).name(), min_by_sto);
 
   double res = std::max(std::max(min_by_cpu, min_by_net), min_by_sto);
   if (min_by_cpu >= 0.0 && min_by_cpu < res)
-	res = min_by_cpu;
+    res = min_by_cpu;
   if (min_by_net >= 0.0 && min_by_net < res)
-	res = min_by_net;
+    res = min_by_net;
   if (min_by_sto >= 0.0 && min_by_sto < res)
-	res = min_by_sto;
+    res = min_by_sto;
   return res;
 }
 
