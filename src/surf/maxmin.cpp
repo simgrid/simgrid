46da570ffa3c7a469d8ec170be2d2eade4be6338
degomme <augustin.degomme@unibas.ch>
2016-02-02 09:43:44 +0100

    Fix build with enable_latency_bound_tracking

--- src/surf/maxmin.cpp
+++ src/surf/maxmin.cpp
@@ -1144,7 +1144,7 @@ XBT_INLINE lmm_constraint_t lmm_get_next_active_constraint(lmm_system_t
 }
 
 #ifdef HAVE_LATENCY_BOUND_TRACKING
-XBT_INLINE int lmm_is_variable_limited_by_latency(lmm_variable_t var)
+XBT_PUBLIC(int) lmm_is_variable_limited_by_latency(lmm_variable_t var)
 {
   return (double_equals(var->bound, var->value, var->bound*sg_maxmin_precision));
 }
