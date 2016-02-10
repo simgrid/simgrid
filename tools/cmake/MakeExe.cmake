7f23120ee4688215bc1dc9d8b746be1731320e00
Frederic Suter <frederic.suter@cc.in2p3.fr>
2016-02-10 22:24:54 +0100

    remove examples/xbt there

--- tools/cmake/MakeExe.cmake
+++ tools/cmake/MakeExe.cmake
@@ -79,8 +79,6 @@ add_subdirectory(${CMAKE_HOME_DIRECTORY}/examples/smpi/energy)
 add_subdirectory(${CMAKE_HOME_DIRECTORY}/examples/smpi/energy/f77)
 add_subdirectory(${CMAKE_HOME_DIRECTORY}/examples/smpi/energy/f90)
 
-add_subdirectory(${CMAKE_HOME_DIRECTORY}/examples/xbt)
-
 add_subdirectory(${CMAKE_HOME_DIRECTORY}/teshsuite/java)
 add_subdirectory(${CMAKE_HOME_DIRECTORY}/teshsuite/java/semaphore)
 add_subdirectory(${CMAKE_HOME_DIRECTORY}/teshsuite/java/sleep_host_off)
