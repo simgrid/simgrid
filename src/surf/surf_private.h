84c998f37a79bac94865047e272a49baa48985de
Martin Quinson <martin.quinson@loria.fr>
2016-02-11 12:56:44 +0100

    Inline another function about the parsing of clusters

--- src/surf/surf_private.h
+++ src/surf/surf_private.h
@@ -72,8 +72,6 @@ XBT_PUBLIC(void) routing_model_create(void *loopback);
 XBT_PUBLIC(void) routing_exit(void);
 XBT_PUBLIC(void) storage_register_callbacks(void);
 
-XBT_PRIVATE void routing_new_cluster(sg_platf_cluster_cbarg_t cluster);
-
 XBT_PUBLIC(void) routing_register_callbacks(void);
 XBT_PUBLIC(void) generic_free_route(sg_platf_route_cbarg_t route); // FIXME rename to routing_route_free
  // FIXME: make previous function private to routing again?
