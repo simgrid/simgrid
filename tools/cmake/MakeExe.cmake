c5d9d129b045f290995f17d701bb7e6c81cf01e9
Frederic Suter <frederic.suter@cc.in2p3.fr>
2016-02-16 00:24:16 +0100

    another try to fix distcheck

--- tools/cmake/MakeExe.cmake
+++ tools/cmake/MakeExe.cmake
@@ -56,6 +56,8 @@ add_subdirectory(${CMAKE_HOME_DIRECTORY}/examples/msg/token_ring)
 add_subdirectory(${CMAKE_HOME_DIRECTORY}/examples/msg/tracing)
 
 add_subdirectory(${CMAKE_HOME_DIRECTORY}/examples/s4u)
+add_subdirectory(${CMAKE_HOME_DIRECTORY}/examples/s4u/basic)
+add_subdirectory(${CMAKE_HOME_DIRECTORY}/examples/s4u/io)
 
 add_subdirectory(${CMAKE_HOME_DIRECTORY}/examples/simdag)
 add_subdirectory(${CMAKE_HOME_DIRECTORY}/examples/simdag/dax)
