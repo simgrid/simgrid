19fee70e045c5e8bf8efe4b8223e17b0bba8a660
Martin Quinson <martin.quinson@loria.fr>
2016-02-27 17:31:43 +0100

    that bug is fixed now

--- teshsuite/bug-17132/bug-17132.c
+++ teshsuite/bug-17132/bug-17132.c
@@ -4,6 +4,8 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
+/* Bug report: https://gforge.inria.fr/tracker/index.php?func=detail&aid=17132&group_id=12&atid=165 */
+ 
 #include "xbt/log.h"
 #include <stdio.h>
 #include <mpi.h>
