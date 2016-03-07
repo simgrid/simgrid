872ebddf3297a212a6cf6c2b2ed816be9d5e9c51
Frederic Suter <frederic.suter@cc.in2p3.fr>
2016-03-07 12:08:00 +0100

    did I mention that I hate blank lines?

--- tools/sg_unit_extractor.pl
+++ tools/sg_unit_extractor.pl
@@ -5,10 +5,6 @@
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the license (GNU LGPL) which comes with this package.
 
-
-
-use strict;
-
 use strict;
 use Getopt::Long qw(GetOptions);
 
