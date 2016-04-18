b1dfbcb15b4a2b87a06482ebaebd9f1951af3d43
Frederic Suter <frederic.suter@cc.in2p3.fr>
2016-04-18 23:38:32 +0200

    wip... unalvining the tutorial

--- doc/msg-tuto-src/masterworker0.c
+++ doc/msg-tuto-src/masterworker0.c
@@ -4,41 +4,24 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include <stdio.h>
 #include "simgrid/msg.h"            /* Yeah! If you want to use msg, you need to include simgrid/msg.h */
-#include "xbt/sysdep.h"         /* calloc, printf */
 
-/* Create a log channel to have nice outputs. */
-#include "xbt/log.h"
-#include "xbt/asserts.h"
-XBT_LOG_NEW_DEFAULT_CATEGORY(msg_test,
-                             "Messages specific for this msg example");
-
-int master(int argc, char *argv[]);
-int worker(int argc, char *argv[]);
-msg_error_t test_all(const char *platform_file,
-                     const char *application_file);
+XBT_LOG_NEW_DEFAULT_CATEGORY(msg_test, "Messages specific for this msg example");
 
 #define FINALIZE ((void*)221297)        /* a magic number to tell people to stop working */
 
-/** Emitter function  */
-int master(int argc, char *argv[])
+/** Master expects 3+ arguments given in the XML deployment file: */
+static int master(int argc, char *argv[])
 {
   int workers_count = 0;
   msg_host_t *workers = NULL;
   msg_task_t *todo = NULL;
-  int number_of_tasks = 0;
-  double task_comp_size = 0;
-  double task_comm_size = 0;
 
   int i;
 
-  XBT_ATTRIB_UNUSED int res = sscanf(argv[1], "%d", &number_of_tasks);
-  xbt_assert(res,"Invalid argument %s\n", argv[1]);
-  res = sscanf(argv[2], "%lg", &task_comp_size);
-  xbt_assert(res, "Invalid argument %s\n", argv[2]);
-  res = sscanf(argv[3], "%lg", &task_comm_size);
-  xbt_assert(res, "Invalid argument %s\n", argv[3]);
+  long number_of_tasks = xbt_str_parse_int(argv[1], "Invalid amount of tasks: %s");    /** - Number of tasks      */
+  double comp_size = xbt_str_parse_double(argv[2], "Invalid computational size: %s");  /** - Task compute cost    */
+  double comm_size = xbt_str_parse_double(argv[3], "Invalid communication size: %s");  /** - Task communication size */
 
   {                             /*  Task creation */
     char sprintf_buffer[64];
@@ -47,9 +30,7 @@ int master(int argc, char *argv[])
 
     for (i = 0; i < number_of_tasks; i++) {
       sprintf(sprintf_buffer, "Task_%d", i);
-      todo[i] =
-          MSG_task_create(sprintf_buffer, task_comp_size, task_comm_size,
-                          NULL);
+      todo[i] =  MSG_task_create(sprintf_buffer, task_comp_size, task_comm_size, NULL);
     }
   }
 
@@ -59,17 +40,14 @@ int master(int argc, char *argv[])
 
     for (i = 4; i < argc; i++) {
       workers[i - 4] = MSG_get_host_by_name(argv[i]);
-      xbt_assert(workers[i - 4] != NULL, "Unknown host %s. Stopping Now! ",
-                  argv[i]);
+      xbt_assert(workers[i - 4] != NULL, "Unknown host %s. Stopping Now! ", argv[i]);
     }
   }
 
-  XBT_INFO("Got %d workers and %d tasks to process", workers_count,
-        number_of_tasks);
+  XBT_INFO("Got %d workers and %ld tasks to process", workers_count, number_of_tasks);
 
   for (i = 0; i < number_of_tasks; i++) {
-    XBT_INFO("Sending \"%s\" to \"%s\"",
-          todo[i]->name, MSG_host_get_name(workers[i % workers_count]));
+    XBT_INFO("Sending \"%s\" to \"%s\"", todo[i]->name, MSG_host_get_name(workers[i % workers_count]));
     if (MSG_host_self() == workers[i % workers_count]) {
       XBT_INFO("Hey ! It's me ! :)");
     }
@@ -78,8 +56,7 @@ int master(int argc, char *argv[])
     XBT_INFO("Sent");
   }
 
-  XBT_INFO
-      ("All tasks have been dispatched. Let's tell everybody the computation is over.");
+  XBT_INFO("All tasks have been dispatched. Let's tell everybody the computation is over.");
   for (i = 0; i < workers_count; i++) {
     msg_task_t finalize = MSG_task_create("finalize", 0, 0, FINALIZE);
     MSG_task_send(finalize, MSG_host_get_name(workers[i]));
@@ -91,8 +68,8 @@ int master(int argc, char *argv[])
   return 0;
 }                               /* end_of_master */
 
-/** Receiver function  */
-int worker(int argc, char *argv[])
+/** Worker expects a single argument given in the XML deployment file: */
+static int worker(int argc, char *argv[])
 {
   msg_task_t task = NULL;
   XBT_ATTRIB_UNUSED int res;
@@ -116,42 +93,24 @@ int worker(int argc, char *argv[])
   return 0;
 }                               /* end_of_worker */
 
-/** Test function */
-msg_error_t test_all(const char *platform_file,
-                     const char *application_file)
-{
-  msg_error_t res = MSG_OK;
-
-  {                             /*  Simulation setting */
-    MSG_create_environment(platform_file);
-  }
-  {                             /*   Application deployment */
-    MSG_function_register("master", master);
-    MSG_function_register("worker", worker);
-    MSG_launch_application(application_file);
-  }
-  res = MSG_main();
-
-  XBT_INFO("Simulation time %g", MSG_get_clock());
-  return res;
-}                               /* end_of_test_all */
-
-
 /** Main function */
 int main(int argc, char *argv[])
 {
   msg_error_t res = MSG_OK;
 
   MSG_init(&argc, argv);
-  if (argc < 3) {
-    printf("Usage: %s platform_file deployment_file\n", argv[0]);
-    printf("example: %s msg_platform.xml msg_deployment.xml\n", argv[0]);
-    exit(1);
+  xbt_assert(argc > 2, "Usage: %s platform_file deployment_file\n"
+             "\tExample: %s msg_platform.xml msg_deployment.xml\n", argv[0], argv[0]);
+  {                             /*  Simulation setting */
+    MSG_create_environment(argv[1]);
   }
-  res = test_all(argv[1], argv[2]);
+  {                             /*   Application deployment */
+    MSG_function_register("master", master);
+    MSG_function_register("worker", worker);
+    MSG_launch_application(argv[2]);
+  }
+  res = MSG_main();
 
-  if (res == MSG_OK)
-    return 0;
-  else
-    return 1;
+  XBT_INFO("Simulation time %g", MSG_get_clock());
+  return (res != MSG_OK);
 }                               /* end_of_main */
