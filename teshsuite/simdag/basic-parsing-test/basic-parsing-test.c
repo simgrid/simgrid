84f15c2e42762a20a3c923615c6424241421fda9
Martin Quinson <martin.quinson@loria.fr>
2016-04-20 19:33:46 +0200

    log to stderr to fix an output ordering issue on windows

--- teshsuite/simdag/basic-parsing-test/basic-parsing-test.c
+++ teshsuite/simdag/basic-parsing-test/basic-parsing-test.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 
   /* creation of the environment */
   SD_create_environment(argv[1]);
-  printf("Workstation number: %zu, link number: %d\n", sg_host_count(), sg_link_count());
+  fprintf(stderr, "Workstation number: %zu, link number: %d\n", sg_host_count(), sg_link_count());
 
   sg_host_t *hosts = sg_host_list();
   if (argc >= 3) {
@@ -23,14 +23,14 @@ int main(int argc, char **argv)
       const char *name1 = sg_host_get_name(h1);
       const char *name2 = sg_host_get_name(h2);
 
-      printf("Route between %s and %s\n", name1, name2);
+      fprintf(stderr, "Route between %s and %s\n", name1, name2);
       SD_link_t *route = SD_route_get_list(h1, h2);
       int route_size = SD_route_get_size(h1, h2);
-      printf("Route size %d\n", route_size);
+      fprintf(stderr, "Route size %d\n", route_size);
       for (int i = 0; i < route_size; i++)
-        printf("  Link %s: latency = %f, bandwidth = %f\n", sg_link_name(route[i]),
+        fprintf(stderr, "  Link %s: latency = %f, bandwidth = %f\n", sg_link_name(route[i]),
                sg_link_latency(route[i]), sg_link_bandwidth(route[i]));
-      printf("Route latency = %f, route bandwidth = %f\n",
+      fprintf(stderr, "Route latency = %f, route bandwidth = %f\n",
              SD_route_get_latency(h1, h2), SD_route_get_bandwidth(h1, h2));
       xbt_free(route);
     }
@@ -42,22 +42,22 @@ int main(int argc, char **argv)
         for (int j = 0; j < list_size; j++) {
           sg_host_t h2 = hosts[j];
           const char *name2 = sg_host_get_name(h2);
-          printf("Route between %s and %s\n", name1, name2);
+          fprintf(stderr, "Route between %s and %s\n", name1, name2);
           SD_link_t *route = SD_route_get_list(h1, h2);
           int route_size = SD_route_get_size(h1, h2);
-          printf("  Route size %d\n", route_size);
+          fprintf(stderr, "  Route size %d\n", route_size);
           for (int k = 0; k < route_size; k++) {
-            printf("  Link %s: latency = %f, bandwidth = %f\n",
+            fprintf(stderr, "  Link %s: latency = %f, bandwidth = %f\n",
                 sg_link_name(route[k]), sg_link_latency(route[k]), sg_link_bandwidth(route[k]));
           }
-          printf("  Route latency = %f, route bandwidth = %f\n",
+          fprintf(stderr, "  Route latency = %f, route bandwidth = %f\n",
                  SD_route_get_latency(h1, h2), SD_route_get_bandwidth(h1, h2));
           xbt_free(route);
         }
       }
     }
     if (!strcmp(argv[2], "PROP"))
-      printf("SG_TEST_mem: %s\n", sg_host_get_property_value(sg_host_by_name("host1"), "SG_TEST_mem"));
+      fprintf(stderr,"SG_TEST_mem: %s\n", sg_host_get_property_value(sg_host_by_name("host1"), "SG_TEST_mem"));
   }
   xbt_free(hosts);
 
