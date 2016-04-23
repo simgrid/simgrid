df3d5e44a868b07107aba9bf07683d8fdb95a6d8
Martin Quinson <martin.quinson@loria.fr>
2016-04-23 09:57:18 +0200

    Calling front() on empty deque is undefined
    - MSG_mailbox_is_empty supposes that it returns nullptr

--- src/simix/smx_network.cpp
+++ src/simix/smx_network.cpp
@@ -70,7 +70,7 @@ smx_mailbox_t SIMIX_mbox_get_by_name(const char *name)
 
 smx_synchro_t SIMIX_mbox_get_head(smx_mailbox_t mbox)
 {
-  return mbox->comm_queue->front();
+  return mbox->comm_queue->empty()? nullptr:mbox->comm_queue->front();
 }
 
 /**
