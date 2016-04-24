1635f65d1a7f3d175dfd6cefaa0403cdb278ffc7
degomme <augustin.degomme@unibas.ch>
2016-04-24 23:21:38 +0200

    Fix build

--- include/simgrid/s4u/mailbox.hpp
+++ include/simgrid/s4u/mailbox.hpp
@@ -2,6 +2,7 @@
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
+#ifdef __cplusplus
 
 #ifndef SIMGRID_S4U_MAILBOX_HPP
 #define SIMGRID_S4U_MAILBOX_HPP
