63e36b5e3deb6682ba14b5f4c7533d0b2c54a024
Gabriel Corona <gabriel.corona@loria.fr>
2016-05-04 14:19:03 +0200

    [mc] Merge mc_diff.cpp and mc_compare.cpp
    in order to ease refactoring.

--- tools/cmake/DefinePackages.cmake
+++ tools/cmake/DefinePackages.cmake
@@ -563,8 +563,7 @@ set(MC_SRC
   src/mc/mc_page_snapshot.cpp
   src/mc/mc_comm_pattern.h
   src/mc/mc_comm_pattern.cpp
-  src/mc/mc_compare.cpp
-  src/mc/mc_diff.cpp
+  src/mc/compare.cpp
   src/mc/mc_dwarf.hpp
   src/mc/mc_dwarf.cpp
   src/mc/mc_dwarf_attrnames.cpp
