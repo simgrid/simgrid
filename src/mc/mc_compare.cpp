a6d6661d57c57d9a3e920002292970fab3a0231c
Gabriel Corona <gabriel.corona@loria.fr>
2016-05-04 14:05:10 +0200

    [mc] Remove __STDC_FORMAT_MACROS
    It is not needed in C++.

--- src/mc/mc_compare.cpp
+++ src/mc/mc_compare.cpp
@@ -4,7 +4,6 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#define __STDC_FORMAT_MACROS
 #include <cinttypes>
 
 #include <utility>
