acc4596bfe40a4bf7f59d5b94bc5643f86806dda
Martin Quinson <martin.quinson@loria.fr>
2016-06-11 01:06:41 +0200

    No need for the OSSP copyright header on the backtrace thing, only for the exceptions

--- include/xbt/backtrace.h
+++ include/xbt/backtrace.h
@@ -1,44 +1,4 @@
-/* Copyright (c) 2005-2019. The SimGrid Team.
- * All rights reserved.                                                     */
-
-/*  Copyright (c) 2002-2004 Ralf S. Engelschall <rse@engelschall.com>       */
-/*  Copyright (c) 2002-2004 The OSSP Project <http://www.ossp.org/>         */
-/*  Copyright (c) 2002-2004 Cable & Wireless <http://www.cw.com/>           */
-/*  All rights reserved.                                                    */
-
-/* This code is inspirated from the OSSP version (as retrieved back in 2004)*/
-/* It was heavily modified to fit the SimGrid framework.                    */
-
-/* The OSSP version has the following copyright notice:
-**  OSSP ex - Exception Handling
-**  Copyright (c) 2002-2004 Ralf S. Engelschall <rse@engelschall.com>
-**  Copyright (c) 2002-2004 The OSSP Project <http://www.ossp.org/>
-**  Copyright (c) 2002-2004 Cable & Wireless <http://www.cw.com/>
-**
-**  This file is part of OSSP ex, an exception handling library
-**  which can be found at http://www.ossp.org/pkg/lib/ex/.
-**
-**  Permission to use, copy, modify, and distribute this software for
-**  any purpose with or without fee is hereby granted, provided that
-**  the above copyright notice and this permission notice appear in all
-**  copies.
-**
-**  THIS SOFTWARE IS PROVIDED `AS IS'' AND ANY EXPRESSED OR IMPLIED
-**  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-**  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-**  IN NO EVENT SHALL THE AUTHORS AND COPYRIGHT HOLDERS AND THEIR
-**  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-**  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-**  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
-**  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-**  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-**  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-**  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-**  SUCH DAMAGE.
- */
-
-/* The extensions made for the SimGrid project can either be distributed    */
-/* under the same license, or under the LGPL v2.1                           */
+/* Copyright (c) 2005-2016. The SimGrid Team. All rights reserved.          */
 
 #ifndef SIMGRIX_XBT_BACKTRACE_H
 #define SIMGRIX_XBT_BACKTRACE_H
