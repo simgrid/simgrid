b47f6fd0559fef467bcdfce8cf608c7c3c40b6f3
adfaure <adrien.faure2@gmail.com>
2016-06-15 15:45:56 +0200

    [s4u] add mutex

---
+++ src/s4u/s4u_mutex.cpp
@@ -0,0 +1,31 @@
+/* Copyright (c) 2006-2015. The SimGrid Team.
+ * All rights reserved.                                                     */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include "xbt/log.h"
+#include "src/msg/msg_private.h"
+#include "src/simix/smx_network_private.h"
+
+#include "simgrid/s4u/mutex.hpp"
+
+
+using namespace simgrid;
+
+s4u::Mutex::Mutex() {
+    smx_mutex_t smx_mutex = simcall_mutex_init();
+    _mutex = std::shared_ptr<s_smx_mutex>(smx_mutex, SIMIX_mutex_destroy  );
+}
+
+void s4u::Mutex::lock() {
+  simcall_mutex_lock(_mutex.get());
+}
+
+void s4u::Mutex::unlock() {
+  simcall_mutex_unlock(_mutex.get());
+}
+
+bool s4u::Mutex::try_lock() {
+  return simcall_mutex_trylock(_mutex.get());
+}
--- tools/cmake/DefinePackages.cmake
+++ tools/cmake/DefinePackages.cmake
@@ -381,6 +381,7 @@ set(S4U_SRC
   src/s4u/s4u_file.cpp  
   src/s4u/s4u_host.cpp  
   src/s4u/s4u_mailbox.cpp
+  src/s4u/s4u_mutex.cpp
   src/s4u/s4u_storage.cpp
 )
 
@@ -644,6 +645,7 @@ set(headers_to_install
   include/simgrid/s4u/file.hpp  
   include/simgrid/s4u/host.hpp  
   include/simgrid/s4u/mailbox.hpp  
+  include/simgrid/s4u/mutex.hpp  
   include/simgrid/s4u/storage.hpp  
   include/simgrid/s4u.h
   include/simgrid/plugins/energy.h
