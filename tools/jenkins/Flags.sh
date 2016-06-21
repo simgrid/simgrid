9df50f62d16906ea5863e6de9516958bbcc3791a
degomme <augustin.degomme@unibas.ch>
2016-06-21 15:07:31 +0200

    replace mallocator flags by debug one for nightly tests

--- tools/jenkins/Flags.sh
+++ tools/jenkins/Flags.sh
@@ -35,16 +35,16 @@ cd $WORKSPACE/build
 
 for buildjava in ON OFF
 do
-  for buildmalloc in ON OFF
+  for builddebug in ON OFF
   do
     for buildsmpi in ON OFF
     do
       for buildmc in ON OFF
       do
-        echo "build with java=${buildjava}, mallocators=${buildmalloc}, SMPI=${buildsmpi}, MC=${buildmc}"
+        echo "build with java=${buildjava}, debug=${builddebug}, SMPI=${buildsmpi}, MC=${buildmc}"
         cmake -Denable_documentation=OFF -Denable_lua=ON -Denable_java=${buildjava} \
               -Denable_compile_optimizations=OFF -Denable_compile_warnings=ON \
-              -Denable_jedule=ON -Denable_mallocators=${buildmalloc} \
+              -Denable_jedule=ON -Denable_mallocators=ON -Denable_debug=${builddebug} \
               -Denable_smpi=${buildsmpi} -Denable_smpi_MPICH3_testsuite=${buildsmpi} -Denable_model-checking=${buildmc} \
               -Denable_memcheck=OFF -Denable_memcheck_xml=OFF -Denable_smpi_ISP_testsuite=OFF -Denable_coverage=OFF $WORKSPACE
         make -j$NUMPROC
