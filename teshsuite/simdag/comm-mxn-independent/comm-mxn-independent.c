ba15f0e8e5fe9918bad627ec253f3fcae8921432
Frederic Suter <frederic.suter@cc.in2p3.fr>
2016-08-02 10:04:43 +0200

    cosmetics in teshsuite

--- teshsuite/simdag/comm-mxn-independent/comm-mxn-independent.c
+++ teshsuite/simdag/comm-mxn-independent/comm-mxn-independent.c
@@ -6,8 +6,8 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include <stdio.h>
 #include "simgrid/simdag.h"
+XBT_LOG_NEW_DEFAULT_SUBCATEGORY(comm_mxn_independent, sd, "SimDag test independent communications");
 
 /*
  * intra communication test
@@ -26,19 +26,16 @@ int main(int argc, char **argv)
                                     0.0, 0.0, 0.0, 1.0,
                                     0.0, 0.0, 0.0, 0.0 };
 
-  double no_cost[] = { 0.0, 0.0, 0.0, 0.0 };
-
   SD_init(&argc, argv);
   SD_create_environment(argv[1]);
 
   SD_task_t task = SD_task_create("Comm 1", NULL, 1.0);
 
-  SD_task_schedule(task, 4, sg_host_list(), no_cost, communication_amount, -1.0);
+  SD_task_schedule(task, 4, sg_host_list(), SD_SCHED_NO_COST, communication_amount, -1.0);
 
   SD_simulate(-1.0);
 
-  printf("%g\n", SD_get_clock());
-  fflush(stdout);
+  XBT_INFO("%g", SD_get_clock());
 
   SD_task_destroy(task);
 
--- teshsuite/simdag/comm-mxn-scatter/comm-mxn-scatter.c
+++ teshsuite/simdag/comm-mxn-scatter/comm-mxn-scatter.c
@@ -6,8 +6,8 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include <stdio.h>
 #include "simgrid/simdag.h"
+XBT_LOG_NEW_DEFAULT_SUBCATEGORY(comm_mxn_scatter, sd, "SimDag test scatter");
 
 /*
  * intra communication test 1
@@ -22,24 +22,21 @@
 
 int main(int argc, char **argv)
 {
-
   double communication_amount[] = { 0.0, 1.0, 2.0, 3.0,
                                     0.0, 0.0, 0.0, 0.0,
                                     0.0, 0.0, 0.0, 0.0,
                                     0.0, 0.0, 0.0, 0.0 };
-  double no_cost[] = { 0.0, 0.0, 0.0, 0.0 };
 
   SD_init(&argc, argv);
   SD_create_environment(argv[1]);
 
   SD_task_t task = SD_task_create("Scatter task", NULL, 1.0);
 
-  SD_task_schedule(task, 4, sg_host_list(), no_cost, communication_amount, -1.0);
+  SD_task_schedule(task, 4, sg_host_list(), SD_SCHED_NO_COST, communication_amount, -1.0);
 
   SD_simulate(-1.0);
 
-  printf("%g\n", SD_get_clock());
-  fflush(stdout);
+  XBT_INFO("%g", SD_get_clock());
 
   SD_task_destroy(task);
 
