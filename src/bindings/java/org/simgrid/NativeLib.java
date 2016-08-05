3a736c9e7ea022b653f0c053e68bad270c2b6d37
Martin Quinson <martin.quinson@loria.fr>
2016-08-06 01:25:03 +0200

    try to greatly simplify this file

--- src/bindings/java/org/simgrid/NativeLib.java
+++ src/bindings/java/org/simgrid/NativeLib.java
@@ -1,5 +1,4 @@
-/* Copyright (c) 2014. The SimGrid Team.
- * All rights reserved.                                                     */
+/* Copyright (c) 2014-2016. The SimGrid Team. All rights reserved.          */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
@@ -14,15 +13,31 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/** Helper class loading the native functions of SimGrid that we use for downcalls 
+ * 
+ * Almost all org.simgrid.msg.* classes contain a static bloc (thus executed when the class is loaded) 
+ * containing a call to this.
+ */
 public final class NativeLib {
-	/* Statically load the library which contains all native functions used in here */
 	private static boolean isNativeInited = false;
-	static Path tempDir = null;
+	static Path tempDir = null; // where the embeeded libraries are unpacked before loading them
 
+	/** A static-only "class" don't need no constructor */
 	private NativeLib() {
 		throw new IllegalAccessError("Utility class");
 	}
 
+	/** Hidden debug main() function 
+	 * 
+	 * It is not the Main-Class defined in src/bindings/java/MANIFEST.in (org.simgrid.msg.Msg is),
+	 * so it won't get executed by default. But that's helpful to debug linkage errors, if you
+	 * know that it exists. It's used by cmake during the configure, to inform the user.
+	 */
+	public static void main(String[] args) {
+		System.out.println("This jarfile searches the native code under: " +getPath());
+	}
+	
+	/** Main function loading all the native classes that we need */
 	public static void nativeInit() {
 		if (isNativeInited)
 			return;
@@ -35,38 +50,95 @@ public final class NativeLib {
 		isNativeInited = true;
 	}
 
+	/** Helper function trying to load one requested library */
 	public static void nativeInit(String name) {
+		Throwable cause = null;
 		try {
 			/* Prefer the version of the library bundled into the jar file and use it */
-			loadLib(name);
-		} catch (LinkageException embeededException) {
-			/* If not found, try to see if we can find a version on disk */
+			if (loadLibAsStream(name))
+				return;
+		} catch (UnsatisfiedLinkError|SecurityException|IOException e) {
+			cause = e;
+		}
+		
+		/* If not found, try to see if we can find a version on disk */
+		try {
+			System.loadLibrary(name);
+			return;
+		} catch (UnsatisfiedLinkError systemException) { /* don't care */ }
+		
+		System.err.println("\nCannot load the bindings to the "+name+" library in path "+getPath()+" and no usable SimGrid installation found on disk.");
+		if (cause != null) {
+			if (cause.getMessage().matches(".*libcgraph.so.*"))
+				System.err.println("HINT: Try to install the libcgraph package (sudo apt-get install libcgraph).");
+			else if (cause.getMessage().matches(".*libboost_context.so.*"))
+				System.err.println("HINT: Try to install the boost-context package (sudo apt-get install libboost-context-dev).");
+			else
+				System.err.println("Try to install the missing dependencies, if any. Read carefully the following error message.");
+
+			System.err.println();
+			cause.printStackTrace();
+		} else {
+			System.err.println("This jar file does not seem to fit your system, and no usable SimGrid installation found on disk.");
+		}
+		System.exit(1);
+	}
+
+	/** Try to extract the library from the jarfile before loading it */
+	private static boolean loadLibAsStream (String name) throws SecurityException, IOException, UnsatisfiedLinkError {
+		String path = NativeLib.getPath();
+		
+		/* For each possible filename of the given library on all possible OSes, try it */
+		for (String filename : new String[] 
+		   { name, 
+		     "lib"+name+".so",               /* linux */
+		     name+".dll", "lib"+name+".dll", /* windows (pure and mingw) */
+		     "lib"+name+".dylib"             /* mac osx */}) {
+			
+			InputStream in = NativeLib.class.getClassLoader().getResourceAsStream(path+filename);
+			if (in == null)
+				continue; // Try the next name: no such file found
+			
+			OutputStream out = null;
 			try {
-				System.loadLibrary(name);
-			} catch (UnsatisfiedLinkError systemException) {
-				if (! "boost_context".equals(name)) { // Ignore when we cannot load boost_context
-					
-					System.err.println("\nCannot load the bindings to the "+name+" library in path "+getPath());
-					Throwable cause = embeededException.getCause();
-					if (cause instanceof java.lang.UnsatisfiedLinkError) {
-						if (cause.getMessage().matches(".*libcgraph.so.*"))
-							System.err.println("HINT: Try to install the libcgraph package (sudo apt-get install libcgraph).");
-						else if (cause.getMessage().matches(".*libboost_context.so.*"))
-							System.err.println("HINT: Try to install the boost-context package (sudo apt-get install libboost-context-dev).");
-						else
-							System.err.println("Try to install the missing dependencies, which name should appear above.");
-					} else {
-						System.err.println("This jar file does not seem to fit your system, and no usable SimGrid installation found on disk.");
-					}
-					System.err.println();
-					cause.printStackTrace();
-					System.exit(1);
+				// We must write the lib onto the disk before loading it -- stupid operating systems
+				if (tempDir == null) {
+					tempDir = Files.createTempDirectory("simgrid-java-");
+					// don't leak the files on disk, but remove it on JVM shutdown
+					Runtime.getRuntime().addShutdownHook(new Thread(new FileCleaner(tempDir.toFile())));
+				}
+				File fileOut = new File(tempDir.toFile().getAbsolutePath() + File.separator + filename);
+				
+				/* copy the library in position */  
+				out = new FileOutputStream(fileOut);
+				byte[] buffer = new byte[4096]; 
+				int bytesRead; 
+				while ((bytesRead = in.read(buffer)) != -1)     // Read until EOF
+					out.write(buffer, 0, bytesRead); 
+				
+				/* load that shit */
+				System.load(fileOut.getAbsolutePath());
+				
+				/* It loaded! we're good */
+				return true;
+				
+			} finally {
+				/* Always close all descriptors, no matter success or error */
+				try { 
+					in.close();
+					out.close();
+				} catch (IOException e) {
+					/* Too bad. I dont care. */
 				}
 			}
 		}
+		
+		/* No suitable name found */
+		return false;
 	}
 
-	public static String getPath() {
+	/** Find where to search for the library in the jar -- keep it aligned with where cmake puts it! */
+	private static String getPath() {
 		// Inspiration: https://github.com/xerial/snappy-java/blob/develop/src/main/java/org/xerial/snappy/OSInfo.java
 		String prefix = "NATIVE";
 		String os = System.getProperty("os.name");
@@ -87,72 +159,8 @@ public final class NativeLib {
 
 		return prefix + "/" + os + "/" + arch + "/";
 	}
-
-	private static void loadLib (String name) throws LinkageException {
-		String path = NativeLib.getPath();
-
-		String filename=name;
-		InputStream in = NativeLib.class.getClassLoader().getResourceAsStream(path+filename);
-		OutputStream out = null;
-		
-		if (in == null) {
-			filename = "lib"+name+".so";
-			in = NativeLib.class.getClassLoader().getResourceAsStream(path+filename);
-		}
-		if (in == null) {
-			filename = name+".dll";
-			in =  NativeLib.class.getClassLoader().getResourceAsStream(path+filename);
-		}
-		if (in == null) {
-			filename = "lib"+name+".dll";
-			in =  NativeLib.class.getClassLoader().getResourceAsStream(path+filename);
-		}
-		if (in == null) {
-			filename = "lib"+name+".dylib";
-			in =  NativeLib.class.getClassLoader().getResourceAsStream(path+filename);
-		}
-		if (in == null) {
-			throw new LinkageException("Cannot find library "+name+" in path "+path+". Sorry, but this jar does not seem to be usable on your machine.");
-		}
-		try {
-			// We must write the lib onto the disk before loading it -- stupid operating systems
-			if (tempDir == null) {
-				tempDir = Files.createTempDirectory("simgrid-java-");
-				// don't leak the files on disk, but remove it on JVM shutdown
-				Runtime.getRuntime().addShutdownHook(new Thread(new FileCleaner(tempDir.toFile())));
-			}
-			File fileOut = new File(tempDir.toFile().getAbsolutePath() + File.separator + filename);
-
-			/* copy the library in position */  
-			out = new FileOutputStream(fileOut);
-			byte[] buffer = new byte[4096]; 
-			int bytesRead; 
-			while ((bytesRead = in.read(buffer)) != -1)     // Read until EOF
-				out.write(buffer, 0, bytesRead); 
-
-			/* load that shit */
-			System.load(fileOut.getAbsolutePath());
-		} catch (SecurityException|UnsatisfiedLinkError|IOException e) {
-			throw new LinkageException("Cannot load the bindings to the "+name+" library in path "+getPath(), e);
-		} finally {
-			/* Always close all descriptors, no matter success or error */
-			try { 
-				in.close();
-			} catch (IOException e) {
-				/* Too bad. I dont care. */
-			}
-			
-			if (out != null) {
-				try { 
-				out.close();
-				} catch (IOException e) {
-					/* Too bad too. I dont care either. */
-				}
-			}
-		}
-	}
-
-	/* A hackish mechanism used to remove the file containing our library when the JVM shuts down */
+	
+	/** A hackish mechanism used to remove the file containing our library when the JVM shuts down */
 	private static class FileCleaner implements Runnable {
 		private File dir;
 		public FileCleaner(File dir) {
@@ -171,21 +179,5 @@ public final class NativeLib {
 				e.printStackTrace();
 			}
 		}
-	}
-
-
-	public static void main(String[] args) {
-		System.out.println("This jarfile searches the native code under: " +getPath());
-	}
-}
-
-class LinkageException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public LinkageException(String msg) {
-		super(msg);
-	}
-
-	public LinkageException(String msg, Throwable e) {
-		super(msg,e);
 	}
 }
