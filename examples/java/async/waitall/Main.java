2200b1c5deda280da75aed5cc437bb5bce918e89
Martin Quinson <martin.quinson@loria.fr>
2016-08-23 02:36:28 +0200

    java does not like uppercased packages

--- examples/java/async/waitall/Main.java
+++ examples/java/async/waitall/Main.java
@@ -4,7 +4,7 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-package async.waitAll;
+package async.waitall;
 
 /** This example demonstrates the use of the asynchrounous communications
  * 
