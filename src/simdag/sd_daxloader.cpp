734403661f401a4ddabf38086e7ee443f7680a98
Martin Quinson <martin.quinson@loria.fr>
2016-09-26 01:17:59 +0200

    merge a collapsible if statement to please sonar

--- src/simdag/sd_daxloader.cpp
+++ src/simdag/sd_daxloader.cpp
@@ -87,11 +87,9 @@ bool acyclic_graph_detail(xbt_dynar_t dag){
   if(!all_marked){
     XBT_VERB("there is at least one cycle in your task graph");
     xbt_dynar_foreach(dag,count,task){
-      if(task->kind != SD_TASK_COMM_E2E) {
-        if(task->predecessors->empty() && task->inputs->empty()){
-          task->marked = 1;
-          current.push_back(task);
-        }
+      if(task->kind != SD_TASK_COMM_E2E && task->predecessors->empty() && task->inputs->empty()){
+	task->marked = 1;
+	current.push_back(task);
       }
     }
     //test if something has to be done for the next iteration
