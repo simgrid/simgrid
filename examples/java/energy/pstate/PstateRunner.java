648a27ac5171a4e9fa142617ff7104aae77be888
Martin Quinson <martin.quinson@loria.fr>
2017-01-20 00:30:04 +0100

    please sonar, even if it is really pesky sometimes

--- examples/java/energy/pstate/PstateRunner.java
+++ examples/java/energy/pstate/PstateRunner.java
@@ -25,54 +25,53 @@ public class PstateRunner extends Process {
 		@Override
 		public void main(String[] strings) throws HostNotFoundException, HostFailureException, TaskCancelledException {
 			double workload = 100E6;
-			int new_peak_index=2;
+			int newPstate = 2;
 			Host host = getHost();
 
 			int nb = host.getPstatesCount();
 			Msg.info("Count of Processor states="+ nb);
 
-			double current_peak = host.getCurrentPowerPeak();
-			Msg.info("Current power peak=" + current_peak);
+			double currentPeak = host.getCurrentPowerPeak();
+			Msg.info("Current power peak=" + currentPeak);
 
 			// Run a task
 			Task task1 = new Task("t1", workload, 0);
 			task1.execute();
 
-			double task_time = Msg.getClock();
-			Msg.info("Task1 simulation time: "+ task_time);
+			double taskTime = Msg.getClock();
+			Msg.info("Task1 simulation time: "+ taskTime);
 
 			// Change power peak
-			if ((new_peak_index >= nb) || (new_peak_index < 0)){
-				Msg.info("Cannot set pstate "+new_peak_index+"%d, host supports only "+nb+" pstates.");
+			if ((newPstate >= nb) || (newPstate < 0)){
+				Msg.info("Cannot set pstate "+newPstate+"%d, host supports only "+nb+" pstates.");
 				return;
 			}
 
-			double peak_at = host.getPowerPeakAt(new_peak_index);
-			Msg.info("Changing power peak value to "+peak_at+" (at index "+new_peak_index+")");
+			double peakAt = host.getPowerPeakAt(newPstate);
+			Msg.info("Changing power peak value to "+peakAt+" (at index "+newPstate+")");
 
-			host.setPstate(new_peak_index);
+			host.setPstate(newPstate);
 
-			current_peak = host.getCurrentPowerPeak();
-			Msg.info("Current power peak="+ current_peak);
+			currentPeak = host.getCurrentPowerPeak();
+			Msg.info("Current power peak="+ currentPeak);
 
 			// Run a second task
-			task1 = new Task("t1", workload, 0);
+			new Task("t1", workload, 0).execute();;
 
-			task_time = Msg.getClock() - task_time;
-			Msg.info("Task2 simulation time: "+ task_time);
+			taskTime = Msg.getClock() - taskTime;
+			Msg.info("Task2 simulation time: "+ taskTime);
 
 			// Verify the default pstate is set to 0
 			host = Host.getByName("MyHost2");
 			int nb2 = host.getPstatesCount();
 			Msg.info("Count of Processor states="+ nb2);
 
-			double current_peak2 = host.getCurrentPowerPeak();
-			Msg.info("Current power peak=" + current_peak2);
-			return ;
+			double currentPeak2 = host.getCurrentPowerPeak();
+			Msg.info("Current power peak=" + currentPeak2);
 		}
 	}
 
-	PstateRunner(Host host, String name, String[] args) throws HostNotFoundException, NativeException  {
+	PstateRunner(Host host, String name, String[] args) throws NativeException  {
 		super(host, name, args);
 	}
 
