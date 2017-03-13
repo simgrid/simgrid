bfed6f86796925bff5073bec92557352098fa635
Martin Quinson <martin.quinson@loria.fr>
2017-03-13 18:15:13 +0100

    cosmetics

--- src/bindings/java/jmsg_process.cpp
+++ src/bindings/java/jmsg_process.cpp
@@ -107,7 +107,7 @@ JNIEXPORT void JNICALL Java_org_simgrid_msg_Process_create(JNIEnv * env, jobject
   /* Actually build the MSG process */
   process = MSG_process_create_with_environment(name, [](int argc, char** argv) -> int {
               msg_process_t process = MSG_process_self();
-              // This is the jprocess passed as environment.
+              // This is the jprocess passed as process data.
               // It would be simpler if we could use a closure.
               jobject jprocess = (jobject) MSG_process_get_data(process);
               simgrid::kernel::context::java_main_jprocess(jprocess);
