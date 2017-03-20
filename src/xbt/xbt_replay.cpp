e9a351f4dbb9fdf5c9eb9eaaddb47dd357b9ea0d
Frederic Suter <frederic.suter@cc.in2p3.fr>
2017-03-20 14:30:06 +0100

    yet another simplification pass

--- src/xbt/xbt_replay.cpp
+++ src/xbt/xbt_replay.cpp
@@ -4,17 +4,11 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include "src/internal_config.h"
 #include "xbt/ex.hpp"
 #include "xbt/log.h"
 #include "xbt/replay.hpp"
-#include "xbt/str.h"
-#include "xbt/sysdep.h"
 
 #include <boost/algorithm/string.hpp>
-#include <ctype.h>
-#include <errno.h>
-#include <wchar.h>
 
 XBT_LOG_NEW_DEFAULT_SUBCATEGORY(replay,xbt,"Replay trace reader");
 
@@ -26,8 +20,10 @@ std::unordered_map<std::string, action_fun> action_funs;
 
 static void read_and_trim_line(std::ifstream* fs, std::string* line)
 {
-  std::getline(*fs, *line);
-  boost::trim(*line);
+  do {
+    std::getline(*fs, *line);
+    boost::trim(*line);
+  } while (!fs->eof() && (line->length() == 0 || line->front() == '#'));
   XBT_DEBUG("got from trace: %s", line->c_str());
 }
 
@@ -36,17 +32,12 @@ class ReplayReader {
   std::string line;
 
 public:
-  char* filename_;
-  int linenum = 0;
-
   explicit ReplayReader(const char* filename)
   {
-    filename_ = xbt_strdup(filename);
-    fs        = new std::ifstream(filename, std::ifstream::in);
+    fs = new std::ifstream(filename, std::ifstream::in);
   }
   ~ReplayReader()
   {
-    free(filename_);
     delete fs;
   }
   bool get(ReplayAction* action);
@@ -55,17 +46,9 @@ public:
 bool ReplayReader::get(ReplayAction* action)
 {
   read_and_trim_line(fs, &line);
-  linenum++;
 
-  if (line.length() > 0 && line.find("#") == std::string::npos) {
-    boost::split(*action, line, boost::is_any_of(" \t"), boost::token_compress_on);
-    return !fs->eof();
-  } else {
-    if (fs->eof())
-      return false;
-    else
-      return this->get(action);
-  }
+  boost::split(*action, line, boost::is_any_of(" \t"), boost::token_compress_on);
+  return !fs->eof();
 }
 
 static ReplayAction* get_action(char* name)
@@ -76,33 +59,30 @@ static ReplayAction* get_action(char* name)
   if (action_queues.find(std::string(name)) != action_queues.end())
     myqueue = action_queues.at(std::string(name));
   if (myqueue == nullptr || myqueue->empty()) { // Nothing stored for me. Read the file further
-    if (action_fs == nullptr) {                 // File closed now. There's nothing more to read. I'm out of here
-      goto todo_done;
-    }
     // Read lines until I reach something for me (which breaks in loop body) or end of file reached
-    while (!action_fs->eof()) {
+    while (true) {
       std::string action_line;
       read_and_trim_line(action_fs, &action_line);
-      if (action_line.length() > 0 && action_line.find("#") == std::string::npos) {
-        /* we cannot split in place here because we parse&store several lines for the colleagues... */
-        action = new ReplayAction();
-        boost::split(*action, action_line, boost::is_any_of(" \t"), boost::token_compress_on);
+      if (action_fs->eof())
+        break;
+      /* we cannot split in place here because we parse&store several lines for the colleagues... */
+      action = new ReplayAction();
+      boost::split(*action, action_line, boost::is_any_of(" \t"), boost::token_compress_on);
 
-        // if it's for me, I'm done
-        std::string evtname = action->front();
-        if (evtname.compare(name) == 0) {
-          return action;
-        } else {
-          // Else, I have to store it for the relevant colleague
-          std::queue<ReplayAction*>* otherqueue = nullptr;
-          if (action_queues.find(evtname) != action_queues.end())
-            otherqueue = action_queues.at(evtname);
-          if (otherqueue == nullptr) { // Damn. Create the queue of that guy
-            otherqueue = new std::queue<ReplayAction*>();
-            action_queues.insert({evtname, otherqueue});
-          }
-          otherqueue->push(action);
+      // if it's for me, I'm done
+      std::string evtname = action->front();
+      if (evtname.compare(name) == 0) {
+        return action;
+      } else {
+        // Else, I have to store it for the relevant colleague
+        std::queue<ReplayAction*>* otherqueue = nullptr;
+        if (action_queues.find(evtname) != action_queues.end())
+          otherqueue = action_queues.at(evtname);
+        else { // Damn. Create the queue of that guy
+          otherqueue = new std::queue<ReplayAction*>();
+          action_queues.insert({evtname, otherqueue});
         }
+        otherqueue->push(action);
       }
     }
     // end of file reached while searching in vain for more work
@@ -111,13 +91,6 @@ static ReplayAction* get_action(char* name)
     action = myqueue->front();
     myqueue->pop();
     return action;
-  }
-
-// All my actions in the file are done and either I or a colleague closed the file. Let's cleanup before leaving.
-todo_done:
-  if (myqueue != nullptr) {
-    delete myqueue;
-    action_queues.erase(std::string(name));
   }
   return nullptr;
 }
@@ -164,6 +137,11 @@ int replay_runner(int argc, char* argv[])
       simgrid::xbt::handle_action(evt);
       delete evt;
     }
+    if (action_queues.find(std::string(argv[0])) != action_queues.end()) {
+      std::queue<ReplayAction*>* myqueue = action_queues.at(std::string(argv[0]));
+      delete myqueue;
+      action_queues.erase(std::string(argv[0]));
+    }
   } else { // Should have got my trace file in argument
     simgrid::xbt::ReplayAction* evt = new simgrid::xbt::ReplayAction();
     xbt_assert(argc >= 2, "No '%s' agent function provided, no simulation-wide trace file provided, "
@@ -171,10 +149,10 @@ int replay_runner(int argc, char* argv[])
                argv[0]);
     simgrid::xbt::ReplayReader* reader = new simgrid::xbt::ReplayReader(argv[1]);
     while (reader->get(evt)) {
-      if (evt->at(0).compare(argv[0]) == 0) {
+      if (evt->front().compare(argv[0]) == 0) {
         simgrid::xbt::handle_action(evt);
       } else {
-        XBT_WARN("%s:%d: Ignore trace element not for me", reader->filename_, reader->linenum);
+        XBT_WARN("Ignore trace element not for me");
       }
       evt->clear();
     }
