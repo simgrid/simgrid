459121e78aced14777d84c5c93610fa0305935fa
Martin Quinson <martin.quinson@loria.fr>
2017-03-21 14:49:18 +0100

    make it easier to run that example from eclipse directly

--- examples/java/app/pingpong/Main.java
+++ examples/java/app/pingpong/Main.java
@@ -1,10 +1,11 @@
-/* Copyright (c) 2006-2014, 2016. The SimGrid Team.
- * All rights reserved.                                                     */
+/* Copyright (c) 2006-2017. The SimGrid Team. All rights reserved.          */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
 package app.pingpong;
+import java.io.File;
+
 import org.simgrid.msg.HostNotFoundException;
 import org.simgrid.msg.Msg;
  
@@ -16,13 +17,16 @@ class Main {
 
   public static void main(String[] args) throws HostNotFoundException {
     Msg.init(args);
-    if(args.length < 1) {
-      Msg.info("Usage   : Main platform_file");
-      Msg.info("example : Main ../platforms/platform.xml");
-      System.exit(1);
-    }
-
-    Msg.createEnvironment(args[0]);
+    
+    String platfFile = "../../examples/platforms/small_platform.xml";
+    if (args.length == 1)
+    	platfFile = args[0];
+    
+    File f = new File(platfFile); 
+    if (!f.exists())
+    	System.err.println("File "+platfFile+" does not exist in "+System.getProperty("user.dir"));
+    
+    Msg.createEnvironment(platfFile);
     new Sender("Jacquelin", "Sender", new String[] {"Boivin"}).start();
     new Receiver ("Boivin", "Receiver", null).start();
 
