297cc676934878678d97ee003c00212a80822216
Martin Quinson <martin.quinson@loria.fr>
2017-04-14 15:25:07 +0200

    cosmetics

--- src/smpi/smpi_main.c
+++ src/smpi/smpi_main.c
@@ -1,5 +1,4 @@
-/* Copyright (c) 2007-2015. The SimGrid Team.
- * All rights reserved.                                                     */
+/* Copyright (c) 2007-2017. The SimGrid Team. All rights reserved.          */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
@@ -12,7 +11,7 @@
 int main(int argc, char **argv)
 {
   if (argc < 2) {
-    fprintf(stderr, "Missing SMPI program to launch\n");
+    fprintf(stderr, "Usage: smpi_main <program to launch>\n");
     exit(1);
   }
   return smpi_main(argv[1], argc - 1, argv + 1);
