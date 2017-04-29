60478d1730b5d604df65e9d5b10f3b77cd770b76
Martin Quinson <martin.quinson@loria.fr>
2017-04-30 00:00:21 +0200

    fix some doxygen errors

--- src/msg/msg_process.cpp
+++ src/msg/msg_process.cpp
@@ -22,8 +22,8 @@ SG_BEGIN_DECL()
 
 /******************************** Process ************************************/
 /**
- * \brief Cleans the MSG data of a process.
- * \param smx_proc a SIMIX process
+ * \brief Cleans the MSG data of an actor
+ * \param smx_actor a SIMIX actor
  */
 void MSG_process_cleanup_from_SIMIX(smx_actor_t smx_actor)
 {
