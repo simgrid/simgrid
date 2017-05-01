39423c5e63cbaa7fc3c41a4af6037948c4033225
Martin Quinson <martin.quinson@loria.fr>
2017-05-01 09:42:56 +0200

    Fix java builds

--- src/bindings/java/jmsg_task.cpp
+++ src/bindings/java/jmsg_task.cpp
@@ -5,7 +5,7 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include <simgrid/s4u/host.hpp>
+#include "simgrid/s4u/Host.hpp"
 
 #include "jmsg.h"
 #include "jmsg_host.h"
