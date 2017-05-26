1e7a5ec3b49493264a7950dff4b595d790f800d0
Martin Quinson <martin.quinson@loria.fr>
2017-05-26 16:14:53 +0200

    fix MC on 32bits

--- src/mc/Process.cpp
+++ src/mc/Process.cpp
@@ -1,8 +1,9 @@
-/* Copyright (c) 2014-2015. The SimGrid Team.
- * All rights reserved.                                                     */
+/* Copyright (c) 2014-2017. The SimGrid Team. All rights reserved.          */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#define _FILE_OFFSET_BITS 64 /* needed for pread_whole to work as expected on 32bits */
 
 #include <assert.h>
 #include <stddef.h>
