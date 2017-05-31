eb585bd41bf4d62a48c8f0e86a99608a4a228b76
Martin Quinson <martin.quinson@loria.fr>
2017-05-31 17:05:24 +0200

    Make it somehow easier to debug NS3/SimGrid

--- src/surf/network_ns3.cpp
+++ src/surf/network_ns3.cpp
@@ -163,7 +163,7 @@ NetworkNS3Model::NetworkNS3Model() : NetworkModel() {
 
   simgrid::kernel::routing::NetPoint::onCreation.connect([](simgrid::kernel::routing::NetPoint* pt) {
     pt->extension_set<NetPointNs3>(new NetPointNs3());
-
+    XBT_VERB("SimGrid's %s is node %d within NS3", pt->cname(), pt->extension<NetPointNs3>()->node_num);
   });
   simgrid::surf::on_cluster.connect(&clusterCreation_cb);
   simgrid::s4u::onPlatformCreated.connect(&postparse_cb);
@@ -320,6 +320,7 @@ void NetworkNS3Action::resume() {
 std::list<LinkImpl*> NetworkNS3Action::links()
 {
   THROW_UNIMPLEMENTED;
+  return nullptr;
 }
 
 /* Test whether a flow is suspended */
