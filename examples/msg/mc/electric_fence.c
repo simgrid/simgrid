1a3e24862f3cbccfe7604af2e812b0d94af72e3b
Martin Quinson <martin.quinson@loria.fr>
2017-06-02 10:15:25 +0200

    please sonar a tiny bit

--- examples/msg/mc/electric_fence.c
+++ examples/msg/mc/electric_fence.c
@@ -1,5 +1,4 @@
-/* Copyright (c) 2013-2015. The SimGrid Team.
- * All rights reserved.                                                     */
+/* Copyright (c) 2013-2017. The SimGrid Team. All rights reserved.          */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
@@ -21,7 +20,8 @@ XBT_LOG_NEW_DEFAULT_CATEGORY(electric_fence, "Example to check the soundness of 
 
 static int server(int argc, char *argv[])
 {
-  msg_task_t task1 = NULL, task2 = NULL;
+  msg_task_t task1 = NULL;
+  msg_task_t task2 = NULL;
 
   msg_comm_t comm_received1 = MSG_task_irecv(&task1, "mymailbox");
   msg_comm_t comm_received2 = MSG_task_irecv(&task2, "mymailbox");
--- examples/smpi/mc/bugged1.c
+++ examples/smpi/mc/bugged1.c
@@ -1,7 +1,6 @@
 /* A simple bugged MPI_ISend and MPI_IRecv test */
 
-/* Copyright (c) 2009, 2011, 2013-2014. The SimGrid Team.
- * All rights reserved.                                                     */
+/* Copyright (c) 2009-2017. The SimGrid Team. All rights reserved.          */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
@@ -12,11 +11,12 @@
 
 int main(int argc, char **argv)
 {
-  int recv_buff, err, size, rank;
+  int size;
+  int rank;
   MPI_Status status;
 
   /* Initialize MPI */
-  err = MPI_Init(&argc, &argv);
+  int err = MPI_Init(&argc, &argv);
   if (err != MPI_SUCCESS) {
     printf("MPI initialization failed!\n");
     exit(1);
@@ -33,6 +33,7 @@ int main(int argc, char **argv)
   if (rank == 0) {
     printf("MPI_ISend / MPI_IRecv Test \n");
 
+    int recv_buff;
     for (int i = 0; i < size - 1; i++) {
       MPI_Recv(&recv_buff, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
       printf("Message received from %d\n", recv_buff);
