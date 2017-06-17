6790e546408da0be4fb4ca5df2420666e538003b
Martin Quinson <martin.quinson@loria.fr>
2017-06-17 02:41:29 +0200

    forgot the copyright header

--- teshsuite/s4u/comm-waitany/comm-waitany.cpp
+++ teshsuite/s4u/comm-waitany/comm-waitany.cpp
@@ -1,3 +1,8 @@
+/* Copyright (c) 2017. The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
 #include <iostream>
 #include <simgrid/s4u.hpp>
 #include <stdlib.h>
