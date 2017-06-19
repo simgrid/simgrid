21e27099b1e10ffc0c7a724ef9c38f365f185c83
Rafael Keller Tesser <rktesser@inf.ufrgs.br>
2017-06-19 15:03:07 -0300

    We don't need this file anymore.

--- src/include/smpi/smpi_interface.h
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2012-2017. The SimGrid Team. All rights reserved.          */
-
-/* This program is free software; you can redistribute it and/or modify it
- * under the terms of the license (GNU LGPL) which comes with this package. */
-
-#ifndef SMPI_INTERFACE_H
-#define SMPI_INTERFACE_H
-
-#endif
