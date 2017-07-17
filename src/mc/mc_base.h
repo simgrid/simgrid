dd7150adabf3473303e6564c703941dc6239c8b1
Martin Quinson <martin.quinson@loria.fr>
2017-07-17 08:40:34 +0200

    fix non-MC builds

--- src/mc/mc_base.h
+++ src/mc/mc_base.h
@@ -28,6 +28,17 @@ XBT_PRIVATE extern std::vector<double> processes_time;
 /** Execute a given simcall */
 XBT_PRIVATE void handle_simcall(smx_simcall_t req, int req_num);
 
+/** Is the process ready to execute its simcall?
+ *
+ *  This is true if the request associated with the process is ready.
+ *
+ *  Most requests are always enabled but WAIT and WAITANY
+ *  are not always enabled: a WAIT where the communication does not
+ *  have both a source and a destination yet is not enabled
+ *  (unless timeout is enabled in the wait and enabeld in SimGridMC).
+ */
+XBT_PRIVATE bool actor_is_enabled(smx_actor_t process);
+
 /** Check if the given simcall is visible
  *
  *  \return `TRUE` or `FALSE`
