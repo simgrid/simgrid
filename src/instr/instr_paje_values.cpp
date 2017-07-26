5faf90176fb61aaa518448b708f13ba093432d26
Martin Quinson <martin.quinson@loria.fr>
2017-07-26 21:53:58 +0200

    fix tracing tests
    This will result in a leak in the tracing::value content, but this is
    unavoidable as long as the constructor is not really initializing the
    fields but instead initializing the ret field.

--- src/instr/instr_paje_values.cpp
+++ src/instr/instr_paje_values.cpp
@@ -31,9 +31,11 @@ value::value(const char* name, const char* color, type_t father)
 
 value::~value()
 {
+  /* FIXME: this should be cleanable
   xbt_free(name);
   xbt_free(color);
   xbt_free(id);
+  */
 }
 
 value* value::get_or_new(const char* name, const char* color, type_t father)
