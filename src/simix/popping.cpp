56c55046092ec6dc7a577e07131f8e0c5a6f60c8
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2017-08-02 10:35:41 +0200

    Remove now superfluous assert on critical path.

--- src/simix/popping.cpp
+++ src/simix/popping.cpp
@@ -24,8 +24,6 @@ void SIMIX_simcall_answer(smx_simcall_t simcall)
     XBT_DEBUG("Answer simcall %s (%d) issued by %s (%p)", SIMIX_simcall_name(simcall->call), (int)simcall->call,
         simcall->issuer->name.c_str(), simcall->issuer);
     simcall->issuer->simcall.call = SIMCALL_NONE;
-    xbt_assert(SIMIX_is_maestro(), "Ugh! This code path is reserved for maestro, but I'm '%s' on '%s'",
-               SIMIX_process_self()->cname(), sg_host_get_name(SIMIX_process_self()->host));
     /* This check should be useless and slows everyone. Reactivate if you see something weird in process scheduling. */
     // if (xbt_dynar_member(simix_global->process_to_run, &(simcall->issuer)))
     //   DIE_IMPOSSIBLE;
