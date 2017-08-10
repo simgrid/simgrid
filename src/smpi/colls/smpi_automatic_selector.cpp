12e92834eda64b9a17b7bfd58a8d0c1ccc867a6c
Takishipp <toufik.boubehziz@gmail.com>
2017-08-10 17:31:25 +0200

    Add s_type_event_new as a method to s_type class

--- src/smpi/colls/smpi_automatic_selector.cpp
+++ src/smpi/colls/smpi_automatic_selector.cpp
@@ -18,7 +18,7 @@
   if (TRACE_is_enabled()) {                                                                                            \
     type_t type = s_type::s_type_get_or_null(#cat, PJ_type_get_root());                                                       \
     if (not type) {                                                                                                    \
-      type = PJ_type_event_new(#cat, PJ_type_get_root());                                                              \
+      type = s_type::s_type_event_new(#cat, PJ_type_get_root());                                                              \
     }                                                                                                                  \
     char cont_name[25];                                                                                                \
     snprintf(cont_name, 25, "rank-%d", smpi_process()->index());                                                       \
