9f445ea00e7791874928d3022fb97bc3f2a38946
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2017-08-21 13:25:22 +0200

    This method can be made const.

--- include/simgrid/s4u/Host.hpp
+++ include/simgrid/s4u/Host.hpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2015. The SimGrid Team. All rights reserved.          */
+/* Copyright (c) 2006-2017. The SimGrid Team. All rights reserved.          */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
@@ -67,7 +67,7 @@ public:
   static s4u::Host* current();
 
   simgrid::xbt::string const& getName() const { return name_; }
-  const char* getCname() { return name_.c_str(); }
+  const char* getCname() const { return name_.c_str(); }
 
   void actorList(std::vector<ActorPtr> * whereto);
 
