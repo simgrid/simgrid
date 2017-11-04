75b65a5359d9a02cc2f199c2eb34838e85650eac
Martin Quinson <martin.quinson@loria.fr>
2017-11-04 21:55:27 +0100

    ignore another cruft seen on a CI system

--- tools/tesh/tesh.py
+++ tools/tesh/tesh.py
@@ -431,6 +431,7 @@ if __name__ == '__main__':
            re.compile("For details see http://code.google.com/p/address-sanitizer/issues/detail\\?id=189"),
            re.compile("For details see https://github.com/google/sanitizers/issues/189"),
            re.compile("Python runtime initialized with LC_CTYPE=C .*"),
+           re.compile("^cmake: .*? no version information available (required by cmake)"), # Seen on CircleCI
            ]
         TeshState().jenkins = True # This is a Jenkins build
 
