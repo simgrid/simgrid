d5c7479480cbe31b662c1130a0497df4d87963cf
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2017-11-27 09:57:26 +0100

    Rename file .h -> .hpp.

--- src/include/surf/datatypes.hpp
+++ src/include/surf/datatypes.hpp
@@ -3,8 +3,8 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#ifndef MAXMIN_DATATYPES_H
-#define MAXMIN_DATATYPES_H
+#ifndef MAXMIN_DATATYPES_HPP
+#define MAXMIN_DATATYPES_HPP
 
 typedef struct s_lmm_element_t* lmm_element_t;
 typedef struct s_lmm_variable_t* lmm_variable_t;
