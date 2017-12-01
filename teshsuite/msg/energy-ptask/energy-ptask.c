707b11fe424a58eb8e870fc71978568c58006a3e
Frederic Suter <frederic.suter@cc.in2p3.fr>
2017-12-01 18:34:50 +0100

    one more MSG example to go to the dungeon

--- teshsuite/msg/energy-ptask/energy-ptask.c
+++ teshsuite/msg/energy-ptask/energy-ptask.c
@@ -3,8 +3,8 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include "simgrid/msg.h"
 #include "simgrid/plugins/energy.h"
+#include "simgrid/msg.h"
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(msg_test, "Messages specific for this msg example");
 
@@ -16,26 +16,26 @@ XBT_LOG_NEW_DEFAULT_CATEGORY(msg_test, "Messages specific for this msg example")
  *   interfaces, but it's not possible ATM).
  */
 
-static int runner(int argc, char *argv[])
+static int runner(int argc, char* argv[])
 {
   /* Retrieve the list of all hosts as an array of hosts */
-  int hosts_count = MSG_get_host_number();
-  msg_host_t *hosts = xbt_dynar_to_array(MSG_hosts_as_dynar());
+  int hosts_count   = MSG_get_host_number();
+  msg_host_t* hosts = xbt_dynar_to_array(MSG_hosts_as_dynar());
 
   XBT_INFO("First, build a classical parallel task, with 1 Gflop to execute on each node, "
            "and 10MB to exchange between each pair");
-  double *computation_amounts = xbt_new0(double, hosts_count);
-  double *communication_amounts = xbt_new0(double, hosts_count * hosts_count);
+  double* computation_amounts   = xbt_new0(double, hosts_count);
+  double* communication_amounts = xbt_new0(double, hosts_count* hosts_count);
 
-  for (int i = 0; i < hosts_count; i++)
+  for (int i               = 0; i < hosts_count; i++)
     computation_amounts[i] = 1e9; // 1 Gflop
 
   for (int i = 0; i < hosts_count; i++)
-    for (int j = i + 1; j < hosts_count; j++)
+    for (int j                                   = i + 1; j < hosts_count; j++)
       communication_amounts[i * hosts_count + j] = 1e7; // 10 MB
 
   msg_task_t ptask =
-    MSG_parallel_task_create("parallel task", hosts_count, hosts, computation_amounts, communication_amounts, NULL);
+      MSG_parallel_task_create("parallel task", hosts_count, hosts, computation_amounts, communication_amounts, NULL);
   MSG_parallel_task_execute(ptask);
   MSG_task_destroy(ptask);
   xbt_free(communication_amounts);
@@ -59,28 +59,29 @@ static int runner(int argc, char *argv[])
 
   XBT_INFO("Then, build a parallel task involving only computations and no communication (1 Gflop per node)");
   computation_amounts = xbt_new0(double, hosts_count);
-  for (int i = 0; i < hosts_count; i++)
+  for (int i               = 0; i < hosts_count; i++)
     computation_amounts[i] = 1e9; // 1 Gflop
-  ptask = MSG_parallel_task_create("parallel exec", hosts_count, hosts, computation_amounts, NULL/* no comm */, NULL);
+  ptask = MSG_parallel_task_create("parallel exec", hosts_count, hosts, computation_amounts, NULL /* no comm */, NULL);
   MSG_parallel_task_execute(ptask);
   MSG_task_destroy(ptask);
   xbt_free(computation_amounts);
 
   XBT_INFO("Then, build a parallel task with no computation nor communication (synchro only)");
-  computation_amounts = xbt_new0(double, hosts_count);
-  communication_amounts = xbt_new0(double, hosts_count * hosts_count); /* memset to 0 by xbt_new0 */
-  ptask = MSG_parallel_task_create("parallel sync", hosts_count, hosts, computation_amounts, communication_amounts, NULL);
+  computation_amounts   = xbt_new0(double, hosts_count);
+  communication_amounts = xbt_new0(double, hosts_count* hosts_count); /* memset to 0 by xbt_new0 */
+  ptask =
+      MSG_parallel_task_create("parallel sync", hosts_count, hosts, computation_amounts, communication_amounts, NULL);
   MSG_parallel_task_execute(ptask);
   MSG_task_destroy(ptask);
   xbt_free(communication_amounts);
   xbt_free(computation_amounts);
 
   XBT_INFO("Finally, trick the ptask to do a 'remote execution', on host %s", MSG_host_get_name(hosts[1]));
-  computation_amounts = xbt_new0(double, 1);
+  computation_amounts    = xbt_new0(double, 1);
   computation_amounts[0] = 1e9; // 1 Gflop
-  msg_host_t *remote = xbt_new(msg_host_t,1);
-  remote[0] = hosts[1];
-  ptask = MSG_parallel_task_create("remote exec", 1, remote, computation_amounts, NULL/* no comm */, NULL);
+  msg_host_t* remote     = xbt_new(msg_host_t, 1);
+  remote[0]              = hosts[1];
+  ptask = MSG_parallel_task_create("remote exec", 1, remote, computation_amounts, NULL /* no comm */, NULL);
   MSG_parallel_task_execute(ptask);
   MSG_task_destroy(ptask);
   xbt_free(remote);
@@ -91,7 +92,7 @@ static int runner(int argc, char *argv[])
   return 0;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   MSG_init(&argc, argv);
   MSG_config("host/model", "ptask_L07");
@@ -99,14 +100,14 @@ int main(int argc, char *argv[])
   xbt_assert(argc <= 3, "1Usage: %s <platform file> [--energy]", argv[0]);
   xbt_assert(argc >= 2, "2Usage: %s <platform file> [--energy]", argv[0]);
 
-  if(argc == 3 && argv[2][2] == 'e')
+  if (argc == 3 && argv[2][2] == 'e')
     sg_host_energy_plugin_init();
 
   MSG_create_environment(argv[1]);
 
   /* Pick a process, no matter which, from the platform file */
   xbt_dynar_t all_hosts = MSG_hosts_as_dynar();
-  msg_host_t first_host = xbt_dynar_getfirst_as(all_hosts,msg_host_t);
+  msg_host_t first_host = xbt_dynar_getfirst_as(all_hosts, msg_host_t);
   xbt_dynar_free(&all_hosts);
 
   MSG_process_create("test", runner, NULL, first_host);
