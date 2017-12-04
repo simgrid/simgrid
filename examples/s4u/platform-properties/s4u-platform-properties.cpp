084ad52d4bc9b1c53f08795703e7fe2d0ada7764
Martin Quinson <martin.quinson@loria.fr>
2017-12-05 00:12:30 +0100

    doc improvements

--- examples/s4u/platform-properties/s4u-platform-properties.cpp
+++ examples/s4u/platform-properties/s4u-platform-properties.cpp
@@ -3,6 +3,8 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
+// TODO: also test the properties attached to links
+
 #include <simgrid/s4u.hpp>
 #include <string>
 
