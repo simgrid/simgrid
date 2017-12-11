5b4afebffb0ca5c8c09c203c30c72242c8e9ad80
Frederic Suter <frederic.suter@cc.in2p3.fr>
2017-12-11 15:41:38 +0100

    mv trace-platform from MSG to S4U the MSG version is not kept in the dungeon because:  - the code does absolutely nothing  - the tesh comprises a huge Pajé output for G5K

--- examples/s4u/trace-platform/s4u-trace-platform.cpp
+++ examples/s4u/trace-platform/s4u-trace-platform.cpp
@@ -3,13 +3,12 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include "simgrid/msg.h"
+#include "simgrid/s4u.hpp"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  MSG_init(&argc, argv);
-  xbt_assert(argc > 1, "Usage: %s platform_file\n\tExample: %s msg_platform.xml\n", argv[0], argv[0]);
-  MSG_create_environment(argv[1]);
-  MSG_main();
+  simgrid::s4u::Engine e(&argc, argv);
+  e.loadPlatform(argv[1]);
+  e.run();
   return 0;
 }
