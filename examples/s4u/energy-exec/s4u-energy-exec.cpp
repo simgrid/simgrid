95cd4e6e0c85be242b34630726a82bf5e8e6be17
Martin Quinson <martin.quinson@loria.fr>
2017-12-14 16:33:37 +0100

    convert another example to s4u, and move the MSG version away

---
+++ examples/s4u/energy-exec/s4u-energy-exec.cpp
@@ -0,0 +1,81 @@
+/* Copyright (c) 2007-2017. The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include "simgrid/s4u.hpp"
+#include "simgrid/plugins/energy.h"
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_test, "Messages specific for this s4u example");
+
+static void dvfs()
+{
+  simgrid::s4u::Host* host1 = simgrid::s4u::Host::by_name("MyHost1");
+  simgrid::s4u::Host* host2 = simgrid::s4u::Host::by_name("MyHost2");
+
+  XBT_INFO("Energetic profile: %s", host1->getProperty("watt_per_state"));
+  XBT_INFO("Initial peak speed=%.0E flop/s; Energy dissipated =%.0E J", host1->getSpeed(),
+           sg_host_get_consumed_energy(host1));
+
+  double start = simgrid::s4u::Engine::getClock();
+  XBT_INFO("Sleep for 10 seconds");
+  simgrid::s4u::this_actor::sleep_for(10);
+  XBT_INFO("Done sleeping (duration: %.2f s). Current peak speed=%.0E; Energy dissipated=%.2f J",
+           simgrid::s4u::Engine::getClock() - start, host1->getSpeed(), sg_host_get_consumed_energy(host1));
+
+  // Execute something
+  start             = simgrid::s4u::Engine::getClock();
+  double flopAmount = 100E6;
+  XBT_INFO("Run a task of %.0E flops", flopAmount);
+  simgrid::s4u::this_actor::execute(flopAmount);
+  XBT_INFO("Task done (duration: %.2f s). Current peak speed=%.0E flop/s; Current consumption: from %.0fW to %.0fW"
+           " depending on load; Energy dissipated=%.0f J",
+           simgrid::s4u::Engine::getClock() - start, host1->getSpeed(),
+           sg_host_get_wattmin_at(host1, host1->getPstate()), sg_host_get_wattmax_at(host1, host1->getPstate()),
+           sg_host_get_consumed_energy(host1));
+
+  // ========= Change power peak =========
+  int pstate = 2;
+  host1->setPstate(pstate);
+  XBT_INFO("========= Requesting pstate %d (speed should be of %.0E flop/s and is of %.0E flop/s)", pstate,
+           host1->getPstateSpeed(pstate), host1->getSpeed());
+
+  // Run another task
+  start = simgrid::s4u::Engine::getClock();
+  XBT_INFO("Run a task of %.0E flops", flopAmount);
+  simgrid::s4u::this_actor::execute(flopAmount);
+  XBT_INFO("Task done (duration: %.2f s). Current peak speed=%.0E flop/s; Energy dissipated=%.0f J",
+           simgrid::s4u::Engine::getClock() - start, host1->getSpeed(), sg_host_get_consumed_energy(host1));
+
+  start = simgrid::s4u::Engine::getClock();
+  XBT_INFO("Sleep for 4 seconds");
+  simgrid::s4u::this_actor::sleep_for(4);
+  XBT_INFO("Done sleeping (duration: %.2f s). Current peak speed=%.0E flop/s; Energy dissipated=%.0f J",
+           simgrid::s4u::Engine::getClock() - start, host1->getSpeed(), sg_host_get_consumed_energy(host1));
+
+  // =========== Turn the other host off ==========
+  XBT_INFO("Turning MyHost2 off, and sleeping another 10 seconds. MyHost2 dissipated %.0f J so far.",
+           sg_host_get_consumed_energy(host2));
+  host2->turnOff();
+  start = simgrid::s4u::Engine::getClock();
+  simgrid::s4u::this_actor::sleep_for(10);
+  XBT_INFO("Done sleeping (duration: %.2f s). Current peak speed=%.0E flop/s; Energy dissipated=%.0f J",
+           simgrid::s4u::Engine::getClock() - start, host1->getSpeed(), sg_host_get_consumed_energy(host1));
+}
+
+int main(int argc, char* argv[])
+{
+  sg_host_energy_plugin_init();
+  simgrid::s4u::Engine e(&argc, argv);
+
+  xbt_assert(argc == 2, "Usage: %s platform_file\n\tExample: %s msg_platform.xml\n", argv[0], argv[0]);
+
+  e.loadPlatform(argv[1]);
+  simgrid::s4u::Actor::createActor("dvfs_test", simgrid::s4u::Host::by_name("MyHost1"), dvfs);
+
+  e.run();
+
+  XBT_INFO("End of simulation.");
+
+  return 0;
+}
--- teshsuite/msg/energy-consumption/energy-consumption.c
+++ teshsuite/msg/energy-consumption/energy-consumption.c
@@ -4,16 +4,16 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include "simgrid/msg.h"
 #include "simgrid/plugins/energy.h"
+#include "simgrid/msg.h"
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(msg_test, "Messages specific for this msg example");
 
-static int dvfs(int argc, char *argv[])
+static int dvfs(int argc, char* argv[])
 {
   msg_host_t host = MSG_host_by_name("MyHost1");
 
-  XBT_INFO("Energetic profile: %s",MSG_host_get_property_value(host,"watt_per_state"));
+  XBT_INFO("Energetic profile: %s", MSG_host_get_property_value(host, "watt_per_state"));
   XBT_INFO("Initial peak speed=%.0E flop/s; Energy dissipated =%.0E J", MSG_host_get_speed(host),
            sg_host_get_consumed_energy(host));
 
@@ -24,10 +24,10 @@ static int dvfs(int argc, char *argv[])
            MSG_get_clock() - start, MSG_host_get_speed(host), sg_host_get_consumed_energy(host));
 
   // Run a task
-  start = MSG_get_clock();
-  msg_task_t task1 = MSG_task_create ("t1", 100E6, 0, NULL);
+  start            = MSG_get_clock();
+  msg_task_t task1 = MSG_task_create("t1", 100E6, 0, NULL);
   XBT_INFO("Run a task of %.0E flops", MSG_task_get_flops_amount(task1));
-  MSG_task_execute (task1);
+  MSG_task_execute(task1);
   MSG_task_destroy(task1);
   XBT_INFO("Task done (duration: %.2f s). Current peak speed=%.0E flop/s; Current consumption: from %.0fW to %.0fW"
            " depending on load; Energy dissipated=%.0f J",
@@ -35,16 +35,16 @@ static int dvfs(int argc, char *argv[])
            sg_host_get_wattmax_at(host, MSG_host_get_pstate(host)), sg_host_get_consumed_energy(host));
 
   // ========= Change power peak =========
-  int pstate=2;
+  int pstate = 2;
   MSG_host_set_pstate(host, pstate);
   XBT_INFO("========= Requesting pstate %d (speed should be of %.0E flop/s and is of %.0E flop/s)", pstate,
            MSG_host_get_power_peak_at(host, pstate), MSG_host_get_speed(host));
 
   // Run a second task
   start = MSG_get_clock();
-  task1 = MSG_task_create ("t2", 100E6, 0, NULL);
+  task1 = MSG_task_create("t2", 100E6, 0, NULL);
   XBT_INFO("Run a task of %.0E flops", MSG_task_get_flops_amount(task1));
-  MSG_task_execute (task1);
+  MSG_task_execute(task1);
   MSG_task_destroy(task1);
   XBT_INFO("Task done (duration: %.2f s). Current peak speed=%.0E flop/s; Energy dissipated=%.0f J",
            MSG_get_clock() - start, MSG_host_get_speed(host), sg_host_get_consumed_energy(host));
@@ -57,7 +57,7 @@ static int dvfs(int argc, char *argv[])
 
   // =========== Turn the other host off ==========
   XBT_INFO("Turning MyHost2 off, and sleeping another 10 seconds. MyHost2 dissipated %.0f J so far.",
-      sg_host_get_consumed_energy(MSG_host_by_name("MyHost2")) );
+           sg_host_get_consumed_energy(MSG_host_by_name("MyHost2")));
   MSG_host_off(MSG_host_by_name("MyHost2"));
   start = MSG_get_clock();
   MSG_process_sleep(10);
@@ -66,7 +66,7 @@ static int dvfs(int argc, char *argv[])
   return 0;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   sg_host_energy_plugin_init();
   MSG_init(&argc, argv);
