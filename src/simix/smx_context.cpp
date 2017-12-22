4a9216f3de0f34def576bfb38ee5ffc2f6729254
Augustin Degomme <augustin.degomme@unibas.ch>
2017-12-22 05:22:31 +0100

    put back the fix for broken dlopen+thread on freebsd

--- src/simix/smx_context.cpp
+++ src/simix/smx_context.cpp
@@ -111,10 +111,14 @@ void SIMIX_context_mod_init()
 #endif
 #if defined(__FreeBSD__)
   if (xbt_cfg_get_string("smpi/privatization") == "mmap") {
-    XBT_WARN("mmap broken on FreeBSD, but dlopen+thread broken too. Switching to dlopen+raw contexts.");
-    context_factory_name = "raw";
     xbt_cfg_set_string("smpi/privatization", "dlopen");
   }
+
+  if (context_factory_name == "thread" && xbt_cfg_get_string("smpi/privatization") != "no"){
+    XBT_WARN("mmap broken on FreeBSD, but dlopen+thread broken too. Switching to dlopen+raw contexts.");
+    context_factory_name = "raw";
+  }
+}
 #endif
 
   /* select the context factory to use to create the contexts */
