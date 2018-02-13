d8d0d2b34dd529790ca6a0a1b45925d4e81657a3
degomme <augustin.degomme@unibas.ch>
2018-02-13 15:24:34 +0100

    rename two files in examples Other files have the same name elsewhere, and this confuses coverage tool in sonar

--- teshsuite/msg/app-bittorrent/bittorrent.c
+++ teshsuite/msg/app-bittorrent/bittorrent.c
@@ -5,7 +5,7 @@
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
 #include "bittorrent.h"
-#include "peer.h"
+#include "bittorrent-peer.h"
 #include "tracker.h"
 #include <simgrid/msg.h>
 #include <xbt/RngStream.h>
