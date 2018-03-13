06dff582cdb609f521035d7a138c21aa46848220
Millian Poquet <millian.poquet@inria.fr>
2018-03-13 13:27:05 +0100

    [tesh] read files in utf-8
    Previously, system's default encoding was used.

--- tools/tesh/tesh.py
+++ tools/tesh/tesh.py
@@ -139,7 +139,7 @@ class FileReader(Singleton):
             self.filename_raw = filename
             self.filename = os.path.basename(filename)
             self.abspath = os.path.abspath(filename)
-            self.f = open(self.filename_raw)
+            self.f = open(self.filename_raw, encoding="utf-8")
 
         self.linenumber = 0
 
