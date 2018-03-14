1d83152923e6cdedca3b28400f0582c9fb9c5419
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2018-03-14 09:24:35 +0100

    Typo.

--- include/xbt/ex.hpp
+++ include/xbt/ex.hpp
@@ -41,7 +41,7 @@ public:
     simgrid::xbt::WithContextException(throwpoint, simgrid::xbt::backtrace())
   {}
 
-  ~xbt_ex(); // DO NOT define it here -- see ex.cpp for a rational
+  ~xbt_ex(); // DO NOT define it here -- see ex.cpp for a rationale
 
   /** Category (what went wrong) */
   xbt_errcat_t category = unknown_error;
