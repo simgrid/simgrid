e2d280829e07d8b5e96c96bd4b2fd815e8311102
Martin Quinson <martin.quinson@loria.fr>
2018-03-19 20:42:35 +0100

    only use public headers from MC examples too

--- examples/msg/mc/bugged1_liveness.c
+++ examples/msg/mc/bugged1_liveness.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017. The SimGrid Team. All rights reserved.          */
+/* Copyright (c) 2012-2018. The SimGrid Team. All rights reserved.          */
 
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
@@ -15,8 +15,8 @@
 #include <unistd.h>
 #endif
 
-#include "simgrid/msg.h"
-#include "mc/mc.h"
+#include <simgrid/modelchecker.h>
+#include <simgrid/msg.h>
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(bugged1_liveness, "my log messages");
 
