28d54bec9f86b7f4ead97559ab906c86a4f6d3ca
Frederic Suter <frederic.suter@cc.in2p3.fr>
2018-03-20 14:38:54 +0100

    cosmetics (reorder)

--- include/simgrid/vm.h
+++ include/simgrid/vm.h
@@ -22,6 +22,8 @@ SG_BEGIN_DECL()
  * Usual lack of guaranty of any kind applies here, and is even increased.
  *
  */
+XBT_PUBLIC sg_vm_t sg_vm_create_core(sg_host_t pm, const char* name);
+XBT_PUBLIC sg_vm_t sg_vm_create_multicore(sg_host_t pm, const char* name, int coreAmount);
 
 XBT_PUBLIC int sg_vm_is_created(sg_vm_t vm);
 XBT_PUBLIC int sg_vm_is_running(sg_vm_t vm);
@@ -30,17 +32,15 @@ XBT_PUBLIC int sg_vm_is_suspended(sg_vm_t vm);
 XBT_PUBLIC const char* sg_vm_get_name(sg_vm_t vm);
 XBT_PUBLIC void sg_vm_set_ramsize(sg_vm_t vm, size_t size);
 XBT_PUBLIC size_t sg_vm_get_ramsize(sg_vm_t vm);
-
-XBT_PUBLIC sg_vm_t sg_vm_create_core(sg_host_t pm, const char* name);
-XBT_PUBLIC sg_vm_t sg_vm_create_multicore(sg_host_t pm, const char* name, int coreAmount);
+XBT_PUBLIC void sg_vm_set_bound(sg_vm_t vm, double bound);
+XBT_PUBLIC sg_host_t sg_vm_get_pm(sg_vm_t vm);
 
 XBT_PUBLIC void sg_vm_start(sg_vm_t vm);
 XBT_PUBLIC void sg_vm_suspend(sg_vm_t vm);
 XBT_PUBLIC void sg_vm_resume(sg_vm_t vm);
 XBT_PUBLIC void sg_vm_shutdown(sg_vm_t vm);
 XBT_PUBLIC void sg_vm_destroy(sg_vm_t vm);
-XBT_PUBLIC sg_host_t sg_vm_get_pm(sg_vm_t vm);
-XBT_PUBLIC void sg_vm_set_bound(sg_vm_t vm, double bound);
+
 SG_END_DECL()
 
 #endif /* INCLUDE_SIMGRID_VM_H_ */
