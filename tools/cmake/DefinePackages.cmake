26b8e3a2283ff87015ca75f88e2cdc630284dbb0
Christian Heinrich <franz-christian.heinrich@inria.fr>
2018-03-20 10:48:36 +0100

    [MSG/Replay] Delete old msg/replay code

--- tools/cmake/DefinePackages.cmake
+++ tools/cmake/DefinePackages.cmake
@@ -442,7 +442,6 @@ set(SIMGRID_SRC
 set(MSG_SRC
   src/msg/instr_msg_process.cpp
   src/msg/instr_msg_task.cpp
-  src/msg/msg_actions.cpp
   src/msg/msg_deployment.cpp
   src/msg/msg_environment.cpp
   src/msg/msg_global.cpp
