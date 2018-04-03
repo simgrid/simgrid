24d67cebdedcbdd59000ed9439c1a8c51cd98a24
degomme <augustin.degomme@unibas.ch>
2018-04-03 22:33:11 +0200

    avoid undefined NULL

--- include/smpi/mpi.h
+++ include/smpi/mpi.h
@@ -17,13 +17,6 @@
 
 #include <sys/time.h> /* Load it before the define next line to not mess with the system headers */
 
-#define sleep(x) smpi_sleep(x)
-#define usleep(x) smpi_usleep(x)
-#define gettimeofday(x, y) smpi_gettimeofday(x, NULL)
-#if _POSIX_TIMERS > 0
-#define nanosleep(x, y) smpi_nanosleep(x, y)
-#define clock_gettime(x, y) smpi_clock_gettime(x, y)
-#endif
 #if SIMGRID_HAVE_MC
 #undef assert
 #define assert(x) MC_assert(x)
