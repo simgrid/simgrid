173b3916e2bf66ae93d66d3a07ad9974610ac7ce
Martin Quinson <martin.quinson@loria.fr>
2018-04-04 18:13:27 +0200

    reduce the amount of included headers

--- src/plugins/file_system/s4u_FileSystem.cpp
+++ src/plugins/file_system/s4u_FileSystem.cpp
@@ -3,13 +3,9 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include "xbt/log.h"
 
 #include "simgrid/plugins/file_system.h"
 #include "simgrid/s4u/Actor.hpp"
-#include "simgrid/s4u/Host.hpp"
-#include "simgrid/s4u/Storage.hpp"
-#include "simgrid/simix.hpp"
 #include "src/surf/HostImpl.hpp"
 
 #include <algorithm>
