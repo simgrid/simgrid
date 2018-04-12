8391d02b6a83f0cececc6957e5d7b9e70781fe09
Frederic Suter <frederic.suter@cc.in2p3.fr>
2018-04-12 15:20:30 +0200

    reindent

--- src/surf/network_interface.cpp
+++ src/surf/network_interface.cpp
@@ -23,38 +23,38 @@ LinkImpl* LinkImpl::byName(std::string name)
 {
   auto link = links->find(name);
   return link == links->end() ? nullptr : link->second;
+}
+/** @brief Returns the amount of links in the platform */
+int LinkImpl::linksCount()
+{
+  return links->size();
+}
+void LinkImpl::linksList(std::vector<s4u::Link*>* linkList)
+{
+  for (auto const& kv : *links) {
+    linkList->push_back(&kv.second->piface_);
   }
-  /** @brief Returns the amount of links in the platform */
-  int LinkImpl::linksCount()
-  {
-    return links->size();
-  }
-  void LinkImpl::linksList(std::vector<s4u::Link*>* linkList)
-  {
-    for (auto const& kv : *links) {
-      linkList->push_back(&kv.second->piface_);
-    }
-  }
+}
 
-  /** @brief Returns a list of all existing links */
-  LinkImpl** LinkImpl::linksList()
-  {
-    LinkImpl** res = xbt_new(LinkImpl*, (int)links->size());
-    int i          = 0;
-    for (auto const& kv : *links) {
-      res[i] = kv.second;
-      i++;
-    }
-    return res;
+/** @brief Returns a list of all existing links */
+LinkImpl** LinkImpl::linksList()
+{
+  LinkImpl** res = xbt_new(LinkImpl*, (int)links->size());
+  int i          = 0;
+  for (auto const& kv : *links) {
+    res[i] = kv.second;
+    i++;
   }
-  /** @brief destructor of the static data */
-  void LinkImpl::linksExit()
-  {
-    for (auto const& kv : *links)
-      (kv.second)->destroy();
-    delete links;
-  }
-  }
+  return res;
+}
+/** @brief destructor of the static data */
+void LinkImpl::linksExit()
+{
+  for (auto const& kv : *links)
+    (kv.second)->destroy();
+  delete links;
+}
+}
 }
 } // namespace simgrid
 
@@ -85,147 +85,148 @@ NetworkModel::~NetworkModel() = default;
 double NetworkModel::latencyFactor(double /*size*/)
 {
   return sg_latency_factor;
-    }
+}
 
-    double NetworkModel::bandwidthFactor(double /*size*/) {
-      return sg_bandwidth_factor;
-    }
+double NetworkModel::bandwidthFactor(double /*size*/)
+{
+  return sg_bandwidth_factor;
+}
 
-    double NetworkModel::bandwidthConstraint(double rate, double /*bound*/, double /*size*/) {
-      return rate;
-    }
+double NetworkModel::bandwidthConstraint(double rate, double /*bound*/, double /*size*/)
+{
+  return rate;
+}
 
-    double NetworkModel::next_occuring_event_full(double now)
-    {
-      double minRes = Model::next_occuring_event_full(now);
+double NetworkModel::next_occuring_event_full(double now)
+{
+  double minRes = Model::next_occuring_event_full(now);
 
-      for (Action const& action : *get_running_action_set()) {
-        const NetworkAction& net_action = static_cast<const NetworkAction&>(action);
-        if (net_action.latency_ > 0)
-          minRes = (minRes < 0) ? net_action.latency_ : std::min(minRes, net_action.latency_);
-      }
-
-      XBT_DEBUG("Min of share resources %f", minRes);
-
-      return minRes;
-    }
-
-    /************
-     * Resource *
-     ************/
-
-    LinkImpl::LinkImpl(NetworkModel* model, const std::string& name, lmm::Constraint* constraint)
-        : Resource(model, name, constraint), piface_(this)
-    {
-
-      if (name != "__loopback__")
-        xbt_assert(not LinkImpl::byName(name), "Link '%s' declared several times in the platform.", name.c_str());
-
-      latency_.scale   = 1;
-      bandwidth_.scale = 1;
-
-      links->insert({name, this});
-      XBT_DEBUG("Create link '%s'", name.c_str());
-    }
-
-    /** @brief use destroy() instead of this destructor */
-    LinkImpl::~LinkImpl()
-    {
-      xbt_assert(currentlyDestroying_, "Don't delete Links directly. Call destroy() instead.");
-    }
-    /** @brief Fire the required callbacks and destroy the object
-     *
-     * Don't delete directly a Link, call l->destroy() instead.
-     */
-    void LinkImpl::destroy()
-    {
-      if (not currentlyDestroying_) {
-        currentlyDestroying_ = true;
-        s4u::Link::onDestruction(this->piface_);
-        delete this;
-      }
-    }
-
-    bool LinkImpl::is_used()
-    {
-      return get_model()->get_maxmin_system()->constraint_used(get_constraint());
-    }
-
-    double LinkImpl::latency()
-    {
-      return latency_.peak * latency_.scale;
-    }
-
-    double LinkImpl::bandwidth()
-    {
-      return bandwidth_.peak * bandwidth_.scale;
-    }
-
-    s4u::Link::SharingPolicy LinkImpl::sharingPolicy()
-    {
-      return get_constraint()->get_sharing_policy();
-    }
-
-    void LinkImpl::turn_on()
-    {
-      if (is_off()) {
-        Resource::turn_on();
-        s4u::Link::onStateChange(this->piface_);
-      }
-    }
-    void LinkImpl::turn_off()
-    {
-      if (is_on()) {
-        Resource::turn_off();
-        s4u::Link::onStateChange(this->piface_);
-      }
-    }
-    void LinkImpl::setStateTrace(tmgr_trace_t trace)
-    {
-      xbt_assert(stateEvent_ == nullptr, "Cannot set a second state trace to Link %s", get_cname());
-      stateEvent_ = future_evt_set->add_trace(trace, this);
-    }
-    void LinkImpl::setBandwidthTrace(tmgr_trace_t trace)
-    {
-      xbt_assert(bandwidth_.event == nullptr, "Cannot set a second bandwidth trace to Link %s", get_cname());
-      bandwidth_.event = future_evt_set->add_trace(trace, this);
-    }
-    void LinkImpl::setLatencyTrace(tmgr_trace_t trace)
-    {
-      xbt_assert(latency_.event == nullptr, "Cannot set a second latency trace to Link %s", get_cname());
-      latency_.event = future_evt_set->add_trace(trace, this);
-    }
-
-
-    /**********
-     * Action *
-     **********/
-
-    void NetworkAction::set_state(Action::State state)
-    {
-      Action::set_state(state);
-      s4u::Link::onCommunicationStateChange(this);
-    }
-
-    /** @brief returns a list of all Links that this action is using */
-    std::list<LinkImpl*> NetworkAction::links()
-    {
-      std::list<LinkImpl*> retlist;
-      int llen = get_variable()->get_number_of_constraint();
-
-      for (int i = 0; i < llen; i++) {
-        /* Beware of composite actions: ptasks put links and cpus together */
-        // extra pb: we cannot dynamic_cast from void*...
-        Resource* resource = static_cast<Resource*>(get_variable()->get_constraint(i)->get_id());
-        LinkImpl* link     = dynamic_cast<LinkImpl*>(resource);
-        if (link != nullptr)
-          retlist.push_back(link);
-      }
-
-      return retlist;
-    }
+  for (Action const& action : *get_running_action_set()) {
+    const NetworkAction& net_action = static_cast<const NetworkAction&>(action);
+    if (net_action.latency_ > 0)
+      minRes = (minRes < 0) ? net_action.latency_ : std::min(minRes, net_action.latency_);
   }
-  } // namespace kernel
+
+  XBT_DEBUG("Min of share resources %f", minRes);
+
+  return minRes;
+}
+
+/************
+ * Resource *
+ ************/
+
+LinkImpl::LinkImpl(NetworkModel* model, const std::string& name, lmm::Constraint* constraint)
+    : Resource(model, name, constraint), piface_(this)
+{
+
+  if (name != "__loopback__")
+    xbt_assert(not LinkImpl::byName(name), "Link '%s' declared several times in the platform.", name.c_str());
+
+  latency_.scale   = 1;
+  bandwidth_.scale = 1;
+
+  links->insert({name, this});
+  XBT_DEBUG("Create link '%s'", name.c_str());
+}
+
+/** @brief use destroy() instead of this destructor */
+LinkImpl::~LinkImpl()
+{
+  xbt_assert(currentlyDestroying_, "Don't delete Links directly. Call destroy() instead.");
+}
+/** @brief Fire the required callbacks and destroy the object
+ *
+ * Don't delete directly a Link, call l->destroy() instead.
+ */
+void LinkImpl::destroy()
+{
+  if (not currentlyDestroying_) {
+    currentlyDestroying_ = true;
+    s4u::Link::onDestruction(this->piface_);
+    delete this;
+  }
+}
+
+bool LinkImpl::is_used()
+{
+  return get_model()->get_maxmin_system()->constraint_used(get_constraint());
+}
+
+double LinkImpl::latency()
+{
+  return latency_.peak * latency_.scale;
+}
+
+double LinkImpl::bandwidth()
+{
+  return bandwidth_.peak * bandwidth_.scale;
+}
+
+s4u::Link::SharingPolicy LinkImpl::sharingPolicy()
+{
+  return get_constraint()->get_sharing_policy();
+}
+
+void LinkImpl::turn_on()
+{
+  if (is_off()) {
+    Resource::turn_on();
+    s4u::Link::onStateChange(this->piface_);
+  }
+}
+void LinkImpl::turn_off()
+{
+  if (is_on()) {
+    Resource::turn_off();
+    s4u::Link::onStateChange(this->piface_);
+  }
+}
+void LinkImpl::setStateTrace(tmgr_trace_t trace)
+{
+  xbt_assert(stateEvent_ == nullptr, "Cannot set a second state trace to Link %s", get_cname());
+  stateEvent_ = future_evt_set->add_trace(trace, this);
+}
+void LinkImpl::setBandwidthTrace(tmgr_trace_t trace)
+{
+  xbt_assert(bandwidth_.event == nullptr, "Cannot set a second bandwidth trace to Link %s", get_cname());
+  bandwidth_.event = future_evt_set->add_trace(trace, this);
+}
+void LinkImpl::setLatencyTrace(tmgr_trace_t trace)
+{
+  xbt_assert(latency_.event == nullptr, "Cannot set a second latency trace to Link %s", get_cname());
+  latency_.event = future_evt_set->add_trace(trace, this);
+}
+
+/**********
+ * Action *
+ **********/
+
+void NetworkAction::set_state(Action::State state)
+{
+  Action::set_state(state);
+  s4u::Link::onCommunicationStateChange(this);
+}
+
+/** @brief returns a list of all Links that this action is using */
+std::list<LinkImpl*> NetworkAction::links()
+{
+  std::list<LinkImpl*> retlist;
+  int llen = get_variable()->get_number_of_constraint();
+
+  for (int i = 0; i < llen; i++) {
+    /* Beware of composite actions: ptasks put links and cpus together */
+    // extra pb: we cannot dynamic_cast from void*...
+    Resource* resource = static_cast<Resource*>(get_variable()->get_constraint(i)->get_id());
+    LinkImpl* link     = dynamic_cast<LinkImpl*>(resource);
+    if (link != nullptr)
+      retlist.push_back(link);
+  }
+
+  return retlist;
+}
+}
+} // namespace kernel
 }
 
 #endif /* NETWORK_INTERFACE_CPP_ */
