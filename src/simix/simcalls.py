31ec42c7c3a970448512dcc6b61c5dc08d3b17d4
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2018-05-01 22:44:01 +0200

    Complete search path for simcall_HANDLER_*.

--- src/simix/simcalls.py
+++ src/simix/simcalls.py
@@ -52,7 +52,7 @@ class Simcall(object):
         # smx_host_t h)
         if self.simcalls_pre is None:
             self.simcalls_pre = set()
-            for fn in glob.glob('smx_*') + glob.glob('ActorImpl*') + glob.glob('../mc/*cpp'):
+            for fn in glob.glob('smx_*') + glob.glob('ActorImpl*') + glob.glob('../mc/*cpp') + glob.glob('../kernel/activity/*cpp'):
                 f = open(fn)
                 self.simcalls_pre |= set(re.findall(r'simcall_HANDLER_(.*?)\(', f.read()))
                 f.close()
