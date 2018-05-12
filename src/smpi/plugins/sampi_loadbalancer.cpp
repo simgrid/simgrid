e07649791cb63debbcc39a41fb3621dee457c477
Martin Quinson <martin.quinson@loria.fr>
2018-05-12 02:48:13 +0200

    Missing copyright header

--- src/smpi/plugins/sampi_loadbalancer.cpp
+++ src/smpi/plugins/sampi_loadbalancer.cpp
@@ -1,9 +1,14 @@
+/* Copyright (c) 2018.      The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
 #include <simgrid/plugins/load_balancer.h>
 #include <simgrid/s4u/Actor.hpp>
 #include <simgrid/smpi/replay.hpp>
 #include <xbt/replay.hpp>
 
-XBT_LOG_NEW_DEFAULT_SUBCATEGORY(plugin_load_balancer, surf, "Logging specific to the SURF energy plugin");
+XBT_LOG_NEW_DEFAULT_SUBCATEGORY(plugin_load_balancer, surf, "Logging specific to the SMPI load balancing plugin");
 
 namespace simgrid {
 namespace smpi {
