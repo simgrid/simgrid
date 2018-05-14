a53d7cf0445e5a6c3f2bb6606e6bd3b2b33b7867
Frederic Suter <frederic.suter@cc.in2p3.fr>
2018-05-14 21:49:34 +0200

    fix callback (& not *)

--- src/instr/instr_platform.cpp
+++ src/instr/instr_platform.cpp
@@ -232,11 +232,11 @@ static void instr_host_on_creation(simgrid::s4u::Host& host)
   }
 }
 
-static void instr_host_on_speed_change(simgrid::s4u::Host* host)
+static void instr_host_on_speed_change(simgrid::s4u::Host& host)
 {
-  simgrid::instr::Container::by_name(host->get_cname())
+  simgrid::instr::Container::by_name(host.get_cname())
       ->get_variable("power")
-      ->set_event(surf_get_clock(), host->getCoreCount() * host->get_available_speed());
+      ->set_event(surf_get_clock(), host.getCoreCount() * host.get_available_speed());
 }
 
 static void instr_cpu_action_on_state_change(simgrid::surf::CpuAction* action,
