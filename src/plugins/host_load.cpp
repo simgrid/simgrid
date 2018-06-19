e9bee908f58f910dc08a8a3ed2c40b9b72dc21f0
Frederic Suter <frederic.suter@cc.in2p3.fr>
2018-06-19 13:41:24 +0200

    snake case another plugin

--- src/plugins/host_load.cpp
+++ src/plugins/host_load.cpp
@@ -21,42 +21,38 @@ class HostLoad {
 public:
   static simgrid::xbt::Extension<simgrid::s4u::Host, HostLoad> EXTENSION_ID;
 
-  explicit HostLoad(simgrid::s4u::Host* ptr);
-  ~HostLoad();
+  explicit HostLoad(simgrid::s4u::Host* ptr)
+      : host_(ptr)
+      , last_updated_(surf_get_clock())
+      , last_reset_(surf_get_clock())
+      , current_speed_(host_->getSpeed())
+      , current_flops_(host_->pimpl_cpu->get_constraint()->get_usage())
+      , theor_max_flops_(0)
+      , was_prev_idle_(current_flops_ == 0)
+  {
+  }
+  ~HostLoad() = default;
 
-  double getCurrentLoad();
-  double getComputedFlops();
-  double getAverageLoad();
-  double getIdleTime();
+  double get_current_load();
+  double get_average_load() { return (theor_max_flops_ == 0) ? 0 : computed_flops_ / theor_max_flops_; };
+  double get_computed_flops() { return computed_flops_; }
+  double get_idle_time() { return idle_time_; } /** Return idle time since last reset */
   void update();
   void reset();
 
 private:
-  simgrid::s4u::Host* host = nullptr;
-  double last_updated      = 0;
-  double last_reset        = 0;
-  double current_speed     = 0;
-  double current_flops     = 0;
-  double computed_flops    = 0;
-  double idle_time         = 0;
-  double theor_max_flops   = 0;
-  bool was_prev_idle       = true; /* A host is idle at the beginning */
+  simgrid::s4u::Host* host_ = nullptr;
+  double last_updated_      = 0;
+  double last_reset_        = 0;
+  double current_speed_     = 0;
+  double current_flops_     = 0;
+  double computed_flops_    = 0;
+  double idle_time_         = 0;
+  double theor_max_flops_   = 0;
+  bool was_prev_idle_       = true; /* A host is idle at the beginning */
 };
 
 simgrid::xbt::Extension<simgrid::s4u::Host, HostLoad> HostLoad::EXTENSION_ID;
-
-HostLoad::HostLoad(simgrid::s4u::Host* ptr)
-    : host(ptr)
-    , last_updated(surf_get_clock())
-    , last_reset(surf_get_clock())
-    , current_speed(host->getSpeed())
-    , current_flops(host->pimpl_cpu->get_constraint()->get_usage())
-    , theor_max_flops(0)
-    , was_prev_idle(current_flops == 0)
-{
-}
-
-HostLoad::~HostLoad() = default;
 
 void HostLoad::update()
 {
@@ -64,58 +60,34 @@ void HostLoad::update()
 
   /* Current flop per second computed by the cpu; current_flops = k * pstate_speed_in_flops, k \in {0, 1, ..., cores}
    * number of active cores */
-  current_flops = host->pimpl_cpu->get_constraint()->get_usage();
+  current_flops_ = host_->pimpl_cpu->get_constraint()->get_usage();
 
   /* flops == pstate_speed * cores_being_currently_used */
-  computed_flops += (now - last_updated) * current_flops;
+  computed_flops_ += (now - last_updated_) * current_flops_;
 
-  if (was_prev_idle) {
-    idle_time += (now - last_updated);
+  if (was_prev_idle_) {
+    idle_time_ += (now - last_updated_);
   }
 
-  theor_max_flops += current_speed * host->get_core_count() * (now - last_updated);
-  current_speed = host->getSpeed();
-  last_updated  = now;
-  was_prev_idle = (current_flops == 0);
+  theor_max_flops_ += current_speed_ * host_->get_core_count() * (now - last_updated_);
+  current_speed_ = host_->getSpeed();
+  last_updated_  = now;
+  was_prev_idle_ = (current_flops_ == 0);
 }
 
 /**
- * WARNING: This function does not guarantee that you have the real load at any time
- * imagine all actions on your CPU terminate at time t. Your load is then 0. Then
- * you query the load (still 0) and then another action starts (still at time t!).
- * This means that the load was never really 0 (because the time didn't advance) but
- * it will still be reported as 0.
+ * WARNING: This function does not guarantee that you have the real load at any time imagine all actions on your CPU
+ * terminate at time t. Your load is then 0. Then you query the load (still 0) and then another action starts (still at
+ * time t!). This means that the load was never really 0 (because the time didn't advance) but it will still be reported
+ * as 0.
  *
  * So, use at your own risk.
  */
-double HostLoad::getCurrentLoad()
+double HostLoad::get_current_load()
 {
-  // We don't need to call update() here because it is called everytime an
-  // action terminates or starts
+  // We don't need to call update() here because it is called every time an action terminates or starts
   // FIXME: Can this happen at the same time? stop -> call to getCurrentLoad, load = 0 -> next action starts?
-  return current_flops / static_cast<double>(host->getSpeed() * host->get_core_count());
-}
-
-/**
- * Return idle time since last reset
- */
-double HostLoad::getIdleTime()
-{
-  return idle_time;
-}
-
-double HostLoad::getAverageLoad()
-{
-  if (theor_max_flops == 0) { // Avoid division by 0
-    return 0;
-  }
-
-  return computed_flops / theor_max_flops;
-}
-
-double HostLoad::getComputedFlops()
-{
-  return computed_flops;
+  return current_flops_ / static_cast<double>(host_->getSpeed() * host_->get_core_count());
 }
 
 /*
@@ -123,14 +95,14 @@ double HostLoad::getComputedFlops()
  */
 void HostLoad::reset()
 {
-  last_updated    = surf_get_clock();
-  last_reset      = surf_get_clock();
-  idle_time       = 0;
-  computed_flops  = 0;
-  theor_max_flops = 0;
-  current_flops   = host->pimpl_cpu->get_constraint()->get_usage();
-  current_speed   = host->getSpeed();
-  was_prev_idle   = (current_flops == 0);
+  last_updated_    = surf_get_clock();
+  last_reset_      = surf_get_clock();
+  idle_time_       = 0;
+  computed_flops_  = 0;
+  theor_max_flops_ = 0;
+  current_flops_   = host_->pimpl_cpu->get_constraint()->get_usage();
+  current_speed_   = host_->getSpeed();
+  was_prev_idle_   = (current_flops_ == 0);
 }
 } // namespace plugin
 } // namespace simgrid
@@ -140,7 +112,7 @@ using simgrid::plugin::HostLoad;
 /* **************************** events  callback *************************** */
 /* This callback is fired either when the host changes its state (on/off) or its speed
  * (because the user changed the pstate, or because of external trace events) */
-static void onHostChange(simgrid::s4u::Host& host)
+static void on_host_change(simgrid::s4u::Host& host)
 {
   if (dynamic_cast<simgrid::s4u::VirtualMachine*>(&host)) // Ignore virtual machines
     return;
@@ -149,7 +121,7 @@ static void onHostChange(simgrid::s4u::Host& host)
 }
 
 /* This callback is called when an action (computation, idle, ...) terminates */
-static void onActionStateChange(simgrid::surf::CpuAction* action)
+static void on_action_state_change(simgrid::surf::CpuAction* action)
 {
   for (simgrid::surf::Cpu* const& cpu : action->cpus()) {
     simgrid::s4u::Host* host = cpu->get_host();
@@ -184,9 +156,9 @@ void sg_host_load_plugin_init()
     host.extension_set(new HostLoad(&host));
   });
 
-  simgrid::surf::CpuAction::on_state_change.connect(&onActionStateChange);
-  simgrid::s4u::Host::on_state_change.connect(&onHostChange);
-  simgrid::s4u::Host::on_speed_change.connect(&onHostChange);
+  simgrid::surf::CpuAction::on_state_change.connect(&on_action_state_change);
+  simgrid::s4u::Host::on_state_change.connect(&on_host_change);
+  simgrid::s4u::Host::on_speed_change.connect(&on_host_change);
 }
 
 /** @brief Returns the current load of the host passed as argument
@@ -198,7 +170,7 @@ double sg_host_get_current_load(sg_host_t host)
   xbt_assert(HostLoad::EXTENSION_ID.valid(),
              "The Load plugin is not active. Please call sg_host_load_plugin_init() during initialization.");
 
-  return host->extension<HostLoad>()->getCurrentLoad();
+  return host->extension<HostLoad>()->get_current_load();
 }
 
 /** @brief Returns the current load of the host passed as argument
@@ -210,7 +182,7 @@ double sg_host_get_avg_load(sg_host_t host)
   xbt_assert(HostLoad::EXTENSION_ID.valid(),
              "The Load plugin is not active. Please call sg_host_load_plugin_init() during initialization.");
 
-  return host->extension<HostLoad>()->getAverageLoad();
+  return host->extension<HostLoad>()->get_average_load();
 }
 
 /** @brief Returns the time this host was idle since the last reset
@@ -222,7 +194,7 @@ double sg_host_get_idle_time(sg_host_t host)
   xbt_assert(HostLoad::EXTENSION_ID.valid(),
              "The Load plugin is not active. Please call sg_host_load_plugin_init() during initialization.");
 
-  return host->extension<HostLoad>()->getIdleTime();
+  return host->extension<HostLoad>()->get_idle_time();
 }
 
 double sg_host_get_computed_flops(sg_host_t host)
@@ -230,7 +202,7 @@ double sg_host_get_computed_flops(sg_host_t host)
   xbt_assert(HostLoad::EXTENSION_ID.valid(),
              "The Load plugin is not active. Please call sg_host_load_plugin_init() during initialization.");
 
-  return host->extension<HostLoad>()->getComputedFlops();
+  return host->extension<HostLoad>()->get_computed_flops();
 }
 
 void sg_host_load_reset(sg_host_t host)
