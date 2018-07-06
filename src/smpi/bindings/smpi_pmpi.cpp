13d441fdabaa1ce8765fdee7ee61c83253dfa123
Augustin Degomme <degomme@wasabi>
2018-07-06 14:16:49 +0200

    - Support MPI_Error_string TODO : - better and extensible way to store errors/errorstrings, now it's stupid

--- src/smpi/bindings/smpi_pmpi.cpp
+++ src/smpi/bindings/smpi_pmpi.cpp
@@ -208,6 +208,38 @@ int PMPI_Error_class(int errorcode, int* errorclass) {
   return MPI_SUCCESS;
 }
 
+int PMPI_Error_string(int errorcode, char* string, int* resultlen){
+//TODO: put this somewhere else ?
+  const char* smpi_error_strings[] {
+    "MPI_SUCCESS","MPI_ERR_COMM","MPI_ERR_ARG","MPI_ERR_TYPE","MPI_ERR_REQUEST",
+    "MPI_ERR_INTERN","MPI_ERR_COUNT","MPI_ERR_RANK","MPI_ERR_TAG",
+    "MPI_ERR_TRUNCATE","MPI_ERR_GROUP","MPI_ERR_OP","MPI_ERR_OTHER",
+    "MPI_ERR_IN_STATUS","MPI_ERR_PENDING","MPI_ERR_BUFFER","MPI_ERR_NAME",
+    "MPI_ERR_DIMS","MPI_ERR_TOPOLOGY","MPI_ERR_NO_MEM","MPI_ERR_WIN",
+    "MPI_ERR_INFO_VALUE","MPI_ERR_INFO_KEY","MPI_ERR_INFO_NOKEY","MPI_ERR_ROOT",
+    "MPI_ERR_UNKNOWN","MPI_ERR_KEYVAL","MPI_ERR_BASE","MPI_ERR_SPAWN",
+    "MPI_ERR_PORT","MPI_ERR_SERVICE","MPI_ERR_SIZE","MPI_ERR_DISP","MPI_ERR_INFO",
+    "MPI_ERR_LOCKTYPE","MPI_ERR_ASSERT","MPI_RMA_CONFLICT","MPI_RMA_SYNC",
+    "MPI_ERR_FILE","MPI_ERR_NOT_SAME","MPI_ERR_AMODE","MPI_ERR_UNSUPPORTED_DATAREP",
+    "MPI_ERR_UNSUPPORTED_OPERATION","MPI_ERR_NO_SUCH_FILE","MPI_ERR_FILE_EXISTS",
+    "MPI_ERR_BAD_FILE","MPI_ERR_ACCESS","MPI_ERR_NO_SPACE","MPI_ERR_QUOTA",
+    "MPI_ERR_READ_ONLY","MPI_ERR_FILE_IN_USE","MPI_ERR_DUP_DATAREP",
+    "MPI_ERR_CONVERSION","MPI_ERR_IO","MPI_ERR_RMA_ATTACH","MPI_ERR_RMA_CONFLICT",
+    "MPI_ERR_RMA_RANGE","MPI_ERR_RMA_SHARED","MPI_ERR_RMA_SYNC",
+    "MPI_ERR_RMA_FLAVOR","MPI_T_ERR_CANNOT_INIT","MPI_T_ERR_NOT_INITIALIZED",
+    "MPI_T_ERR_MEMORY","MPI_T_ERR_INVALID_INDEX","MPI_T_ERR_INVALID_ITEM",
+    "MPI_T_ERR_INVALID_SESSION","MPI_T_ERR_INVALID_HANDLE",
+    "MPI_T_ERR_OUT_OF_HANDLES","MPI_T_ERR_OUT_OF_SESSIONS",
+    "MPI_T_ERR_CVAR_SET_NOT_NOW","MPI_T_ERR_CVAR_SET_NEVER",
+    "MPI_T_ERR_PVAR_NO_WRITE","MPI_T_ERR_PVAR_NO_STARTSTOP",
+    "MPI_T_ERR_PVAR_NO_ATOMIC"
+  };
+
+  *resultlen = strlen(smpi_error_strings[errorcode]);
+  strncpy(string, smpi_error_strings[errorcode], *resultlen);
+  return MPI_SUCCESS;  
+}
+
 int PMPI_Keyval_create(MPI_Copy_function* copy_fn, MPI_Delete_function* delete_fn, int* keyval, void* extra_state) {
   smpi_copy_fn _copy_fn={copy_fn,nullptr,nullptr};
   smpi_delete_fn _delete_fn={delete_fn,nullptr,nullptr};
