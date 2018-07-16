7bb44227a9dadf58237d2cfda064b157f1e821b9
Frederic Suter <frederic.suter@cc.in2p3.fr>
2018-07-16 16:53:18 +0200

    MSG to S4U conversion. This example is currently broken, hence not tested

---
+++ examples/s4u/platform-failures/s4u-platform-failures.cpp
@@ -0,0 +1,133 @@
+/* Copyright (c) 2007-2018. The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include "simgrid/s4u.hpp"
+#include "xbt/ex.hpp"
+#include "xbt/str.h"
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_test, "Messages specific for this s4u example");
+
+static int master(int argc, char* argv[])
+{
+  simgrid::s4u::MailboxPtr mailbox;
+  long number_of_tasks = xbt_str_parse_int(argv[1], "Invalid amount of tasks: %s");
+  double comp_size     = xbt_str_parse_double(argv[2], "Invalid computational size: %s");
+  double comm_size     = xbt_str_parse_double(argv[3], "Invalid communication size: %s");
+  long workers_count   = xbt_str_parse_int(argv[4], "Invalid amount of workers: %s");
+
+  XBT_INFO("Got %ld workers and %ld tasks to process", workers_count, number_of_tasks);
+
+  for (int i = 0; i < number_of_tasks; i++) {
+    mailbox         = simgrid::s4u::Mailbox::by_name(std::string("worker-") + std::to_string(i % workers_count));
+    double* payload = new double(comp_size);
+    try {
+      mailbox->put(payload, comm_size, 10.0);
+      XBT_INFO("Send completed");
+    } catch (xbt_ex& e) {
+      switch (e.category) {
+        case host_error:
+          XBT_INFO("Gloups. The cpu on which I'm running just turned off!. See you!");
+          return -1;
+          break;
+        case network_error:
+          XBT_INFO("Mmh. Something went wrong with '%s'. Nevermind. Let's keep going!", mailbox->get_cname());
+          break;
+        case timeout_error:
+          XBT_INFO("Mmh. Got timeouted while speaking to '%s'. Nevermind. Let's keep going!", mailbox->get_cname());
+          break;
+        default:
+          xbt_die("Unexpected behavior");
+      }
+      delete payload;
+    }
+  }
+
+  XBT_INFO("All tasks have been dispatched. Let's tell everybody the computation is over.");
+  for (int i = 0; i < workers_count; i++) {
+    /* - Eventually tell all the workers to stop by sending a "finalize" task */
+    mailbox         = simgrid::s4u::Mailbox::by_name(std::string("worker-") + std::to_string(i));
+    double* payload = new double(-1.0);
+    try {
+      mailbox->put(payload, 0, 1.0);
+    } catch (xbt_ex& e) {
+      delete payload;
+      switch (e.category) {
+        case host_error:
+          XBT_INFO("Gloups. The cpu on which I'm running just turned off!. See you!");
+          break;
+        case network_error:
+          XBT_INFO("Mmh. Something went wrong with '%s'. Nevermind. Let's keep going!", mailbox->get_cname());
+          break;
+        case timeout_error:
+          XBT_INFO("Mmh. Got timeouted while speaking to '%s'. Nevermind. Let's keep going!", mailbox->get_cname());
+          break;
+        default:
+          xbt_die("Unexpected behavior");
+      }
+    }
+  }
+
+  XBT_INFO("Goodbye now!");
+  return 0;
+}
+
+static int worker(int argc, char* argv[])
+{
+  long id                          = xbt_str_parse_int(argv[1], "Invalid argument %s");
+  simgrid::s4u::MailboxPtr mailbox = simgrid::s4u::Mailbox::by_name(std::string("worker-") + std::to_string(id));
+  double* payload                  = nullptr;
+  double comp_size;
+  while (1) {
+    try {
+      payload   = static_cast<double*>(mailbox->get());
+      comp_size = *payload;
+      delete payload;
+    } catch (xbt_ex& e) {
+      switch (e.category) {
+        case host_error:
+          XBT_INFO("Gloups. The cpu on which I'm running just turned off!. See you!");
+          return -1;
+        case network_error:
+          XBT_INFO("Mmh. Something went wrong. Nevermind. Let's keep going!");
+          break;
+        default:
+          xbt_die("Unexpected behavior");
+      }
+    }
+    xbt_assert(payload != nullptr, "mailbox->get() failed");
+    if (comp_size < 0) { /* - Exit when -1.0 is received */
+      XBT_INFO("I'm done. See you!");
+      break;
+    }
+    /*  - Otherwise, process the task */
+    try {
+      simgrid::s4u::this_actor::execute(comp_size);
+    } catch (xbt_ex& e) {
+      switch (e.category) {
+        case host_error:
+          XBT_INFO("Gloups. The cpu on which I'm running just turned off!. See you!");
+          return -1;
+        default:
+          xbt_die("Unexpected behavior");
+      }
+    }
+  }
+  XBT_INFO("I'm done. See you!");
+  return 0;
+}
+
+int main(int argc, char* argv[])
+{
+  simgrid::s4u::Engine e(&argc, argv);
+  e.load_platform(argv[1]);
+  e.register_function("master", master);
+  e.register_function("worker", worker);
+  e.load_deployment(argv[2]);
+
+  e.run();
+
+  XBT_INFO("Simulation time %g", simgrid::s4u::Engine::get_clock());
+  return 0;
+}
