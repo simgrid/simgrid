4873a1a7c156e34fcd14cd9b0f32c0bae6f23317
Christian Heinrich <franz-christian.heinrich@inria.fr>
2018-08-10 11:36:55 +0200

    [S(A)MPI] Add copyright header

--- src/smpi/plugins/ampi/ampi.cpp
+++ src/smpi/plugins/ampi/ampi.cpp
@@ -1,3 +1,8 @@
+/* Copyright (c) 2018.      The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
 #include <simgrid/plugins/load_balancer.h>
 #include <simgrid/s4u/Actor.hpp>
 #include <src/smpi/include/smpi_comm.hpp>
