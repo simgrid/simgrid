afb3115dd4b934bd56c68a1c310c0875c073fd86
Martin Quinson <martin.quinson@loria.fr>
2018-08-24 16:11:36 +0200

    these examples should not diverge

--- examples/s4u/async-waitall/s4u-async-waitall.cpp
+++ examples/s4u/async-waitall/s4u-async-waitall.cpp
@@ -46,7 +46,6 @@ public:
 
     /* Start dispatching all messages to receivers, in a round robin fashion */
     for (int i = 0; i < messages_count; i++) {
-
       std::string msg_content = std::string("Message ") + std::to_string(i);
       // Copy the data we send: 'msg_content' is not a stable storage location.
       // It will be destroyed when this actor leaves the loop, ie before the receiver gets it
