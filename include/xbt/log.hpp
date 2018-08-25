29379a85ce9802fd3174f4237b57d240a704cb45
Martin Quinson <martin.quinson@loria.fr>
2018-08-25 11:07:11 +0200

    Merge the content of xbt/exception.hpp into simgrid/Exception.hpp

--- include/xbt/log.hpp
+++ include/xbt/log.hpp
@@ -3,8 +3,8 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
+#include <simgrid/Exception.hpp>
 #include <xbt/log.h>
-#include <xbt/exception.hpp>
 
 namespace simgrid {
 namespace xbt {
