b4e3bc52a926fe5852e61148e7a0b8902203b284
Martin Quinson <martin.quinson@loria.fr>
2018-10-01 23:01:36 +0200

    s4u::Semaphore: use a simpler example with no mutex
    Sorry Millian, I'm a psychopath.

--- examples/s4u/synchro-semaphore/s4u-synchro-semaphore.cpp
+++ examples/s4u/synchro-semaphore/s4u-synchro-semaphore.cpp
@@ -3,64 +3,51 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
+// This example implements a simple producer/consumer schema,
+// passing a bunch of items from one to the other
+
 #include "simgrid/s4u.hpp"
 
 #include <memory>
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_test, "a sample log category");
 
-// This example implements a one-time use barrier with 1 semaphore and 1 mutex.
+const char* buffer;                                                        /* Where the data is exchanged */
+simgrid::s4u::SemaphorePtr sem_empty = simgrid::s4u::Semaphore::create(1); /* indicates whether the buffer is empty */
+simgrid::s4u::SemaphorePtr sem_full  = simgrid::s4u::Semaphore::create(0); /* indicates whether the buffer is full */
+;
 
-static void worker(simgrid::s4u::SemaphorePtr semaphore, simgrid::s4u::MutexPtr mutex, int process_count, std::shared_ptr<int> count)
+static void producer(std::vector<std::string>* args)
 {
-  mutex->lock();
-  XBT_INFO("Got mutex. Incrementing count.");
-  XBT_INFO("Count is %d", *count);
-  *count = (*count) + 1;
-  XBT_INFO("Count is now %d. Process count is %d.", *count, process_count);
-
-  if (*count == process_count) {
-    XBT_INFO("Releasing the semaphore %d times.", process_count-1);
-    for (int i = 0; i < process_count-1; i++) {
-      semaphore->release();
-    }
-
-    XBT_INFO("Releasing mutex.");
-    mutex->unlock();
-  }
-  else {
-    XBT_INFO("Releasing mutex.");
-    mutex->unlock();
-    XBT_INFO("Acquiring semaphore.");
-    semaphore->acquire();
+  for (auto str : *args) {
+    sem_empty->acquire();
+    XBT_INFO("Pushing '%s'", str.c_str());
+    buffer = str.c_str();
+    sem_full->release();
   }
 
   XBT_INFO("Bye!");
 }
-
-static void master(unsigned int process_count)
+static void consumer()
 {
-  simgrid::s4u::SemaphorePtr semaphore = simgrid::s4u::Semaphore::create(0);
-  simgrid::s4u::MutexPtr mutex = simgrid::s4u::Mutex::create();
-  std::shared_ptr<int> count(new int);
-  *count = 0;
+  std::string str;
+  do {
+    sem_full->acquire();
+    str = buffer;
+    XBT_INFO("Receiving '%s'", str.c_str());
+    sem_empty->release();
+  } while (str != "");
 
-  XBT_INFO("Spawning %d workers", process_count);
-  for (unsigned int i = 0; i < process_count; i++) {
-    simgrid::s4u::Actor::create("worker", simgrid::s4u::Host::by_name("Jupiter"), worker, semaphore, mutex, process_count, count);
-  }
+  XBT_INFO("Bye!");
 }
 
 int main(int argc, char **argv)
 {
-  // Parameter: Number of processes in the barrier
-  xbt_assert(argc >= 2, "Usage: %s <process-count>\n", argv[0]);
-  unsigned int process_count = std::stoi(argv[1]);
-  xbt_assert(process_count > 0, "<process-count> must be greater than 0");
-
+  std::vector<std::string> args = std::vector<std::string>({"one", "two", "three", ""});
   simgrid::s4u::Engine e(&argc, argv);
   e.load_platform("../../platforms/two_hosts.xml");
-  simgrid::s4u::Actor::create("master", simgrid::s4u::Host::by_name("Tremblay"), master, process_count);
+  simgrid::s4u::Actor::create("producer", simgrid::s4u::Host::by_name("Tremblay"), producer, &args);
+  simgrid::s4u::Actor::create("consumer", simgrid::s4u::Host::by_name("Jupiter"), consumer);
   e.run();
 
   return 0;
