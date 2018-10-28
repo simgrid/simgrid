65a7a10e6d1e5aaddaabfe7e68af9a6b21769772
Martin Quinson <martin.quinson@loria.fr>
2018-10-28 21:58:57 +0100

    BuildSimGrid.sh: Try to install the lib if possible

--- BuildSimGrid.sh
+++ BuildSimGrid.sh
@@ -11,7 +11,16 @@ if [ ! -e Makefile ] ; then
   exit 1
 fi
 
+target=all
+
+install_path=`grep ^CMAKE_INSTALL_PREFIX:PATH= CMakeCache.txt|sed 's/^[^=]*=//'`
+if [ -e ${install_path} -a -d ${install_path} -a -x ${install_path} ] ; then
+  target=install
+fi
+
 (
-  (nice make -j4 || make) && nice ctest -j4 --output-on-failure ; date
+  echo "install_path: ${install_path}"
+  echo "Target: ${target}"
+  (nice make -j4 ${target} || make) && nice ctest -j4 --output-on-failure ; date
 ) 2>&1 | tee BuildSimGrid.sh.log
 exit 0
