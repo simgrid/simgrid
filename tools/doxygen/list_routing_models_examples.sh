3fcca34fc436367aea1b814fe86f223edfe8cd3e
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2018-11-19 15:22:37 +0100

    Stabilize output by sorting.
    Should improve framagit issue simgrid/simgrid#4.

--- tools/doxygen/list_routing_models_examples.sh
+++ tools/doxygen/list_routing_models_examples.sh
@@ -2,11 +2,13 @@
 
 cd $(dirname $0)/../../
 
+export LC_ALL=C
+
 # -R -- we want to search recursively
 # -l -- only filenames, nothing else
 # $1 -- argument to this script, name of the routing model (e.g., "Floyd")
 # .  -- search in the examples folder and search EVERYTHING
 # --include -- but only include results that end in ".xml"
-grep -R -l "$1" examples/ --include "*.xml"
+grep -R -l "$1" examples/ --include "*.xml" | sort
 
 exit 0
