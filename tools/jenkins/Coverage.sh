b1cd824ac08b0de1868c3966b3e371cf60ac5835
Augustin Degomme <degomme@wasabi>
2019-01-08 15:02:04 +0100

    add sonarqube to the coverage build

--- tools/jenkins/Coverage.sh
+++ tools/jenkins/Coverage.sh
@@ -59,7 +59,8 @@ cmake -Denable_documentation=OFF -Denable_lua=ON -Denable_java=ON \
       -Denable_smpi_papi=ON \
       -Denable_memcheck=OFF -Denable_memcheck_xml=OFF -Denable_smpi_ISP_testsuite=ON -Denable_coverage=ON $WORKSPACE
 
-make -j$NUMPROC
+#build with sonarqube scanner wrapper
+/home/ci/build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-outputs make -j$NUMPROC
 JACOCO_PATH="/usr/local/share/jacoco"
 export JAVA_TOOL_OPTIONS="-javaagent:${JACOCO_PATH}/lib/jacocoagent.jar"
 
