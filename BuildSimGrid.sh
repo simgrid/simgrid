b44c1d2758d3216bce3be647492546cd2d9e04ce
Martin Quinson <martin.quinson@ens-rennes.fr>
2019-01-12 21:22:06 +0100

    Improve BuildSimGrid.sh

--- BuildSimGrid.sh
+++ BuildSimGrid.sh
@@ -6,21 +6,25 @@
 # Copyright (c) 2017-2019 The SimGrid Team. Licence: LGPL of WDFPL, as you want.
 
 if [ ! -e Makefile ] ; then
-  echo "Please configure SimGrid before building it:"
-  echo "   ccmake ."
-  exit 1
+  if [ -e build/default/Makefile ] ; then
+    cd build/default
+  else
+    echo "Please configure SimGrid before building it:"
+    echo "   ccmake ."
+    exit 1
+  fi
 fi
 
 target=tests
 
 install_path=$(sed -n 's/^CMAKE_INSTALL_PREFIX:PATH=//p' CMakeCache.txt)
-if [ -e ${install_path} -a -d ${install_path} -a -x ${install_path} ] ; then
+if [ -e ${install_path} -a -d ${install_path} -a -x ${install_path} -a -w ${install_path} ] ; then
   target=install
 fi
 
 (
   echo "install_path: ${install_path}"
   echo "Target: ${target}"
-  (nice make -j4 ${target} || make) && nice ctest -j4 --output-on-failure ; date
+  (nice make -j4 ${target} tests || make ${target} tests) && nice ctest -j4 --output-on-failure ; date
 ) 2>&1 | tee BuildSimGrid.sh.log
 exit 0
