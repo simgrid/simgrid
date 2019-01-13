35d1d278388686415e326750a4c56c20f227c492
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2019-01-13 15:25:52 +0100

    Remove useless symlink on distcheck.

--- tools/cmake/Distrib.cmake
+++ tools/cmake/Distrib.cmake
@@ -319,9 +319,6 @@ add_custom_target(distcheck
 
   COMMAND ${CMAKE_COMMAND} -E echo "XXX Install"
   COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_BINARY_TEST_DIR}/${PROJECT_NAME}-${release_version}/_build ${CMAKE_MAKE_PROGRAM} install
-  COMMAND ${CMAKE_COMMAND} -E create_symlink
-  ${CMAKE_BINARY_TEST_DIR}/${PROJECT_NAME}-${release_version}/_inst/lib/libsimgrid.so
-  ${CMAKE_BINARY_TEST_DIR}/${PROJECT_NAME}-${release_version}/_inst/lib/libsimgridtest.so
 
   COMMAND ${CMAKE_COMMAND} -E echo "XXX Install with documentation"
   COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_BINARY_TEST_DIR}/${PROJECT_NAME}-${release_version}/_build ${CMAKE_MAKE_PROGRAM} install
