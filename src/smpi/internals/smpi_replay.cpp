8447c8d8e06bb152cf265e23331f9cab0f5cce9e
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2019-01-24 22:47:41 +0100

    Help scan-build not seeing that "status" is initialized.
    Also kill TODO: source is not known when it's irecv.

--- src/smpi/internals/smpi_replay.cpp
+++ src/smpi/internals/smpi_replay.cpp
@@ -457,16 +457,17 @@ void RecvAction::kernel(simgrid::xbt::ReplayAction& action)
     args.size = status.count;
   }
 
+  bool is_recv = false;
   if (name == "recv") {
     Request::recv(nullptr, args.size, args.datatype1, args.partner, args.tag, MPI_COMM_WORLD, &status);
+    is_recv = true;
   } else if (name == "irecv") {
     MPI_Request request = Request::irecv(nullptr, args.size, args.datatype1, args.partner, args.tag, MPI_COMM_WORLD);
     req_storage.add(request);
   }
 
   TRACE_smpi_comm_out(my_proc_id);
-  // TODO: Check why this was only activated in the "recv" case and not in the "irecv" case
-  if (name == "recv" && not TRACE_smpi_view_internals()) {
+  if (is_recv && not TRACE_smpi_view_internals()) {
     int src_traced = MPI_COMM_WORLD->group()->actor(status.MPI_SOURCE)->get_pid();
     TRACE_smpi_recv(src_traced, my_proc_id, args.tag);
   }
