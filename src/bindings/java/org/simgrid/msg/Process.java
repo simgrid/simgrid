362722dbe49ac72a11b8c459ceeb77737681990a
Martin Quinson <martin.quinson@ens-rennes.fr>
2019-01-30 08:06:52 +0100

    sonar: kill unused imports in java

--- src/bindings/java/org/simgrid/msg/Process.java
+++ src/bindings/java/org/simgrid/msg/Process.java
@@ -5,12 +5,8 @@
 
 package org.simgrid.msg;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadInfo;
-import java.lang.management.ThreadMXBean;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * A process may be defined as a code, with some private data, executing
