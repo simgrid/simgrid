6366f4c5942aaaf7264d96aac038a4a7c9983195
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2019-02-04 10:24:15 +0100

    [codacy] minor issues in shell scripts.

--- docs/Build.sh
+++ docs/Build.sh
@@ -32,16 +32,16 @@ PYTHONPATH=../lib sphinx-build -M html source build ${SPHINXOPTS}
 
 set +x
 
-cat source/img/graphical-toc.svg \
- | perl -pe 's/(xlink:href="http)/target="_top" $1/' \
+< source/img/graphical-toc.svg \
+   perl -pe 's/(xlink:href="http)/target="_top" $1/' \
  | perl -pe 's/(xlink:href=".*?.html)/target="_top" $1/' \
  > build/html/graphical-toc.svg
 
 echo "List of missing references:"
-for f in `(grep '<name>' build/xml/msg_8h.xml; \
-           grep '<name>' build/xml/namespacesimgrid_1_1s4u.xml; \
- 	   grep '<innerclass refid=' build/xml/namespacesimgrid_1_1s4u.xml ; \
-          ) |sed 's/<[^>]*>//g'|sort` 
+for f in $( (grep '<name>' build/xml/msg_8h.xml; \
+             grep '<name>' build/xml/namespacesimgrid_1_1s4u.xml; \
+             grep '<innerclass refid=' build/xml/namespacesimgrid_1_1s4u.xml ; \
+            ) | sed 's/<[^>]*>//g' | sort )
 do
 
   if grep $f source/*rst | grep -q '.. doxygen[^::]*:: '"$f"'$' ||
