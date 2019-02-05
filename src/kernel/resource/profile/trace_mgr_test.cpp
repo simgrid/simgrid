6a04725375d75975fd853eaf36d4a5d40c5a3ec2
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2019-02-05 14:01:13 +0100

    Provide our own main() for unit-tests.
    The goal is to be able to use the --log=... command line parameters.

--- src/kernel/resource/profile/trace_mgr_test.cpp
+++ src/kernel/resource/profile/trace_mgr_test.cpp
@@ -3,8 +3,7 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
-#include "src/include/catch.hpp"
+#include "catch.hpp"
 
 #include "simgrid/kernel/resource/Resource.hpp"
 #include "src/kernel/resource/profile/trace_mgr.hpp"
