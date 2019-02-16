48ec45df6c4779f97976a3d72665698f24959f4f
Frederic Suter <frederic.suter@cc.in2p3.fr>
2019-02-16 22:54:44 +0100

    dead header

--- src/kernel/context/Context.hpp
+++ src/kernel/context/Context.hpp
@@ -139,8 +139,6 @@ XBT_PRIVATE void SIMIX_context_runall();
 
 XBT_PUBLIC int SIMIX_process_get_maxpid();
 
-XBT_PRIVATE void SIMIX_post_create_environment();
-
 XBT_PRIVATE simgrid::simix::ActorCodeFactory& SIMIX_get_actor_code_factory(const std::string& name);
 
 #endif
