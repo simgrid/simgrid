e615aa81621620d3a5ba51b3e0066bd1edbf81fe
degomme <adegomme@users.noreply.github.com>
2019-04-03 15:11:06 +0200

    fix f77 tests

--- teshsuite/smpi/mpich3-test/f77/rma/attraints.h
+++ teshsuite/smpi/mpich3-test/f77/rma/attraints.h
@@ -3,4 +3,4 @@ C
 C  (C) 2003 by Argonne National Laboratory.
 C      See COPYRIGHT in top-level directory.
 C
-       integer extrastate, valin, valout, val
+       integer(kind=MPI_ADDRESS_KIND) extrastate, valin, valout, val
