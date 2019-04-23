6d50895ca70f5e7bfe97c5dbaad4d506207e20fd
degomme <adegomme@users.noreply.github.com>
2019-04-23 14:50:37 +0200

    If size read is 0 (eof), don't initiate faulty comm which will crash.

--- src/plugins/file_system/s4u_FileSystem.cpp
+++ src/plugins/file_system/s4u_FileSystem.cpp
@@ -102,7 +102,7 @@ sg_size_t File::read(sg_size_t size)
   sg_size_t read_size = local_storage_->read(std::min(size, size_ - current_position_));
   current_position_ += read_size;
 
-  if (host->get_name() != Host::current()->get_name()) {
+  if (host->get_name() != Host::current()->get_name() && read_size > 0) {
     /* the file is hosted on a remote host, initiate a communication between src and dest hosts for data transfer */
     XBT_DEBUG("File is on %s remote host, initiate data transfer of %llu bytes.", host->get_cname(), read_size);
     std::vector<Host*> m_host_list   = {Host::current(), host};
