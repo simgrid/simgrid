26897ac6e3ae5add8d64c16c97cc9121ba6e70bf
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2019-05-03 22:26:32 +0200

    [sonar] Catch a more specific exception.

--- teshsuite/mc/dwarf-expression/dwarf-expression.cpp
+++ teshsuite/mc/dwarf-expression/dwarf-expression.cpp
@@ -36,10 +36,9 @@ uintptr_t eval_binary_operation(
   ops[2].atom = op;
 
   simgrid::dwarf::ExpressionStack stack;
-
   try {
     simgrid::dwarf::execute(ops, 3, state, stack);
-  } catch (const std::runtime_error&) {
+  } catch (const simgrid::dwarf::evaluation_error&) {
     fprintf(stderr,"Expression evaluation error");
   }
 
@@ -124,7 +123,7 @@ void basic_test(simgrid::dwarf::ExpressionContext const& state) {
   assert(stack.top()  == a);
   assert(stack.top(1) == b);
 
-  } catch (const std::runtime_error&) {
+  } catch (const simgrid::dwarf::evaluation_error&) {
     fprintf(stderr,"Expression evaluation error");
   }
 }
@@ -146,7 +145,7 @@ void test_deref(simgrid::dwarf::ExpressionContext const& state) {
   assert(stack.size() == 1);
   assert(stack.top()  == foo);
 
-  } catch (const std::runtime_error&) {
+  } catch (const simgrid::dwarf::evaluation_error&) {
     fprintf(stderr,"Expression evaluation error");
   }
 }
