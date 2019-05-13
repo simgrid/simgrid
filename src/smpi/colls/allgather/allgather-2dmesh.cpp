34f9387eaac8b66b6f35293ee1aab7f2b7143ee2
Augustin Degomme <adegomme@gmail.com>
2019-05-13 10:26:16 +0200

    On some systems, sqrt(int) is not defined properly. On others its just implemented with a conversion to double.

--- src/smpi/colls/allgather/allgather-2dmesh.cpp
+++ src/smpi/colls/allgather/allgather-2dmesh.cpp
@@ -69,7 +69,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 static int is_2dmesh(int num, int *i, int *j)
 {
   int x, max = num / 2;
-  x = sqrt(num);
+  x = sqrt(double(num));
 
   while (x <= max) {
     if ((num % x) == 0) {
