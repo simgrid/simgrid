12e502742be678b40d9aa89db6abff76b36e62f8
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2019-05-16 11:49:25 +0200

    Don't install empty doc/html/.
    Fix simgrid/simgrid#14 (FramaGit).

--- tools/cmake/Distrib.cmake
+++ tools/cmake/Distrib.cmake
@@ -3,9 +3,7 @@
 #########################################
 
 # doc
-file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/doc/html/)
-install(DIRECTORY "${CMAKE_BINARY_DIR}/doc/html/"
-  DESTINATION doc/simgrid/html/)
+install(DIRECTORY "${CMAKE_BINARY_DIR}/doc/html/" DESTINATION doc/simgrid/html/ OPTIONAL)
 
 # binaries
 if(enable_smpi)
