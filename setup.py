265db5af7be70be60c7ffb3ea3c2f2ee3bbd766d
Martin Quinson <martin.quinson@ens-rennes.fr>
2019-06-25 15:22:37 +0200

    start the dev of 3.23.1

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='simgrid',
-    version='3.23',
+    version='3.23.1',
     author='Da SimGrid Team',
     author_email='simgrid-devel@lists.gforge.inria.fr',
     description='Toolkit for scalable simulation of distributed applications',
