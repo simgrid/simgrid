c26845520f5716e5f94c8e52dfd744466c5b5506
adegomme <13270544+adegomme@users.noreply.github.com>
2019-07-04 16:24:05 +0200

    Add windows to travis (#327)
    travis: compile on windows (and drop appveyor)

--- src/bindings/java/org/simgrid/NativeLib.java
+++ src/bindings/java/org/simgrid/NativeLib.java
@@ -163,9 +163,9 @@ public final class NativeLib {
 		public void run() {
 			try {
 				for (File f : dir.listFiles())
-					if (! f.delete() && !f.getAbsolutePath().contains("appveyor")) // Be silent on AppVeyor to not break the tests. Ugly trick :)
+					if (! f.delete() && !f.getAbsolutePath().contains("travis")) // Be silent on AppVeyor to not break the tests. Ugly trick :)
 						System.out.println("Unable to clean temporary file "+f.getAbsolutePath()+" during shutdown.");
-				if (! dir.delete() && !dir.getAbsolutePath().contains("appveyor") )
+				if (! dir.delete() && !dir.getAbsolutePath().contains("travis") )
 					System.out.println("Unable to clean temporary file "+dir.getAbsolutePath()+" during shutdown.");			    	
 			} catch(Exception e) {
 				System.out.println("Error while cleaning temporary file "+dir.getAbsolutePath()+" during shutdown: "+e.getCause());
