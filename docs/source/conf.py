9f808ab5abb0829e8621127b3d27dea8630e117a
Martin Quinson <martin.quinson@ens-rennes.fr>
2019-07-08 19:39:24 +0200

    start the cycle of 3.23.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ copyright = u'2002-2019, The SimGrid Team'
 author = u'The SimGrid Team'
 
 # The short X.Y version
-version = u'3.23.2'
+version = u'3.23.3'
 # The full version, including alpha/beta/rc tags
 #release = u'3.23 alpha'
 
