2e7e74ae66e984ba0f9c910f0cbe68e0f0ad25b2
clement-dell <clement.mommessin@inria.fr>
2019-10-08 16:35:48 +0200

    Revert "[Energy] Can now retrieve idle power of specified pstate"
    This reverts commit 174b26fda3ba93a0a72fe9c18085088a63cb9a5c.

--- include/simgrid/plugins/energy.h
+++ include/simgrid/plugins/energy.h
@@ -14,7 +14,7 @@ SG_BEGIN_DECL()
 XBT_PUBLIC void sg_host_energy_plugin_init();
 XBT_PUBLIC void sg_host_energy_update_all();
 XBT_PUBLIC double sg_host_get_consumed_energy(sg_host_t host);
-XBT_PUBLIC double sg_host_get_wattidle_at(sg_host_t host, int pstate);
+XBT_PUBLIC double sg_host_get_idle_consumption(sg_host_t host);
 XBT_PUBLIC double sg_host_get_wattmin_at(sg_host_t host, int pstate);
 XBT_PUBLIC double sg_host_get_wattmax_at(sg_host_t host, int pstate);
 XBT_PUBLIC double sg_host_get_power_range_slope_at(sg_host_t host, int pstate);
@@ -27,7 +27,6 @@ XBT_PUBLIC int sg_link_energy_is_inited();
 
 #define MSG_host_energy_plugin_init() sg_host_energy_plugin_init()
 #define MSG_host_get_consumed_energy(host) sg_host_get_consumed_energy(host)
-#define MSG_host_get_wattidle_at(host,pstate) sg_host_get_wattidle_at((host), (pstate))
 #define MSG_host_get_wattmin_at(host,pstate) sg_host_get_wattmin_at((host), (pstate))
 #define MSG_host_get_wattmax_at(host,pstate) sg_host_get_wattmax_at((host), (pstate))
 #define MSG_host_get_power_range_slope_at(host,pstate) sg_host_get_power_range_slope_at((host), (pstate))
