ff3801e182d909d2f6f6a5d5faba0fe341022939
Martin Quinson <martin.quinson@ens-rennes.fr>
2020-01-02 17:16:01 +0100

    automatically determine the core amount

--- BuildSimGrid.sh
+++ BuildSimGrid.sh
@@ -16,6 +16,7 @@ if [ ! -e Makefile ] ; then
 fi
 
 target=tests
+ncores=$(grep -c processor /proc/cpuinfo)
 
 install_path=$(sed -n 's/^CMAKE_INSTALL_PREFIX:PATH=//p' CMakeCache.txt)
 if [ -e ${install_path} ] && [ -d ${install_path} ] && [ -x ${install_path} ] && [ -w ${install_path} ] ; then
@@ -25,6 +26,7 @@ fi
 (
   echo "install_path: ${install_path}"
   echo "Target: ${target}"
-  (nice make -j4 ${target} tests || make ${target} tests) && nice ctest -j4 --output-on-failure ; date
+  echo "Cores: ${ncores}"
+  (nice make -j${ncores} ${target} tests || make ${target} tests) && nice ctest -j${ncores} --output-on-failure ; date
 ) 2>&1 | tee BuildSimGrid.sh.log
 
