50241f61c039617ee59a80d84fb13bbd57c22c96
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2020-01-30 21:57:16 +0100

    Remove old workaround.

--- tools/jenkins/build.sh
+++ tools/jenkins/build.sh
@@ -183,13 +183,6 @@ else
   MAY_DISABLE_SOURCE_CHANGE="-DCMAKE_DISABLE_SOURCE_CHANGES=ON"
 fi
 
-if [ "$os" = "NixOS" ] && [ "$(gcc -dumpversion)" = "7.4.0" ]; then
-    echo "Temporary disable LTO, believed to be broken on this system."
-    MAY_DISABLE_LTO=-Denable_lto=OFF
-else
-    MAY_DISABLE_LTO=
-fi
-
 cmake -G"$GENERATOR" ${INSTALL:+-DCMAKE_INSTALL_PREFIX=$INSTALL} \
   -Denable_debug=ON -Denable_documentation=OFF -Denable_coverage=OFF \
   -Denable_model-checking=$(onoff test "$build_mode" = "ModelChecker") \
@@ -200,7 +193,7 @@ cmake -G"$GENERATOR" ${INSTALL:+-DCMAKE_INSTALL_PREFIX=$INSTALL} \
   -Denable_memcheck=$(onoff test "$build_mode" = "DynamicAnalysis") \
   -Denable_compile_warnings=$(onoff test "$GENERATOR" != "MSYS Makefiles") -Denable_smpi=ON \
   -Denable_ns3=$(onoff test "$have_NS3" = "yes" -a "$build_mode" = "Debug") \
-  -Denable_jedule=OFF -Denable_lua=OFF ${MAY_DISABLE_SOURCE_CHANGE} ${MAY_DISABLE_LTO} \
+  -Denable_jedule=OFF -Denable_lua=OFF ${MAY_DISABLE_SOURCE_CHANGE} \
   -Denable_java=$(onoff test "$build_mode" = "ModelChecker") \
   -Denable_MSG=$(onoff test "$build_mode" = "ModelChecker") \
   $SRCFOLDER
