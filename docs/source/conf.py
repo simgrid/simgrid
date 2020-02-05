d3fe1642bcdd56d478d88c9141695a4aeaa163a4
Julien EMMANUEL <julien.emmanuel@atos.net>
2020-02-05 14:47:23 +0100

    Fix 'TOC' image size in doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -117,7 +117,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -144,6 +144,10 @@ html_context = {
     "gitlab_version": "master",  # Version
     "conf_py_path": "/docs/source/",  # Path in the checkout to the docs root
 }
+
+html_css_files = [
+    'css/custom.css',
+]
 
 # -- Other options
 
