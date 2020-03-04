0e4dfc88e449759be5ef90a53c7fcb828e555af2
Frederic Suter <frederic.suter@cc.in2p3.fr>
2020-03-04 13:15:28 +0100

    convert and simplify msg/process-lifetime

---
+++ examples/c/actor-lifetime/actor-lifetime.c
@@ -0,0 +1,48 @@
+/* Copyright (c) 2007-2020. The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include "simgrid/actor.h"
+#include "simgrid/engine.h"
+
+#include "xbt/asserts.h"
+#include "xbt/log.h"
+
+#include <stdio.h> /* snprintf */
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(test, "Messages specific for this example");
+
+/* Executed on process termination*/
+static int my_onexit(XBT_ATTRIB_UNUSED int ignored1, XBT_ATTRIB_UNUSED void* ignored2)
+{
+  XBT_INFO("Exiting now (done sleeping or got killed)."); /* - Just display an informative message (see tesh file) */
+  return 0;
+}
+
+/* Just sleep until termination */
+static void sleeper(int argc, char* argv[])
+{
+  sg_actor_on_exit(my_onexit, NULL);
+
+  XBT_INFO("Hello! I go to sleep.");
+  sg_actor_sleep_for(10);
+  XBT_INFO("Done sleeping.");
+}
+
+int main(int argc, char* argv[])
+{
+  simgrid_init(&argc, argv);
+  xbt_assert(argc > 2,
+             "Usage: %s platform_file deployment_file\n"
+             "\tExample: %s msg_platform.xml msg_deployment.xml\n",
+             argv[0], argv[0]);
+
+  simgrid_load_platform(argv[1]); /* - Load the platform description */
+  simgrid_register_function("sleeper", sleeper);
+  simgrid_load_deployment(argv[2]); /* - Deploy the sleeper processes with explicit start/kill times */
+
+  simgrid_run(); /* - Run the simulation */
+
+  return 0;
+}
