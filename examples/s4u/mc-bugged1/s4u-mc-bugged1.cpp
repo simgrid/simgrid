100b572b5cdf8a4762953413ddb28be5a66b8af5
Frederic Suter <frederic.suter@cc.in2p3.fr>
2020-03-23 16:43:06 +0100

    convert mc-bugged1 and mc-bugged2

---
+++ examples/s4u/mc-bugged1/s4u-mc-bugged1.cpp
@@ -0,0 +1,58 @@
+/* Copyright (c) 2010-2020. The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+/******************** Non-deterministic message ordering  *********************/
+/* Server assumes a fixed order in the reception of messages from its clients */
+/* which is incorrect because the message ordering is non-deterministic       */
+/******************************************************************************/
+
+#include <simgrid/modelchecker.h>
+#include <simgrid/s4u.hpp>
+
+#define N 3
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(example, "this example");
+
+static void server()
+{
+  void* received = nullptr;
+  int count      = 0;
+  while (count < N) {
+    if (received) {
+      delete static_cast<int*>(received);
+      received = nullptr;
+    }
+    received = simgrid::s4u::Mailbox::by_name("mymailbox")->get();
+    count++;
+  }
+  int value_got = *(static_cast<int*>(received));
+  MC_assert(value_got == 3);
+
+  XBT_INFO("OK");
+}
+
+static void client(int id)
+{
+  int* payload = new int();
+  *payload     = id;
+  simgrid::s4u::Mailbox::by_name("mymailbox")->put(payload, 10000);
+
+  XBT_INFO("Sent!");
+}
+
+int main(int argc, char* argv[])
+{
+  simgrid::s4u::Engine e(&argc, argv);
+
+  e.load_platform(argv[1]);
+
+  simgrid::s4u::Actor::create("server", simgrid::s4u::Host::by_name("HostA"), server);
+  simgrid::s4u::Actor::create("client", simgrid::s4u::Host::by_name("HostB"), client, 1);
+  simgrid::s4u::Actor::create("client", simgrid::s4u::Host::by_name("HostC"), client, 2);
+  simgrid::s4u::Actor::create("client", simgrid::s4u::Host::by_name("HostD"), client, 3);
+
+  e.run();
+  return 0;
+}
---
+++ examples/s4u/mc-bugged2/s4u-mc-bugged2.cpp
@@ -0,0 +1,71 @@
+/* Copyright (c) 2010-2020. The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+/******************** Non-deterministic message ordering  *********************/
+/* Server assumes a fixed order in the reception of messages from its clients */
+/* which is incorrect because the message ordering is non-deterministic       */
+/******************************************************************************/
+
+#include <simgrid/modelchecker.h>
+#include <simgrid/s4u.hpp>
+#define N 3
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(example, "this example");
+
+static void server()
+{
+  void* received1 = nullptr;
+  void* received2 = nullptr;
+
+  received1 = simgrid::s4u::Mailbox::by_name("mymailbox")->get();
+  long val1 = *(static_cast<int*>(received1));
+  received1 = nullptr;
+  XBT_INFO("Received %ld", val1);
+
+  received2 = simgrid::s4u::Mailbox::by_name("mymailbox")->get();
+  long val2 = *(static_cast<int*>(received2));
+  received2 = nullptr;
+  XBT_INFO("Received %ld", val2);
+
+  MC_assert(std::min(val1, val2) == 1);
+
+  received1 = simgrid::s4u::Mailbox::by_name("mymailbox")->get();
+  val1      = *(static_cast<int*>(received1));
+  XBT_INFO("Received %ld", val1);
+
+  received2 = simgrid::s4u::Mailbox::by_name("mymailbox")->get();
+  val2      = *(static_cast<int*>(received2));
+  XBT_INFO("Received %ld", val2);
+
+  XBT_INFO("OK");
+}
+
+static void client(int id)
+{
+  int* payload1 = new int();
+  *payload1     = id;
+  int* payload2 = new int();
+  *payload2     = id;
+
+  XBT_INFO("Send %d", id);
+  simgrid::s4u::Mailbox::by_name("mymailbox")->put(payload1, 10000);
+
+  XBT_INFO("Send %d", id);
+  simgrid::s4u::Mailbox::by_name("mymailbox")->put(payload2, 10000);
+}
+
+int main(int argc, char* argv[])
+{
+  simgrid::s4u::Engine e(&argc, argv);
+
+  e.load_platform(argv[1]);
+
+  simgrid::s4u::Actor::create("server", simgrid::s4u::Host::by_name("HostA"), server);
+  simgrid::s4u::Actor::create("client", simgrid::s4u::Host::by_name("HostB"), client, 1);
+  simgrid::s4u::Actor::create("client", simgrid::s4u::Host::by_name("HostC"), client, 2);
+
+  e.run();
+  return 0;
+}
