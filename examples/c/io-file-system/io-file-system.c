afc75f7692eff7f345826abe82d1e23ac1c18763
Frederic Suter <frederic.suter@cc.in2p3.fr>
2020-03-27 15:58:45 +0100

    please sonar

--- examples/c/io-file-system/io-file-system.c
+++ examples/c/io-file-system/io-file-system.c
@@ -22,7 +22,7 @@ static void show_info(unsigned int disk_count, const sg_disk_t* disks)
   XBT_INFO("Storage info on %s:", sg_host_self_get_name());
 
   for (unsigned int i = 0; i < disk_count; i++) {
-    sg_disk_t d = disks[i];
+    const sg_disk_t d = disks[i];
     // Retrieve disk's information
     XBT_INFO("    %s (%s) Used: %llu; Free: %llu; Total: %llu.", sg_disk_name(d), sg_disk_get_mount_point(d),
              sg_disk_get_size_used(d), sg_disk_get_size_free(d), sg_disk_get_size(d));
