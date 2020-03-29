aa27ecff47c7397cf34c1adf7b803bfe5b669538
Frederic Suter <frederic.suter@cc.in2p3.fr>
2020-03-29 14:08:42 +0200

    convert graphicator to C++ also test the output .dot file in tesh

--- tools/graphicator/graphicator.cpp
+++ tools/graphicator/graphicator.cpp
@@ -4,19 +4,20 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include "simgrid/msg.h"
+#include "simgrid/instr.h"
+#include "simgrid/s4u.hpp"
 #include "xbt/graph.h"
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-  MSG_init(&argc, argv);
+  simgrid::s4u::Engine e(&argc, argv);
 
   xbt_assert(argc == 3, "Usage: %s <platform_file.xml> <graphviz_file.dot>", argv[0]);
 
-  MSG_create_environment(argv[1]);
-  int status = TRACE_platform_graph_export_graphviz (argv[2]);
+  e.load_platform(argv[1]);
+  int status = TRACE_platform_graph_export_graphviz(argv[2]);
 
   xbt_assert(status != 0, "%s expects --cfg=tracing:yes --cfg=tracing/platform:yes", argv[0]);
-  MSG_main(); /* useless, except for correctly cleaning memory */
+  e.run(); /* useless, except for correctly cleaning memory */
   return 0;
 }
