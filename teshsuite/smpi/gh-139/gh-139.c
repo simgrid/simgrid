22e875c804fc69d71ceac52df5573300482113ee
Frederic Suter <frederic.suter@cc.in2p3.fr>
2020-03-30 17:09:37 +0200

    do not include msg.h when not needed

--- teshsuite/smpi/gh-139/gh-139.c
+++ teshsuite/smpi/gh-139/gh-139.c
@@ -5,7 +5,7 @@
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
 #include <mpi.h>
-#include <simgrid/msg.h>
+#include <simgrid/actor.h>
 #include <simgrid/simix.h>
 #include <stdio.h>
 
