9d6ea602ef05872cef8d285db63abf6d5e836318
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2020-06-04 17:39:59 +0200

    Prefer doublequotes (codacy).

--- docs/source/_ext/showfile.js
+++ docs/source/_ext/showfile.js
@@ -6,7 +6,7 @@ $(function() {
     $(".hidden-content").hide();
     $(".toggle-header").click(function () {
         $(this).toggleClass("open");
-        $(this).next(".toggle-content").toggle('400');
+        $(this).next(".toggle-content").toggle("400");
     });
 });
 
