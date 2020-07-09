05049d03744ae8d14380eb3d7890a89717a82d23
Martin Quinson <martin.quinson@ens-rennes.fr>
2020-07-10 01:11:45 +0200

    Fix make distcheck

--- tools/cmake/DefinePackages.cmake
+++ tools/cmake/DefinePackages.cmake
@@ -1196,6 +1196,7 @@ set(PLATFORMS_EXAMPLES
   examples/platforms/two_hosts_platform_with_availability_included.xml
   examples/platforms/two_peers.xml
   examples/platforms/vivaldi.xml
+  examples/platforms/wifi_energy.xml
   examples/platforms/wifi.xml
   )
 
