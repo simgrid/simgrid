simgrid/simgrid

commit b024b9e443552c94609fc56a240cdb199a329853

Replace #define with constexpr declarations.

src/smpi/internals/smpi_shared.cpp
@@ -180,11 +180,11 @@ static void *smpi_shared_malloc_local(size_t size, const char *file, int line)
 }
 
 // Align functions, from http://stackoverflow.com/questions/4840410/how-to-align-a-pointer-in-c
-#define PAGE_SIZE 0x1000
 #define ALIGN_UP(n, align) (((n) + (align)-1) & -(align))
 #define ALIGN_DOWN(n, align) ((n) & -(align))
 
-#define HUGE_PAGE_SIZE 1<<21
+constexpr unsigned PAGE_SIZE      = 0x1000;
+constexpr unsigned HUGE_PAGE_SIZE = 1U << 21;
 
 /* Similar to smpi_shared_malloc, but only sharing the blocks described by shared_block_offsets.
  * This array contains the offsets (in bytes) of the block to share.

examples/s4u/app-bittorrent/s4u-bittorrent.hpp
@@ -10,33 +10,33 @@
 #include <simgrid/s4u.hpp>
 #include <xbt/RngStream.h>
 
-#define TRACKER_MAILBOX "tracker_mailbox"
+constexpr char TRACKER_MAILBOX[] = "tracker_mailbox";
 /** Max number of peers sent by the tracker to clients */
-#define MAXIMUM_PEERS 50
+constexpr int MAXIMUM_PEERS = 50;
 /** Interval of time where the peer should send a request to the tracker */
-#define TRACKER_QUERY_INTERVAL 1000
+constexpr int TRACKER_QUERY_INTERVAL = 1000;
 /** Communication size for a task to the tracker */
-#define TRACKER_COMM_SIZE 1
-#define GET_PEERS_TIMEOUT 10000
+constexpr unsigned TRACKER_COMM_SIZE = 1;
+constexpr double GET_PEERS_TIMEOUT   = 10000.0;
 /** Number of peers that can be unchocked at a given time */
-#define MAX_UNCHOKED_PEERS 4
+constexpr int MAX_UNCHOKED_PEERS = 4;
 /** Interval between each update of the choked peers */
-#define UPDATE_CHOKED_INTERVAL 30
+constexpr int UPDATE_CHOKED_INTERVAL = 30;
 
 /** Message sizes
  * Sizes based on report by A. Legout et al, Understanding BitTorrent: An Experimental Perspective
  * http://hal.inria.fr/inria-00000156/en
  */
-#define MESSAGE_HANDSHAKE_SIZE 68
-#define MESSAGE_CHOKE_SIZE 5
-#define MESSAGE_UNCHOKE_SIZE 5
-#define MESSAGE_INTERESTED_SIZE 5
-#define MESSAGE_NOTINTERESTED_SIZE 5
-#define MESSAGE_HAVE_SIZE 9
-#define MESSAGE_BITFIELD_SIZE 5
-#define MESSAGE_REQUEST_SIZE 17
-#define MESSAGE_PIECE_SIZE 13
-#define MESSAGE_CANCEL_SIZE 17
+constexpr unsigned MESSAGE_HANDSHAKE_SIZE     = 68;
+constexpr unsigned MESSAGE_CHOKE_SIZE         = 5;
+constexpr unsigned MESSAGE_UNCHOKE_SIZE       = 5;
+constexpr unsigned MESSAGE_INTERESTED_SIZE    = 5;
+constexpr unsigned MESSAGE_NOTINTERESTED_SIZE = 5;
+constexpr unsigned MESSAGE_HAVE_SIZE          = 9;
+constexpr unsigned MESSAGE_BITFIELD_SIZE      = 5;
+constexpr unsigned MESSAGE_REQUEST_SIZE       = 17;
+constexpr unsigned MESSAGE_PIECE_SIZE         = 13;
+constexpr unsigned MESSAGE_CANCEL_SIZE        = 17;
 
 /** Types of messages exchanged between two peers. */
 enum e_message_type {

examples/s4u/app-bittorrent/s4u-peer.cpp
@@ -15,15 +15,15 @@ XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_bt_peer, "Messages specific for the peers");
  * User parameters for transferred file data. For the test, the default values are :
  * File size: 10 pieces * 5 blocks/piece * 16384 bytes/block = 819200 bytes
  */
-#define FILE_PIECES 10UL
-#define PIECES_BLOCKS 5UL
-#define BLOCK_SIZE 16384
+constexpr unsigned long FILE_PIECES   = 10UL;
+constexpr unsigned long PIECES_BLOCKS = 5UL;
+constexpr int BLOCK_SIZE              = 16384;
 
 /** Number of blocks asked by each request */
-#define BLOCKS_REQUESTED 2UL
+constexpr unsigned long BLOCKS_REQUESTED = 2UL;
 
-#define ENABLE_END_GAME_MODE 1
-#define SLEEP_DURATION 1
+constexpr bool ENABLE_END_GAME_MODE = true;
+constexpr double SLEEP_DURATION     = 1.0;
 #define BITS_TO_BYTES(x) (((x) / 8 + (x) % 8) ? 1 : 0)
 
 Peer::Peer(std::vector<std::string> args)
@@ -453,9 +453,8 @@ int Peer::selectPieceToDownload(Connection* remote_peer)
 
   // end game mode
   if (countPieces(current_pieces) >= (FILE_PIECES - countPieces(bitfield_)) && isInterestedBy(remote_peer)) {
-#if ENABLE_END_GAME_MODE == 0
-    return -1;
-#endif
+    if (not ENABLE_END_GAME_MODE)
+      return -1;
     int nb_interesting_pieces = 0;
     // compute the number of interesting pieces
     for (unsigned int i = 0; i < FILE_PIECES; i++)

examples/s4u/app-chainsend/s4u-app-chainsend.cpp
@@ -6,9 +6,9 @@
 #include "simgrid/s4u.hpp"
 #include <vector>
 
-#define PIECE_SIZE 65536
-#define MESSAGE_BUILD_CHAIN_SIZE 40
-#define MESSAGE_SEND_DATA_HEADER_SIZE 1
+constexpr unsigned PIECE_SIZE                    = 65536;
+constexpr unsigned MESSAGE_BUILD_CHAIN_SIZE      = 40;
+constexpr unsigned MESSAGE_SEND_DATA_HEADER_SIZE = 1;
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_chainsend, "Messages specific for chainsend");
 

examples/s4u/dht-chord/s4u-dht-chord.hpp
@@ -10,12 +10,12 @@
 #include <xbt/RngStream.h>
 #include <xbt/str.h>
 
-#define MAX_SIMULATION_TIME 1000
-#define PERIODIC_STABILIZE_DELAY 20
-#define PERIODIC_FIX_FINGERS_DELAY 120
-#define PERIODIC_CHECK_PREDECESSOR_DELAY 120
-#define PERIODIC_LOOKUP_DELAY 10
-#define SLEEP_DELAY 4.9999
+constexpr double MAX_SIMULATION_TIME              = 1000;
+constexpr double PERIODIC_STABILIZE_DELAY         = 20;
+constexpr double PERIODIC_FIX_FINGERS_DELAY       = 120;
+constexpr double PERIODIC_CHECK_PREDECESSOR_DELAY = 120;
+constexpr double PERIODIC_LOOKUP_DELAY            = 10;
+constexpr double SLEEP_DELAY                      = 4.9999;
 
 extern int nb_bits;
 extern int nb_keys;

examples/s4u/dht-kademlia/node.cpp
@@ -58,13 +58,13 @@ bool Node::join(unsigned int known_id)
 
   /* Second step: Send a FIND_NODE to a a random node in buckets */
   unsigned int bucket_id = table->findBucket(known_id)->getId();
-  xbt_assert(bucket_id <= identifier_size);
-  for (i = 0; ((bucket_id > i) || (bucket_id + i) <= identifier_size) && i < JOIN_BUCKETS_QUERIES; i++) {
+  xbt_assert(bucket_id <= IDENTIFIER_SIZE);
+  for (i = 0; ((bucket_id > i) || (bucket_id + i) <= IDENTIFIER_SIZE) && i < JOIN_BUCKETS_QUERIES; i++) {
     if (bucket_id > i) {
       unsigned int id_in_bucket = get_id_in_prefix(id_, bucket_id - i);
       findNode(id_in_bucket, false);
     }
-    if (bucket_id + i <= identifier_size) {
+    if (bucket_id + i <= IDENTIFIER_SIZE) {
       unsigned int id_in_bucket = get_id_in_prefix(id_, bucket_id + i);
       findNode(id_in_bucket, false);
     }
@@ -90,7 +90,7 @@ void Node::sendFindNode(unsigned int id, unsigned int destination)
 }
 
 /**
-  * Sends to the best "kademlia_alpha" nodes in the "node_list" array a "FIND_NODE" request, to ask them for their best
+  * Sends to the best "KADEMLIA_ALPHA" nodes in the "node_list" array a "FIND_NODE" request, to ask them for their best
   * nodes
   */
 unsigned int Node::sendFindNodeToBest(Answer* node_list)
@@ -99,14 +99,14 @@ unsigned int Node::sendFindNodeToBest(Answer* node_list)
   unsigned int j           = 0;
   unsigned int destination = node_list->getDestinationId();
   for (auto node_to_query : node_list->nodes) {
-    /* We need to have at most "kademlia_alpha" requests each time, according to the protocol */
+    /* We need to have at most "KADEMLIA_ALPHA" requests each time, according to the protocol */
     /* Gets the node we want to send the query to */
     if (node_to_query.first != id_) { /* No need to query ourselves */
       sendFindNode(node_to_query.first, destination);
       j++;
     }
     i++;
-    if (j == kademlia_alpha)
+    if (j == KADEMLIA_ALPHA)
       break;
   }
   return i;
@@ -148,26 +148,26 @@ Answer* Node::findClosest(unsigned int destination_id)
   /* We find the corresponding bucket for the id */
   Bucket* bucket = table->findBucket(destination_id);
   int bucket_id  = bucket->getId();
-  xbt_assert((bucket_id <= identifier_size), "Bucket found has a wrong identifier");
+  xbt_assert((bucket_id <= IDENTIFIER_SIZE), "Bucket found has a wrong identifier");
   /* So, we copy the contents of the bucket unsigned into our answer */
   answer->addBucket(bucket);
 
-  /* However, if we don't have enough elements in our bucket, we NEED to include at least "bucket_size" elements
-   * (if, of course, we know at least "bucket_size" elements. So we're going to look unsigned into the other buckets.
+  /* However, if we don't have enough elements in our bucket, we NEED to include at least "BUCKET_SIZE" elements
+   * (if, of course, we know at least "BUCKET_SIZE" elements. So we're going to look unsigned into the other buckets.
    */
-  for (int i = 1; answer->getSize() < BUCKET_SIZE && ((bucket_id - i > 0) || (bucket_id + i < identifier_size)); i++) {
+  for (int i = 1; answer->getSize() < BUCKET_SIZE && ((bucket_id - i > 0) || (bucket_id + i < IDENTIFIER_SIZE)); i++) {
     /* We check the previous buckets */
     if (bucket_id - i >= 0) {
       Bucket* bucket_p = table->buckets[bucket_id - i];
       answer->addBucket(bucket_p);
     }
     /* We check the next buckets */
-    if (bucket_id + i <= identifier_size) {
+    if (bucket_id + i <= IDENTIFIER_SIZE) {
       Bucket* bucket_n = table->buckets[bucket_id + i];
       answer->addBucket(bucket_n);
     }
   }
-  /* We trim the array to have only bucket_size or less elements */
+  /* We trim the array to have only BUCKET_SIZE or less elements */
   std::sort(answer->nodes.begin(), answer->nodes.end(), sortbydistance);
   answer->trim();
 
@@ -183,7 +183,7 @@ bool Node::findNode(unsigned int id_to_find, bool count_in_stats)
   unsigned int answers;
   bool destination_found   = false;
   unsigned int nodes_added = 0;
-  double global_timeout    = simgrid::s4u::Engine::get_clock() + find_node_global_timeout;
+  double global_timeout    = simgrid::s4u::Engine::get_clock() + FIND_NODE_GLOBAL_TIMEOUT;
   unsigned int steps       = 0;
 
   /* First we build a list of who we already know */
@@ -196,7 +196,7 @@ bool Node::findNode(unsigned int id_to_find, bool count_in_stats)
     answers        = 0;
     queries        = sendFindNodeToBest(node_list);
     nodes_added    = 0;
-    double timeout = simgrid::s4u::Engine::get_clock() + find_node_timeout;
+    double timeout = simgrid::s4u::Engine::get_clock() + FIND_NODE_TIMEOUT;
     steps++;
     double time_beginreceive = simgrid::s4u::Engine::get_clock();
 

examples/s4u/dht-kademlia/routing_table.cpp
@@ -14,15 +14,15 @@ namespace kademlia {
 /** @brief Initialization of a node routing table.  */
 RoutingTable::RoutingTable(unsigned int node_id) : id_(node_id)
 {
-  buckets = new Bucket*[identifier_size + 1];
-  for (unsigned int i = 0; i < identifier_size + 1; i++)
+  buckets = new Bucket*[IDENTIFIER_SIZE + 1];
+  for (unsigned int i = 0; i < IDENTIFIER_SIZE + 1; i++)
     buckets[i]        = new Bucket(i);
 }
 
 RoutingTable::~RoutingTable()
 {
   // Free the buckets.
-  for (unsigned int i = 0; i <= identifier_size; i++) {
+  for (unsigned int i = 0; i <= IDENTIFIER_SIZE; i++) {
     delete buckets[i];
   }
   delete[] buckets;
@@ -32,7 +32,7 @@ void RoutingTable::print()
 {
   XBT_INFO("Routing table of %08x:", id_);
 
-  for (unsigned int i = 0; i <= identifier_size; i++) {
+  for (unsigned int i = 0; i <= IDENTIFIER_SIZE; i++) {
     if (not buckets[i]->nodes.empty()) {
       XBT_INFO("Bucket number %u: ", i);
       int j = 0;
@@ -51,8 +51,8 @@ void RoutingTable::print()
 Bucket* RoutingTable::findBucket(unsigned int id)
 {
   unsigned int xor_number = id_ ^ id;
-  unsigned int prefix     = get_node_prefix(xor_number, identifier_size);
-  xbt_assert(prefix <= identifier_size, "Tried to return a  bucket that doesn't exist.");
+  unsigned int prefix     = get_node_prefix(xor_number, IDENTIFIER_SIZE);
+  xbt_assert(prefix <= IDENTIFIER_SIZE, "Tried to return a  bucket that doesn't exist.");
   return buckets[prefix];
 }
 

examples/s4u/dht-kademlia/s4u-dht-kademlia.cpp
@@ -40,7 +40,7 @@ static int node(int argc, char* argv[])
   if (join_success) {
     XBT_VERB("Ok, I'm joining the network with id %u", node->getId());
     // We start the main loop
-    double next_lookup_time = simgrid::s4u::Engine::get_clock() + random_lookup_interval;
+    double next_lookup_time = simgrid::s4u::Engine::get_clock() + RANDOM_LOOKUP_INTERVAL;
 
     XBT_VERB("Main loop start");
 
@@ -64,7 +64,7 @@ static int node(int argc, char* argv[])
         /* We search for a pseudo random node */
         if (simgrid::s4u::Engine::get_clock() >= next_lookup_time) {
           node->randomLookup();
-          next_lookup_time += random_lookup_interval;
+          next_lookup_time += RANDOM_LOOKUP_INTERVAL;
         } else {
           // Didn't get a message: sleep for a while...
           simgrid::s4u::this_actor::sleep_for(1);

commit 341a739ceeceeb3969d4463330813cf92225a409

Fix shared-malloc with huge pages
This fixes a bug introduced in b024b9e443552c94609fc56a240cdb199a329853

The constant HUGE_PAGE_SIZE needs to be 64 bits, otherwise the following
has an integer overflow (line 187 in smpi_shared.cpp):
    mem = (void*)ALIGN_UP(allocated_ptr, HUGE_PAGE_SIZE);

This commit also modifies the declaration of PAGE_SIZE for the sake of
uniformity.

src/smpi/internals/smpi_shared.cpp
@@ -145,8 +145,8 @@ static void *smpi_shared_malloc_local(size_t size, const char *file, int line)
 #define ALIGN_UP(n, align) (((int64_t)(n) + (int64_t)(align) - 1) & -(int64_t)(align))
 #define ALIGN_DOWN(n, align) ((int64_t)(n) & -(int64_t)(align))
 
-constexpr unsigned PAGE_SIZE      = 0x1000;
-constexpr unsigned HUGE_PAGE_SIZE = 1U << 21;
+constexpr long unsigned PAGE_SIZE      = 0x1000;
+constexpr long unsigned HUGE_PAGE_SIZE = 1U << 21;
 
 /* Similar to smpi_shared_malloc, but only sharing the blocks described by shared_block_offsets.
  * This array contains the offsets (in bytes) of the block to share.