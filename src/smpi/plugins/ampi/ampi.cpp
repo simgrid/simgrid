2a2a7f852061b03a320de8f8febadf7adbca1889
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2020-12-04 10:13:09 +0100

    Drop unused smpi/plugins/load_balancer.
    There's no example, and it seems that it's not used since nearly 2 years.

--- src/smpi/plugins/ampi/ampi.cpp
+++ src/smpi/plugins/ampi/ampi.cpp
@@ -3,7 +3,6 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include <simgrid/plugins/load_balancer.h>
 #include <simgrid/s4u/Actor.hpp>
 #include <src/instr/instr_smpi.hpp>
 #include <src/smpi/include/smpi_actor.hpp>
