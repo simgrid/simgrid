6c0cc528bbc407f1a8233474fdd7d597ef5b4be5
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2020-12-15 22:59:54 +0100

    Improve deprecation warning.
    In fact, we remove deprecated feature at the start of each dev cycle, thus any following
    minor version have them removed too.

--- include/xbt/base.h
+++ include/xbt/base.h
@@ -54,11 +54,11 @@
 #endif
 
 #define XBT_ATTRIB_DEPRECATED_v328(mesg)                                                                               \
-  XBT_ATTRIB_DEPRECATED(mesg " (this compatibility wrapper will be dropped in v3.28)")
+  XBT_ATTRIB_DEPRECATED(mesg " (this compatibility wrapper will be dropped after v3.27)")
 #define XBT_ATTRIB_DEPRECATED_v329(mesg)                                                                               \
-  XBT_ATTRIB_DEPRECATED(mesg " (this compatibility wrapper will be dropped in v3.29)")
+  XBT_ATTRIB_DEPRECATED(mesg " (this compatibility wrapper will be dropped after v3.28)")
 #define XBT_ATTRIB_DEPRECATED_v330(mesg)                                                                               \
-  XBT_ATTRIB_DEPRECATED(mesg " (this compatibility wrapper will be dropped in v3.30)")
+  XBT_ATTRIB_DEPRECATED(mesg " (this compatibility wrapper will be dropped after v3.29)")
 
 #if !defined(__APPLE__)
 #  define XBT_ATTRIB_CONSTRUCTOR(prio) __attribute__((__constructor__(prio)))
