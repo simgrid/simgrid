e27e197d20bda6dec2518d1aebd51dda39844ba7
eazimi <azimi.ehsan@outlook.com>
2021-01-13 10:44:15 +0100

    udpor_global.cpp defined

---
+++ src/mc/udpor_global.cpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2008-2021. The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include "udpor_global.hpp"
+#include "xbt/log.h"
+
+XBT_LOG_NEW_DEFAULT_SUBCATEGORY(mc_udpor_global, mc, "udpor_global");
+
+namespace simgrid {
+namespace mc {
+
+} // namespace mc
+} // namespace simgrid
--- tools/cmake/DefinePackages.cmake
+++ tools/cmake/DefinePackages.cmake
@@ -678,6 +678,7 @@ set(MC_SRC
   src/mc/mc_smx.cpp
   src/mc/mc_exit.hpp
   src/mc/Transition.hpp
+  src/mc/udpor_global.cpp
   src/mc/udpor_global.hpp
   )
 
