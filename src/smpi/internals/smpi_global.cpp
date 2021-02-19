35126e077f320f497bb4356de585bbeb898c7827
Augustin Degomme <adegomme@users.noreply.github.com>
2021-02-19 08:53:24 +0100

    that didn't work as intended

--- src/smpi/internals/smpi_global.cpp
+++ src/smpi/internals/smpi_global.cpp
@@ -639,15 +639,17 @@ void SMPI_finalize()
     if (simgrid::smpi::F2C::lookup()->size() > simgrid::smpi::F2C::get_num_default_handles()){
       XBT_WARN("Probable Leaks in your code: SMPI detected %zu unfreed MPI handles : "
                "display types and addresses (n max) with --cfg=smpi/list-leaks:n.\n"
-               "Running smpirun with -wrapper \"valgrind --leak-check=full\" can provide more information", 
+               "Running smpirun with -wrapper \"valgrind --leak-check=full\" can provide more information",
                simgrid::smpi::F2C::lookup()->size() - simgrid::smpi::F2C::get_num_default_handles());
       int n = simgrid::config::get_value<int>("smpi/list-leaks");
       if (n > 0) {
         std::for_each(simgrid::smpi::F2C::lookup()->begin(),
                   simgrid::smpi::F2C::lookup()->end(),
                   [n](const std::pair<unsigned int, simgrid::smpi::F2C*> &p) {
-                      if(p.first >= simgrid::smpi::F2C::get_num_default_handles() && p.first < simgrid::smpi::F2C::get_num_default_handles() + n) 
+                      static int printed = 0;
+                      if(p.first >= simgrid::smpi::F2C::get_num_default_handles() && printed < n)
                         XBT_WARN ("Leak %p of type %s", p.second, boost::core::demangle(typeid(*(p.second)).name()).c_str() );
+                        printed++;
                   });
       }
     }
