463dc080c0efaea4a700be46cd4f00814ba8ea9b
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2021-03-11 15:07:15 +0100

    [sonar] Use :: to call static member functions.

--- examples/cpp/exec-ptask-multicore/s4u-exec-ptask-multicore.cpp
+++ examples/cpp/exec-ptask-multicore/s4u-exec-ptask-multicore.cpp
@@ -16,19 +16,19 @@ static void runner()
   std::vector<double> comm(4, 0.0);
   // Different hosts.
   std::vector<sg4::Host*> hosts_diff = {e->host_by_name("MyHost2"), e->host_by_name("MyHost3")};
-  double start_time                  = e->get_clock();
+  double start_time                  = sg4::Engine::get_clock();
   sg4::this_actor::parallel_execute(hosts_diff, comp, comm);
   XBT_INFO("Computed 2-core activity on two different hosts. Took %g s", e->get_clock() - start_time);
 
   // Same host, monocore.
   std::vector<sg4::Host*> monocore_hosts = {e->host_by_name("MyHost2"), e->host_by_name("MyHost2")};
-  start_time                             = e->get_clock();
+  start_time                             = sg4::Engine::get_clock();
   sg4::this_actor::parallel_execute(monocore_hosts, comp, comm);
   XBT_INFO("Computed 2-core activity one 1-core host. Took %g s", e->get_clock() - start_time);
 
   // Same host, multicore.
   std::vector<sg4::Host*> multicore_host = {e->host_by_name("MyHost1"), e->host_by_name("MyHost1")};
-  start_time                             = e->get_clock();
+  start_time                             = sg4::Engine::get_clock();
   sg4::this_actor::parallel_execute(multicore_host, comp, comm);
   XBT_INFO("Computed 2-core activity on one 2-core host. Took %g s", e->get_clock() - start_time);
 
@@ -36,7 +36,7 @@ static void runner()
   std::vector<double> comp6(6, 1e9);
   std::vector<double> comm6(36, 0.0);
   std::vector<sg4::Host*> multicore_overload(6, e->host_by_name("MyHost1"));
-  start_time = e->get_clock();
+  start_time = sg4::Engine::get_clock();
   sg4::this_actor::parallel_execute(multicore_overload, comp6, comm6);
   XBT_INFO("Computed 6-core activity of a 4-core host. Took %g s", e->get_clock() - start_time);
 }
