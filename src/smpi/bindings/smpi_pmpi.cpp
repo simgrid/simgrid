34578bad78b318039da7a1462c987ab6a29917b9
Augustin Degomme <adegomme@gmail.com>
2021-04-03 14:51:49 +0200

    Abort : check input, and remove fixme... Actual implementations do the same

--- src/smpi/bindings/smpi_pmpi.cpp
+++ src/smpi/bindings/smpi_pmpi.cpp
@@ -124,10 +124,11 @@ int PMPI_Is_thread_main(int *flag)
   }
 }
 
-int PMPI_Abort(MPI_Comm /*comm*/, int /*errorcode*/)
+int PMPI_Abort(MPI_Comm comm, int /*errorcode*/)
 {
   smpi_bench_end();
-  // FIXME: should kill all processes in comm instead
+  CHECK_COMM(1)
+  // Should kill all processes in comm instead.. but most real implementations also kill everything at this point
   XBT_WARN("MPI_Abort was called, something went probably wrong in this simulation ! Killing this process");
   smx_actor_t actor = SIMIX_process_self();
   simgrid::kernel::actor::simcall([actor] { actor->exit(); });
