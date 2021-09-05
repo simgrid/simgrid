bf715fe2cc9ef8b2bcb7805a72eef25a87b87aaa
SUTER Frederic <frederic.suter@cc.in2p3.fr>
2021-09-05 16:44:26 +0200

    fix 32-bit builds

--- src/surf/cpu_interface.cpp
+++ src/surf/cpu_interface.cpp
@@ -69,9 +69,9 @@ CpuImpl* CpuImpl::set_pstate(unsigned long pstate_index)
 {
   xbt_assert(
       pstate_index <= speed_per_pstate_.size(),
-      "Invalid parameters for CPU %s (pstate %lu > length of pstates %lu). Please fix your platform file, or your "
+      "Invalid parameters for CPU %s (pstate %lu > length of pstates %d). Please fix your platform file, or your "
       "call to change the pstate.",
-      get_cname(), pstate_index, speed_per_pstate_.size());
+      get_cname(), pstate_index, static_cast<int>(speed_per_pstate_.size()));
 
   double new_peak_speed = speed_per_pstate_[pstate_index];
   pstate_               = pstate_index;
