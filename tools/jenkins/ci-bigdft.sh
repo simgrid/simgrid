cc7f416d5d043ec872a2443d464f058fb029288a
Augustin Degomme <adegomme@gmail.com>
2021-09-14 18:45:17 +0200

    switch to bigdft sdk container for building bigdft, it already has most packages (soon all)

--- tools/jenkins/ci-bigdft.sh
+++ tools/jenkins/ci-bigdft.sh
@@ -9,7 +9,7 @@ set -ex
 echo "XXXXXXXXXXXXXXXX Install APT dependencies"
 SUDO="" # to ease the local testing
 $SUDO apt-get -y update
-$SUDO apt-get -y install git build-essential gfortran python-is-python3 python3-six python3-distutils python3-setuptools pkg-config automake cmake libboost-dev libblas-dev liblapack-dev wget
+$SUDO apt-get -y install python-is-python3 python3-setuptools libboost-dev
 
 echo "XXXXXXXXXXXXXXXX build and test BigDFT (git version)"
 git clone --depth=1 https://gitlab.com/l_sim/bigdft-suite.git
