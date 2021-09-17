a9ab9eaa4865e6b19550cc7519d7f534c4df2f0f
Mael Madon <mael.madon@irit.fr>
2021-09-17 16:23:44 +0200

    Extend 1 unit test and add 1 other for multicore ptast exec. Both don't pass right now but should. Related to #95

--- examples/cpp/exec-ptask-multicore-latency/s4u-exec-ptask-multicore-latency.cpp
+++ examples/cpp/exec-ptask-multicore-latency/s4u-exec-ptask-multicore-latency.cpp
@@ -15,16 +15,10 @@ static void runner()
   std::vector<double> comp(2, 1e9);
   std::vector<double> comm(4, 0.0);
   // Different hosts.
-  std::vector<sg4::Host*> hosts_diff = {e->host_by_name("MyHost2"), e->host_by_name("MyHost3")};
+  std::vector<sg4::Host*> hosts_diff = {e->host_by_name("MyHost1"), e->host_by_name("MyHost2")};
   double start_time                  = sg4::Engine::get_clock();
   sg4::this_actor::parallel_execute(hosts_diff, comp, comm);
   XBT_INFO("Computed 2-core activity on two different hosts. Took %g s", e->get_clock() - start_time);
-
-  // Same host, monocore.
-  std::vector<sg4::Host*> monocore_hosts = {e->host_by_name("MyHost2"), e->host_by_name("MyHost2")};
-  start_time                             = sg4::Engine::get_clock();
-  sg4::this_actor::parallel_execute(monocore_hosts, comp, comm);
-  XBT_INFO("Computed 2-core activity one 1-core host. Took %g s", e->get_clock() - start_time);
 
   // Same host, multicore.
   std::vector<sg4::Host*> multicore_host = {e->host_by_name("MyHost1"), e->host_by_name("MyHost1")};
@@ -42,7 +36,7 @@ static void runner()
 
   // Same host, adding some communication
   std::vector<double> comm2 = {0, 1E7, 1E7, 0};
-  start_time                = sg4::Engine::get_clock();
+  start_time = sg4::Engine::get_clock();
   sg4::this_actor::parallel_execute(multicore_host, comp, comm2);
   XBT_INFO("Computed 2-core activity on a 4-core host with some communication. Took %g s", e->get_clock() - start_time);
 
@@ -52,34 +46,13 @@ static void runner()
   XBT_INFO("Switching back to pstate 0.");
   e->host_by_name("MyHost1")->set_pstate(0);
 
-  start_time = sg4::Engine::get_clock();
+  start_time                             = sg4::Engine::get_clock();
   sg4::this_actor::parallel_execute(multicore_host, comp, comm);
   XBT_INFO("Computed 2-core activity on one 4-core host. Took %g s", e->get_clock() - start_time);
 
-  start_time = sg4::Engine::get_clock();
+  start_time                  = sg4::Engine::get_clock();
   sg4::this_actor::parallel_execute(hosts_diff, comp, comm);
   XBT_INFO("Computed 2-core activity on two different hosts. Took %g s", e->get_clock() - start_time);
-
-  // Add a background task and change ptask on the fly
-  auto MyHost1                          = e->host_by_name("MyHost1");
-  simgrid::s4u::ExecPtr background_task = MyHost1->exec_async(5e9);
-  background_task->start();
-  XBT_INFO("Start a 1-core background task on the 4-core host.");
-
-  start_time = sg4::Engine::get_clock();
-  sg4::this_actor::parallel_execute(multicore_host, comp, comm);
-  XBT_INFO("Computed 2-core activity on the 4-core host. Took %g s", e->get_clock() - start_time);
-  XBT_INFO("Remaining amount of work for the background task: %.0f%%",
-           100 * background_task->get_remaining_ratio());
-
-  XBT_INFO("Switching to pstate 1 while background task is still running.");
-  MyHost1->set_pstate(1);
-  start_time = sg4::Engine::get_clock();
-  sg4::this_actor::parallel_execute(multicore_host, comp, comm);
-  XBT_INFO("Computed again the same 2-core activity on it. Took %g s", e->get_clock() - start_time);
-
-  background_task->wait();
-  XBT_INFO("The background task has ended.");
 }
 
 int main(int argc, char* argv[])
@@ -89,7 +62,7 @@ int main(int argc, char* argv[])
   xbt_assert(argc == 2, "Usage: %s <platform file>", argv[0]);
 
   e.load_platform(argv[1]);
-  sg4::Actor::create("test", e.host_by_name("MyHost1"), runner);
+  sg4::Actor::create("test", sg4::Host::by_name("MyHost1"), runner);
 
   e.run();
   XBT_INFO("Simulation done.");
