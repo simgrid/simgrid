f40be2cfa61af3f40b56dee36cd226d4f6888270
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2021-10-08 10:04:08 +0200

    Start the dev of 3.30.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ copyright = u'2002-2021, The SimGrid Team'
 author = u'The SimGrid Team'
 
 # The short X.Y version
-version = u'3.29'
+version = u'3.29.1'
 
 # -- General configuration ---------------------------------------------------
 
