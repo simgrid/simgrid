792d345c65ef01853c9f0b32015ee7e28fdf4ea7
SUTER Frederic <frederic.suter@cc.in2p3.fr>
2022-01-20 09:11:03 +0100

    remove 2 avoidable deployment files

--- examples/cpp/comm-waitany/s4u-comm-waitany.cpp
+++ examples/cpp/comm-waitany/s4u-comm-waitany.cpp
@@ -25,102 +25,78 @@ namespace sg4 = simgrid::s4u;
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_comm_waitall, "Messages specific for this s4u example");
 
-class Sender {
-  long messages_count;  /* - number of messages */
-  long receivers_count; /* - number of receivers */
-  long msg_size;        /* - message size in bytes */
+static void sender(unsigned int messages_count, unsigned int receivers_count, long msg_size)
+{
+  /* Vector in which we store all ongoing communications */
+  std::vector<sg4::CommPtr> pending_comms;
 
-public:
-  explicit Sender(std::vector<std::string> args)
-  {
-    xbt_assert(args.size() == 4, "Expecting 3 parameters from the XML deployment file but got %zu", args.size());
-    messages_count  = std::stol(args[1]);
-    msg_size        = std::stol(args[2]);
-    receivers_count = std::stol(args[3]);
+  /* Make a vector of the mailboxes to use */
+  std::vector<sg4::Mailbox*> mboxes;
+  for (unsigned int i = 0; i < receivers_count; i++)
+    mboxes.push_back(sg4::Mailbox::by_name(std::string("receiver-") + std::to_string(i)));
+
+  /* Start dispatching all messages to receivers, in a round robin fashion */
+  for (unsigned int i = 0; i < messages_count; i++) {
+    std::string msg_content = std::string("Message ") + std::to_string(i);
+    // Copy the data we send: the 'msg_content' variable is not a stable storage location.
+    // It will be destroyed when this actor leaves the loop, ie before the receiver gets it
+    auto* payload = new std::string(msg_content);
+
+    XBT_INFO("Send '%s' to '%s'", msg_content.c_str(), mboxes[i % receivers_count]->get_cname());
+
+    /* Create a communication representing the ongoing communication, and store it in pending_comms */
+    sg4::CommPtr comm = mboxes[i % receivers_count]->put_async(payload, msg_size);
+    pending_comms.push_back(comm);
   }
-  void operator()() const
-  {
-    /* Vector in which we store all ongoing communications */
-    std::vector<sg4::CommPtr> pending_comms;
 
-    /* Make a vector of the mailboxes to use */
-    std::vector<sg4::Mailbox*> mboxes;
-    for (int i = 0; i < receivers_count; i++)
-      mboxes.push_back(sg4::Mailbox::by_name(std::string("receiver-") + std::to_string(i)));
-
-    /* Start dispatching all messages to receivers, in a round robin fashion */
-    for (int i = 0; i < messages_count; i++) {
-      std::string msg_content = std::string("Message ") + std::to_string(i);
-      // Copy the data we send: the 'msg_content' variable is not a stable storage location.
-      // It will be destroyed when this actor leaves the loop, ie before the receiver gets it
-      auto* payload = new std::string(msg_content);
-
-      XBT_INFO("Send '%s' to '%s'", msg_content.c_str(), mboxes[i % receivers_count]->get_cname());
-
-      /* Create a communication representing the ongoing communication, and store it in pending_comms */
-      sg4::CommPtr comm = mboxes[i % receivers_count]->put_async(payload, msg_size);
-      pending_comms.push_back(comm);
-    }
-
-    /* Start sending messages to let the workers know that they should stop */
-    for (int i = 0; i < receivers_count; i++) {
-      XBT_INFO("Send 'finalize' to 'receiver-%d'", i);
-      sg4::CommPtr comm = mboxes[i]->put_async(new std::string("finalize"), 0);
-      pending_comms.push_back(comm);
-    }
-    XBT_INFO("Done dispatching all messages");
-
-    /* Now that all message exchanges were initiated, wait for their completion, in order of termination.
-     *
-     * This loop waits for first terminating message with wait_any() and remove it with erase(), until all comms are
-     * terminated
-     * Even in this simple example, the pending comms do not terminate in the exact same order of creation.
-     */
-    while (not pending_comms.empty()) {
-      ssize_t changed_pos = sg4::Comm::wait_any(pending_comms);
-      pending_comms.erase(pending_comms.begin() + changed_pos);
-      if (changed_pos != 0)
-        XBT_INFO("Remove the %zdth pending comm: it terminated earlier than another comm that was initiated first.",
-                 changed_pos);
-    }
-
-    XBT_INFO("Goodbye now!");
+  /* Start sending messages to let the workers know that they should stop */
+  for (unsigned int i = 0; i < receivers_count; i++) {
+    XBT_INFO("Send 'finalize' to 'receiver-%u'", i);
+    sg4::CommPtr comm = mboxes[i]->put_async(new std::string("finalize"), 0);
+    pending_comms.push_back(comm);
   }
-};
+  XBT_INFO("Done dispatching all messages");
+
+  /* Now that all message exchanges were initiated, wait for their completion, in order of termination.
+   *
+   * This loop waits for first terminating message with wait_any() and remove it with erase(), until all comms are
+   * terminated
+   * Even in this simple example, the pending comms do not terminate in the exact same order of creation.
+   */
+  while (not pending_comms.empty()) {
+    ssize_t changed_pos = sg4::Comm::wait_any(pending_comms);
+    pending_comms.erase(pending_comms.begin() + changed_pos);
+    if (changed_pos != 0)
+      XBT_INFO("Remove the %zdth pending comm: it terminated earlier than another comm that was initiated first.",
+               changed_pos);
+  }
+
+  XBT_INFO("Goodbye now!");
+}
 
 /* Receiver actor expects 1 argument: its ID */
-class Receiver {
-  sg4::Mailbox* mbox;
-
-public:
-  explicit Receiver(std::vector<std::string> args)
-  {
-    xbt_assert(args.size() == 2, "Expecting one parameter from the XML deployment file but got %zu", args.size());
-    std::string mboxName = std::string("receiver-") + args[1];
-    mbox                 = sg4::Mailbox::by_name(mboxName);
+static void receiver(int id)
+{
+  sg4::Mailbox* mbox = sg4::Mailbox::by_name(std::string("receiver-") + std::to_string(id));
+  XBT_INFO("Wait for my first message");
+  for (bool cont = true; cont;) {
+    auto received = mbox->get_unique<std::string>();
+    XBT_INFO("I got a '%s'.", received->c_str());
+    cont = (*received != "finalize"); // If it's a finalize message, we're done
+    // Receiving the message was all we were supposed to do
   }
-  void operator()()
-  {
-    XBT_INFO("Wait for my first message");
-    for (bool cont = true; cont;) {
-      auto received = mbox->get_unique<std::string>();
-      XBT_INFO("I got a '%s'.", received->c_str());
-      cont = (*received != "finalize"); // If it's a finalize message, we're done
-      // Receiving the message was all we were supposed to do
-    }
-  }
-};
+}
 
 int main(int argc, char* argv[])
 {
-  xbt_assert(argc > 2, "Usage: %s platform_file deployment_file\n", argv[0]);
-
   sg4::Engine e(&argc, argv);
-  e.register_actor<Sender>("sender");
-  e.register_actor<Receiver>("receiver");
 
   e.load_platform(argv[1]);
-  e.load_deployment(argv[2]);
+
+  sg4::Actor::create("sender", e.host_by_name("Tremblay"), sender, 6, 2, 1e6);
+  sg4::Actor::create("receiver", e.host_by_name("Fafard"), receiver, 0);
+  sg4::Actor::create("receiver", e.host_by_name("Jupiter"), receiver, 1);
+
   e.run();
 
   return 0;
