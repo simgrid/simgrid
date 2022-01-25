292ac5b892a26eaf1bac04922331998ca4ba66b0
Martin Quinson <martin.quinson@ens-rennes.fr>
2022-01-25 10:25:09 +0100

    jenkins: actually pass SIMGRID_PYTHON_LIBDIR to cmake, bummer

--- tools/jenkins/build.sh
+++ tools/jenkins/build.sh
@@ -214,6 +214,7 @@ cmake -G"$GENERATOR" ${INSTALL:+-DCMAKE_INSTALL_PREFIX=$INSTALL} \
   -Denable_memcheck=$(onoff test "$build_mode" = "DynamicAnalysis") \
   -Denable_compile_warnings=$(onoff test "$GENERATOR" != "MSYS Makefiles") -Denable_smpi=ON \
   -Denable_ns3=$(onoff test "$have_NS3" = "yes" -a "$build_mode" = "Debug") \
+  -DSIMGRID_PYTHON_LIBDIR=${SIMGRID_PYTHON_LIBDIR} \
   ${MAY_DISABLE_SOURCE_CHANGE} ${MAY_DISABLE_LTO} \
   -Denable_java=$(onoff test "$build_mode" = "ModelChecker") \
   -Denable_msg=$(onoff test "$build_mode" = "ModelChecker") \
