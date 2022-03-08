2bb20391d5ba5b526023998c0ae75226bbf6eb96
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2022-03-08 13:08:45 +0100

    Missing include.

--- src/s4u/s4u_Comm.cpp
+++ src/s4u/s4u_Comm.cpp
@@ -3,9 +3,7 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-//#include "src/msg/msg_private.hpp"
-//#include "xbt/log.h"
-
+#include <cmath>
 #include <simgrid/Exception.hpp>
 #include <simgrid/comm.h>
 #include <simgrid/s4u/Comm.hpp>
