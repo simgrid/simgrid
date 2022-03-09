86bc5d4af000586c7c882dc61bc3b0d0d7685168
Martin Quinson <martin.quinson@ens-rennes.fr>
2022-03-09 10:40:43 +0100

    Fix clang builds (unused lambda capture)

--- src/surf/sg_platf.cpp
+++ src/surf/sg_platf.cpp
@@ -486,7 +486,7 @@ void sg_platf_new_actor(simgrid::kernel::routing::ActorCreationArgs* actor)
                                                  auto_restart, /*daemon=*/false, /*restart_count=*/0);
 
     XBT_DEBUG("Actor %s@%s will be started at time %f", arg->name.c_str(), arg->host->get_cname(), start_time);
-    simgrid::kernel::timer::Timer::set(start_time, [arg, auto_restart]() {
+    simgrid::kernel::timer::Timer::set(start_time, [arg]() {
       simgrid::kernel::actor::ActorImplPtr new_actor = simgrid::kernel::actor::ActorImpl::create(arg);
       delete arg;
     });
