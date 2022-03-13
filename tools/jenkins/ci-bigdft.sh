cec453d2e478bef90a9681df9612c4eb84dfc56c
Augustin Degomme <adegomme@gmail.com>
2022-03-13 20:43:59 +0000

    add eigen to bigdft test

--- tools/jenkins/ci-bigdft.sh
+++ tools/jenkins/ci-bigdft.sh
@@ -9,7 +9,7 @@ set -ex
 echo "XXXXXXXXXXXXXXXX Install APT dependencies"
 SUDO="" # to ease the local testing
 $SUDO apt-get -y update
-$SUDO apt-get -y install python-is-python3 python3-setuptools libboost-dev
+$SUDO apt-get -y install python-is-python3 python3-setuptools libboost-dev libeigen3-dev
 $SUDO apt-get -y install --only-upgrade ca-certificates
 
 echo "XXXXXXXXXXXXXXXX build and test BigDFT (git version)"
