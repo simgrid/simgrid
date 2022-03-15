464d34f9d99aad1e50166cea9ed461f46c6099dd
Bruno Donassolo <bruno.donassolo@inria.fr>
2022-03-15 15:11:01 +0100

    Rename file maxmin to System
    Preparing to move maxmin solver to another subclass

--- tools/cmake/DefinePackages.cmake
+++ tools/cmake/DefinePackages.cmake
@@ -294,7 +294,8 @@ set(NS3_SRC  src/surf/network_ns3.cpp
 set(SURF_SRC
   src/kernel/lmm/fair_bottleneck.cpp
   src/kernel/lmm/maxmin.hpp
-  src/kernel/lmm/maxmin.cpp
+  src/kernel/lmm/System.hpp
+  src/kernel/lmm/System.cpp
 
   src/kernel/resource/Action.cpp
   src/kernel/resource/CpuImpl.cpp
