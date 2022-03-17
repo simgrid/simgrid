e469af84f265bb6bd333528c6d7aff6a232b2ee8
Fabien Chaix <chaix@ics.forth.gr>
2022-03-18 01:05:53 +0200

    Guard MailboxImpl::clear() in a simcall

--- src/s4u/s4u_Mailbox.cpp
+++ src/s4u/s4u_Mailbox.cpp
@@ -136,7 +136,7 @@ Mailbox::iprobe(int type, const std::function<bool(void*, void*, kernel::activit
 }
 
 void Mailbox::clear() {
-  pimpl_->clear();
+    kernel::actor::simcall_answered([this]() { this->pimpl_->clear(); });
 }
 
 } // namespace s4u
