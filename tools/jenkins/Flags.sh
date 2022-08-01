0f26424e86ac41a2abc3b9826997b4d3d04939e4
Augustin Degomme <26892-adegomme@users.noreply.framagit.org>
2022-08-01 20:34:20 +0000

    [ci-skip] Flags - let's see if this trailing whitespace was breaking everything..

--- tools/jenkins/Flags.sh
+++ tools/jenkins/Flags.sh
@@ -92,7 +92,7 @@ cmake -Denable_documentation=OFF -Denable_java=${buildjava} -Denable_msg=${build
       -Denable_smpi=${buildsmpi} -Denable_smpi_MPICH3_testsuite=${buildsmpi} -Denable_model-checking=${buildmc} \
       -Denable_memcheck=OFF -Denable_memcheck_xml=OFF -Denable_smpi_MBI_testsuite=OFF \
       -Denable_ns3=$(onoff test "$buildmc" != "ON") -DNS3_HINT=/builds/ns-3-dev/build/ \
-      -Denable_coverage=OFF -DLTO_EXTRA_FLAG="auto" -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \ 
+      -Denable_coverage=OFF -DLTO_EXTRA_FLAG="auto" -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
       "$WORKSPACE"
 
 make -j$NUMPROC tests
