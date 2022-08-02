c52b563defdd521c601ba1c591dc3a5f7f369761
Augustin Degomme <26892-adegomme@users.noreply.framagit.org>
2022-08-02 18:07:42 +0000

    [ci-skip] activate ccache on sanitizer builds

--- tools/jenkins/Sanitizers.sh
+++ tools/jenkins/Sanitizers.sh
@@ -80,7 +80,8 @@ cmake -Denable_documentation=OFF -Denable_java=OFF \
       -Denable_smpi=ON -Denable_smpi_MPICH3_testsuite=ON -Denable_model-checking=OFF \
       -Denable_ns3=ON \
       -Denable_memcheck=OFF -Denable_memcheck_xml=OFF -Denable_smpi_MBI_testsuite=OFF -Denable_coverage=OFF\
-      -Denable_fortran=OFF -Denable_python=OFF -DLTO_EXTRA_FLAG="auto" ${SANITIZER_OPTIONS} "$WORKSPACE"
+      -Denable_fortran=OFF -Denable_python=OFF -DLTO_EXTRA_FLAG="auto" -DCMAKE_CXX_COMPILER_LAUNCHER=ccache\
+      ${SANITIZER_OPTIONS} "$WORKSPACE"
 
 make -j$NUMPROC tests
 ctest --no-compress-output -D ExperimentalTest || true
