ab71da4cf3e28f55f7aef47f5299519135429872
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2022-08-26 14:51:20 +0200

    Reduce depth of nested statements

--- src/plugins/file_system/s4u_FileSystem.cpp
+++ src/plugins/file_system/s4u_FileSystem.cpp
@@ -418,28 +418,28 @@ static void on_platform_created()
 {
   for (auto const& host : simgrid::s4u::Engine::get_instance()->get_all_hosts()) {
     const char* remote_disk_str = host->get_property("remote_disk");
-    if (remote_disk_str) {
-      std::vector<std::string> tokens;
-      boost::split(tokens, remote_disk_str, boost::is_any_of(":"));
-      std::string mount_point         = tokens[0];
-      simgrid::s4u::Host* remote_host = simgrid::s4u::Host::by_name_or_null(tokens[2]);
-      xbt_assert(remote_host, "You're trying to access a host that does not exist. Please check your platform file");
+    if (not remote_disk_str)
+      continue;
+    std::vector<std::string> tokens;
+    boost::split(tokens, remote_disk_str, boost::is_any_of(":"));
+    std::string mount_point         = tokens[0];
+    simgrid::s4u::Host* remote_host = simgrid::s4u::Host::by_name_or_null(tokens[2]);
+    xbt_assert(remote_host, "You're trying to access a host that does not exist. Please check your platform file");
 
-      const simgrid::s4u::Disk* disk = nullptr;
-      for (auto const& d : remote_host->get_disks())
-        if (d->get_name() == tokens[1]) {
-          disk = d;
-          break;
-        }
+    const simgrid::s4u::Disk* disk = nullptr;
+    for (auto const& d : remote_host->get_disks())
+      if (d->get_name() == tokens[1]) {
+        disk = d;
+        break;
+      }
 
-      xbt_assert(disk, "You're trying to mount a disk that does not exist. Please check your platform file");
-      disk->extension<FileSystemDiskExt>()->add_remote_mount(remote_host, mount_point);
-      host->add_disk(disk);
+    xbt_assert(disk, "You're trying to mount a disk that does not exist. Please check your platform file");
+    disk->extension<FileSystemDiskExt>()->add_remote_mount(remote_host, mount_point);
+    host->add_disk(disk);
 
-      XBT_DEBUG("Host '%s' wants to mount a remote disk: %s of %s mounted on %s", host->get_cname(), disk->get_cname(),
-                remote_host->get_cname(), mount_point.c_str());
-      XBT_DEBUG("Host '%s' now has %zu disks", host->get_cname(), host->get_disks().size());
-    }
+    XBT_DEBUG("Host '%s' wants to mount a remote disk: %s of %s mounted on %s", host->get_cname(), disk->get_cname(),
+              remote_host->get_cname(), mount_point.c_str());
+    XBT_DEBUG("Host '%s' now has %zu disks", host->get_cname(), host->get_disks().size());
   }
 }
 
