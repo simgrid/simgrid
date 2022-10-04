aa4c6499f0eae7992715a60875e59152f5eafe4a
Martin Quinson <martin.quinson@ens-rennes.fr>
2022-10-04 22:44:07 +0200

    We don't use doxygen to build the docs anymore
    This integrates another debian patch

--- tools/cmake/Documentation.cmake
+++ tools/cmake/Documentation.cmake
@@ -1,72 +1,24 @@
 ###
 ### Generate all parts of the documentation on non-Windows systems
 ###
-###   - HTML with doxygen (reference and manual)
 ###   - Javadoc (reference)
 ###   - manpages (reference of tools)
 ###
 ###  This file is not loaded on windows
 
 #### Generate the html documentation
-if (enable_documentation)
-  find_package(Doxygen REQUIRED)
-else()
-  find_package(Doxygen)
-endif()
-
 find_path(FIG2DEV_PATH  NAMES fig2dev  PATHS NO_DEFAULT_PATHS)
 
 if(enable_documentation)
-  ADD_CUSTOM_TARGET(documentation
-    COMMENT "Generating the SimGrid documentation..."
-    DEPENDS ${DOC_SOURCES} ${source_doxygen}
-    COMMAND ${CMAKE_COMMAND} -E make_directory   ${CMAKE_BINARY_DIR}/doc/doxygen
-    COMMAND ${CMAKE_COMMAND} -E make_directory   ${CMAKE_BINARY_DIR}/doc/example_lists
-    COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/doc/html
-    COMMAND ${CMAKE_COMMAND} -E make_directory   ${CMAKE_BINARY_DIR}/doc/html
-    COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/doc/xml
-    COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/docs/source/api
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/doc
-  )
-
-  message(STATUS "Doxygen version: ${DOXYGEN_VERSION}")
-
-  if(DOXYGEN_VERSION VERSION_LESS "1.8")
-    ADD_CUSTOM_TARGET(error_doxygen
-        COMMAND ${CMAKE_COMMAND} -E echo "Doxygen must be at least version 1.8 to generate documentation. Version found: ${DOXYGEN_VERSION}"
-      COMMAND false
-    )
-    add_dependencies(documentation error_doxygen)
-  endif()
-
-  foreach(file ${DOC_IMG})
-    ADD_CUSTOM_COMMAND(TARGET documentation COMMAND ${CMAKE_COMMAND} -E copy ${file} ${CMAKE_BINARY_DIR}/doc/html/)
-  endforeach()
-
-  ADD_CUSTOM_COMMAND(TARGET documentation
-    COMMAND pwd
-    COMMAND ${CMAKE_COMMAND} -E echo "XX Generate list of files in examples/ for routing models"
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/doc/example_lists/
-    COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/list_routing_models_examples.sh Floyd > ${CMAKE_BINARY_DIR}/doc/example_lists/example_filelist_routing_floyd
-    COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/list_routing_models_examples.sh Dijkstra > ${CMAKE_BINARY_DIR}/doc/example_lists/example_filelist_routing_dijkstra
-    COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/list_routing_models_examples.sh DijkstraCache > ${CMAKE_BINARY_DIR}/doc/example_lists/example_filelist_routing_dijkstra_cache
-    COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/list_routing_models_examples.sh 'routing="None"' > ${CMAKE_BINARY_DIR}/doc/example_lists/example_filelist_routing_none
-    COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/list_routing_models_examples.sh 'routing="Cluster"' > ${CMAKE_BINARY_DIR}/doc/example_lists/example_filelist_routing_cluster
-    COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/list_routing_models_examples.sh 'routing="Vivaldi"' > ${CMAKE_BINARY_DIR}/doc/example_lists/example_filelist_routing_vivaldi
-    COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/list_routing_models_examples.sh 'routing="Full"' > ${CMAKE_BINARY_DIR}/doc/example_lists/example_filelist_routing_full
-    COMMAND ${CMAKE_COMMAND} -E echo "XX Generate list of files in examples/ for XML tags"
-    COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/list_routing_models_examples.sh '<mount ' > ${CMAKE_BINARY_DIR}/doc/example_lists/example_filelist_xmltag_mount
-    COMMAND ${CMAKE_HOME_DIRECTORY}/tools/doxygen/list_routing_models_examples.sh '<link_ctn ' > ${CMAKE_BINARY_DIR}/doc/example_lists/example_filelist_xmltag_linkctn
-    COMMAND ${CMAKE_COMMAND} -E echo "XX Run doxygen"
-    COMMAND ${DOXYGEN_EXECUTABLE} Doxyfile
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/doc
-  )
-
   if (Java_FOUND)
     find_path(JAVADOC_PATH  NAMES javadoc   PATHS NO_DEFAULT_PATHS)
     mark_as_advanced(JAVADOC_PATH)
 
-    ADD_CUSTOM_COMMAND(TARGET documentation
+   ADD_CUSTOM_TARGET(documentation
+      COMMENT "Generating the SimGrid documentation..."
+      COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/doc/html
+    )
+   ADD_CUSTOM_COMMAND(TARGET documentation
       COMMAND ${CMAKE_COMMAND} -E echo "XX Javadoc pass"
       COMMAND ${JAVADOC_PATH}/javadoc -quiet -d ${CMAKE_BINARY_DIR}/doc/html/javadoc/ ${CMAKE_HOME_DIRECTORY}/src/bindings/java/org/simgrid/*.java ${CMAKE_HOME_DIRECTORY}/src/bindings/java/org/simgrid/*/*.java
       WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/doc
