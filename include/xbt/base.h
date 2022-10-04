9697515f3e18f7609aadcc63c27802d5ebbc9497
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2022-10-04 08:54:20 +0200

    Define macro XBT_ATTRIB_DEPRECATED_v337.

--- include/xbt/base.h
+++ include/xbt/base.h
@@ -59,6 +59,8 @@
   XBT_ATTRIB_DEPRECATED(mesg " (this compatibility wrapper will be dropped after v3.34)")
 #define XBT_ATTRIB_DEPRECATED_v336(mesg)                                                                               \
   XBT_ATTRIB_DEPRECATED(mesg " (this compatibility wrapper will be dropped after v3.35)")
+#define XBT_ATTRIB_DEPRECATED_v337(mesg)                                                                               \
+  XBT_ATTRIB_DEPRECATED(mesg " (this compatibility wrapper will be dropped after v3.36)")
 
 /* Work around https://github.com/microsoft/vscode-cpptools/issues/4503 */
 #ifdef __INTELLISENSE__
