4a51df13a5942cd7cc33d2f1bcce308040945be2
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2022-10-11 15:06:29 +0200

    Enable simple sthread example.

--- examples/sthread/sthread-mutex-simple.c
+++ examples/sthread/sthread-mutex-simple.c
@@ -5,11 +5,12 @@
 
 sthread_mutex_t mutex;
 
-static void* thread_fun(void* ignore)
+static void* thread_fun(void* val)
 {
   sthread_mutex_lock(&mutex);
   sthread_mutex_unlock(&mutex);
 
+  fprintf(stderr, "The thread %d is terminating.\n", *(int*)val);
   return NULL;
 }
 
@@ -17,15 +18,17 @@ int main(int argc, char* argv[])
 {
   sthread_mutex_init(&mutex, NULL);
 
+  int id[2] = {0, 1};
   sthread_t thread1;
   sthread_t thread2;
-  sthread_create(&thread1, NULL, thread_fun, NULL);
-  sthread_create(&thread2, NULL, thread_fun, NULL);
+  sthread_create(&thread1, NULL, thread_fun, (void*)&id[0]);
+  sthread_create(&thread2, NULL, thread_fun, (void*)&id[1]);
+  fprintf(stderr, "All threads are started.\n");
   sthread_join(thread1, NULL);
   sthread_join(thread2, NULL);
 
   sthread_mutex_destroy(&mutex);
 
-  fprintf(stderr, "done\n");
+  fprintf(stderr, "User's main is terminating.\n");
   return 0;
 }
