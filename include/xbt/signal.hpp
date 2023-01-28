9952628e8e09510ab16f47e7262dff27533119c1
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2023-01-28 16:33:29 +0100

    Kill unused function.

--- include/xbt/signal.hpp
+++ include/xbt/signal.hpp
@@ -42,8 +42,6 @@ public:
   void disconnect(unsigned int id) { handlers_.erase(id); }
   /** Remove all callbacks */
   void disconnect_slots() { handlers_.clear(); }
-  /** Get the amount of callbacks */
-  int get_slot_count() { return handlers_.size(); }
 };
 }
 }
