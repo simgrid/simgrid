748fabf6540d3b6e2d58a36d0363985f0ddb85d9
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2023-05-24 14:38:50 +0200

    Please Sonar and scan-build.

--- src/plugins/host_energy.cpp
+++ src/plugins/host_energy.cpp
@@ -514,6 +514,7 @@ void sg_host_energy_plugin_init()
       simgrid::s4u::Host* host = activity.get_host();
       if (const auto* vm = dynamic_cast<simgrid::s4u::VirtualMachine*>(host))
         host = vm->get_pm();
+      xbt_assert(host != nullptr);
       host->extension<HostEnergy>()->update();
     }
   });
