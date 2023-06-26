036c801d55e3ab07b470c79640109080fed049a1
Fred Suter <suterf@ornl.gov>
2023-06-26 12:35:39 -0400

    add missing copyright notes

--- src/plugins/battery.cpp
+++ src/plugins/battery.cpp
@@ -1,3 +1,7 @@
+/* Copyright (c) 2023. The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
 #include <simgrid/Exception.hpp>
 #include <simgrid/plugins/battery.hpp>
 #include <simgrid/s4u/Actor.hpp>
