43274f822da3564ac0b71ca73df3016459d9ecdd
Adrien Gougeon <adrien.gougeon@ens-rennes.fr>
2023-09-15 09:29:10 +0200

    remove deprecated include

--- examples/cpp/solar-panel-simple/s4u-solar-panel-simple.cpp
+++ examples/cpp/solar-panel-simple/s4u-solar-panel-simple.cpp
@@ -3,7 +3,6 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
-#include "simgrid/plugins/photovoltaic.hpp"
 #include "simgrid/plugins/solar_panel.hpp"
 #include "simgrid/s4u.hpp"
 #include <simgrid/s4u/Actor.hpp>
