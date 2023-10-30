2da930cb71158d192643133d1cb374f94525fa63
Martin Quinson <martin.quinson@ens-rennes.fr>
2023-10-30 05:01:36 +0100

    Fix make distcheck for Fred :)

--- tools/cmake/DefinePackages.cmake
+++ tools/cmake/DefinePackages.cmake
@@ -702,6 +702,7 @@ set(headers_to_install
   include/simgrid/s4u/Link.hpp
   include/simgrid/s4u/Mailbox.hpp
   include/simgrid/s4u/MessageQueue.hpp
+  include/simgrid/s4u/Mess.hpp
   include/simgrid/s4u/Mutex.hpp
   include/simgrid/s4u/NetZone.hpp
   include/simgrid/s4u/Semaphore.hpp
