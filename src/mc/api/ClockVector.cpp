31ffc294d75ea5ad768d0e7e23a44a8d7bad96d6
mlaurent <mathieu.laurent@ens-rennes.fr>
2024-01-19 11:32:31 +0100

    Optimize some core computations in MC algorithms

--- src/mc/api/ClockVector.cpp
+++ src/mc/api/ClockVector.cpp
@@ -9,13 +9,10 @@ namespace simgrid::mc {
 
 ClockVector ClockVector::max(const ClockVector& cv1, const ClockVector& cv2)
 {
-  auto max_vector = ClockVector();
-
-  for (const auto& [aid, value] : cv1.contents)
-    max_vector[aid] = std::max(value, cv2.get(aid).value_or(0));
+  auto max_vector = ClockVector(cv1);
 
   for (const auto& [aid, value] : cv2.contents)
-    max_vector[aid] = std::max(value, cv1.get(aid).value_or(0));
+    max_vector[aid] = std::max(value, max_vector.get(aid).value_or(0));
 
   return max_vector;
 }
