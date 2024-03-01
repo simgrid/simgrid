484976e9f6aa224d79a0546883579d3c31c3f118
Martin Quinson <martin.quinson@ens-rennes.fr>
2024-03-01 14:59:49 +0100

    Document a C function

--- include/simgrid/comm.h
+++ include/simgrid/comm.h
@@ -15,7 +15,9 @@ SG_BEGIN_DECL
 XBT_PUBLIC int sg_comm_isinstance(sg_activity_t acti);
 
 XBT_PUBLIC void sg_comm_detach(sg_comm_t comm, void (*clean_function)(void*));
+/** Returns whether the communication is finished */
 XBT_PUBLIC int sg_comm_test(sg_comm_t comm);
+/** Block this actor until this communication is finished */
 XBT_PUBLIC sg_error_t sg_comm_wait(sg_comm_t comm);
 XBT_PUBLIC sg_error_t sg_comm_wait_for(sg_comm_t comm, double timeout);
 XBT_PUBLIC void sg_comm_unref(sg_comm_t comm);
