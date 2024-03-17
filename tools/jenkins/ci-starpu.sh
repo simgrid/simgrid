ce00761945472d74edb7bc4197b5c07f6ebe2ebe
Augustin Degomme <26892-adegomme@users.noreply.framagit.org>
2024-03-17 22:03:36 +0000

    [ci skip] add dependency for starpu (python setuptools)

--- tools/jenkins/ci-starpu.sh
+++ tools/jenkins/ci-starpu.sh
@@ -10,7 +10,7 @@ export SUDO=""
 
 echo "XXXXXXXXXXXXXXXX Install APT dependencies"
 $SUDO apt-get update
-$SUDO apt-get -y install build-essential libboost-all-dev wget git xsltproc
+$SUDO apt-get -y install build-essential libboost-all-dev wget git xsltproc python3-setuptools
 
 for i in master starpu-1.3 ; do
   echo "XXXXXXXXXXXXXXXX Build and test StarPU $i"
