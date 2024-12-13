bb4d116db4a1353f770abc38ef011306ffcc2fe2
Fred Suter <suterf@ornl.gov>
2024-12-13 10:26:35 -0500

    add test of impact on I/Os of turning a host off and on

---
+++ teshsuite/s4u/host-on-off-disks/host-on-off-disks.cpp
@@ -0,0 +1,57 @@
+/* Copyright (c) 2010-2024. The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+#include "simgrid/Exception.hpp"
+#include "simgrid/s4u.hpp"
+namespace sg4 = simgrid::s4u;
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_test, "Messages specific for this s4u example");
+
+static void io_manager()
+{
+  XBT_INFO("Starting I/O manager on 'host1'");
+  auto disks = sg4::this_actor::get_host()->get_disks();
+  /* Write for 2 seconds on each disk */
+  sg4::IoPtr write_on_disk1 = disks[0]->write_async(2e6);
+  sg4::IoPtr write_on_disk2 = disks[1]->write_async(2e6);
+  sg4::ActivitySet pending_activities({write_on_disk1, write_on_disk2});
+  pending_activities.wait_all();
+  XBT_INFO("All I/Os are complete. Exit now.");
+}
+
+static void host_killer()
+{
+  auto* host1 = sg4::Engine::get_instance()->host_by_name("host1");
+  sg4::this_actor::sleep_for(1);
+  XBT_INFO("Turning off 'host1'");
+  host1->turn_off();
+  sg4::this_actor::sleep_for(1);
+  XBT_INFO("Turning 'host1' back on");
+  host1->turn_on();
+}
+int main(int argc, char* argv[])
+{
+  sg4::Engine e(&argc, argv);
+
+  auto* root  = sg4::create_full_zone("root");
+  auto* host1 = root->create_host("host1", 1e9);
+  host1->create_disk("disk-1", 1e9, 1e6);
+  host1->create_disk("disk-2", 1e9, 1e6);
+
+  auto* host2 = root->create_host("host2", 1e9);
+
+  const auto* link = root->create_link("link", 1e9);
+  root->add_route(host1, host2, {link});
+  root->seal();
+
+  sg4::Actor::create("I/O manager", host1, io_manager)->set_auto_restart();
+  sg4::Actor::create("host_killer", host2, host_killer);
+
+  e.run();
+
+  XBT_INFO("Simulation time %g", sg4::Engine::get_clock());
+
+  return 0;
+}
