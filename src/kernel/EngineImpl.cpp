a1b362a77f3f0f1c6e6fc792db4966c41596191c
Fred Suter <suterf@ornl.gov>
2025-01-03 15:33:57 -0500

    trigger on_platform_sealed

--- src/kernel/EngineImpl.cpp
+++ src/kernel/EngineImpl.cpp
@@ -319,6 +319,9 @@ void EngineImpl::seal_platform() const
 
   /* seal netzone root, recursively seal children netzones, hosts and disks */
   netzone_root_->seal();
+
+  // Now the platform is officially sealed and thus completely created
+  simgrid::s4u::Engine::on_platform_sealed();
 }
 
 void EngineImpl::load_platform(const std::string& platf)
