32853596a05fa7593ddec8ad27772e373fbd903d
Martin Quinson <martin.quinson@ens-rennes.fr>
2025-01-26 02:06:17 +0100

    Let the last dag examples work in Java

--- examples/java/cloud_migration/cloud_migration.java
+++ examples/java/cloud_migration/cloud_migration.java
@@ -88,6 +88,7 @@ public class cloud_migration {
   public static void main(String[] args)
   {
     Engine e = new Engine(args);
+    e.plugin_vm_live_migration_init();
 
     e.load_platform(args[0]);
     e.add_actor("Main", e.host_by_name("Fafard"), new Main());
