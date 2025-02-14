d213923722c4dfe38421091289ee1ea6dce72dde
Martin Quinson <martin.quinson@ens-rennes.fr>
2025-02-14 14:01:45 +0100

    Fix CheckerSide::get_aid_of_next_transition()

--- src/mc/remote/mc_protocol.h
+++ src/mc/remote/mc_protocol.h
@@ -71,6 +71,7 @@ struct s_mc_message_simcall_execute_t {
 };
 struct s_mc_message_simcall_execute_answer_t {
   simgrid::mc::MessageType type;
+  aid_t aid;
   std::array<char, SIMCALL_SERIALIZATION_BUFFER_SIZE> buffer;
 };
 
