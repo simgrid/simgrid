e0b1cf1c4f1352e46bcc3811e990bb164236c5c5
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2025-02-25 15:57:33 +0100

    Add some missing copyright notices.
    [ci-skip]

--- tools/cmake/test_prog/prog_musl.c
+++ tools/cmake/test_prog/prog_musl.c
@@ -1,3 +1,8 @@
+/* Copyright (c) 2024-2025. The SimGrid Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
 // This little program only compiles on Linux, and returns 1 on MUSL and 0 on GNU system
 // Loosely inspired by https://stackoverflow.com/a/70211227
 
